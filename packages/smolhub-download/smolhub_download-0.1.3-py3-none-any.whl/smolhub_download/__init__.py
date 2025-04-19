65927f8a4c38f3ee62838c6f3f4899e6af222dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:03:57 +0000

    Add package smolhub-download/0.1.3/smolhub_download-0.1.3-py3-none-any.whl

---
+++ packages/smolhub-download/smolhub_download-0.1.3-py3-none-any.whl/smolhub_download/__init__.py
@@ -0,0 +1,8 @@
+"""
+SmolHub Download - A Python client for interacting with SmolHub models and datasets
+"""
+
+from .client import SmolHubClient
+
+__version__ = "0.1.2"
+__all__ = ["SmolHubClient"]
