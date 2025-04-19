c3a0e88486e30a93be99997f951bc1c6f92d789d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:46 +0000

    Add package asfa/0.1.4/asfa-0.1.4.tar.gz

---
+++ packages/asfa/asfa-0.1.4.tar.gz/asfa-0.1.4/asfa/_config.py
@@ -0,0 +1,8 @@
+import base64
+
+# Base64-encoded version of your base URL
+# e.g., "https://your-api.aws.com" → encoded to:
+ENCODED_URL = "aHR0cDovLzEzLjIzMy4xNTQuMTAzOjgwMDA="
+
+def get_api_url():
+    return "3.110.48.110:8000"  # Replace with your actual API URL
