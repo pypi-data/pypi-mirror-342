441c5355d291e2bd0072de36a1d0c88b625900a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:03:36 +0000

    Add package live-radio/0.1.2/live_radio-0.1.2.tar.gz

---
+++ packages/live-radio/live_radio-0.1.2.tar.gz/live_radio-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='live_radio',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'pyradios',
+        'setuptools',
+    ],
+    author='Peter Nyando',
+    author_email='nyandopeter2@gmail.com', 
+    description='Get live radio station streams by country',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/anomalous254/live_radio',
+    license='MIT', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+)
