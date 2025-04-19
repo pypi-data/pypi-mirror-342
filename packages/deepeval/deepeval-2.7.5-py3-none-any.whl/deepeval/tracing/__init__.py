261dc6a670b2f54a43a9c91479573ee21bddbf1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:18 +0000

    Add package deepeval/2.7.5/deepeval-2.7.5-py3-none-any.whl

---
+++ packages/deepeval/deepeval-2.7.5-py3-none-any.whl/deepeval/tracing/__init__.py
@@ -0,0 +1,9 @@
+from .tracing import (
+    observe,
+    update_current_span_attributes,
+    update_current_span_test_case_parameters,
+    LlmAttributes,
+    RetrieverAttributes,
+    ToolAttributes,
+    AgentAttributes,
+)
