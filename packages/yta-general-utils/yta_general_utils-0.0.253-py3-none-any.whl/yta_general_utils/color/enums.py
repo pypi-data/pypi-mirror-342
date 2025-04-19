28cc31d4cb39cdba05903b939bba765663b5d5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 16:03:51 +0000

    Add package yta-general-utils/0.0.253/yta_general_utils-0.0.253-py3-none-any.whl

---
+++ packages/yta-general-utils/yta_general_utils-0.0.253-py3-none-any.whl/yta_general_utils/color/enums.py
@@ -0,0 +1,10 @@
+from yta_general_utils.programming.enum import YTAEnum as Enum
+
+
+class ColorString(Enum):
+    WHITE = '#FFFFFF'
+    BLACK = '#000000'
+    RED = '#FF000000'
+    GREEN = '#00FF00'
+    BLUE = '#0000FF'
+    # TODO: Add colors from Manim or other libraries
