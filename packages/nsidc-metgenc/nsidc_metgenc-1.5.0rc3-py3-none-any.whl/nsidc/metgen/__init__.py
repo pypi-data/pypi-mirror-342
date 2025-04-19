826918ff5a398e29d494c51c75cd1047b39abcc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:41 +0000

    Add package nsidc-metgenc/1.5.0rc3/nsidc_metgenc-1.5.0rc3-py3-none-any.whl

---
+++ packages/nsidc-metgenc/nsidc_metgenc-1.5.0rc3-py3-none-any.whl/nsidc/metgen/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "v1.5.0rc3"
+
+
+__all__ = [
+    "__version__",
+    "aws",
+    "cli",
+    "config",
+    "constants",
+    "metgen",
+    "netcdf_reader",
+]
+
+from . import aws, cli, config, constants, metgen, netcdf_reader
