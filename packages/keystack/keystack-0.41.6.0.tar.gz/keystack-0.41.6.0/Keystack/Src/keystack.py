844c1d30b79915c12def10e79e1ad97b81b589de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:25 +0000

    Add package keystack/0.41.6.0/keystack-0.41.6.0.tar.gz

---
+++ packages/keystack/keystack-0.41.6.0.tar.gz/keystack-0.41.6.0/Keystack/Src/keystack.py
@@ -0,0 +1,43 @@
+import os, sys, click
+
+version = None
+currentDir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, currentDir)
+
+try:
+    from commonLib import showVersion
+    from CLI.RunKeystack import commands as runKeystackParams
+    version = showVersion(stdout=False)
+except:
+    pass
+
+from CLI.InstallKeystack import commands as installKeystackParams
+from CLI.Env import commands as Env
+# from CLI.Env.Reserve import commands as ReserveEnv
+# from CLI.Env.Release import commands as ReleaseEnv
+# from CLI.Env.ReleaseOnFailure import commands as ReleaseEnvOnFailure
+# from CLI.Env.Reset import commands as Reset
+
+class Keystack:
+    @click.version_option(version)
+    @click.group(help="CLI commands to manage Keystack")    
+    def run():    
+        pass
+
+
+if os.path.exists('/opt/KeystackTests'):
+    Keystack().run.add_command(runKeystackParams.run, name='run')
+    
+Keystack().run.add_command(installKeystackParams.setup, name='setup')
+Keystack().run.add_command(installKeystackParams.upgrade, name='upgrade')
+Keystack().run.add_command(Env.manage, name='env')
+#Keystack().run.add_command(Env.reserve, name='reserveEnv')
+#Keystack().run.add_command(Env.release, name='releaseEnv')
+#Keystack().run.add_command(Env.releaseOnFailure, name='releaseEnvOnFailure')
+#Keystack().run.add_command(Env.reset, name='reset')
+
+if __name__ == "__main__":
+    Keystack().run()
+    
+
+        
