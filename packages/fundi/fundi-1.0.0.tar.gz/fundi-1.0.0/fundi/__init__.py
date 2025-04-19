41839d9a61cccaccfaa2ef517be2c3e61601fd36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:10 +0000

    Add package fundi/1.0.0/fundi-1.0.0.tar.gz

---
+++ packages/fundi/fundi-1.0.0.tar.gz/fundi-1.0.0/fundi/__init__.py
@@ -0,0 +1,6 @@
+from .scan import scan
+from .from_ import from_
+from .resolve import resolve
+from .util import tree, order
+from .inject import inject, ainject
+from .types import CallableInfo, TypeResolver
