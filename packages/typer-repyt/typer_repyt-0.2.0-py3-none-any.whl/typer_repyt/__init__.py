cbfe2119616bc713a2233d5ee149ec902cfda246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:04:00 +0000

    Add package typer-repyt/0.2.0/typer_repyt-0.2.0-py3-none-any.whl

---
+++ packages/typer-repyt/typer_repyt-0.2.0-py3-none-any.whl/typer_repyt/__init__.py
@@ -0,0 +1,3 @@
+from typer_repyt.build_command import build_command, ArgDef, OptDef
+
+__all__ = ["build_command", "ArgDef", "OptDef"]
---
+++ packages/typer-repyt/typer_repyt-0.2.0-py3-none-any.whl/typer_repyt/exceptions.py
@@ -0,0 +1,9 @@
+from buzz import Buzz
+
+
+class RepytError(Buzz):
+    """
+    Base class for exceptions used in the `typer_repyt` module.
+    """
+
+    pass
