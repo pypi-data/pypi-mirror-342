6ecd00d8a997246b6301c415fa7d24556beccf23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:17 +0000

    Add package mywork-deva1/0.0.1/mywork_deva1-0.0.1-py3-none-any.whl

---
+++ packages/mywork-deva1/mywork_deva1-0.0.1-py3-none-any.whl/mywork_deva/EORODD.py
@@ -0,0 +1,7 @@
+def even_odd(n):
+    if (n%2==0):
+        print(" the given number is even ")
+    else:
+        print(" the given number is odd ")
+n=int(input(" enter the number of your choice : ")
+even_odd(n)
