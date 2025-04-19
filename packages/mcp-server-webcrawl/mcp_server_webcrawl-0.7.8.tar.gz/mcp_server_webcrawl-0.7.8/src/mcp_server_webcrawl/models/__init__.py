b8b5cfce5ac1534aa2f621391c5096771aec0c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:03:49 +0000

    Add package mcp-server-webcrawl/0.7.8/mcp_server_webcrawl-0.7.8.tar.gz

---
+++ packages/mcp-server-webcrawl/mcp_server_webcrawl-0.7.8.tar.gz/mcp_server_webcrawl-0.7.8/src/mcp_server_webcrawl/models/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+from typing import Union
+
+# obscene to place this everywhere, contain it
+try:
+    UTC = datetime.UTC
+except AttributeError:
+    # python <=3.10
+    from datetime import timezone
+    UTC = timezone.utc
+
+# this is what is acceptable metadata content for crawl results
+METADATA_VALUE_TYPE = Union[str, int, float, bool, list[str], list[int], list[float], None]
