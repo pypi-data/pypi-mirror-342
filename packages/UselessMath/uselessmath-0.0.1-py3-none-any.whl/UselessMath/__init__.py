c6b2a88db415857edd67e751cd9604b47fc1225e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:47 +0000

    Add package UselessMath/0.0.1/uselessmath-0.0.1-py3-none-any.whl

---
+++ packages/UselessMath/uselessmath-0.0.1-py3-none-any.whl/UselessMath/__init__.py
@@ -0,0 +1,17 @@
+#__init__.py
+from .UselessMath import *
+__all__ = [
+    "isOdd",
+    "isEven",
+    "Sqrt",
+    "Opposite",
+    "Add_Zero",
+    "Add_One",
+    "Add",
+    "Sub",
+    "Multiplication",
+    "Division",
+    "isNegative",
+    "isNotNegative",
+    "What the fuck is this number"
+]
