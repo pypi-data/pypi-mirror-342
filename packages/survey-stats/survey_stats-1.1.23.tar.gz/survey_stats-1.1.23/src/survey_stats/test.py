3a29de7fc5d0728dfdcf18531d03c5de30ea7271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:33 +0000

    Add package survey-stats/1.1.23/survey_stats-1.1.23.tar.gz

---
+++ packages/survey-stats/survey_stats-1.1.23.tar.gz/survey_stats-1.1.23/src/survey_stats/test.py
@@ -0,0 +1,13 @@
+import os, time, math, datetime
+os.system('cls')
+import numpy as np
+
+import jdatetime
+from survey_stats.data_process import Data, Date
+from survey_stats.functions import subsets, list_to_str, match_str, match_in
+
+import shutdown
+
+# shutdown.shutdown(10)
+
+
