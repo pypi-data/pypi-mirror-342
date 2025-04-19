ba6bd9adbab541718076db74c7cedd175d20f4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:28 +0000

    Add package MemoryDiagnosticTool/0.13.0/MemoryDiagnosticTool-0.13.0-py3-none-any.whl

---
+++ packages/MemoryDiagnosticTool/MemoryDiagnosticTool-0.13.0-py3-none-any.whl/MDT/MDT_html_template.py
@@ -0,0 +1,95 @@
+
+
+        
+def parse_to_html(mdt_html, pmu_train_status, summary_table):
+
+  
+    html_template = """
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Line Graph and Table Toggle Example</title>
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            background-color: #f4f4f9;
+        }
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+        .section {
+            background-color: #fff;
+            border: 1px solid #ddd;
+            padding: 20px;
+            margin-bottom: 20px;
+            border-radius: 8px;
+        }
+        .section h2 {
+            color: #007BFF;
+        }
+        .content {
+            font-size: 16px;
+            color: #333;
+        }
+        .error-state {
+            background-color: #f8d7da;
+            border-left: 6px solid #f5c6cb;
+        }
+        .warning-state {
+            background-color: #fff3cd;
+            border-left: 6px solid #ffeeba;
+        }
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+          padding: 8px;
+          text-align: center;
+        }
+
+      </style>
+    </head>
+    """
+    
+
+    html_template += f"""
+    <body>
+
+        <h1>MDT Analysis Report</h1>
+
+        <!-- Error State Section -->
+        <div class="section error-state">
+            <h2>Error State</h2>
+            <div class="content">
+                <p>{pmu_train_status}</p>
+                
+                <table border='1'>
+                {summary_table}
+                </table>
+            </div>
+        </div>
+
+        <!-- Warning State Section -->
+        <div class="section warning-state">
+            <h2>Warning State</h2>
+            <div class="content">
+                <p><strong>No warning detected.</strong></p>
+
+
+            </div>
+        </div>
+
+
+    </body>
+    <script>       
+    </script>
+    </html>
+    """
+    
+
+    f = open(mdt_html, 'w')   
+    f.write(html_template)    
+    f.close()
