f2b30a3976e272474b2e94b432f0ee0a89e18bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:56 +0000

    Add package snipar/0.0.22/snipar-0.0.22-cp38-cp38-win32.whl

---
+++ packages/snipar/snipar-0.0.22-cp38-cp38-win32.whl/snipar/tests/__init__.py
@@ -0,0 +1,10 @@
+import os, unittest
+
+#from snipar.tests.test_sibreg import *
+from snipar.tests.test_impute_from_sibs import *
+from snipar.tests.test_impute import *
+from snipar.tests.test_pedigree_creation import *
+from snipar.tests.test_example import *
+
+if __name__ == '__main__':
+    unittest.main()
