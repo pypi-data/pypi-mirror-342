00bfc6954b2547aa8fc7704c691a28196a0e4924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:04:29 +0000

    Add package snick/2.0.0/snick-2.0.0-py3-none-any.whl

---
+++ packages/snick/snick-2.0.0-py3-none-any.whl/snick/__init__.py
@@ -0,0 +1,25 @@
+from snick.methods import (
+    conjoin,
+    dedent,
+    dedent_all,
+    enboxify,
+    indent,
+    indent_wrap,
+    pretty_format,
+    pretty_print,
+    strip_whitespace,
+    unwrap,
+)
+
+__all__ = [
+    "dedent",
+    "conjoin",
+    "dedent_all",
+    "enboxify",
+    "indent",
+    "indent_wrap",
+    "pretty_format",
+    "pretty_print",
+    "strip_whitespace",
+    "unwrap",
+]
