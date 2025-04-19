4111e63a91b15f56131c1dd3321839f7d28a21ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:47 +0000

    Add package Django-auths-users/0.1.1/django_auths_users-0.1.1.tar.gz

---
+++ packages/Django-auths-users/django_auths_users-0.1.1.tar.gz/django_auths_users-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Django-auths-users',
+    version='0.1.1',
+    author='djangoUser',
+    author_email='django4234@gmail.com',
+    description='django-authUser',
+    long_description=open('README.md').read(),
+    license_file='OSI Approved :: MIT License',
+    long_description_content_type='text/markdown',
+    url='https://github.com/delss28/Django-auths.git',  
+    packages=find_packages(),
+    package_data={
+        'my_library': ['slaveni/*'],  
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
