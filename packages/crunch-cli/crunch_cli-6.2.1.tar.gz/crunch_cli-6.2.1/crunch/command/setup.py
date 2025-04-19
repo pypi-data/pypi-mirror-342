369fde59809e3dc40a07bcd399e0941d50ddc61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:17 +0000

    Add package crunch-cli/6.2.1/crunch_cli-6.2.1.tar.gz

---
+++ packages/crunch-cli/crunch_cli-6.2.1.tar.gz/crunch_cli-6.2.1/crunch/command/setup.py
@@ -0,0 +1,48 @@
+import typing
+
+from .. import api, command, utils
+
+
+def setup(
+    clone_token: str,
+    submission_number: str,
+    competition_name: str,
+    project_name: str,
+    directory: str,
+    model_directory: str,
+    force: bool,
+    no_model: bool,
+    show_quickstarters: bool,
+    quickstarter_name: typing.Optional[str],
+    show_notebook_quickstarters: bool,
+    data_size_variant: api.SizeVariant,
+):
+    command.init(
+        clone_token,
+        competition_name,
+        project_name,
+        directory,
+        model_directory,
+        force,
+        data_size_variant,
+    )
+
+    _, project = api.Client.from_project()
+
+    try:
+        urls = project.clone(
+            submission_number=submission_number,
+            include_model=not no_model,
+        )
+
+        for path, url in urls.items():
+            utils.download(url, path)
+    except api.NeverSubmittedException:
+        if show_quickstarters:
+            command.quickstarter(
+                quickstarter_name,
+                show_notebook_quickstarters,
+                True,
+            )
+        else:
+            print(f"you appear to have never submitted code before")
