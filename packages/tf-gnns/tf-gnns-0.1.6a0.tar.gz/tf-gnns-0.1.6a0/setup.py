6fe683acbc502dca7d2369e126a1cab2a14c25b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:23 +0000

    Add package tf-gnns/0.1.6a0/tf-gnns-0.1.6a0.tar.gz

---
+++ packages/tf-gnns/tf-gnns-0.1.6a0.tar.gz/tf-gnns-0.1.6a0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import re
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
+    return result.group(1)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tf-gnns", # Replace with your own username
+    version="0.1.6a",
+    author="Charilaos Mylonas",
+    author_email="mylonas.charilaos@gmail.com",
+    description="A hackable graphnets library for tensorflow-keras.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mylonasc/tf_gnns.git",
+    packages=setuptools.find_packages(),
+    package_data ={'':['assets/html_css/*css','assets/html_css/*js']},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
