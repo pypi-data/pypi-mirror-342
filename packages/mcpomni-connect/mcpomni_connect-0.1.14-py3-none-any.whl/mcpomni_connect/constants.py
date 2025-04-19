0ac1c60c7b85ef18895d9ad1aff89fa58a2a00f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:40 +0000

    Add package mcpomni-connect/0.1.14/mcpomni_connect-0.1.14-py3-none-any.whl

---
+++ packages/mcpomni-connect/mcpomni_connect-0.1.14-py3-none-any.whl/mcpomni_connect/constants.py
@@ -0,0 +1,9 @@
+TOOL_ACCEPTING_PROVIDERS = {
+    "groq",
+    "openai",
+    "openrouter",
+    "gemini",
+    "deepseek",
+}
+
+AGENTS_REGISTRY = {}
---
+++ packages/mcpomni-connect/mcpomni_connect-0.1.14-py3-none-any.whl/mcpomni_connect/types.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union, Dict, Any
+from enum import Enum
+
+
+class ContextInclusion(str, Enum):
+    NONE = "none"
+    THIS_SERVER = "thisServer"
+    ALL_SERVERS = "allServers"
+
+
+class AgentState(str, Enum):
+    IDLE = "idle"
+    RUNNING = "running"
+    TOOL_CALLING = "tool_calling"
+    OBSERVING = "observing"
+    FINISHED = "finished"
+    ERROR = "error"
+    STUCK = "stuck"
