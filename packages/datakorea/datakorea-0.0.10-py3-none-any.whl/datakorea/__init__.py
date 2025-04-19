8e6094aa32010f760c412643d2d7162658f411a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:16 +0000

    Add package datakorea/0.0.10/datakorea-0.0.10-py3-none-any.whl

---
+++ packages/datakorea/datakorea-0.0.10-py3-none-any.whl/datakorea/__init__.py
@@ -0,0 +1,12 @@
+from datakorea.version_info import __project__, __version__, __author__, __email__
+from datakorea.kakao.kakaoapi import *
+
+__all__ = [
+    "__project__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "kakaoapi",
+]
+
+
