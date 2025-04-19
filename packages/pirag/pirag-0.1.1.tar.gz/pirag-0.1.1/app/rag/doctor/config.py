c67bfb0ca79a65016d0b719b048e7baf65e90da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:04 +0000

    Add package pirag/0.1.1/pirag-0.1.1.tar.gz

---
+++ packages/pirag/pirag-0.1.1.tar.gz/pirag-0.1.1/app/rag/doctor/config.py
@@ -0,0 +1,20 @@
+import argparse
+
+help = "Diagnose the RAG system."
+
+parser = argparse.ArgumentParser(
+    description = f"{help} Performs comprehensive health checks on all components, validates configurations, and reports issues to ensure optimal system operation",
+    add_help = False,
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+)
+
+subparsers = parser.add_subparsers(
+    title = "subcommands",
+    dest = "subcommand",
+)
+
+subparsers.add_parser(
+    "check",
+    help = "Check RAG System. Scan all components of the system and diagnose status",
+    description = "Check RAG System. Scan all components of the system and diagnose status",
+)
---
+++ packages/pirag/pirag-0.1.1.tar.gz/pirag-0.1.1/app/rag/test/service.py
@@ -0,0 +1,9 @@
+# https://milvus.io/docs/integrate_with_ragas.md
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+    context_precision,
+    context_recall,
+)
+
+
