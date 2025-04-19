8bdecf07f646d03eb15d458b18e12b2bcf3f7d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:03:59 +0000

    Add package aws-cost-lens/0.1.3/aws_cost_lens-0.1.3-py3-none-any.whl

---
+++ packages/aws-cost-lens/aws_cost_lens-0.1.3-py3-none-any.whl/aws_cost_lens/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+AWS Cost Lens - Package Entry Point
+
+This module allows the package to be executed directly with:
+python -m aws_cost_lens
+"""
+
+from aws_cost_lens.cli import entry_point
+
+if __name__ == "__main__":
+    entry_point()
