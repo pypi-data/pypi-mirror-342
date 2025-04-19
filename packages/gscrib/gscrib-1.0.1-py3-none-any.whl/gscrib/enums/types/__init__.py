dd021b4c0ca345515b0bd7b90c96a76dd991f021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:58 +0000

    Add package gscrib/1.0.1/gscrib-1.0.1-py3-none-any.whl

---
+++ packages/gscrib/gscrib-1.0.1-py3-none-any.whl/gscrib/enums/types/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+Type definitions for machine control parameters.
+
+This package provides enumeration classes that define various machine
+control parameters and operational modes.
+"""
+
+from .axis import Axis
+from .bed_temperature import BedTemperature
+from .chamber_temperature import ChamberTemperature
+from .coolant_mode import CoolantMode
+from .direction import Direction
+from .distance_mode import DistanceMode
+from .extrusion_mode import ExtrusionMode
+from .fan_mode import FanMode
+from .feed_mode import FeedMode
+from .halt_mode import HaltMode
+from .hotend_temperature import HotendTemperature
+from .plane import Plane
+from .positioning_mode import PositioningMode
+from .power_mode import PowerMode
+from .spin_mode import SpinMode
+from .tool_swap_mode import ToolSwapMode
+
+__all__ = [
+    "Axis",
+    "BedTemperature",
+    "ChamberTemperature",
+    "CoolantMode",
+    "Direction",
+    "DistanceMode",
+    "ExtrusionMode",
+    "FanMode",
+    "FeedMode",
+    "HaltMode",
+    "HotendTemperature",
+    "Plane",
+    "PositioningMode",
+    "PowerMode",
+    "SpinMode",
+    "ToolSwapMode",
+]
