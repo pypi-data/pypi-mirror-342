d6a498703858f424132b8d202f0ff0c02122be82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:03:52 +0000

    Add package conector-total/0.2.0/conector_total-0.2.0.tar.gz

---
+++ packages/conector-total/conector_total-0.2.0.tar.gz/conector_total-0.2.0/conector_total/__main__.py
@@ -0,0 +1,7 @@
+from .speakers import Agustin
+
+def main():
+    Agustin().print_name()
+    
+if __name__ == "__main__":
+    main()
---
+++ packages/conector-total/conector_total-0.2.0.tar.gz/conector_total-0.2.0/conector_total/speakers/agustin.py
@@ -0,0 +1,4 @@
+from conector_total.speaker import Speaker
+
+class Agustin(Speaker):
+    name = "Agustin"
