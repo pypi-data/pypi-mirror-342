042d46b793f16093d1f8c9e1f51f9f5b4ffad7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:03:55 +0000

    Add package asfa/0.1.2/asfa-0.1.2.tar.gz

---
+++ packages/asfa/asfa-0.1.2.tar.gz/asfa-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Function to read requirements from a file
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file.readlines() if line.strip() and not line.startswith("#")]
+
+# Get dependencies from requirements.txt
+install_requires = parse_requirements('requirements.txt')
+
+# Read the long description from README.md
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='asfa',                # Name of your package
+    version='0.1.2',            # Version
+    packages=find_packages(),   # Automatically find packages in the 'your_package' directory
+    install_requires=install_requires,  # List dependencies read from requirements.txt
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
