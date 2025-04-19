1c01fab4b8a7ded24e2fcb571898237247d05b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:06 +0000

    Add package quipus-generate/0.1.4/quipus_generate-0.1.4.tar.gz

---
+++ packages/quipus-generate/quipus_generate-0.1.4.tar.gz/quipus_generate-0.1.4/amautta_project/git_generator.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+import subprocess
+import os
+
+def run_command(command, cwd=None):
+    subprocess.run(
+        command,
+        cwd=cwd,
+        shell=True,
+        check=True,
+        stdout=subprocess.DEVNULL,   # No mostrar salida estándar
+    )
+
+def create(proccess, name):
+    if proccess == "init":
+        gitignore = (
+            ".env\n"
+            ".DS_Store\n"
+            ".vscode/\n"
+            ".idea/\n"
+            "example_entity\n"
+            "nginx/sites/example_entity.conf\n"
+        )
+    elif proccess == "microservice":
+        gitignore = (
+            "# Entornos virtuales\n"
+            "venv/\n"
+            ".env/\n"
+            "\n"
+            "# Archivos de Python compilados\n"
+            "__pycache__/\n"
+            "*.py[cod]\n"
+            "*.pyo\n"
+            "\n"
+            "# Configuración de IDE\n"
+            ".vscode/\n"
+            ".idea/\n"
+            "\n"
+            "# Archivos temporales\n"
+            "*.log\n"
+            ".DS_Store\n"
+            "\n"
+            "# Build\n"
+            "dist/\n"
+            "build/\n"
+            "*.egg-info/\n"
+            "\n"
+            "# Archivos de configuración de Docker\n"
+            "Dockerfile\n"
+            ".dockerignore\n"
+        )
+
+    archivos = {
+        ".gitignore": gitignore,
+    }
+
+    # Crear archivos
+    for archivo, content in archivos.items():
+        ruta_archivo = os.path.join(name, archivo)
+        with open(ruta_archivo, "w") as f:
+            f.write(content)
+
+    if proccess == "init":
+        tipo = "proyecto"
+    elif proccess == "microservice":
+        tipo = "microservicio"
+
+    print(f"✅ Se creo el archivo .gitignore para el {tipo} '{name}' con éxito.")
+
+    # Ejecutar comandos de git en el directorio del proyecto
+    run_command("git init", cwd=name)
+    run_command("git add .", cwd=name)
+    run_command("git commit -m 'Initial commit'", cwd=name)
+    run_command("git branch -M main", cwd=name)
+
+    print(f"✅ Se inicializó el repositorio git para el {tipo} '{name}' con éxito.")
+
