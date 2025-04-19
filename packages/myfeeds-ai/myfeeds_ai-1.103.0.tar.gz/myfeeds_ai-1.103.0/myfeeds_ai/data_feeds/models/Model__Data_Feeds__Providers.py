195a9bf0db97a8e6682d5a8394102a58a7d88dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:50 +0000

    Add package myfeeds-ai/1.103.0/myfeeds_ai-1.103.0.tar.gz

---
+++ packages/myfeeds-ai/myfeeds_ai-1.103.0.tar.gz/myfeeds_ai-1.103.0/myfeeds_ai/data_feeds/models/Model__Data_Feeds__Providers.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Model__Data_Feeds__Providers(Enum):
+    HACKER_NEWS         : str = 'hacker-news'
+    OPEN_SECURITY_SUMMIT: str = 'open-security-summit'
+    PERSONAS            : str = 'personas'
+
