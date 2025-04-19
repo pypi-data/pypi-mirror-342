97a1d2bd87778436b91beaa462e0f578f00d22fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:12 +0000

    Add package vpype-gscrib/1.0.1/vpype_gscrib-1.0.1.tar.gz

---
+++ packages/vpype-gscrib/vpype_gscrib-1.0.1.tar.gz/vpype_gscrib-1.0.1/vpype_gscrib/excepts/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level plugin exceptions and error handling.
+
+This module defines a collection of exceptions to handle errors related
+to G-code processing and generation.
+"""
+
+from .plugin_errors import ImageLoadError
+from .plugin_errors import VpypeGscribError
+
+__all__ = [
+    "ImageLoadError",
+    "VpypeGscribError",
+]
