7135477a5524b5802a418ca5255bfd05b039299d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 16:03:52 +0000

    Add package rumdl/0.0.45/rumdl-0.0.45.tar.gz

---
+++ packages/rumdl/rumdl-0.0.45.tar.gz/rumdl-0.0.45/tests/lib.rs
@@ -0,0 +1,9 @@
+mod advanced_integration_tests;
+mod cli_duplication_test;
+mod cli_integration_tests;
+mod comprehensive_integration_tests;
+mod init_command_test;
+mod rules;
+mod utils;
+mod output_format_tests;
+mod pyproject_config_tests;
