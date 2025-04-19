19af371f3f4ac97db298a1b3300743085c49188d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:42 +0000

    Add package funJplot/0.0.0.2/funjplot-0.0.0.2.tar.gz

---
+++ packages/funJplot/funjplot-0.0.0.2.tar.gz/funjplot-0.0.0.2/funJplot/__init__.py
@@ -0,0 +1,14 @@
+from .base import GeometricAnimation, GuiInterface
+from .matrix.vector_rotation import VectorRotation
+from .parametric_curves import ParametricCurve, Spiral, Ellipse, Lissajous, LorenzAttractor
+
+__all__ = [
+    'GeometricAnimation',
+    'GuiInterface',
+    'VectorRotation',
+    'ParametricCurve',
+    'Spiral',
+    'Ellipse',
+    'Lissajous',
+    'LorenzAttractor'
+]
