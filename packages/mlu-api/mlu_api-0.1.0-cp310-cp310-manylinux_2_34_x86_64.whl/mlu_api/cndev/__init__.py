80b7b71551fc4934b2297bfca85e6c940b9b89ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:03:49 +0000

    Add package mlu-api/0.1.0/mlu_api-0.1.0-cp310-cp310-manylinux_2_34_x86_64.whl

---
+++ packages/mlu-api/mlu_api-0.1.0-cp310-cp310-manylinux_2_34_x86_64.whl/mlu_api/cndev/__init__.py
@@ -0,0 +1,3 @@
+from ._cndev_core import get_core_utilizations
+
+__all__ = ['get_core_utilizations']
---
+++ packages/mlu-api/mlu_api-0.1.0-cp310-cp310-manylinux_2_34_x86_64.whl/mlu_api/cndev/_cndev_core.pyi
@@ -0,0 +1,4 @@
+from typing import List
+
+def get_core_utilizations(card_id: int) -> List[int]:
+    ...
