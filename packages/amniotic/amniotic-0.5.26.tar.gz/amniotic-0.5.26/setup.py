63e8d11edfa0458110f06852af26df0c2a42d643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:11 +0000

    Add package amniotic/0.5.26/amniotic-0.5.26.tar.gz

---
+++ packages/amniotic/amniotic-0.5.26.tar.gz/amniotic-0.5.26/setup.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+packages = find_packages()
+name = next(iter(packages))
+path_base = Path(__file__).absolute().parent
+path = path_base / name / 'version'
+__version__ = path.read_text().strip()
+
+setup(
+    long_description=(path_base / 'readme.md').read_text(),
+    long_description_content_type='text/markdown',
+    name=name,
+    version=__version__,
+    url=f'https://fmtr.link/{name}',
+    license='Copyright © 2022 Frontmatter. All rights reserved.',
+    author='Frontmatter',
+    description='A multi-output ambient sound mixer for Home Assistant',
+    keywords='ambient sound audio white noise masking sleep',
+    packages=packages,
+    package_data={
+        name: [f'version'],
+    },
+    install_requires=[
+        'paho-mqtt',
+        'python-vlc',
+        'getmac',
+        'pyyaml',
+        'appdirs',
+        'johnnydep',
+        'pytubefix',
+        'cachetools'
+    ],
+    extras_require={
+        'debug': [
+            'psutil'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'amniotic = amniotic.mqtt.start:start',
+        ],
+    }
+)
