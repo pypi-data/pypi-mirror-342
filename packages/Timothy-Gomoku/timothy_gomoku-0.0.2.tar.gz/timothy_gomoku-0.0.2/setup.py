67e59a2a73c2e5100ea7a4cc275edbbd858f3626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:43 +0000

    Add package Timothy-Gomoku/0.0.2/timothy_gomoku-0.0.2.tar.gz

---
+++ packages/Timothy-Gomoku/timothy_gomoku-0.0.2.tar.gz/timothy_gomoku-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Timothy-Gomoku",
+    version="0.0.2",
+    description="A simple Gomoku game powered by reinforcement learning.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zeyu Xie",
+    author_email="xie.zeyu20@gmail.com",
+    url="https://github.com/Zeyu-Xie/Timothy-Gomoku",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
