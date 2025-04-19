a6751e7ead7e7ba59a46b4caec511f3640fdea59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 04:03:38 +0000

    Add package meshcore/1.9.7/meshcore-1.9.7-py3-none-any.whl

---
+++ packages/meshcore/meshcore-1.9.7-py3-none-any.whl/meshcore/packets.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+# Packet prefixes for the protocol
+class PacketType(Enum):
+    OK = 0
+    ERROR = 1
+    CONTACT_START = 2
+    CONTACT = 3
+    CONTACT_END = 4
+    SELF_INFO = 5
+    MSG_SENT = 6
+    CONTACT_MSG_RECV = 7
+    CHANNEL_MSG_RECV = 8
+    CURRENT_TIME = 9
+    NO_MORE_MSGS = 10
+    CONTACT_URI = 11
+    BATTERY = 12
+    DEVICE_INFO = 13
+    CLI_RESPONSE = 50
+    
+    # Push notifications
+    ADVERTISEMENT = 0x80
+    PATH_UPDATE = 0x81
+    ACK = 0x82
+    MESSAGES_WAITING = 0x83
+    RAW_DATA = 0x84
+    LOGIN_SUCCESS = 0x85
+    LOGIN_FAILED = 0x86
+    STATUS_RESPONSE = 0x87
+    LOG_DATA = 0x88
+    TRACE_DATA = 0x89
