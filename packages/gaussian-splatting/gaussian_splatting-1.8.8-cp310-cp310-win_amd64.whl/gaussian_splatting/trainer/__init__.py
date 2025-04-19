6a4666534b8f8dc1ab9cbf1f961b2d2473e753fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 05:03:33 +0000

    Add package gaussian-splatting/1.8.8/gaussian_splatting-1.8.8-cp310-cp310-win_amd64.whl

---
+++ packages/gaussian-splatting/gaussian_splatting-1.8.8-cp310-cp310-win_amd64.whl/gaussian_splatting/trainer/__init__.py
@@ -0,0 +1,13 @@
+from .abc import AbstractTrainer, TrainerWrapper
+from .base import BaseTrainer
+from .densifier import AbstractDensifier, Densifier, DensificationTrainer, BaseDensificationTrainer, DensificationInstruct
+from .camera_trainable import CameraOptimizer, BaseCameraTrainer
+from .opacity_reset import OpacityResetter
+from .sh_lift import SHLifter, BaseSHLiftTrainer
+from .combinations import OpacityResetDensificationTrainer, OpacityResetDensificationCameraTrainer
+from .combinations import SHLiftCameraTrainer, SHLiftDensificationTrainer, SHLiftOpacityResetDensificationTrainer, SHLiftOpacityResetDensificationCameraTrainer
+from .depth import DepthTrainer, BaseDepthTrainer
+from .combinations import DepthTrainerWrapper, DepthCameraTrainerWrapper
+from .combinations import DepthSHLiftTrainer, DepthCameraTrainer
+from .combinations import DepthOpacityResetDensificationTrainer, DepthOpacityResetDensificationCameraTrainer
+from .combinations import DepthSHLiftCameraTrainer, DepthSHLiftDensificationTrainer, DepthSHLiftOpacityResetDensificationTrainer, DepthSHLiftOpacityResetDensificationCameraTrainer
