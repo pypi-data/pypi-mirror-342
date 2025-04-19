fa4aecf49c208c59eb2e38e1739fa0bab3a37a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:04:04 +0000

    Add package fund-insight-engine/0.2.7/fund_insight_engine-0.2.7-py3-none-any.whl

---
+++ packages/fund-insight-engine/fund_insight_engine-0.2.7-py3-none-any.whl/fund_insight_engine/__init__.py
@@ -0,0 +1,4 @@
+from .s3_retriever import *
+from .mongodb_retriever import *
+from .mongodb_retriever.pseudo_consts import MAPPING_FUND_NAMES, MAPPING_INCEPTION_DATES
+from .mongodb_retriever.mappings import get_mapping_fund_names_mongodb as get_mapping_fund_names
