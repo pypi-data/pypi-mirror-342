fc12e0ab1e92d3619f674d9766811e7ae593e0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:03 +0000

    Add package plover-stenobee/0.8.20/plover_stenobee-0.8.20-py3-none-any.whl

---
+++ packages/plover-stenobee/plover_stenobee-0.8.20-py3-none-any.whl/stenobee/metas.py
@@ -0,0 +1,21 @@
+# stenobee/metas.py
+
+import inflect
+
+def pluralize(ctx, _):
+  p = inflect.engine()
+
+  last_word = ctx.last_fragments()[0]
+
+  action = ctx.copy_last_action()
+  action.prev_replace = last_word
+  action.text = p.plural(last_word)
+  action.prev_attach = True
+
+  return action
+
+k = pluralize
+
+
+
+
