dd32b7aee6b94043217e8d7ace68fbe5537bff2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:03:59 +0000

    Add package nurikit/0.1.0a14.post1/nurikit-0.1.0a14.post1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nurikit/nurikit-0.1.0a14.post1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nuri/__init__.py
@@ -0,0 +1,29 @@
+#
+# Project NuriKit - Copyright 2023 SNU Compbio Lab.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# pyright: reportUnusedImport=false
+"""
+Project NuriKit: *the* fundamental software platform for chem- and
+bio-informatics."""
+
+__all__ = [
+    "readfile",
+    "readstring",
+    "to_smiles",
+    "to_mol2",
+    "to_sdf",
+    "to_pdb",
+    "periodic_table",
+    "__version__",
+]
+
+try:
+    from ._version import __full_version__, __version__
+except ImportError:
+    __version__ = __full_version__ = "unknown"
+
+from . import _log_adapter
+from .core import periodic_table
+from .fmt import readfile, readstring, to_mol2, to_sdf, to_smiles, to_pdb
