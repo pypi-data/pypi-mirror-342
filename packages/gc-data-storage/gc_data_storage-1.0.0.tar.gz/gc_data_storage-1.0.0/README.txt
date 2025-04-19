b0cd5d10bb7b86e53d5af72b1ace7d91afeb27e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:13:49 +0000

    Add package gc-data-storage/1.0.0/gc_data_storage-1.0.0.tar.gz

---
+++ packages/gc-data-storage/gc_data_storage-1.0.0.tar.gz/gc_data_storage-1.0.0/README.txt
@@ -0,0 +1,18 @@
+Functions in the 'gc_data_storage' package.
+- save_data_to_bucket()
+- read_data_from_bucket()
+- list_saved_data()
+- copy_from_bucket_to_bucket()
+
+Using the package
+
+```
+pip install gc_data_storage
+from gc_data_storage import gc_data_storage as gs
+gs = gs()
+
+gs.list_saved_data()
+
+```
+
+For detailed information, including examples, see https://github.com/AymoneKouame/google-could-data-storage/.
---
+++ packages/gc-data-storage/gc_data_storage-1.0.0.tar.gz/gc_data_storage-1.0.0/gc_data_storage/__init__.py
@@ -0,0 +1,37 @@
+#INIT FOR GC_DATA_STORAGE - updted 4/18/2025
+
+from gc_data_storage.gc_data_storage import gc_data_storage
+
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("subprocess", "pkg_resources")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(f"{dependency}: {e}")
+
+if missing_dependencies:
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(missing_dependencies)
+        +".\nPease install module(s) before proceeding."
+    )
+
+#install other packages if not installed
+import pkg_resources
+import subprocess
+import os
+
+def install_if_not_installed(modules):  
+    uninstalled_packages = [p for p in modules if p not in sorted(["%s==%s" % (i.key, i.version) for i in pkg_resources.working_set])]
+    uninstalled_packages_f = " ".join(uninstalled_packages)
+    res = subprocess.run(['pip', 'install', '--upgrade', uninstalled_packages_f], capture_output=True)
+
+print('Installing dependencies. You may need to restart your kernel afterwards.')
+install_if_not_installed(modules = ['pyarrow', 'openpyxl', 'pandas', 'google-cloud', 'google-api-core', 'ipython'])
+
+import pandas as pd
+from google.cloud import storage
+from google.api_core import exceptions
+from IPython.display import Image
