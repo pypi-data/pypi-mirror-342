7b8935d320caf9fdd3b6891cd502a90958b702b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:03:49 +0000

    Add package ttt-ex/0.666/ttt-ex-0.666.tar.gz

---
+++ packages/ttt-ex/ttt-ex-0.666.tar.gz/ttt-ex-0.666/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='ttt-ex',
+    version='0.666',     
+    packages=find_packages(),    
+    install_requires=[],    
+    author='corpbros',    
+    author_email='corpbros06@gmail.com',
+    description='description',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
