8951d94869d00452e0404ffb7c0c4969c38c1d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:04:25 +0000

    Add package phantasma-sdk-py/1.0.11/phantasma_sdk_py-1.0.11.tar.gz

---
+++ packages/phantasma-sdk-py/phantasma_sdk_py-1.0.11.tar.gz/phantasma_sdk_py-1.0.11/src/phantasma_py/VM/TypeAuction.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+class TypeAuction(IntEnum):
+    Fixed = 0
+    Classic = 1
+    Reserve = 2
+    Dutch = 3
---
+++ packages/phantasma-sdk-py/phantasma_sdk_py-1.0.11.tar.gz/phantasma_sdk_py-1.0.11/src/phantasma_py/__init__.py
@@ -0,0 +1,15 @@
+from .VM import *
+from .Utils import *
+from .Types import *
+from .Tx import *
+from .Interfaces import *
+from .RPC import *
+
+__all__ = [
+    "VM",
+    "Utils",
+    "Types",
+    "Tx",
+    "Interfaces",
+    "RPC"
+]
