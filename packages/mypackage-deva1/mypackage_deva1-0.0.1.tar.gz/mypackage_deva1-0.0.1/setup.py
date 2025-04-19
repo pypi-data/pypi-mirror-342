3e11d740d063341f0857adf1e72a955eb989341f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 03:14:48 +0000

    Add package mypackage-deva1/0.0.1/mypackage_deva1-0.0.1.tar.gz

---
+++ packages/mypackage-deva1/mypackage_deva1-0.0.1.tar.gz/mypackage_deva1-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="mypackage_deva1",
+version="0.0.1",
+author="devadharshinie",
+author_email="mdevadharshinie257@gmail.com",
+packages=find_packages(),
+)
