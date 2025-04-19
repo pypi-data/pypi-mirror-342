1cbfc5a5ccad721fc74d8c512321b3c7e6198698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 14:04:31 +0000

    Add package pymimir/0.12.14/pymimir-0.12.14-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pymimir/pymimir-0.12.14-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pymimir/advanced/common/__init__.pyi
@@ -0,0 +1,10 @@
+
+
+class FlatBitset:
+    def __init__(self) -> None: ...
+
+class FlatDoubleList:
+    def __init__(self) -> None: ...
+
+class FlatIndexList:
+    def __init__(self) -> None: ...
