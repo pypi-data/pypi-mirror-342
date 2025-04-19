1dae99eb1b2f59575297d6ba77c5f71d1d57c4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:03:45 +0000

    Add package wren-tools/0.5.0/wren_tools-0.5.0.tar.gz

---
+++ packages/wren-tools/wren_tools-0.5.0.tar.gz/wren_tools-0.5.0/wren/cli.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import subprocess
+from random import choice
+from wren.core import (
+    create_new_task,
+    get_summary,
+    get_task_content,
+    get_task_file,
+    get_tasks,
+    mark_task_done,
+    prepend_to_filename,
+    notes_dir,
+    config_file,
+    data_dir,
+    __version__,
+)
+
+editor = os.environ.get("EDITOR", "vi")
+
+
+def create_file(name):
+    filename = create_new_task(name)
+    print("created task:", filename)
+
+
+def list_files(s="", done=False):
+    tasks = get_tasks(s, done)
+    print("".join(map(lambda t: "➜ " + t + "\n", tasks))[:-1])
+
+
+def print_random():
+    tasks = get_tasks("")
+    print(choice(tasks))
+
+
+def print_summary():
+    summary = get_summary()
+    print(summary)
+
+
+def edit_content(name):
+    found, filename = get_task_file(name)
+    if found:
+        filepath = os.path.join(notes_dir, filename)
+        subprocess.run([editor, filepath])
+
+
+def read_content(name):
+    content = get_task_content(name)
+    print(content)
+
+
+def prepend_to_task(name, text):
+    content = prepend_to_filename(name, text)
+    print(content)
+
+
+def mark_done(name):
+    message = mark_task_done(name)
+    print(message)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("task", nargs="*", help="a new task to be created")
+    parser.add_argument(
+        "-l",
+        "--ls",
+        "--list",
+        type=str,
+        help="List all current tasks. add -d to list done tasks",
+        nargs="?",
+        const="",
+        default=None,
+    )
+    parser.add_argument(
+        "-d",
+        "--done",
+        metavar="foo",
+        type=str,
+        nargs="?",
+        const="",
+        help="Mark a task as done",
+    )
+    parser.add_argument(
+        "-r", "--read", metavar="foo", type=str, help="Read a task content"
+    )
+    parser.add_argument(
+        "-e", "--edit", metavar="foo", type=str, help="Edit a task content"
+    )
+    parser.add_argument(
+        "--prepend",
+        metavar="foo",
+        type=str,
+        help="Prepend something to the task's filename",
+    )
+
+    parser.add_argument(
+        "-o", "--one", action="store_true", help="Print one random task"
+    )
+    parser.add_argument(
+        "-s", "--summary", action="store_true", help="Generate a summary"
+    )
+    parser.add_argument("--telegram", action="store_true", help="Start Telegram bot")
+    parser.add_argument("--matrix", action="store_true", help="Start Matrix bot")
+    parser.add_argument("--http", action="store_true", help="Start HTTP server")
+    parser.add_argument("--version", action="store_true", help="Show Wren version")
+
+    args = parser.parse_args()
+
+    if args.ls != None:
+        list_files(args.ls, args.done != None)
+    elif args.version:
+        print("Wren " + __version__)
+        print("\nconfig: " + config_file)
+        print("data directory: " + data_dir)
+    elif args.http:
+        from wren.http_server import start_server
+
+        start_server()
+    elif args.telegram:
+        from wren.telegram import start_bot
+
+        start_bot()
+    elif args.matrix:
+        from wren.matrix import start_bot
+
+        start_bot()
+    elif args.one:
+        print_random()
+    elif args.edit:
+        edit_content(args.edit)
+    elif args.prepend:
+        prepend_to_task(" ".join(args.task), args.prepend)
+    elif args.summary:
+        print_summary()
+    elif args.read:
+        read_content(args.read)
+    elif args.done:
+        mark_done(args.done)
+    else:
+        if args.task:
+            create_file(" ".join(args.task))
+        else:
+            list_files("", args.done != None)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/wren-tools/wren_tools-0.5.0.tar.gz/wren_tools-0.5.0/wren/telegram.py
@@ -0,0 +1,179 @@
+try:
+    import telebot
+    from apscheduler.schedulers.background import BackgroundScheduler
+    from apscheduler.triggers.cron import CronTrigger
+except:
+    print(
+        "Please install the Telegram bot dependencies: pip install 'wren-notes[telegram]'"
+    )
+    exit(1)
+import os
+import json
+from platformdirs import user_data_dir
+from croniter import croniter
+from wren.core import (
+    create_new_task,
+    get_summary,
+    mark_task_done,
+    get_tasks,
+    get_task_content,
+    config,
+)
+
+bot = telebot.TeleBot(config["telegram_token"])
+allowed_chats = config["allowed_telegram_chats"]
+scheduler = BackgroundScheduler()
+
+data_dir = user_data_dir("wren", "wren")
+schedules_path = os.path.join(data_dir, "schedules.json")
+
+
+def only_allowed_chats(func):
+    def authenticate(*args, **kwargs):
+        chat_id = args[0].chat.id
+        if chat_id not in allowed_chats:
+            bot.send_message(
+                chat_id,
+                "please add this chat id to your allowed chats: " + str(chat_id),
+            )
+            return
+        result = func(*args, **kwargs)
+        return result
+
+    return authenticate
+
+
+def get_all_schedules():
+    try:
+        with open(schedules_path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+@bot.message_handler(commands=["list"])
+@only_allowed_chats
+def list_tasks(message):
+    filter = " ".join(message.text.split(" ")[1:])
+    tasks = get_tasks(filter)
+    response = "".join(map(lambda t: "- " + t + "\n", tasks))
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=["summary"])
+@only_allowed_chats
+def summary(message):
+    summary = get_summary()
+    bot.send_message(message.chat.id, summary)
+
+
+@bot.message_handler(commands=["done", "d", "do", "don"])
+@only_allowed_chats
+def mark_as_done(message):
+    name = " ".join(message.text.split(" ")[1:])
+    response = mark_task_done(name)
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=["read"])
+@only_allowed_chats
+def read_task(message):
+    name = " ".join(message.text.split(" ")[1:])
+    response = get_task_content(name)
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=["help"])
+@only_allowed_chats
+def help(message):
+    bot.send_message(
+        message.chat.id,
+        "wren\n\n- enter any text to save it as task\n- mark a task as done by `/done foo`",
+    )
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.set_my_commands(
+        [
+            telebot.types.BotCommand(command="help", description="Help"),
+            telebot.types.BotCommand(command="summary", description="Summary"),
+            telebot.types.BotCommand(command="list", description="List"),
+            telebot.types.BotCommand(command="schedule", description="Schedule"),
+        ]
+    )
+    bot.set_chat_menu_button(
+        message.chat.id, telebot.types.MenuButtonCommands("commands")
+    )
+    bot.send_message(message.chat.id, "wren.\n\nuse /help to get help")
+
+
+@bot.message_handler(commands=["schedule"])
+@only_allowed_chats
+def create_scheduled_message(message):
+    schedule = " ".join(message.text.split(" ")[1:])
+    job = [message.chat.id, schedule]
+
+    # read current schedules
+    schedules = get_all_schedules()
+
+    if not schedule:
+        my_schedules = [s[1] for s in schedules if s[0] == message.chat.id]
+        if my_schedules:
+            bot.send_message(
+                message.chat.id,
+                f'your summaries are scheduled for: {", ".join(my_schedules)}',
+            )
+        else:
+            bot.send_message(message.chat.id, f"you got nothing scheduled!")
+        return
+
+    if croniter.is_valid(schedule):
+        # save new schedule
+        schedules.extend([job])
+        with open(schedules_path, "w") as file:
+            json.dump(schedules, file, indent=2)
+
+        # register it
+        scheduler.add_job(
+            send_summary,
+            CronTrigger.from_crontab(schedule),
+            kwargs={"chat_id": message.chat.id},
+        )
+        bot.send_message(message.chat.id, f"Added a schedule: {schedule}")
+    else:
+        bot.send_message(message.chat.id, f"Invalid cron format: {schedule}")
+
+
+@bot.message_handler(
+    func=lambda message: len(message.text) > 3 and not message.text.startswith("/")
+)
+@only_allowed_chats
+def add(message):
+    filename = create_new_task(message.text)
+    bot.send_message(message.chat.id, "added: " + filename)
+
+
+@bot.message_handler(func=lambda message: True)
+@only_allowed_chats
+def reply_no(message):
+    bot.reply_to(message, "not sure what you want. seek /help")
+
+
+def send_summary(chat_id):
+    bot.send_message(chat_id, get_summary())
+
+
+schedules = get_all_schedules()
+for schedule in schedules:
+    scheduler.add_job(
+        send_summary,
+        CronTrigger.from_crontab(schedule[1]),
+        kwargs={"chat_id": schedule[0]},
+    )
+
+
+def start_bot():
+    print("Starting telegram bot")
+    scheduler.start()
+    bot.infinity_polling(timeout=10, long_polling_timeout=5)
