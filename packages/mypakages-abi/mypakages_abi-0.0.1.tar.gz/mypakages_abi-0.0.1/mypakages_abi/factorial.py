b2ec3403d341432f912c7e6128a177c17a1e0a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-19 15:04:58 +0000

    Add package mypakages-abi/0.0.1/mypakages_abi-0.0.1.tar.gz

---
+++ packages/mypakages-abi/mypakages_abi-0.0.1.tar.gz/mypakages_abi-0.0.1/mypakages_abi/factorial.py
@@ -0,0 +1,10 @@
+def fact(n):
+    if n<0:
+        return factorial is not for negative numbers
+    elif n==0 or n==1:
+        return factorial = 1
+    else:
+        return n*fact(n-1)
+n=int(input("enter a number:))
+print("factorial=",fact(n))
+
---
+++ packages/mypakages-abi/mypakages_abi-0.0.1.tar.gz/mypakages_abi-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+name="mypakages_abi",
+version="0.0.1",
+author="devadharshinie",
+author_email="mdevadharshinie257@gmail.com",
+packages=find_packages(),
+)
