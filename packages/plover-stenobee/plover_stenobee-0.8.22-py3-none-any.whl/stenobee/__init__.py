7ffbfd6c6d1fb4f0171ccfb9615cdb2f0ba34bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:04:19 +0000

    Add package plover-stenobee/0.8.22/plover_stenobee-0.8.22-py3-none-any.whl

---
+++ packages/plover-stenobee/plover_stenobee-0.8.22-py3-none-any.whl/stenobee/__init__.py
@@ -0,0 +1,75 @@
+from plover.system.english_stenotype import *
+
+KEYS = ('^-', 'X-', 'K-', 'P-', 'M-', 'S-', 'H-', 'L-', 'T-', 'J-', 'Q-', 'C-', 'N-', 'R-', 'D-', 'G-', 'F-', 'V-', 'B-', 'Z-', '-O', '-A', '-*', '-E', '-I')
+
+IMPLICIT_HYPHEN_KEYS = ()
+SUFFIX_KEYS = ()
+NUMBERS = {}
+NUMBER_KEY = None
+ORTHOGRAPHY_RULES = []
+ORTHOGRAPHY_WORDLIST = None
+FERAL_NUMBER_KEY = False
+ORTHOGRAPHY_RULES_ALIASES = {}
+UNDO_STROKE_STENO = "-*"
+
+KEYMAPS = {
+ 'Keyboard': {
+	'^-'         : '6',
+	'X-'         : 'x',
+	'K-'         : 'k',
+	'P-'         : 'p',
+	'M-'         : 'm',
+	'S-'         : 's',
+	'H-'         : 'h',
+	'L-'         : 'l',
+	'T-'         : 't',
+	'J-'         : 'j',
+	'Q-'         : 'q',
+	'C-'         : 'c',
+	'N-'         : 'n',
+	'R-'         : 'r',
+	'D-'         : 'd',
+	'G-'         : 'g',
+	'F-'         : 'f',
+	'V-'         : 'v',
+	'B-'         : 'b',
+	'Z-'         : 'z',
+	'-O'         : 'o',
+	'-A'         : 'a',
+	'-*'         : 'u',
+	'-E'         : 'e',
+	'-I'         : 'i',
+        'arpeggiate': 'space',
+        # Suppress adjacent keys to prevent miss-strokes.
+        'no-op'     : ''
+    }
+}
+
+DICTIONARIES_ROOT = 'asset:stenobee:assets'
+
+DEFAULT_DICTIONARIES = (
+    'StenoBee-Core.json',
+    'StenoBee-Common.json',
+    'StenoBee-Proper-Nouns.json',
+    'StenoBee-Numbers.py',
+    'StenoBee-Alt_Ergo_Numbers.json',
+    'StenoBee-Symbols&Spelling.json',
+    'StenoBee-Modifiers.json',
+    'StenoBeeNiche-Mathematics-[MSHTC].json',
+    'StenoBeeNiche-Pokemon-[KPMN].json',
+    'StenoBeeNiche-Emoji-[MJ].json',
+    'StenoBeeNiche-Minecraft-[MTCNRF].json',
+    'StenoBeeNiche-Mineral-[MLNR].json',
+    'StenoBeeNiche-Chemistry-[MSHTCR].json',
+    'StenoBeeNiche-Music&Instruments-[MSTCNRD].json',
+    'StenoBeeNiche-ModeSetSpace-[PMSTCD].json',
+)
+
+
+
+
+
+
+
+
+
