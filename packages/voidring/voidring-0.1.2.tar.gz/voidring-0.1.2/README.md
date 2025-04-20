7b8062ef16e4d87e8deb4e95a7161f1ba859d0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 15:04:03 +0000

    Add package voidring/0.1.2/voidring-0.1.2.tar.gz

---
+++ packages/voidring/voidring-0.1.2.tar.gz/voidring-0.1.2/README.md
@@ -0,0 +1,2 @@
+# voidring
+VoidRing - A RocksDB-powered KV Storage Engine with Spatial Compression
---
+++ packages/voidring/voidring-0.1.2.tar.gz/voidring-0.1.2/src/voidring/__init__.py
@@ -0,0 +1,9 @@
+from .index import IndexedRocksDB
+from .base_rocksdb import BaseRocksDB
+from .cache.cache_rocksdb import CachedRocksDB
+
+import tempfile
+
+default_rocksdb = IndexedRocksDB(
+    path=tempfile.mkdtemp(prefix="__VOIDRING_")
+)
