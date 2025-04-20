e2a0f85ba41b3558c09de80b518c3dc7496d44e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 15:03:58 +0000

    Add package easy-qmainwindow/0.0.1/easy_qmainwindow-0.0.1.tar.gz

---
+++ packages/easy-qmainwindow/easy_qmainwindow-0.0.1.tar.gz/easy_qmainwindow-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='easy_qmainwindow',
+    version='0.0.1',
+    description='A simple PyQt5 wrapper for QMainWindow',
+    author='HuangHaoHua',
+    author_email='13140752715@163.com',
+    url='https://github.com/Locked-chess-official/easy_qmainwindow',
+    py_modules=['easy_qmainwindow'],
+    install_requires=[
+        'PyQt5',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',]
+    )
