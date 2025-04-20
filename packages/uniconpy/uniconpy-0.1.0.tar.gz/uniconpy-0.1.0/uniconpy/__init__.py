786aeebe1582687e7514b603eb67308aeb2d8dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:28 +0000

    Add package uniconpy/0.1.0/uniconpy-0.1.0.tar.gz

---
+++ packages/uniconpy/uniconpy-0.1.0.tar.gz/uniconpy-0.1.0/uniconpy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#ACESSANDO DIRETAMETE AS FUNÇÕES
+#from .massa import *
+#from .temperatura import *
+#from .volume import *
+
+#ACESSANDO NOME DO MÓDULO + FUNÇÃO
+from . import massa
+from . import temperatura
+from . import volume
+
+
