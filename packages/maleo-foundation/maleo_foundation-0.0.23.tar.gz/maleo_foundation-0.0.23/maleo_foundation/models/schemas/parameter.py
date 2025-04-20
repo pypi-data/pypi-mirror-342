e4d02cd39037ee13fce4dd8e4a0d53d2a31caded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:04 +0000

    Add package maleo-foundation/0.0.23/maleo_foundation-0.0.23.tar.gz

---
+++ packages/maleo-foundation/maleo_foundation-0.0.23.tar.gz/maleo_foundation-0.0.23/maleo_foundation/models/schemas/parameter.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from maleo_foundation.models.enums import BaseEnums
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.models.types.general import BaseGeneralTypes
+from maleo_foundation.models.types.parameter import BaseParameterTypes
+
+class BaseParameterSchemas:
+    class Filters(BaseModel):
+        filters:BaseGeneralTypes.ListOfStrings = Field([], description="Filters for date range, e.g. 'created_at|from::<ISO_DATETIME>|to::<ISO_DATETIME>'.")
+
+    class DateFilters(BaseModel):
+        date_filters:BaseParameterTypes.ListOfDateFilters = Field([], description="Date filters to be applied")
+
+    class Sorts(BaseModel):
+        sorts:BaseGeneralTypes.ListOfStrings = Field(["id.asc"], description="Sorting columns in 'column_name.asc' or 'column_name.desc' format.")
+
+    class SortColumns(BaseModel):
+        sort_columns:BaseParameterTypes.ListOfSortColumns = Field([BaseGeneralSchemas.SortColumn(name="id", order=BaseEnums.SortOrder.ASC)], description="List of columns to be sorted")
