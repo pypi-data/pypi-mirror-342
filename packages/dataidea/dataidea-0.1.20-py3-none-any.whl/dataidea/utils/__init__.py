7070b4ea9ae1c4eac78fd93c72d0c03c67f1c1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:32 +0000

    Add package dataidea/0.1.20/dataidea-0.1.20-py3-none-any.whl

---
+++ packages/dataidea/dataidea-0.1.20-py3-none-any.whl/dataidea/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+Utility functions and classes
+"""
+from dataidea.utils.timing import timer
+from dataidea.utils.logging import event_log, llm_log
+
+__all__ = ['timer', 'event_log', 'llm_log'] 
