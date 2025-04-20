acf0c0cd4cf1877c923ff3125389df4909f04ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 15:03:59 +0000

    Add package ishelar/0.1.0/ishelar-0.1.0-py3-none-any.whl

---
+++ packages/ishelar/ishelar-0.1.0-py3-none-any.whl/ishelar/__init__.py
@@ -0,0 +1,8 @@
+"""
+ishelar: Like antigravity, but cooler — opens your SaurHub portfolio ✨
+"""
+
+import webbrowser
+
+print("🚀 Opening your portfolio at https://www.saurhub.in/")
+webbrowser.open("https://www.saurhub.in/")
