ce256db3961b8bc191b1669638b2dd123b1d6144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:03:58 +0000

    Add package dataidea/0.1.16/dataidea-0.1.16-py3-none-any.whl

---
+++ packages/dataidea/dataidea-0.1.16-py3-none-any.whl/dataidea/packages.py
@@ -0,0 +1,13 @@
+# most used packages
+from pathlib import Path
+import scipy as sp
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+__all__ = ['sp', 'np', 'pd', 'px', 'go', 'requests']
