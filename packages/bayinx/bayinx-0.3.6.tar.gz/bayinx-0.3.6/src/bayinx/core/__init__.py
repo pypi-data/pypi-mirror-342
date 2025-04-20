595eacd574f6accbfd234534639046dfa00afc48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:35 +0000

    Add package bayinx/0.3.6/bayinx-0.3.6.tar.gz

---
+++ packages/bayinx/bayinx-0.3.6.tar.gz/bayinx-0.3.6/src/bayinx/core/__init__.py
@@ -0,0 +1,7 @@
+from ._constraint import Constraint
+from ._flow import Flow
+from ._model import Model, constrain
+from ._parameter import Parameter
+from ._variational import Variational
+
+__all__ = ["Constraint", "Flow", "Model", "constrain", "Parameter", "Variational"]
