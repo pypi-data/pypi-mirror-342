77c72bc5bfe9bba0af61deee3b82dd5e1e9f8e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:47 +0000

    Add package zenbroker/0.3/zenbroker-0.3-py3-none-any.whl

---
+++ packages/zenbroker/zenbroker-0.3-py3-none-any.whl/zenbroker/client.py
@@ -0,0 +1,44 @@
+import httpx
+from typing import Dict, Any
+from pydantic import BaseModel, Field, HttpUrl
+
+class ZenbrokerClientOptions(BaseModel):
+    base_url: HttpUrl = Field(..., description="URL of the broker service.")
+    application_id: str = Field(..., description="Application ID")
+
+class PostPublishEventPayload(BaseModel):
+    applicationId: str = Field(..., description="Application ID")
+    channel: str = Field(..., description="ChannelID")
+    data: Dict[str, Any]
+
+class PostPublishEventResponse(BaseModel):
+    message: str = Field(..., description="Message from the server")
+    id: str = Field(..., description="ID of the message")
+
+class ZenbrokerClient:
+    def __init__(self, opts: ZenbrokerClientOptions) -> None:
+        self._url: str = str(opts.base_url)
+        self._application_id: str = str(opts.application_id)
+
+        self._api = httpx.Client(base_url=self._url)
+    
+    def publish(self, channel: str, data: Dict[str, Any]) -> PostPublishEventResponse:
+        post_data: PostPublishEventPayload = PostPublishEventPayload(
+            applicationId=self._application_id,
+            channel=channel,
+            data=data
+        )
+
+        response = self._api.post(
+            url="/producer/emit",
+            json=post_data.model_dump()
+        )
+
+        response.raise_for_status()
+        result = response.json()
+
+        return PostPublishEventResponse(
+            id=result.id,
+            message=result.message
+        )
+    
