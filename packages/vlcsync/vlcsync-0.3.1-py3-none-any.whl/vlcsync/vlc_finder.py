4e1622cd532d3f68dbd18bb19cc0938ff39593ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:20 +0000

    Add package vlcsync/0.3.1/vlcsync-0.3.1-py3-none-any.whl

---
+++ packages/vlcsync/vlcsync-0.3.1-py3-none-any.whl/vlcsync/vlc_finder.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import functools
+import getpass
+import sys
+import traceback
+from typing import Set
+from contextlib import contextmanager
+
+import psutil
+from psutil import Process
+
+from vlcsync.vlc_state import VlcId
+
+
+@contextmanager
+def skip_on_error():
+    try:
+        yield
+    except(psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        pass
+        # whatever your common handling is
+
+
+class IVlcListFinder:
+    def get_vlc_list(self) -> Set[VlcId]:
+        raise NotImplementedError()
+
+
+class LocalProcessFinderProvider(IVlcListFinder):
+    def __init__(self, iface: str):
+        self._iface = iface
+
+    def get_vlc_list(self) -> Set[VlcId]:
+        vlc_ports = set()
+
+        for proc in self._find_vlc_procs():
+            port = self._has_listen_port(proc, self._iface)
+            if port:
+                vlc_ports.add(VlcId(self._iface, port, proc.pid))
+
+        return vlc_ports
+
+    def _find_vlc_procs(self):
+        """Return a list of processes matching 'name'."""
+        ls = []
+        for p in psutil.process_iter():
+            with skip_on_error():
+                if self.is_vlc(p) and self.cur_user_own(p):
+                    ls.append(p)
+
+        return ls
+
+    @staticmethod
+    @functools.lru_cache(maxsize=1024)
+    def cur_user_own(p) -> bool:
+        with skip_on_error():
+            is_user_own = getpass.getuser() in p.username()
+            return is_user_own
+
+        # noinspection PyUnreachableCode
+        return False
+
+    @staticmethod
+    @functools.lru_cache(maxsize=1024)
+    def _has_listen_port(proc: Process, iface_ip) -> int | None:
+        with skip_on_error():
+            for p_conn in proc.connections("tcp4"):
+                if p_conn.status == 'LISTEN' and p_conn.laddr.ip == iface_ip:
+                    return p_conn.laddr.port
+
+        # noinspection PyUnreachableCode
+        return None
+
+    @functools.lru_cache(maxsize=1024)
+    def is_vlc(self, proc: Process):
+        with skip_on_error():
+            return proc.name() is not None and proc.name().lower() == 'vlc' or self.vlc_cmdline(proc)
+
+        # noinspection PyUnreachableCode
+        return False
+
+    @staticmethod
+    def vlc_cmdline(proc) -> bool:
+        for part in proc.cmdline():
+            if part is not None and 'vlc' in part.lower():
+                return True
+        return False
+
+
+def print_exc():
+    print("-" * 60)
+    print("Exception in user code: ", flush=True)
+    print("-" * 60)
+    traceback.print_exc(file=sys.stdout)
+    print("-" * 60)
+    print()
+    sys.stdout.flush()
+
+
+class ExtraHostFinder(IVlcListFinder):
+    def __init__(self, extra_hosts: Set[VlcId]):
+        self.extra_host = extra_hosts
+
+    def get_vlc_list(self) -> Set[VlcId]:
+        return self.extra_host
---
+++ packages/vlcsync/vlcsync-0.3.1-py3-none-any.whl/vlcsync/vlc_state.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Optional
+
+from loguru import logger
+
+MAX_DESYNC_SECONDS = 4
+"""
+Tip:
+Sometimes (on win + smb) delay increased/decreased randomly without change position in timeline
+Possible vlc somehow does tricky synchronization during playing
+
+Side by side playing same video visually appear asynchronous for little time
+then it returns into normal synchronous.
+
+In vlc preference by default enabled option "skip frames"
+Max 4 second diff calculated empirically
+"""
+
+
+class PlayState(Enum):
+    PLAYING = "playing"
+    STOPPED = "stopped"
+    PAUSED = "paused"
+    UNKNOWN = None
+
+    @classmethod
+    def _missing_(cls, value):
+        return PlayState.UNKNOWN
+
+    def __repr__(self):
+        return self.value
+
+
+@dataclass
+class State:
+    play_state: PlayState
+    seek: int
+    playlist_order_idx: int
+    """
+    vid_start_at - is abs time of video start in given vlc
+    
+    If this time changed it indicates time seek 
+    
+    See for details:
+       - play_in_same_pos()
+       - Vlc.cur_state() 
+    """
+    vid_start_at: float = field(repr=False)
+
+    def same(self, other: State):
+        playlist_same = self.same_playlist_item(other)
+        full_same = (self.same_play_state(other) and playlist_same and (
+                    self.play_in_same_pos(other) or self.pause_is_same_pos(other) or self.both_stopped(other)))
+        return full_same, playlist_same
+
+    def same_play_state(self, other: State):
+        return self.play_state == other.play_state
+
+    def pause_is_same_pos(self: State, other: State):
+        return self.play_state == other.play_state == PlayState.PAUSED and self.seek == other.seek
+
+    def play_in_same_pos(self: State, other: State):
+        """ Check time_diff only when play """
+        desync_secs = abs(self.vid_start_at - other.vid_start_at)
+
+        if 2 < desync_secs < MAX_DESYNC_SECONDS:
+            logger.debug(f"Asynchronous anomaly between probes: {desync_secs} secs")
+
+        return (
+                self.play_state == other.play_state == PlayState.PLAYING and
+                self.vid_start_at and other.vid_start_at and
+                desync_secs < MAX_DESYNC_SECONDS
+        )
+
+    def both_stopped(self, other: State):
+        return self.play_state == other.play_state == PlayState.STOPPED
+
+    def is_play_or_pause(self):
+        return self.play_state in [PlayState.PLAYING, PlayState.PAUSED]
+
+    def same_playlist_item(self, other):
+        return self.playlist_order_idx == other.playlist_order_idx
+
+
+@dataclass
+class PlayListItem:
+    order_index: int
+    vlc_internal_index: int
+
+
+@dataclass
+class PlayList:
+    items: List[PlayListItem]
+    active_item: Optional[PlayListItem]
+
+    def active_order_index(self) -> Optional[int]:
+        return self.active_item.order_index if self.active_item else None
+
+
+@dataclass(frozen=True)
+class VlcId:
+    addr: str
+    port: int
+    pid: Optional[int] = field(compare=False, hash=False, default=None)
+
+    def __str__(self):
+        return f"{self.addr}:{self.port}" + (f" (pid={self.pid})" if self.pid else "")
