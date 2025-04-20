3fd8abe09548bd093cdb7d938b9150ffaa89d3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:04:33 +0000

    Add package tsne-pso/1.1.2/tsne_pso-1.1.2-py3-none-any.whl

---
+++ packages/tsne-pso/tsne_pso-1.1.2-py3-none-any.whl/tsne_pso/__init__.py
@@ -0,0 +1,12 @@
+"""t-Distributed Stochastic Neighbor Embedding with Particle Swarm Optimization.
+
+This implementation is based on the paper:
+Allaoui, M., Belhaouari, S. B., Hedjam, R., Bouanane, K., & Kherfi, M. L. (2025).
+t-SNE-PSO: Optimizing t-SNE using particle swarm optimization.
+Expert Systems with Applications, 269, 126398.
+"""
+
+from ._tsne_pso import TSNEPSO
+
+__version__ = "1.1.2"
+__all__ = ["TSNEPSO"] 
