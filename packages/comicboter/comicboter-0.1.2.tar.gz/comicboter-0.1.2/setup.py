cd037fa4b61a6948c58c6a6876e93778c5bc3d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:03:56 +0000

    Add package comicboter/0.1.2/comicboter-0.1.2.tar.gz

---
+++ packages/comicboter/comicboter-0.1.2.tar.gz/comicboter-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="comicboter",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        'ncatbot',
+        'jmcomic',
+        'requests',
+        'pyyaml',
+        'configparser',
+    ],
+    entry_points={
+        'console_scripts': [
+            'comicboter = bot:main',
+        ],
+    },
+    author="ycssbc",
+    author_email="ycssbc@126.com",
+    description="A QQ bot for managing and downloading comics",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NapNeko/NapCatQQ",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="Apache License 2.0",
+    license_files=("LICENSE",),
+    python_requires='>=3.6',
+)
