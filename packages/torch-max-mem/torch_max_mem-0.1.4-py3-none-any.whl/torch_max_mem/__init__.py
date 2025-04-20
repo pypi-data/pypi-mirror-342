fd67ba33575f180987b6c934bd9ff89ac7dad929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:51 +0000

    Add package torch-max-mem/0.1.4/torch_max_mem-0.1.4-py3-none-any.whl

---
+++ packages/torch-max-mem/torch_max_mem-0.1.4-py3-none-any.whl/torch_max_mem/__init__.py
@@ -0,0 +1,8 @@
+"""Maximize memory utilization with PyTorch."""
+
+from .api import MemoryUtilizationMaximizer, maximize_memory_utilization
+
+__all__ = [
+    "MemoryUtilizationMaximizer",
+    "maximize_memory_utilization",
+]
