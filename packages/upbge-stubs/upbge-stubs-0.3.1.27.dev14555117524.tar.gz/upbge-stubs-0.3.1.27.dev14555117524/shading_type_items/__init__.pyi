0aa16fae4d7162a9e1bcfed03fdc52fc9770c51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:03:58 +0000

    Add package upbge-stubs/0.3.1.27.dev14555117524/upbge-stubs-0.3.1.27.dev14555117524.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev14555117524.tar.gz/upbge-stubs-0.3.1.27.dev14555117524/shading_type_items/__init__.pyi
@@ -0,0 +1,31 @@
+"""
+
+
+Shading Type Items
+^^^^^^^^^^^^^^^^^^
+
+:WIREFRAME:       
+  Wireframe.
+
+  Display the object as wire edges.
+
+:SOLID:           
+  Solid.
+
+  Display in solid mode.
+
+:MATERIAL:        
+  Material Preview.
+
+  Display in Material Preview mode.
+
+:RENDERED:        
+  Rendered.
+
+  Display render preview.
+
+.. _rna-enum-shading-type-items:
+
+"""
+
+import typing
