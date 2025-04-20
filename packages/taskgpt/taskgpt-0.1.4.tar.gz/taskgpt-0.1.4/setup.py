19de465092ec44bf7bb6556aa9af3df81f687d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:36 +0000

    Add package taskgpt/0.1.4/taskgpt-0.1.4.tar.gz

---
+++ packages/taskgpt/taskgpt-0.1.4.tar.gz/taskgpt-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="taskgpt",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "python-dotenv"
+    ],
+    entry_points={
+        'console_scripts': [
+            'taskgpt = taskgpt.cli:main',
+        ],
+    },
+    author="Ketan Hegde",
+    description="A CLI tool that helps you run GPT-based agents locally",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
