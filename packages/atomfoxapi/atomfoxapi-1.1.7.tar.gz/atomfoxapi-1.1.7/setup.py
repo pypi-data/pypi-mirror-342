532d7867999236e4197fb435d02e674800e8157f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:33 +0000

    Add package atomfoxapi/1.1.7/atomfoxapi-1.1.7.tar.gz

---
+++ packages/atomfoxapi/atomfoxapi-1.1.7.tar.gz/atomfoxapi-1.1.7/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='atomfoxapi',
+    version='1.1.7',
+    author='mc_c0rp',
+    author_email='mc.c0rp@icloud.com',
+    description='ATOM API | R04',
+    packages=['atomfoxapi'],
+    include_package_data=True,
+    python_requires='>=3.9',
+)
