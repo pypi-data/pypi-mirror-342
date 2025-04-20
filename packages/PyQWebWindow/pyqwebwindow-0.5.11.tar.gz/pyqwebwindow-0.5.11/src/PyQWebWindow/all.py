89212395500c5a9ae13bdc9e5f13a0eabc736df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:57 +0000

    Add package PyQWebWindow/0.5.11/pyqwebwindow-0.5.11.tar.gz

---
+++ packages/PyQWebWindow/pyqwebwindow-0.5.11.tar.gz/pyqwebwindow-0.5.11/src/PyQWebWindow/all.py
@@ -0,0 +1,7 @@
+from .QAppManager import QAppManager
+from .QWebWindow  import QWebWindow
+from .ipc.QIpc.server import QIpcServer
+from .ipc.QIpc.client import QIpcClient
+from .ipc.MqIpc.server import IpcServer
+from .ipc.MqIpc.client import IpcClient
+from .utils import Serializable, SerializableCallable
