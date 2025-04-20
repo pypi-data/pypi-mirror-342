c927d86c39b28df07b7fd8bf8907ae27b4286eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:12 +0000

    Add package cinrad/1.9.2/cinrad-1.9.2-cp311-cp311-win_amd64.whl

---
+++ packages/cinrad/cinrad-1.9.2-cp311-cp311-win_amd64.whl/cinrad/calc.py
@@ -0,0 +1,563 @@
+# -*- coding: utf-8 -*-
+# Author: Puyuan Du
+
+import datetime
+import time
+from typing import *
+from functools import wraps
+
+import numpy as np
+from xarray import DataArray, Dataset
+
+try:
+    from pykdtree.kdtree import KDTree
+except ImportError:
+    from scipy.spatial import KDTree
+
+from cinrad.utils import *
+from cinrad.grid import grid_2d, resample
+from cinrad.projection import height, get_coordinate
+from cinrad.error import RadarCalculationError
+from cinrad._typing import Volume_T
+from cinrad.common import get_dtype
+from cinrad.hca import hydro_class as _hca
+
+__all__ = [
+    "quick_cr",
+    "quick_et",
+    "quick_vil",
+    "VCS",
+    "quick_vild",
+    "GridMapper",
+    "hydro_class",
+]
+
+
+def require(var_names: List[str]) -> Callable:
+    def wrap(func: Callable) -> Callable:
+        @wraps(func)
+        def deco(*args, **kwargs) -> Any:
+            if len(args) == 1:
+                varset = args[0]
+            else:
+                varset = list(args)
+            if isinstance(varset, Dataset):
+                var_list = list(varset.keys())
+            elif isinstance(varset, list):
+                var_list = list()
+                for var in varset:
+                    var_list += list(var.keys())
+                var_list = list(set(var_list))
+            for v in var_names:
+                if v not in var_list:
+                    raise ValueError(
+                        "Function {} requires variable {}".format(func.__name__, v)
+                    )
+            return func(*args, **kwargs)
+
+        return deco
+
+    return wrap
+
+
+def _extract(r_list: Volume_T, dtype: str) -> tuple:
+    if len(set(i.range for i in r_list)) > 1:
+        raise ValueError("Input radials must have same data range")
+    adim_shape = set(i.dims["azimuth"] for i in r_list)
+    if max(adim_shape) > 400:
+        # CC radar
+        adim_interp_to = 512
+    else:
+        adim_interp_to = 360
+    r_data = list()
+    elev = list()
+    for i in r_list:
+        x, d, a = resample(
+            i[dtype].values,
+            i["distance"].values,
+            i["azimuth"].values,
+            i.tangential_reso,
+            adim_interp_to,
+        )
+        r_data.append(x)
+        elev.append(i.elevation)
+    data = np.concatenate(r_data).reshape(
+        len(r_list), r_data[0].shape[0], r_data[0].shape[1]
+    )
+    return data, d, a, np.array(elev)
+
+
+@require(["REF"])
+def quick_cr(r_list: Volume_T, resolution: tuple = (1000, 1000)) -> Dataset:
+    r"""Calculate composite reflectivity
+
+    Args:
+        r_list (list(xarray.Dataset)): Reflectivity data.
+
+    Returns:
+        xarray.Dataset: composite reflectivity
+    """
+    r_data = list()
+    # Get grid from the first tilt
+    r, x, y = grid_2d(
+        r_list[0]["REF"].values,
+        r_list[0]["longitude"].values,
+        r_list[0]["latitude"].values,
+        resolution=resolution,
+    )
+    r_data.append(r)
+    for i in r_list[1:]:
+        r, x, y = grid_2d(
+            i["REF"].values,
+            i["longitude"].values,
+            i["latitude"].values,
+            x_out=x,
+            y_out=y,
+            resolution=resolution,
+        )
+        r_data.append(r)
+    cr = np.nanmax(r_data, axis=0)
+    ret = Dataset({"CR": DataArray(cr, coords=[y, x], dims=["latitude", "longitude"])})
+    ret.attrs = i.attrs
+    ret.attrs["elevation"] = 0
+    return ret
+
+
+@require(["REF"])
+def quick_et(r_list: Volume_T) -> Dataset:
+    r"""Calculate echo tops
+
+    Args:
+        r_list (list(xarray.Dataset)): Reflectivity data.
+
+    Returns:
+        xarray.Dataset: echo tops
+    """
+    r_data, d, a, elev = _extract(r_list, "REF")
+    r_data[np.isnan(r_data)] = 0
+    i = r_list[0]
+    et = echo_top(
+        r_data.astype(np.double), d.astype(np.double), elev.astype(np.double), 0.0
+    )
+    azimuth = a[:, 0]
+    distance = d[0]
+    ret = Dataset(
+        {
+            "ET": DataArray(
+                np.ma.masked_less(et, 2),
+                coords=[azimuth, distance],
+                dims=["azimuth", "distance"],
+            )
+        }
+    )
+    ret.attrs = i.attrs
+    ret.attrs["elevation"] = 0
+    lon, lat = get_coordinate(distance, azimuth, 0, i.site_longitude, i.site_latitude)
+    ret["longitude"] = (["azimuth", "distance"], lon)
+    ret["latitude"] = (["azimuth", "distance"], lat)
+    return ret
+
+
+@require(["REF"])
+def quick_vil(r_list: Volume_T) -> Dataset:
+    r"""Calculate vertically integrated liquid.
+
+    This algorithm process data in polar coordinates, which avoids the loss of
+    data. By default, this function calls low-level function `vert_integrated_liquid`
+    in C-extension. If the C-extension is not available, the python version will
+    be used instead but with much slower speed.
+
+    Args:
+        r_list (list(xarray.Dataset)): Reflectivity data.
+
+    Returns:
+        xarray.Dataset: vertically integrated liquid
+    """
+    r_data, d, a, elev = _extract(r_list, "REF")
+    r_data[np.isnan(r_data)] = 0
+    i = r_list[0]
+    vil = vert_integrated_liquid(
+        r_data.astype(np.double), d.astype(np.double), elev.astype(np.double)
+    )
+    azimuth = a[:, 0]
+    distance = d[0]
+    ret = Dataset(
+        {
+            "VIL": DataArray(
+                np.ma.masked_less(vil, 0.1),
+                coords=[azimuth, distance],
+                dims=["azimuth", "distance"],
+            )
+        }
+    )
+    ret.attrs = i.attrs
+    ret.attrs["elevation"] = 0
+    lon, lat = get_coordinate(distance, azimuth, 0, i.site_longitude, i.site_latitude)
+    ret["longitude"] = (["azimuth", "distance"], lon)
+    ret["latitude"] = (["azimuth", "distance"], lat)
+    return ret
+
+
+def quick_vild(r_list: Volume_T) -> Dataset:
+    r"""Calculate vertically integrated liquid density.
+
+    By default, this function calls low-level function `vert_integrated_liquid`
+    in C-extension. If the C-extension is not available, the python version will
+    be used instead but with much slower speed.
+
+    Args:
+        r_list (list(xarray.Dataset)): Reflectivity data.
+
+    Returns:
+        xarray.Dataset: Vertically integrated liquid
+    """
+    r_data, d, a, elev = _extract(r_list, "REF")
+    i = r_list[0]
+    vild = vert_integrated_liquid(
+        r_data.astype(np.double),
+        d.astype(np.double),
+        elev.astype(np.double),
+        density=True,
+    )
+    azimuth = a[:, 0]
+    distance = d[0]
+    ret = Dataset(
+        {
+            "VILD": DataArray(
+                np.ma.masked_less(vild, 0.1),
+                coords=[azimuth, distance],
+                dims=["azimuth", "distance"],
+            )
+        }
+    )
+    ret.attrs = i.attrs
+    ret.attrs["elevation"] = 0
+    lon, lat = get_coordinate(distance, azimuth, 0, i.site_longitude, i.site_latitude)
+    ret["longitude"] = (["azimuth", "distance"], lon)
+    ret["latitude"] = (["azimuth", "distance"], lat)
+    return ret
+
+
+def polar_to_xy(field: Dataset, resolution: tuple = (1000, 1000)) -> Dataset:
+    r"""
+    Interpolate single volume data in polar coordinates into geographic coordinates
+
+    将单仰角数据从极坐标插值转换为经纬度坐标
+
+    Args:
+        field (xarray.Dataset): Original radial.
+
+    Returns:
+        xarray.Dataset: Interpolated data in grid
+    """
+    dtype = get_dtype(field)
+    r, x, y = grid_2d(
+        field[dtype].values,
+        field["longitude"].values,
+        field["latitude"].values,
+        resolution=resolution,
+    )
+    ret = Dataset({dtype: DataArray(r, coords=[y, x], dims=["latitude", "longitude"])})
+    ret.attrs = field.attrs
+    return ret
+
+
+class VCS(object):
+    r"""
+    Class performing vertical cross-section calculation
+
+    Args:
+        r_list (list(xarray.Dataset)): The whole volume scan.
+    """
+
+    def __init__(self, r_list: Volume_T):
+        el = [i.elevation for i in r_list]
+        if len(el) != len(set(el)):
+            self.rl = list()
+            el_list = list()
+            for data in r_list:
+                if data.elevation not in el_list:
+                    self.rl.append(data)
+                    el_list.append(data.elevation)
+        else:
+            self.rl = r_list
+        self.dtype = get_dtype(r_list[0])
+        self.x, self.y, self.h, self.r = self._geocoor()
+        self.attrs = r_list[0].attrs
+
+    def _geocoor(self) -> Tuple[list]:
+        r_data = list()
+        x_data = list()
+        y_data = list()
+        h_data = list()
+        for i in self.rl:
+            _lon = i["longitude"].values
+            _lat = i["latitude"].values
+            r, x, y = grid_2d(i[self.dtype].values, _lon, _lat)
+            r, x, y = grid_2d(i[self.dtype].values, _lon, _lat)
+            r_data.append(r)
+            x_data.append(x)
+            y_data.append(y)
+            hgh_grid, x, y = grid_2d(i["height"].values, _lon, _lat)
+            h_data.append(hgh_grid)
+        return x_data, y_data, h_data, r_data
+
+    def _get_section(
+        self, stp: Tuple[float, float], enp: Tuple[float, float], spacing: int
+    ) -> Dataset:
+        r_sec = list()
+        h_sec = list()
+        for x, y, h, r in zip(self.x, self.y, self.h, self.r):
+            d_x = DataArray(r, [("lat", y), ("lon", x)])
+            d_h = DataArray(h, [("lat", y), ("lon", x)])
+            x_new = DataArray(np.linspace(stp[0], enp[0], spacing), dims="z")
+            y_new = DataArray(np.linspace(stp[1], enp[1], spacing), dims="z")
+            r_section = d_x.interp(lon=x_new, lat=y_new)
+            h_section = d_h.interp(lon=x_new, lat=y_new)
+            r_sec.append(r_section)
+            h_sec.append(h_section)
+        r = np.asarray(r_sec)
+        h = np.asarray(h_sec)
+        x = np.linspace(0, 1, spacing) * np.ones(r.shape[0])[:, np.newaxis]
+        ret = Dataset(
+            {
+                self.dtype: DataArray(r, dims=["distance", "tilt"]),
+                "y_cor": DataArray(h, dims=["distance", "tilt"]),
+                "x_cor": DataArray(x, dims=["distance", "tilt"]),
+            }
+        )
+        r_attr = self.attrs.copy()
+        del r_attr["elevation"], r_attr["tangential_reso"], r_attr["range"]
+        r_attr["start_lon"] = stp[0]
+        r_attr["start_lat"] = stp[1]
+        r_attr["end_lon"] = enp[0]
+        r_attr["end_lat"] = enp[1]
+        ret.attrs = r_attr
+        return ret
+
+    def get_section(
+        self,
+        start_polar: Optional[Tuple[float, float]] = None,
+        end_polar: Optional[Tuple[float, float]] = None,
+        start_cart: Optional[Tuple[float, float]] = None,
+        end_cart: Optional[Tuple[float, float]] = None,
+        spacing: int = 500,
+    ) -> Dataset:
+        r"""
+        Get cross-section data from input points
+
+        Args:
+            start_polar (tuple): polar coordinates of start point i.e.(distance, azimuth)
+
+            end_polar (tuple): polar coordinates of end point i.e.(distance, azimuth)
+
+            start_cart (tuple): geographic coordinates of start point i.e.(longitude, latitude)
+
+            end_cart (tuple): geographic coordinates of end point i.e.(longitude, latitude)
+
+        Returns:
+            xarray.Dataset: Cross-section data
+        """
+        if start_polar and end_polar:
+            stlat = self.rl[0].site_latitude
+            stlon = self.rl[0].site_longitude
+            stp = np.round(
+                get_coordinate(
+                    start_polar[0], np.deg2rad(start_polar[1]), 0, stlon, stlat
+                ),
+                2,
+            )
+            enp = np.round(
+                get_coordinate(end_polar[0], np.deg2rad(end_polar[1]), 0, stlon, stlat),
+                2,
+            )
+        elif start_cart and end_cart:
+            stp = start_cart
+            enp = end_cart
+        else:
+            raise RadarCalculationError("Invalid input")
+        return self._get_section(stp, enp, spacing)
+
+
+class GridMapper(object):
+    r"""
+    This class can merge scans from different radars to a single cartesian grid.
+    support BR or CR or any list(xarray.Dataset).
+    merge_xy method inspiration comes from OLDLee_GIFT@bilibili.
+
+    Args:
+        fields (list(xarray.Dataset)): Lists of scans to be merged.
+
+        max_dist (int, float): The maximum distance in kdtree searching.
+
+    Example:
+        >>> gm = GridMapper([r1, r2, r3])
+        >>> grid = gm(0.1)
+    """
+
+    def __init__(self, fields: Volume_T, max_dist: Number_T = 0.1):
+        # Process data type
+        self.dtype = get_dtype(fields[0])
+        # Process time
+        t_arr = np.array(
+            [
+                time.mktime(
+                    datetime.datetime.strptime(
+                        i.scan_time, "%Y-%m-%d %H:%M:%S"
+                    ).timetuple()
+                )
+                for i in fields
+            ]
+        )
+        if (t_arr.max() - t_arr.min()) / 60 > 10:
+            raise RadarCalculationError(
+                "Time difference of input data should not exceed 10 minutes"
+            )
+        mean_time = t_arr.mean()
+        mean_dtime = datetime.datetime(*time.localtime(int(mean_time))[:6])
+        time_increment = 10
+        time_rest = mean_dtime.minute % time_increment
+        if time_rest > time_increment / 2:
+            mean_dtime += datetime.timedelta(minutes=(time_increment - time_rest))
+        else:
+            mean_dtime -= datetime.timedelta(minutes=time_rest)
+        self.scan_time = mean_dtime
+        self.lon_ravel = np.hstack([i["longitude"].values.ravel() for i in fields])
+        self.lat_ravel = np.hstack([i["latitude"].values.ravel() for i in fields])
+        self.is_polar = "distance" in fields[0].coords
+        if self.is_polar:
+            self.data_ravel = np.ma.hstack(
+                [i[self.dtype].values.ravel() for i in fields]
+            )
+            self.dist_ravel = np.hstack(
+                [
+                    np.broadcast_to(i["distance"], i["longitude"].shape).ravel()
+                    for i in fields
+                ]
+            )
+            self.tree = KDTree(np.dstack((self.lon_ravel, self.lat_ravel))[0])
+        self.md = max_dist
+        self.attr = fields[0].attrs.copy()
+        self.fields = fields
+
+    def _process_grid(self, x_step: Number_T, y_step: Number_T) -> Tuple[np.ndarray]:
+        x_lower = np.round(self.lon_ravel.min(), 2)
+        x_upper = np.round(self.lon_ravel.max(), 2)
+        y_lower = np.round(self.lat_ravel.min(), 2)
+        y_upper = np.round(self.lat_ravel.max(), 2)
+        x_grid = np.arange(x_lower, x_upper + x_step, x_step)
+        y_grid = np.arange(y_lower, y_upper + y_step, y_step)
+        return np.meshgrid(x_grid, y_grid)
+
+    def _map_points(self, x: np.ndarray, y: np.ndarray) -> np.ma.MaskedArray:
+        _MAX_RETURN = 5
+        _FILL_VALUE = -1e5
+        xdim, ydim = x.shape
+        _, idx = self.tree.query(
+            np.dstack((x.ravel(), y.ravel()))[0],
+            distance_upper_bound=self.md,
+            k=_MAX_RETURN,
+        )
+        idx_all = idx.ravel()
+        data_indexing = np.append(self.data_ravel, _FILL_VALUE)
+        dist_indexing = np.append(self.dist_ravel, 0)
+        target_rad = np.ma.masked_equal(data_indexing[idx_all], _FILL_VALUE)
+        weight = dist_indexing[idx_all]
+        inp = target_rad.reshape(xdim, ydim, _MAX_RETURN)
+        wgt = weight.reshape(xdim, ydim, _MAX_RETURN)
+        return np.ma.average(inp, weights=1 / wgt, axis=2)
+
+    def _merge_xy(self, x: np.ndarray, y: np.ndarray) -> Dataset:
+        # interpolate datas to full grid
+        r_data = list()
+        for field in self.fields:
+            field_grid = field.interp(longitude=x[0], latitude=y[:, 0], method="linear")
+            r_data.append(field_grid[self.dtype].values)
+        # select max value in each grid
+        r_data_max = np.nanmax(r_data, axis=0)
+        ret = Dataset(
+            {
+                self.dtype: DataArray(
+                    r_data_max, coords=[y[:, 0], x[0]], dims=["latitude", "longitude"]
+                )
+            }
+        )
+        # interpolate Nan values
+        lat_interp = ret[self.dtype].interpolate_na(
+            "latitude", method="linear", limit=len(self.fields)
+        )
+        lon_interp = lat_interp.interpolate_na(
+            "longitude", method="nearest", limit=len(self.fields)
+        )
+        grid = lon_interp.interp(longitude=x[0], latitude=y[:, 0], method="linear")
+        return grid
+
+    def __call__(self, step: Number_T) -> Dataset:
+        r"""
+        Args:
+            step (int, float): Output grid spacing.
+
+        Returns:
+            xarray.Dataset: Merged grid data.
+        """
+        x, y = self._process_grid(step, step)
+        if self.is_polar:
+            grid = self._map_points(x, y)
+
+        else:
+            grid = self._merge_xy(x, y)
+        grid = np.ma.masked_outside(grid, 0.1, 100)
+        ret = Dataset(
+            {
+                self.dtype: DataArray(
+                    grid, coords=[y[:, 0], x[0]], dims=["latitude", "longitude"]
+                )
+            }
+        )
+        r_attr = self.attr
+        # Keep this attribute temporarily waiting for future fix
+        r_attr["tangential_reso"] = np.nan
+        r_attr["elevation"] = 0
+        r_attr["site_name"] = "RADMAP"
+        r_attr["site_code"] = "RADMAP"
+        r_attr["scan_time"] = self.scan_time.strftime("%Y-%m-%d %H:%M:%S")
+        for k in ["site_longitude", "site_latitude", "nyquist_vel"]:
+            if k in r_attr:
+                del r_attr[k]
+        ret.attrs = r_attr
+        return ret
+
+
+@require(["REF", "ZDR", "RHO", "KDP"])
+def hydro_class(
+    z: Dataset, zdr: Dataset, rho: Dataset, kdp: Dataset, band: str = "S"
+) -> Dataset:
+    r"""Hydrometeor classification
+
+    Args:
+        z (xarray.Dataset): Reflectivity data.
+
+        zdr (xarray.Dataset): Differential reflectivity data.
+
+        rho (xarray.Dataset): Cross-correlation coefficient data.
+
+        kdp (xarray.Dataset): Specific differential phase data.
+
+        band (str): Band of the radar, default to S.
+
+    Returns:
+        xarray.Dataset: Classification result.
+    """
+    z_data = z["REF"].values
+    zdr_data = zdr["ZDR"].values
+    rho_data = rho["RHO"].values
+    kdp_data = kdp["KDP"].values
+    result = _hca(
+        z_data.ravel(), zdr_data.ravel(), rho_data.ravel(), kdp_data.ravel(), band=band
+    )
+    result = result.reshape(z_data.shape).astype(float)
+    result[np.isnan(z_data)] = np.nan
+    hcl = z.copy()
+    hcl["cHCL"] = (["azimuth", "distance"], result)
+    del hcl["REF"]
+    return hcl
---
+++ packages/cinrad/cinrad-1.9.2-cp311-cp311-win_amd64.whl/cinrad/io/_dtype.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+# Author: Puyuan Du
+
+import numpy as np
+
+# fmt: off
+__all__ = [
+    'CC_param', 'CC_header', 'CC_data', 'SDD_header', 'SDD_site', 'SDD_task',
+    'SDD_cut', 'SDD_rad_header', 'SDD_mom_header', 'SAB_dtype', 'CAB_dtype',
+    'SWAN_dtype', 'CD_dtype', 'CD_DATA', 'SDD_pheader', 'L3_radial', 'L3_rblock',
+    'S_SPECIAL_dtype', 'CC2_header', 'CC2_obs', 'CC2_data', 'CC2_other', 'PA_radial',
+    'L3_raster','L3_hail','L3_meso','L3_feature','L3_tvs','L3_sti_header','L3_sti_motion',
+    'L3_sti_position','L3_sti_attribute','L3_sti_component','L3_sti_adaptation',
+    'L3_vwp_header','L3_vwp','L3_swp','L3_uam','mocm_dtype','mocm_si_dtype','mocm_si_block',
+    'L3_wer_header','PA_SDD_site', 'PA_SDD_task', 'PA_SDD_beam', 'PA_SDD_cut', 'PA_SDD_rad_header',
+]
+# fmt: on
+from cinrad.io._radar_struct.CC import (
+    scan_param_dtype as CC_param,
+    header_dtype as CC_header,
+    data_dtype as CC_data,
+)
+from cinrad.io._radar_struct.standard_data import (
+    generic_header_dtype as SDD_header,
+    site_config_dtype as SDD_site,
+    task_config_dtype as SDD_task,
+    cut_config_dtype as SDD_cut,
+    radial_header_dtype as SDD_rad_header,
+    moment_header_dtype as SDD_mom_header,
+    product_header_dtype as SDD_pheader,
+    pa_site_config_dtype as PA_SDD_site,
+    pa_task_config_dtype as PA_SDD_task,
+    pa_beam_dtype as PA_SDD_beam,
+    pa_cut_config_dtype as PA_SDD_cut,
+    pa_radial_header_dtype as PA_SDD_rad_header,
+    l3_radial_header_dtype as L3_radial,
+    l3_radial_block_dtype as L3_rblock,
+    l3_raster_header_dtype as L3_raster,
+    l3_hail_table_dtype as L3_hail,
+    l3_meso_table_dtype as L3_meso,
+    l3_feature_table_dtype as L3_feature,
+    l3_tvs_table_dtype as L3_tvs,
+    l3_sti_header_dtype as L3_sti_header,
+    l3_sti_motion_dtype as L3_sti_motion,
+    l3_sti_position_dtype as L3_sti_position,
+    l3_sti_attribute_dtype as L3_sti_attribute,
+    l3_sti_component_dtype as L3_sti_component,
+    l3_sti_adaptation_dtype as L3_sti_adaptation,
+    l3_vwp_header_dtype as L3_vwp_header,
+    l3_vwp_table_dtype as L3_vwp,
+    l3_swp as L3_swp,
+    l3_uam_dtype as L3_uam,
+    l3_wer_header_dtype as L3_wer_header,
+)
+from cinrad.io._radar_struct.CD import (
+    radarsite_dtype as CD_site,
+    performance_dtype as CD_perf,
+    observation_param_dtype as CD_obs,
+)
+from cinrad.io._radar_struct.CC2 import (
+    header_dtype as CC2_header,
+    observation_param_dtype as CC2_obs,
+    other_info_dtype as CC2_other,
+    data_block_dtype as CC2_data,
+)
+from cinrad.io._radar_struct.PA import radial_dtype as PA_radial
+
+_S_HEADER = [("spare", "u2", 7), ("a", "u2"), ("res", "u2", 6)]
+
+_S_INFO = [
+    ("time", "u4"),
+    ("day", "u2"),
+    ("unambiguous_distance", "u2"),
+    ("azimuth", "u2"),
+    ("radial_num", "u2"),
+    ("radial_state", "u2"),
+    ("elevation", "u2"),
+    ("el_num", "u2"),
+    ("first_gate_r", "u2"),
+    ("first_gate_v", "u2"),
+    ("gate_length_r", "u2"),
+    ("gate_length_v", "u2"),
+    ("gate_num_r", "u2"),
+    ("gate_num_v", "u2"),
+    ("sector_num", "u2"),
+    ("system_coff", "u4"),
+    ("r_pointer", "u2"),
+    ("v_pointer", "u2"),
+    ("w_pointer", "u2"),
+    ("v_reso", "u2"),
+    ("vcp_mode", "u2"),
+    ("res2", "u2", 4),
+    ("r_pointer_2", "u2"),
+    ("v_pointer_2", "u2"),
+    ("w_pointer_2", "u2"),
+    ("nyquist_vel", "u2"),
+]
+
+_S_RES = [("res3", "u2", 19)]
+_S_RES_2 = [("res3", "u2", 21)]
+
+_SAB_DATA = [("r", "u1", 460), ("v", "u1", 920), ("w", "u1", 920), ("res4", "u2", 2)]
+
+_CAB_DATA = [("r", "u1", 800), ("v", "u1", 1600), ("w", "u1", 1600), ("res4", "u2", 2)]
+
+_SPECIAL_DATA = [("r", "u1", 1000), ("v", "u1", 1000), ("w", "u1", 1000)]
+
+SAB_dtype = np.dtype(_S_HEADER + _S_INFO + _S_RES + _SAB_DATA)
+CAB_dtype = np.dtype(_S_HEADER + _S_INFO + _S_RES + _CAB_DATA)
+S_SPECIAL_dtype = np.dtype(_S_HEADER + _S_INFO + _S_RES_2 + _SPECIAL_DATA)
+
+SWAN_HEADER = [
+    ("data_type", "12c"),
+    ("data_name", "38c"),
+    ("name", "8c"),
+    ("version", "8c"),
+    ("year", "u2"),
+    ("month", "u2"),
+    ("day", "u2"),
+    ("hour", "u2"),
+    ("minute", "u2"),
+    ("interval", "u2"),
+    ("x_grid_num", "u2"),
+    ("y_grid_num", "u2"),
+    ("z_grid_num", "u2"),
+    ("radar_count", "i4"),
+    ("start_lon", "f4"),
+    ("start_lat", "f4"),
+    ("center_lon", "f4"),
+    ("center_lat", "f4"),
+    ("x_reso", "f4"),
+    ("y_reso", "f4"),
+    ("height", "40f4"),
+    ("station_names", "20S16"),
+    ("station_lons", "20f4"),
+    ("station_lats", "20f4"),
+    ("station_alts", "20f4"),
+    ("mosaic_flags", "20B"),
+    ("m_data_type", "i2"),
+    ("dimension", "i2"),
+    ("res", "168c"),
+]
+
+SWAN_dtype = np.dtype(SWAN_HEADER)
+
+CD_dtype = np.dtype(
+    [("site_info", CD_site), ("performance", CD_perf), ("obs", CD_obs), ("res", "163c")]
+)
+
+_CD_record = np.dtype([("m_dbz", "B"), ("m_vel", "B"), ("m_undbz", "B"), ("m_sw", "B")])
+
+CD_DATA = np.dtype(
+    [
+        ("s_az", "u2"),
+        ("s_el", "u2"),
+        ("e_az", "u2"),
+        ("e_el", "u2"),
+        ("rec", _CD_record, 998),
+    ]
+)
+
+MOCM_HEADER = [
+    ("label", "4c"),
+    ("version", "4c"),
+    ("file_bytes", "i4"),
+    ("mosaic_id", "u2"),
+    ("coordinate", "u2"),
+    ("varname", "8c"),
+    ("description", "64c"),
+    ("block_pos", "i4"),
+    ("block_len", "i4"),
+    ("time_zone", "i4"),
+    ("year", "u2"),
+    ("month", "u2"),
+    ("day", "u2"),
+    ("hour", "u2"),
+    ("minute", "u2"),
+    ("second", "u2"),
+    ("obs_seconds", "i4"),
+    ("obs_dates", "u2"),
+    ("gen_dates", "u2"),
+    ("gen_seconds", "i4"),
+    ("edge_s", "i4"),
+    ("edge_w", "i4"),
+    ("edge_n", "i4"),
+    ("edge_e", "i4"),
+    ("cx", "i4"),
+    ("cy", "i4"),
+    ("nx", "i4"),
+    ("ny", "i4"),
+    ("dx", "i4"),
+    ("dy", "i4"),
+    ("height", "u2"),
+    ("compress", "u2"),
+    ("num_of_radars", "i4"),
+    ("un_zip_bytes", "i4"),
+    ("scale", "u2"),
+    ("unused", "u2"),
+    ("rgn_id", "8c"),
+    ("units", "8c"),
+    ("res", "60c"),
+]
+
+mocm_dtype = np.dtype(MOCM_HEADER)
+
+MOCM_SI_HEADER = [
+    ("label", "4c"),
+    ("version", "4c"),
+    ("file_bytes", "i4"),
+    ("site_code", "8c"),
+    ("site_name", "20c"),
+    ("province_name", "20c"),
+    ("radar_type", "12c"),
+    ("Latitude", "i4"),
+    ("Longitude", "i4"),
+    ("antenna_height", "i4"),
+    ("time_zone", "i4"),
+    ("year", "u2"),
+    ("month", "u2"),
+    ("day", "u2"),
+    ("hour", "u2"),
+    ("minute", "u2"),
+    ("second", "u2"),
+    ("obs_seconds", "i4"),
+    ("obs_dates", "u2"),
+    ("gen_dates", "u2"),
+    ("gen_seconds", "i4"),
+    ("block_num", "u2"),
+    ("compress", "u2"),
+    ("pid", "64u2"),
+    ("block_pos", "64i4"),
+    ("block_len", "64i4"),
+    ("vcp", "u2"),
+]
+
+mocm_si_dtype = np.dtype(MOCM_SI_HEADER)
+
+MOCM_SI_BLOCK = [
+    ("pid", "u2"),
+    ("coordinate", "u2"),
+    ("varname", "8c"),
+    ("description", "52c"),
+    ("edge_s", "i4"),
+    ("edge_w", "i4"),
+    ("edge_n", "i4"),
+    ("edge_e", "i4"),
+    ("cy", "i4"),
+    ("cx", "i4"),
+    ("ny", "i4"),
+    ("nx", "i4"),
+    ("dy", "i4"),
+    ("dx", "i4"),
+    ("ry", "u2"),
+    ("rx", "u2"),
+    ("range", "u2"),
+    ("height", "u2"),
+    ("block_size", "i4"),
+    ("gc_counts", "i4"),
+    ("scale", "u2"),
+    ("res", "10c"),
+]
+
+mocm_si_block = np.dtype(MOCM_SI_BLOCK)
