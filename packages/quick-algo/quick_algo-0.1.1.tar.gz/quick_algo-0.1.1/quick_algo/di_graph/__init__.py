39e6d7d628fa3e5d78d8e853559deed16514f847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:05:20 +0000

    Add package quick-algo/0.1.1/quick_algo-0.1.1.tar.gz

---
+++ packages/quick-algo/quick_algo-0.1.1.tar.gz/quick_algo-0.1.1/quick_algo/di_graph/__init__.py
@@ -0,0 +1,3 @@
+from .di_graph import DiGraph, DiEdge, DiNode, save_to_file, load_from_file
+
+__all__ = ["DiGraph", "DiEdge", "DiNode", "save_to_file", "load_from_file"]
---
+++ packages/quick-algo/quick_algo-0.1.1.tar.gz/quick_algo-0.1.1/setup.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command, Extension
+
+ext_modules = [
+    Extension(
+        "quick_algo.di_graph.di_graph",
+        sources=[
+            "quick_algo/di_graph/di_graph.cpp",
+            "quick_algo/di_graph/cpp/di_graph_impl.cpp",
+        ],
+        include_dirs=[
+            "quick_algo/di_graph"
+        ],
+        language="c++",
+    ),
+    Extension(
+        "quick_algo.pagerank.pagerank",
+        sources=[
+            "quick_algo/pagerank/pagerank.cpp",
+            "quick_algo/pagerank/cpp/pagerank_impl.cpp",
+        ],
+        include_dirs=[
+            "quick_algo/pagerank",
+            "quick_algo/di_graph",
+        ],
+        language="c++",
+    ),
+]
+
+# Package meta-data.
+NAME = "quick_algo"
+DESCRIPTION = "A simple and fast Graph structure and PPR algorithm implementation based on Cpp&Cython."
+URL = "https://github.com/MaiM-with-u/MaiMBot-LPMM"
+EMAIL = "octautumn2002@gmail.com"
+AUTHOR = "Oct Autumn"
+REQUIRES_PYTHON = ">=3.12"
+VERSION = "0.1.1"
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+    "zip": ["gzip"],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    ext_modules=ext_modules,
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Natural Language :: Chinese (Simplified)",
+        "Programming Language :: C++",
+        "Programming Language :: Cython",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
+)
