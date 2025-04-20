5c61d62d02d5915da270fb54d668f0d4e91e340a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:15 +0000

    Add package riskoptima/1.44.0/riskoptima-1.44.0.tar.gz

---
+++ packages/riskoptima/riskoptima-1.44.0.tar.gz/riskoptima-1.44.0/riskoptima/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Jordi Corbilla
+# Created Date: 2025
+# version ='1.44.0'
+# ---------------------------------------------------------------------------
+
+from .riskoptima import RiskOptima
