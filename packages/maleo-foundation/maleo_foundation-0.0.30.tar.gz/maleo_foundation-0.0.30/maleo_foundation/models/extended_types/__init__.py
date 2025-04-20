d8ad0154083da6ff1dcc7c2b4237d790c2e7ab7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:24 +0000

    Add package maleo-foundation/0.0.30/maleo_foundation-0.0.30.tar.gz

---
+++ packages/maleo-foundation/maleo_foundation-0.0.30.tar.gz/maleo_foundation-0.0.30/maleo_foundation/models/extended_types/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from .query import ExtendedQueryTypes
+from .service import ExtendedServiceTypes
+from .client import ExtendedClientTypes
+
+class ExtendedTypes:
+    Query = ExtendedQueryTypes
+    Service = ExtendedServiceTypes
+    Client = ExtendedClientTypes
