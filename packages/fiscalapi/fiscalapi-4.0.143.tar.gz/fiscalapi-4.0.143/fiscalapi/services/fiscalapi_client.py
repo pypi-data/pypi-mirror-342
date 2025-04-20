f4ef1b259f99eb27f743e1f8c53e455edc247490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:28 +0000

    Add package fiscalapi/4.0.143/fiscalapi-4.0.143.tar.gz

---
+++ packages/fiscalapi/fiscalapi-4.0.143.tar.gz/fiscalapi-4.0.143/fiscalapi/services/fiscalapi_client.py
@@ -0,0 +1,18 @@
+from fiscalapi.models.common_models import FiscalApiSettings
+from fiscalapi.services.catalog_service import CatalogService
+from fiscalapi.services.invoice_service import InvoiceService
+from fiscalapi.services.people_service import PeopleService
+from fiscalapi.services.product_service import ProductService
+from fiscalapi.services.tax_file_servive import TaxFileService
+
+
+
+class FiscalApiClient:
+    
+    def __init__(self, settings: FiscalApiSettings):
+        self.products = ProductService(settings)
+        self.people = PeopleService(settings)
+        self.tax_files = TaxFileService(settings)
+        self.catalogs = CatalogService(settings)
+        self.invoices = InvoiceService(settings)
+        self.settings = settings
