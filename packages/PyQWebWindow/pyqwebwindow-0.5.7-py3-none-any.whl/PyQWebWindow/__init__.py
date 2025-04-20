e7ff1e82efe2db39e692bfe898a4f7c719fa321d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:16 +0000

    Add package PyQWebWindow/0.5.7/pyqwebwindow-0.5.7-py3-none-any.whl

---
+++ packages/PyQWebWindow/pyqwebwindow-0.5.7-py3-none-any.whl/PyQWebWindow/__init__.py
@@ -0,0 +1,4 @@
+from .QAppManager import QAppManager
+from .QWebWindow  import QWebWindow
+from .ipc import QIpcServer, QIpcClient, IpcServer, IpcClient
+from .utils import Serializable, SerializableCallable
