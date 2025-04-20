87a2afec2ced344d2daf3a53974abfb4a7cf248c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:06 +0000

    Add package squarequant/0.2.1/squarequant-0.2.1.tar.gz

---
+++ packages/squarequant/squarequant-0.2.1.tar.gz/squarequant-0.2.1/squarequant/__init__.py
@@ -0,0 +1,52 @@
+"""
+SquareQuant - A Python package for financial risk metrics and stock data analysis
+
+A comprehensive toolkit for quantitative finance, including risk metrics calculation
+and data retrieval from multiple sources (YFinance and Theta Data).
+"""
+
+# Version information
+__version__ = "0.2.1"
+__author__ = "Gabriel Bosch"
+__email__ = "contact@squarequant.org"
+
+# Import main functionality to make it available at the package level
+from .data.data import download_tickers, DownloadConfig
+from .api import (
+    sharpe,
+    sortino,
+    vol,
+    mdd,
+    calmar,
+    var,
+    cvar,
+    semidev,
+    avgdd,
+    ulcer,
+    mad,
+    erm,
+    evar,
+    cdar,
+    edar
+)
+
+# Define what's available when using "from squarequant import *"
+__all__ = [
+    'download_tickers',
+    'DownloadConfig',
+    'sharpe',
+    'sortino',
+    'vol',
+    'mdd',
+    'calmar',
+    'var',
+    'cvar',
+    'semidev',
+    'avgdd',
+    'ulcer',
+    'mad',
+    'erm',
+    'evar',
+    'cdar',
+    'edar'
+]
