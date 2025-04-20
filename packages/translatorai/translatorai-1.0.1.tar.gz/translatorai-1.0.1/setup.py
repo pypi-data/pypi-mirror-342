d5fb8a48e7687480f30f3545caaec61007fd467b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:20:02 +0000

    Add package translatorai/1.0.1/translatorai-1.0.1.tar.gz

---
+++ packages/translatorai/translatorai-1.0.1.tar.gz/translatorai-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages  
+
+setup(  
+    name='translatorai',  
+    version='1.0.1',  
+    packages=find_packages(),  
+    install_requires=[ 
+        'requests'
+    ],  
+    author='Redpiar',  
+    author_email='Regeonwix@gmail.com',  
+    description='easy using AI, more texting models!',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',
+    url='https://github.com/RedPiarOfficial/AiTranslator',
+    classifiers=[  
+        'Programming Language :: Python :: 3',  
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',  
+    ],
+    keywords=[
+        'translation',        
+        'translator',
+        'google',
+        'deepl',
+        'amazon',
+        'ai',
+        'libre'
+    ],
+    python_requires='>=3.8',  
+)  
