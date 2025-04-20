89e53297b58570a13ce61c8632b5b402310babca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 05:04:16 +0000

    Add package parsley-coco/0.1.0/parsley_coco-0.1.0.tar.gz

---
+++ packages/parsley-coco/parsley_coco-0.1.0.tar.gz/parsley_coco-0.1.0/parsley_coco/__init__.py
@@ -0,0 +1,9 @@
+"""Imports the main class from the parlsey module."""
+
+from parsley_coco.parser import Parsley
+from parsley_coco.factory import create_parsley
+
+
+
+
+___all__ = ["Parlsey","create_parsley"]
