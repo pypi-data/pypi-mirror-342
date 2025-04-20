1f55d1592a28f556f613df6043cf808ea1c8aed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 05:04:16 +0000

    Add package atomfoxapi/1.2.0/atomfoxapi-1.2.0.tar.gz

---
+++ packages/atomfoxapi/atomfoxapi-1.2.0.tar.gz/atomfoxapi-1.2.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='atomfoxapi',
+    version='1.2.0',
+    author='mc_c0rp',
+    author_email='mc.c0rp@icloud.com',
+    description='ATOM API | R05',
+    url='https://github.com/mc-c0rp/atomfoxapi',
+    project_urls={
+        'Documentation': 'https://github.com/mc-c0rp/atomfoxapi/blob/main/README.md',
+    },
+    packages=['atomfoxapi'],
+    include_package_data=True,
+    python_requires='>=3.9',
+)
