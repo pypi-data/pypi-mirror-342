4d3847b4e2d9ae1a2719f7a5ff4271e3daace0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 15:04:16 +0000

    Add package try3code/0.1/try3code-0.1.tar.gz

---
+++ packages/try3code/try3code-0.1.tar.gz/try3code-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='try3code',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A custom code reader from text files',
+    author='Your Name',
+)
