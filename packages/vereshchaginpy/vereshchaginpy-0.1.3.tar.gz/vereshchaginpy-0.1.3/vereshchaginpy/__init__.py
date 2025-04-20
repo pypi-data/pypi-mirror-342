1aa6a55fe109b5008084d1d614d42157f4b1e955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:04:26 +0000

    Add package vereshchaginpy/0.1.3/vereshchaginpy-0.1.3.tar.gz

---
+++ packages/vereshchaginpy/vereshchaginpy-0.1.3.tar.gz/vereshchaginpy-0.1.3/vereshchaginpy/__init__.py
@@ -0,0 +1,7 @@
+# VereshchaginPy
+__version__ = "0.1.0"
+
+from .vereshchagin_figures import *
+from .vereshchagin_visualise import *
+from .vereshchagin_core import *
+from .vereshchagin_pairs import *
