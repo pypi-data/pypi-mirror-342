dc3a0bfaa20df68772dbde866eb43d31d395c8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:20:02 +0000

    Add package translatorai/1.0.1/translatorai-1.0.1-py3-none-any.whl

---
+++ packages/translatorai/translatorai-1.0.1-py3-none-any.whl/translatorai/utils/headers.py
@@ -0,0 +1,11 @@
+HEADERS = {
+    "accept": "application/json, text/plain, */*",
+    "api-key": "pDwCjq7CyeAmn1Z3osNunACg2U0SLIhwBTtsp1WqYFMf5UuSIvMBYGS4pt8OIsGMH",
+    "cache-control": "no-cache",
+    "connection": "keep-alive",
+    "content-type": "application/json",
+    "origin": "https://www.aitranslator.com",
+    "referer": "https://www.aitranslator.com/",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+    "domain": 'https://aitranslator.com'
+}
