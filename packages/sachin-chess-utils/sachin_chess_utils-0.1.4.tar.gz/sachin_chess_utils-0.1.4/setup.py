697d7088f062a2d4c0e62a36e1b8fe5e87b6e296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:05:27 +0000

    Add package sachin-chess-utils/0.1.4/sachin_chess_utils-0.1.4.tar.gz

---
+++ packages/sachin-chess-utils/sachin_chess_utils-0.1.4.tar.gz/sachin_chess_utils-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+  description = f.read()
+
+setup(
+    name="sachin_chess_utils",
+    version="0.1.4",
+    author="Sachin Chauhan Infocusp",
+    packages=find_packages(),
+    install_requires=[
+        'chess==1.11.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sachin_hello = sachin_chess_utils:hello'
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
