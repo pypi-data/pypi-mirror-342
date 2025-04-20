bd8eb98e62302fa95c2893056458d98fb45ded2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:05:17 +0000

    Add package cotlette/0.0.24/cotlette-0.0.24-py3-none-any.whl

---
+++ packages/cotlette/cotlette-0.0.24-py3-none-any.whl/cotlette/__init__.py
@@ -0,0 +1,66 @@
+__version__ = "0.0.24"
+
+
+import os
+import logging
+# import importlib.util
+import importlib
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from cotlette.conf import settings
+from cotlette import shortcuts
+
+
+
+logger = logging.getLogger("uvicorn")
+
+
+class Cotlette(FastAPI):
+
+    def __init__(self):
+        super().__init__()
+
+        self.settings = settings
+        self.shortcuts = shortcuts
+
+        # Получить абсолютный путь к текущей диретории
+        self.cotlette_directory = os.path.dirname(os.path.abspath(__file__))
+        
+        # Подключение роутеров
+        self.include_routers()
+        self.include_templates()
+        self.include_static()
+
+    def include_routers(self):
+        # Проверка и импорт установленных приложений
+        logger.info(f"Loading apps and routers:")
+        for app_path in self.settings.INSTALLED_APPS:
+            # Динамически импортируем модуль
+            module = importlib.import_module(app_path)
+            logger.info(f"✅'{app_path}'")
+
+            # Если модуль содержит роутеры, подключаем их
+            if hasattr(module, "router"):
+                self.include_router(module.router)
+                logger.info(f"✅'{app_path}.router'")
+            else:
+                logger.warning(f"⚠️ '{app_path}.router'")
+
+    def include_templates(self):
+        # Подключаем шаблоны указанные пользователем в SETTINGS
+        for template in self.settings.TEMPLATES:
+            template_dirs = template.get("DIRS")
+            template_dirs = [os.path.join(self.settings.BASE_DIR, path) for path in template_dirs]
+
+    def include_static(self):
+
+        # Подключаем static фреймворка
+        static_dir = os.path.join(self.cotlette_directory, "static")
+        self.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+        # Подключаем static указанные пользователем в SETTINGS
+        if self.settings.STATIC_URL:
+            static_dir = os.path.join(self.settings.BASE_DIR, self.settings.STATIC_URL)
+            self.mount("/static", StaticFiles(directory=static_dir), name="static")
---
+++ packages/cotlette/cotlette-0.0.24-py3-none-any.whl/cotlette/core/database/query.py
@@ -0,0 +1,124 @@
+from cotlette.core.database.backends.sqlite3 import db
+from cotlette.core.database.fields.related import ForeignKeyField
+
+
+class QuerySet:
+    def __init__(self, model_class):
+        self.model_class = model_class
+        self.query = f'SELECT * FROM "{model_class.table or model_class.__name__}"'
+        self.params = None
+
+    def filter(self, **kwargs):
+        # Создаем новый QuerySet для цепочки запросов
+        new_queryset = QuerySet(self.model_class)
+        conditions = []
+        params = []
+
+        for field_name, value in kwargs.items():
+            if field_name not in self.model_class._fields:
+                raise KeyError(f"Field '{field_name}' does not exist in model '{self.model_class.__name__}'")
+
+            field = self.model_class._fields[field_name]
+
+            if isinstance(field, ForeignKeyField):
+                related_model = field.get_related_model()
+                if isinstance(value, related_model):
+                    value = value.id  # Извлекаем id, если передан объект модели
+                elif not isinstance(value, int):
+                    raise ValueError(f"Invalid value for foreign key '{field_name}': {value}")
+
+            conditions.append(f'"{field_name}"=?')
+            params.append(value)
+
+        # Формируем новый запрос с условиями
+        new_queryset.query = f"{self.query} WHERE {' AND '.join(conditions)}"
+        new_queryset.params = tuple(params)
+        return new_queryset
+
+    def all(self):
+        result = db.execute(self.query, self.params, fetch=True)
+        return [
+            self.model_class(**{
+                key: value for key, value in zip(self.model_class._fields.keys(), row)
+                if key in self.model_class._fields
+            })
+            for row in result
+        ]
+
+    def first(self):
+        # Добавляем LIMIT 1 к текущему запросу
+        query = f"{self.query} LIMIT 1"
+        result = db.execute(query, self.params, fetch=True)
+
+        if result:
+            row = result[0]
+            return self.model_class(**{
+                key: value for key, value in zip(self.model_class._fields.keys(), row)
+                if key in self.model_class._fields
+            })
+        return None
+
+    def execute(self):
+        # Выполняем текущий запрос и возвращаем результат
+        result = db.execute(self.query, self.params, fetch=True)
+        return [
+            self.model_class(**dict(zip(self.model_class._fields.keys(), row)))
+            for row in result
+        ]
+
+    def create(self, **kwargs):
+        fields = []
+        placeholders = []
+        values = []
+
+        for field_name, value in kwargs.items():
+            # Проверяем, существует ли поле в текущей модели
+            if field_name not in self.model_class._fields:
+                raise KeyError(f"Field '{field_name}' does not exist in model '{self.model_class.__name__}'")
+
+            field = self.model_class._fields[field_name]
+
+            # Обработка внешних ключей
+            if isinstance(field, ForeignKeyField):
+                related_model = field.get_related_model()
+                if isinstance(value, related_model):
+                    value = value.id  # Извлекаем id, если передан объект модели
+                elif not isinstance(value, int):  # Проверяем, что значение — число
+                    raise ValueError(f"Invalid value for foreign key '{field_name}': {value}")
+
+            fields.append(f'"{field_name}"')
+            placeholders.append("?")
+            values.append(value)
+
+        # Формируем SQL-запрос
+        insert_query = f'INSERT INTO "{self.model_class.table or self.model_class.__name__}" ({", ".join(fields)}) VALUES ({", ".join(placeholders)})'
+        cursor = db.execute(insert_query, values)
+        db.commit()
+
+        if cursor.lastrowid is None:
+            raise RuntimeError("Failed to retrieve the ID of the newly created record.")
+        return self.model_class.objects.get(id=cursor.lastrowid)
+
+    def save(self, instance):
+        data = instance.__dict__
+
+        if hasattr(instance, 'id') and instance.id is not None:
+            fields = ', '.join([f'"{key}"=?' for key in data if key != 'id'])
+            values = tuple(data[key] for key in data if key != 'id') + (instance.id,)
+            update_query = f'UPDATE "{self.model_class.__name__}" SET {fields} WHERE id=?'
+            db.execute(update_query, values)
+            db.commit()
+        else:
+            fields = ', '.join([f'"{key}"' for key in data if key != 'id'])
+            placeholders = ', '.join(['?'] * len(data))
+            values = tuple(data[key] for key in data if key != 'id')
+
+            insert_query = f'INSERT INTO "{self.model_class.__name__}" ({fields}) VALUES ({placeholders})'
+            db.execute(insert_query, values)
+            db.commit()
+
+            if not hasattr(db, 'lastrowid') or db.lastrowid is None:
+                raise RuntimeError("Failed to retrieve the ID of the newly created record.")
+            instance.id = db.lastrowid
+
+        return instance
