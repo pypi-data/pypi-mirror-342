df6ae31b146d6262943fad51ac5a7bc014fe8cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:29 +0000

    Add package bluer-sandbox/5.66.1/bluer_sandbox-5.66.1-py3-none-any.whl

---
+++ packages/bluer-sandbox/bluer_sandbox-5.66.1-py3-none-any.whl/bluer_sandbox/.abcli/tests/help.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/env bash
+
+function test_bluer_sandbox_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@assets" \
+        "@assets publish" \
+        \
+        "@docker" \
+        "@docker browse" \
+        "@docker build" \
+        "@docker clear" \
+        "@docker eval" \
+        "@docker push" \
+        "@docker run" \
+        "@docker seed" \
+        \
+        "@notebooks" \
+        "@notebooks open" \
+        "@notebooks build" \
+        "@notebooks code" \
+        "@notebooks connect" \
+        "@notebooks create" \
+        "@notebooks host" \
+        \
+        "@offline_llm" \
+        "@offline_llm install" \
+        "@offline_llm prompt" \
+        \
+        "@sandbox" \
+        \
+        "@sandbox pypi" \
+        "@sandbox pypi browse" \
+        "@sandbox pypi build" \
+        "@sandbox pypi install" \
+        \
+        "@sandbox pytest" \
+        \
+        "@sandbox test" \
+        "@sandbox test list" \
+        \
+        "bluer_sandbox"; do
+        bluer_ai_eval ,$options \
+            bluer_ai_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        bluer_ai_hr
+    done
+
+    return 0
+}
