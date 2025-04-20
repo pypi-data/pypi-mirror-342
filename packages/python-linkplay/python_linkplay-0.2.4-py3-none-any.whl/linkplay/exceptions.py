3cfa3522ee496a46262d1f6d3d1e5e9ba98c8ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:22 +0000

    Add package python-linkplay/0.2.4/python_linkplay-0.2.4-py3-none-any.whl

---
+++ packages/python-linkplay/python_linkplay-0.2.4-py3-none-any.whl/linkplay/exceptions.py
@@ -0,0 +1,10 @@
+class LinkPlayException(Exception):
+    pass
+
+
+class LinkPlayRequestException(LinkPlayException):
+    pass
+
+
+class LinkPlayInvalidDataException(LinkPlayException):
+    pass
