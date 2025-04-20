f76df896d0ecf5a878e211db00a0e9a6dabf761f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 04:04:35 +0000

    Add package xync-client/0.0.40/xync_client-0.0.40-py3-none-any.whl

---
+++ packages/xync-client/xync_client-0.0.40-py3-none-any.whl/xync_client/Bybit/etype/order.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from enum import IntEnum
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class OrderRequest(BaseModel):
+    class Side(IntEnum):
+        BUY = 0
+        SALE = 1
+
+    itemId: str
+    tokenId: str
+    currencyId: str
+    side: Literal["0", "1"]  # 0 покупка, # 1 продажа
+    curPrice: str
+    quantity: str
+    amount: str
+    flag: Literal["amount", "quantity"]
+    version: str = "1.0"
+    securityRiskToken: str = ""
+
+
+class PreOrderResp(BaseModel):
+    price: str  # float
+    curPrice: str
+    totalAmount: float
+    minAmount: float
+    maxAmount: float
+    minQuantity: float
+    maxQuantity: float
+    payments: list[str]  # list[int]
+    status: Literal[10]
+    paymentTerms: list
+    paymentPeriod: Literal[15]
+    lastQuantity: float
+    lastPrice: float
+    isOnline: bool
+    lastLogoutTime: datetime
+    itemPriceAvailableTime: datetime
+    itemPriceValidTime: int  # 45000
+    itemType: Literal["ORIGIN"]
