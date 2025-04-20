f5fb2628006381f860a0e73f3ee1b11b8514803b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:12 +0000

    Add package cloudcode/0.0.4/cloudcode-0.0.4.tar.gz

---
+++ packages/cloudcode/cloudcode-0.0.4.tar.gz/cloudcode-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="cloudcode",
+    version="0.0.4",
+    description="Code the thing that Codes the thing",
+    author="Sean Sullivan",
+    author_email="sean@lmsystems.ai",
+    py_modules=["cloudcode"],
+    install_requires=[
+        "aider-chat>=0.82.1",
+        "e2b-code-interpreter"
+    ],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
