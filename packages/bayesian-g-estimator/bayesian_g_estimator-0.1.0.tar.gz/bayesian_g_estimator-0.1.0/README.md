d93420fb9d802c1864a2367e7bca78bb8139bc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:21 +0000

    Add package bayesian-g-estimator/0.1.0/bayesian_g_estimator-0.1.0.tar.gz

---
+++ packages/bayesian-g-estimator/bayesian_g_estimator-0.1.0.tar.gz/bayesian_g_estimator-0.1.0/README.md
@@ -0,0 +1,8 @@
+# bayesian_g_estimator
+
+Estimate latent traits (g) using Bayesian and frequentist methods.
+
+## Installation
+
+```bash
+pip install bayesian_g_estimator
---
+++ packages/bayesian-g-estimator/bayesian_g_estimator-0.1.0.tar.gz/bayesian_g_estimator-0.1.0/src/bayesian_g_estimator/__init__.py
@@ -0,0 +1,11 @@
+from .core import (
+    bayesian_g_estimator,
+    ols_g_estimator,
+    brute_force_g_estimator,
+)
+
+__all__ = [
+    "bayesian_g_estimator",
+    "ols_g_estimator",
+    "brute_force_g_estimator",
+]
