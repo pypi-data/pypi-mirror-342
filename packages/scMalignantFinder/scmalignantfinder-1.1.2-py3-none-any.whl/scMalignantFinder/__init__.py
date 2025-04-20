54ebfd2e8039842ab6e384a81439eee43ace6315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:36 +0000

    Add package scMalignantFinder/1.1.2/scmalignantfinder-1.1.2-py3-none-any.whl

---
+++ packages/scMalignantFinder/scmalignantfinder-1.1.2-py3-none-any.whl/scMalignantFinder/__init__.py
@@ -0,0 +1,8 @@
+import scMalignantFinder.classifier, scMalignantFinder.spatial
+import warnings
+import pandas as pd
+import logging
+from ._version import __version__
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
