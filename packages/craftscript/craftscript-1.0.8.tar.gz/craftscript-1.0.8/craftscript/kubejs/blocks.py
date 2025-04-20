453053a15b5472f4c701431277849e4f9b0ffb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 14:04:33 +0000

    Add package craftscript/1.0.8/craftscript-1.0.8.tar.gz

---
+++ packages/craftscript/craftscript-1.0.8.tar.gz/craftscript-1.0.8/craftscript/kubejs/blocks.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from ..blocks import CustomBlock
+
+def generate_block_script(block: CustomBlock) -> str:
+    """生成方块注册脚本"""
+    script = [
+        f'    event.create("{block.block_id}")',
+        f'        .material("{block.material}")',
+        f'        .hardness({block.hardness})',
+        f'        .resistance({block.resistance})'
+    ]
+    
+    if block.display_name:
+        script.insert(1, f'        .displayName("{block.display_name}")')
+    
+    return "\n".join(script) + ";"
