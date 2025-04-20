0b577f4e64d45c73df7e2587cb2f89abd713748b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:20:00 +0000

    Add package fx-shared-models/0.2.27/fx_shared_models-0.2.27.tar.gz

---
+++ packages/fx-shared-models/fx_shared_models-0.2.27.tar.gz/fx_shared_models-0.2.27/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='fx-shared-models',
+    version='0.2.27',
+    packages=find_namespace_packages(include=['shared_models*']),
+    install_requires=[
+        'Django>=3.0',
+        'django-environ>=0.10.0',
+    ],
+    python_requires='>=3.8',
+    author='FX Backend',
+    author_email='fxbackend@gmail.com',
+    description='Shared models for FX Backend',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fxbackend/fx-shared-models',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    package_data={
+        'shared_models': [
+            'accounts/*',
+            'common/*',
+            'company_ba/*',
+            'crypto_wallet/*', 
+            'customer_crypto_wallet/*',
+            'customer_upi/*',
+            'customers/*',
+            'email_log/*',
+            'ib_commission/*',
+            'referrals/*',
+            'services/*',
+            'system_settings/*',
+            'transactions/*',
+            'ui_elements/*',
+            'upi_id/*',
+            'users/*',
+        ],
+    },
+) 
---
+++ packages/fx-shared-models/fx_shared_models-0.2.27.tar.gz/fx_shared_models-0.2.27/shared_models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Shared models package for FX Backend
+Contains Customer, System Settings, and IB Commission models
+"""
+
+__version__ = '0.2.27'  
