6a39ebc89504da9acc139ac1f0e07fb358173ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:17 +0000

    Add package ndtools/0.3.0/ndtools-0.3.0-py3-none-any.whl

---
+++ packages/ndtools/ndtools-0.3.0-py3-none-any.whl/ndtools/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "All",
+    "Any",
+    "Combinable",
+    "Equatable",
+    "Orderable",
+    "TotalEquality",
+    "TotalOrdering",
+    "abc",
+    "combination",
+    "comparison",
+    "operators",
+]
+__version__ = "0.3.0"
+
+
+# dependencies
+from . import abc
+from . import combination
+from . import comparison
+from . import operators
+from .abc import *
+from .combination import *
+from .comparison import *
