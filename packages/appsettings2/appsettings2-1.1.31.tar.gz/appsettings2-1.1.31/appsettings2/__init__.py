b9fdabcff669d466d36b7517fc5138f05d1b0a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 15:03:59 +0000

    Add package appsettings2/1.1.31/appsettings2-1.1.31.tar.gz

---
+++ packages/appsettings2/appsettings2-1.1.31.tar.gz/appsettings2-1.1.31/appsettings2/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: © 2024 Shaun Wilson
+# SPDX-License-Identifier: MIT
+
+__version__ = '1.1.31'
+
+from .providers import *
+from .Configuration import Configuration
+from .ConfigurationBuilder import ConfigurationBuilder
+from .ConfigurationException import ConfigurationException
