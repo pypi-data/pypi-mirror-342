ee4c8e2f7c4897f47af94d38d6be627e74ca1762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-20 03:19:23 +0000

    Add package cxalio-studio-tools/0.4.9/cxalio_studio_tools-0.4.9.tar.gz

---
+++ packages/cxalio-studio-tools/cxalio_studio_tools-0.4.9.tar.gz/cxalio_studio_tools-0.4.9/cx_tools_common/app_interface/__init__.py
@@ -0,0 +1,4 @@
+from .config_manager import ConfigManager
+from .iappenv import IAppEnvironment
+from .iapplication import IApplication
+from .progress_task_agent import ProgressTaskAgent
---
+++ packages/cxalio-studio-tools/cxalio_studio_tools-0.4.9.tar.gz/cxalio_studio_tools-0.4.9/cx_tools_common/app_interface/iapplication.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+import sys
+
+
+class IApplication(ABC):
+    def __init__(self, arguments: Sequence[str] | None = None):
+        self.sys_arguments = arguments or sys.argv[1:]
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        return False
+
+    @abstractmethod
+    def run(self):
+        pass
