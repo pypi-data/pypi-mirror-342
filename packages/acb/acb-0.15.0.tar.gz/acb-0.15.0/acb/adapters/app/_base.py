1bed3413d9990c41ddde710324aa08bdbd442395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:08 +0000

    Add package acb/0.15.0/acb-0.15.0.tar.gz

---
+++ packages/acb/acb-0.15.0.tar.gz/acb-0.15.0/acb/adapters/app/_base.py
@@ -0,0 +1,8 @@
+from acb.config import AdapterBase
+from acb.config import AppSettings as AppConfigSettings
+
+
+class AppBaseSettings(AppConfigSettings): ...
+
+
+class AppBase(AdapterBase): ...
---
+++ packages/acb/acb-0.15.0.tar.gz/acb-0.15.0/acb/adapters/cache/memory.py
@@ -0,0 +1,22 @@
+import typing as t
+
+from aiocache.backends.memory import SimpleMemoryCache
+from aiocache.serializers import PickleSerializer
+from acb.depends import depends
+
+from ._base import CacheBase, CacheBaseSettings
+
+
+class CacheSettings(CacheBaseSettings): ...
+
+
+class Cache(CacheBase, SimpleMemoryCache):  # type: ignore
+    async def init(self, *args: t.Any, **kwargs: t.Any) -> None:
+        super().__init__(
+            serializer=PickleSerializer(),
+            namespace=f"{self.config.app.name}:",
+            **kwargs,
+        )
+
+
+depends.set(Cache)
