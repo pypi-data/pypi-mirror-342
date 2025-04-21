2ffa94e096acf607ba16eecef32a032fc0c9d1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:30 +0000

    Add package test-python-package-krish/0.1.0/test_python_package_krish-0.1.0.tar.gz

---
+++ packages/test-python-package-krish/test_python_package_krish-0.1.0.tar.gz/test_python_package_krish-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test-python-package-krish",
+    version="0.1.0",
+    author="Krishan",
+    author_email="krishi87@gmail.com",
+    description="A toy package example",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/krisctl/test_python_package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
