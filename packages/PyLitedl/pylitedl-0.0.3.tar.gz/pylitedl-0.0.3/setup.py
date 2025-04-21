c93a3e0eb71682ddb23e619ef2d938109faa42c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:05:15 +0000

    Add package PyLitedl/0.0.3/pylitedl-0.0.3.tar.gz

---
+++ packages/PyLitedl/pylitedl-0.0.3.tar.gz/pylitedl-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='PyLitedl',
+    version='0.0.3',
+    install_requires=['numpy', 'scipy', 'cv2'],
+    packages=[],
+    url='',
+    license='',
+    author='macbook',
+    author_email='buildaiorg@gmail.com',
+    description='a mini deep learning framework',
+    author_description="this uses scipy and cv2 and it is a good deep "
+
+)
