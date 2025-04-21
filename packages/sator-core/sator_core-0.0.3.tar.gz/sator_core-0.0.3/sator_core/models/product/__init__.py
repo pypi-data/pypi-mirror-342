32deb5d59effed97d25529fe6ed1ea352a5c4542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 16:04:25 +0000

    Add package sator-core/0.0.3/sator_core-0.0.3.tar.gz

---
+++ packages/sator-core/sator_core-0.0.3.tar.gz/sator_core-0.0.3/sator_core/models/product/__init__.py
@@ -0,0 +1,6 @@
+from sator_core.models.product.product import Product
+from sator_core.models.product.locator import ProductLocator
+from sator_core.models.product.metadata import ProductMetadata
+from sator_core.models.product.attributes import ProductAttributes
+from sator_core.models.product.descriptor import ProductDescriptor
+from sator_core.models.product.references import ProductReferences
