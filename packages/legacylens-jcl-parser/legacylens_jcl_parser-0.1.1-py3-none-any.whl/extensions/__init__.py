fbfc09e3372b6cbe993a2df04c8d2bc8d777ae07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:06:47 +0000

    Add package legacylens-jcl-parser/0.1.1/legacylens_jcl_parser-0.1.1-py3-none-any.whl

---
+++ packages/legacylens-jcl-parser/legacylens_jcl_parser-0.1.1-py3-none-any.whl/extensions/__init__.py
@@ -0,0 +1,8 @@
+"""
+Extensions module for the JCL Parser.
+
+This module can be used to add custom parsers or enhancers 
+for specific JCL features or vendor-specific extensions.
+"""
+
+# Import extensions here when they are added 
