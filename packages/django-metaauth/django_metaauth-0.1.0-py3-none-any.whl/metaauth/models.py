7c05b65b794521fbf25924312f28310d2e027170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 05:05:05 +0000

    Add package django-metaauth/0.1.0/django_metaauth-0.1.0-py3-none-any.whl

---
+++ packages/django-metaauth/django_metaauth-0.1.0-py3-none-any.whl/metaauth/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Token(models.Model):
+    token = models.CharField()
+    expires_in = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    external_id = models.CharField(max_length=255, default='1')
+
+    def __str__(self):
+        return self.token
