f259f4ee4156e101be964845ae3432e6bf6ccb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:36 +0000

    Add package testprodgekt/0.1/testprodgekt-0.1.tar.gz

---
+++ packages/testprodgekt/testprodgekt-0.1.tar.gz/testprodgekt-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='testprodgekt',
+    version='0.1',
+    packages = ['testprodgekt'],
+    install_requires=[],
+    python_requires=">=3.11",
+    author="DavidYdin",
+    author_email="David.2280@yandex.ru"
+)
---
+++ packages/testprodgekt/testprodgekt-0.1.tar.gz/testprodgekt-0.1/testprodgekt/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'DavidYdin'
+__version__ = '1.3'
+__email__ = 'David.2280@yandex.ru'
+def a_1 ():
+    print('helo')
+a_1 ()
