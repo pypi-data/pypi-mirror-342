7eebdaac32e1c1fa5447749e14af9e89ef8da37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:01 +0000

    Add package NepTrainKit/2.0.6.dev0/neptrainkit-2.0.6.dev0-cp311-cp311-win32.whl

---
+++ packages/NepTrainKit/neptrainkit-2.0.6.dev0-cp311-cp311-win32.whl/NepTrainKit/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/11/28 12:52
+# @Author  :
+# @email    : 1747193328@qq.com
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_10_0():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'neptrainkit.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_10_0()
+del _delvewheel_patch_1_10_0
+# end delvewheel patch
+
+import os
+
+from loguru import logger
+
+try:
+    # Actual if statement not needed, but keeps code inspectors more happy
+    if __nuitka_binary_dir is not None:
+        is_nuitka_compiled = True
+except NameError:
+    is_nuitka_compiled = False
+
+
+
+if is_nuitka_compiled:
+
+
+    logger.add("./Log/{time:%Y-%m}.log",
+               level="INFO",
+                )
+    module_path="./"
+else:
+
+    module_path = os.path.dirname(__file__)
