3d21d086f7250872aa52131754d6368e753b9b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:04:26 +0000

    Add package keras-cv-attention-models/1.4.2/keras_cv_attention_models-1.4.2-py3-none-any.whl

---
+++ packages/keras-cv-attention-models/keras_cv_attention_models-1.4.2-py3-none-any.whl/kecam/__init__.py
@@ -0,0 +1,7 @@
+from keras_cv_attention_models import *
+_sub_modules = {__name__ + "." + kk: vv for kk, vv in locals().items() if not kk.startswith("_")}
+
+import sys as _sys
+_sys.modules.update(_sub_modules)
+
+from keras_cv_attention_models.version import __version__
