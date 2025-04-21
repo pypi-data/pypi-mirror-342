25cbf704ae40a31e0481e06baf3547bf504706df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:05:09 +0000

    Add package md2html-math/0.1.2/md2html_math-0.1.2.tar.gz

---
+++ packages/md2html-math/md2html_math-0.1.2.tar.gz/md2html_math-0.1.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="md2html-math",
+    version="0.1.2",
+    packages=find_namespace_packages(),
+    install_requires=[
+        "markdown>=3.8",
+        "jinja2>=3.0.0",
+        "python-markdown-math>=0.8",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=8.0.0",
+            "pytest-cov>=6.0.0",
+        ],
+    },
+    python_requires=">=3.11",
+    author="Marco Allanda",
+    author_email="marcoallanda@gmail.com",
+    description="A markdown to HTML converter with LaTeX math support and beautiful styling",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcoallanda01/md2html-math",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "md2html-math=md2html.cli:main",
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "md2html": ["templates/*.html"],
+    },
+) 
