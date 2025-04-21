b66e3340a1b5370d4bf17a7cf201bf4b6c65948a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:27 +0000

    Add package 3dmviewer/0.1/3dmviewer-0.1.tar.gz

---
+++ packages/3dmviewer/3dmviewer-0.1.tar.gz/3dmviewer-0.1/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='3dmviewer',
+    version='0.1',
+    packages=find_packages(),
+    description='A Python library to return hardcoded  examples for 3D viewer or educational tools',
+    author='Chibi',
+    author_email='chibi.editzz@gmail.com',
+    url='https://github.com/AJAY-HaCkR/3dmviewer/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
