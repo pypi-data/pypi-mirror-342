88289cabb5835c066414d13343e60f7586fb3f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:05:16 +0000

    Add package barkr/0.9.7/barkr-0.9.7-py3-none-any.whl

---
+++ packages/barkr/barkr-0.9.7-py3-none-any.whl/barkr/models/message.py
@@ -0,0 +1,42 @@
+"""
+Module to implement a generic, wrapper model for messages.
+
+A message is a generic object that can be posted to a social media network
+OR retrieved from a social media network, with an ID and a message body, as
+well as (potentially) other metadata that each connection can decide
+whether or not to take into account.
+"""
+
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass(frozen=True)
+class Message:
+    """
+    A generic message object with an ID and a message body,
+    as well as (potentially) other metadata that each connection
+    can decide whether or not to take into account.
+    """
+
+    # Main attributes
+    id: str
+    message: str
+
+    # Optional metadata
+    language: Optional[str] = None
+    label: Optional[str] = None
+
+    def has_content(self) -> bool:
+        """
+        Check if the message has content and therefore can be published.
+
+        At this point, the only check is whether the message is empty or not.
+        In the future, as Messages grow in complexity, this method may be
+        extended to check for other conditions (e.g. accounting for
+        a message with no text but with an image).
+
+        :return: True if the message has content, False otherwise
+        """
+
+        return bool(self.message)
