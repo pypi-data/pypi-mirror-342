3ceebb796ebf2131a65984b7e42f3c2aad2febb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:15 +0000

    Add package fundi/1.1.0/fundi-1.1.0-py3-none-any.whl

---
+++ packages/fundi/fundi-1.1.0-py3-none-any.whl/fundi/__init__.py
@@ -0,0 +1,7 @@
+from .scan import scan
+from .from_ import from_
+from . import exceptions
+from .resolve import resolve
+from .inject import inject, ainject
+from .util import tree, order, injection_trace
+from .types import CallableInfo, TypeResolver, InjectionTrace
