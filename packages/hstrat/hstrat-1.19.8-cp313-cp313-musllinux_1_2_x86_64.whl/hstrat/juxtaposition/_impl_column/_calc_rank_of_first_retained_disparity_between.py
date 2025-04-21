704271a8ec705021a0a3ec124fc4ba0bfd276a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:26 +0000

    Add package hstrat/1.19.8/hstrat-1.19.8-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/hstrat/hstrat-1.19.8-cp313-cp313-musllinux_1_2_x86_64.whl/hstrat/juxtaposition/_impl_column/_calc_rank_of_first_retained_disparity_between.py
@@ -0,0 +1,57 @@
+import typing
+
+from ..._auxiliary_lib import HereditaryStratigraphicArtifact
+from ...genome_instrumentation import (
+    HereditaryStratigraphicColumn,
+    HereditaryStratumOrderedStoreList,
+)
+from ._calc_rank_of_first_retained_disparity_between_bsearch import (
+    calc_rank_of_first_retained_disparity_between_bsearch,
+)
+from ._calc_rank_of_first_retained_disparity_between_generic import (
+    calc_rank_of_first_retained_disparity_between_generic,
+)
+
+
+def calc_rank_of_first_retained_disparity_between(
+    first: HereditaryStratigraphicArtifact,
+    second: HereditaryStratigraphicArtifact,
+    confidence_level: float,
+) -> typing.Optional[int]:
+    """Find first mismatching strata between columns.
+
+    Implementation detail. Provides optimized implementation for special
+    case where both self and second use the perfect resolution stratum
+    retention policy.
+    """
+
+    if (
+        not isinstance(first, HereditaryStratigraphicColumn)
+        or not isinstance(second, HereditaryStratigraphicColumn)
+        or first.HasDiscardedStrata()
+        or second.HasDiscardedStrata()
+        # for performance reasons
+        # only apply binary search to stores that support random access
+        or not hasattr(first, "_stratum_ordered_store")
+        or not isinstance(
+            first._stratum_ordered_store, HereditaryStratumOrderedStoreList
+        )
+        or not hasattr(second, "_stratum_ordered_store")
+        or not isinstance(
+            second._stratum_ordered_store,
+            HereditaryStratumOrderedStoreList,
+        )
+        # binary search currently requires no spurious collisions
+        or first.GetStratumDifferentiaBitWidth() < 64
+    ):
+        return calc_rank_of_first_retained_disparity_between_generic(
+            first,
+            second,
+            confidence_level=confidence_level,
+        )
+    else:
+        return calc_rank_of_first_retained_disparity_between_bsearch(
+            first,
+            second,
+            confidence_level=confidence_level,
+        )
