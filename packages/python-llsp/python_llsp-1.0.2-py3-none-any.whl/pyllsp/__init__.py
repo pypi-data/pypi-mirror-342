5adf6a88c747b4554fb29db6b4c78988d93ee94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:04:37 +0000

    Add package python-llsp/1.0.2/python_llsp-1.0.2-py3-none-any.whl

---
+++ packages/python-llsp/python_llsp-1.0.2-py3-none-any.whl/pyllsp/__init__.py
@@ -0,0 +1,11 @@
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "1.0.2"  # changed by semantic-release
+
+
+__version__: str = get_version()
