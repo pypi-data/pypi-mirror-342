efe980f441c6b4e2a04ef84d1c6b2eabef7ed028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 05:05:08 +0000

    Add package fmla/0.0.2/fmla-0.0.2-py3-none-any.whl

---
+++ packages/fmla/fmla-0.0.2-py3-none-any.whl/fmla/__init__.py
@@ -0,0 +1,21 @@
+import random
+from typing import List
+
+
+def get_welcome_messages() -> List[str]:
+    """Return list of welcome messages."""
+    return [
+        "Welcome to FMLA! Your career's last-ditch life preserver!",
+        "PIP got you down? FMLA's got your back!",
+        "Congrats on your 'development opportunity' (PIP). Time for FMLA!",
+        "Remember: It's not procrastination, it's strategic career planning!",
+    ]
+
+
+def welcome() -> None:
+    """Display random welcome message when package is imported."""
+    messages = get_welcome_messages()
+    print(random.choice(messages))
+
+
+welcome()
