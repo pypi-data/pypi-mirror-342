1ae451bb56642ff2d3fa376018141c7c92825282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 05:05:20 +0000

    Add package latexgeneration/0.1.1/latexgeneration-0.1.1.tar.gz

---
+++ packages/latexgeneration/latexgeneration-0.1.1.tar.gz/latexgeneration-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latexgeneration",
+    version="0.1.1",
+    description="LaTeX-code-generator for tables and images ",
+    author="Nikita Savkin",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
