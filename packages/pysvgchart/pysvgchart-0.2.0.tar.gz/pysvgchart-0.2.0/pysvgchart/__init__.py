aa506727df81a185f572c15f4284e6449592badb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:07 +0000

    Add package pysvgchart/0.2.0/pysvgchart-0.2.0.tar.gz

---
+++ packages/pysvgchart/pysvgchart-0.2.0.tar.gz/pysvgchart-0.2.0/pysvgchart/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for py-svg-chart"""
+
+__author__ = 'Alex Rowley'
+__email__ = ''
+__version__ = '0.2.0'
+
+from .charts import LineChart, SimpleLineChart, DonutChart, BarChart
+from .shapes import Text, Line, Circle
+from .styles import render_all_styles, hover_style_name
