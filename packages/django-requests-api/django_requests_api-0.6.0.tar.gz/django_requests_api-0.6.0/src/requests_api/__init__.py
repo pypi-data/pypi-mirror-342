9e5f6657680d97709e14809e0625465cb86f8914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:06:48 +0000

    Add package django-requests-api/0.6.0/django_requests_api-0.6.0.tar.gz

---
+++ packages/django-requests-api/django_requests_api-0.6.0.tar.gz/django_requests_api-0.6.0/src/requests_api/__init__.py
@@ -0,0 +1,21 @@
+"""
+See PEP 386 (https://peps.python.org/pep-0386/)
+"""
+from .requests_api import RequestsApi
+
+__version__ = "0.6.0"
+__version_info__ = tuple(int(i) if i.isdigit() else i for i in __version__.split("."))
+__license__ = "MIT"
+__title__ = "requests_api"
+
+__author__ = "DLRSP"
+__copyright__ = "Copyright 2010-present DLRSP"
+
+# Version synonym
+VERSION = __version_info__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = "iso-8859-1"
+
+# Default datetime input and output formats
+ISO_8601 = "iso-8601"
