1b10159287a1b77be7df993efe7986371b114232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:32 +0000

    Add package mangokit/2.5.6/mangokit-2.5.6-py3-none-any.whl

---
+++ packages/mangokit/mangokit-2.5.6-py3-none-any.whl/mangokit/models/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台# @Description:
+# @Time   : 2023-09-09 23:17
+# @Author : 毛鹏
+from mangokit.models._models import MysqlConingModel, FunctionModel, TestReportModel, ResponseModel, EmailNoticeModel, \
+    WeChatNoticeModel, ClassMethodModel
+from mangokit.models._ui_model import ElementModel, MethodModel, ElementResultModel
+
+__all__ = [
+    'ElementModel',
+    'MethodModel',
+    'ElementResultModel',
+    'MysqlConingModel',
+    'FunctionModel',
+    'TestReportModel',
+    'ResponseModel',
+    'EmailNoticeModel',
+    'WeChatNoticeModel',
+    'ClassMethodModel',
+]
