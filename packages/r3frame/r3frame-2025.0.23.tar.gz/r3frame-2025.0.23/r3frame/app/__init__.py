c10c52da258573b415d32bc542096ef6addba199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:04:34 +0000

    Add package r3frame/2025.0.23/r3frame-2025.0.23.tar.gz

---
+++ packages/r3frame/r3frame-2025.0.23.tar.gz/r3frame-2025.0.23/r3frame/app/__init__.py
@@ -0,0 +1,7 @@
+from r3frame.app.proc import Process
+from r3frame.app.event import EventManager
+from r3frame.app.input import Keyboard, Mouse
+from r3frame.app.resource import Clock, Window, Camera, Renderer, Animation, AssetManager
+
+from r3frame.app.scene import Scene
+from r3frame.app.base import Application
