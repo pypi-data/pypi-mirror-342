6010b1802abc80d3d897d2d44c7c8314ba57dc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:12 +0000

    Add package object-storage-proxy/0.1.11/object_storage_proxy-0.1.11.tar.gz

---
+++ packages/object-storage-proxy/object_storage_proxy-0.1.11.tar.gz/object_storage_proxy-0.1.11/test_server.py
@@ -0,0 +1,62 @@
+from object_storage_proxy import start_server, ProxyServerConfig
+from dotenv import load_dotenv
+import os
+import random
+
+
+load_dotenv()
+
+
+def docreds(bucket) -> str:
+    apikey = os.getenv("COS_API_KEY")
+    if not apikey:
+        raise ValueError("COS_API_KEY environment variable not set")
+    
+    print(f"Fetching credentials for {bucket}...")
+    return apikey
+
+def do_validation(token: str, bucket: str) -> bool:
+    print(f"PYTHON: Validating headers: {token} for {bucket}...")
+    return random.choice([True, False])  # pointless now since cached
+    # return False
+
+
+def main() -> None:
+    apikey = os.getenv("COS_API_KEY")
+    if not apikey:
+        raise ValueError("COS_API_KEY environment variable not set")
+
+    cos_map = {
+        "bucket1": {
+            "host": "s3.eu-de.cloud-object-storage.appdomain.cloud",
+            "port": 443,
+            "apikey": apikey,
+            "ttl": 0
+        },
+        "bucket2": {
+            "host": "s3.eu-de.cloud-object-storage.appdomain.cloud",
+            "port": 443,
+            "apikey": apikey
+        },
+        "proxy-bucket01": {
+            "host": "s3.eu-de.cloud-object-storage.appdomain.cloud",
+            "port": 443,
+            "ttl": 1
+        }
+    }
+
+    ra = ProxyServerConfig(
+        bucket_creds_fetcher=docreds,
+        validator=do_validation,
+        cos_map=cos_map,
+        port=6190
+    )
+
+    start_server(ra)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
