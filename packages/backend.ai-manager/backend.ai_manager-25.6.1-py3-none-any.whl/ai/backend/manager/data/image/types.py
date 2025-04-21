3fcd8e598e717076cd552da0416ef04c2ec03d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:20 +0000

    Add package backend.ai-manager/25.6.1/backend.ai_manager-25.6.1-py3-none-any.whl

---
+++ packages/backend.ai-manager/backend.ai_manager-25.6.1-py3-none-any.whl/ai/backend/manager/data/image/types.py
@@ -0,0 +1,62 @@
+import enum
+import uuid
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from ai.backend.manager.models.image import Resources
+
+
+class ImageStatus(enum.StrEnum):
+    ALIVE = "ALIVE"
+    DELETED = "DELETED"
+
+
+class ImageType(enum.Enum):
+    COMPUTE = "compute"
+    SYSTEM = "system"
+    SERVICE = "service"
+
+
+@dataclass
+class ImageLabelsData:
+    label_data: dict[str, str]
+
+
+@dataclass
+class ImageResourcesData:
+    resources_data: "Resources"
+
+
+@dataclass
+class ImageData:
+    id: uuid.UUID
+    name: str
+    project: Optional[str]
+    image: str
+    created_at: Optional[datetime]
+    tag: Optional[str]
+    registry: str
+    registry_id: uuid.UUID
+    architecture: str
+    config_digest: str
+    size_bytes: int
+    is_local: bool
+    type: ImageType
+    accelerators: Optional[str]
+    labels: ImageLabelsData
+    resources: ImageResourcesData
+    status: ImageStatus
+
+
+@dataclass
+class RescanImagesResult:
+    images: list[ImageData]
+    errors: list[str] = field(default_factory=list)
+
+
+@dataclass
+class ImageAliasData:
+    id: uuid.UUID
+    alias: str
