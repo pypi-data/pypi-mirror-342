bba37b66b94aaea684a8923be63c1544b49c449e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:35 +0000

    Add package rwkv-fla/0.7.202504210826/rwkv_fla-0.7.202504210826.tar.gz

---
+++ packages/rwkv-fla/rwkv_fla-0.7.202504210826.tar.gz/rwkv_fla-0.7.202504210826/rwkvfla/modules/l2norm.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
+
+from typing import Optional
+
+import torch
+import triton
+import triton.language as tl
+
+from rwkvfla.utils import input_guard
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=num_warps)
+        for num_warps in [1, 2, 4, 8, 16, 32]
+    ],
+    key=['N']
+)
+@triton.jit
+def l2norm_fwd_kernel(
+    X,
+    Y,
+    N,
+    eps,
+    BLOCK_N: tl.constexpr,
+):
+    i_m = tl.program_id(0)
+    X += i_m * N
+    Y += i_m * N
+    # Compute mean and variance
+    cols = tl.arange(0, BLOCK_N)
+    mask = cols < N
+    x = tl.load(X + cols, mask=mask, other=0.0).to(tl.float32)
+    var = tl.sum(x * x, axis=0)
+    rstd = 1 / tl.sqrt(var + eps)
+    # tl.store(Rstd + i_m, rstd)
+    # Normalize and apply linear transformation
+    y = x * rstd
+    # Write output
+    tl.store(Y + cols, y, mask=mask)
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=num_warps)
+        for num_warps in [1, 2, 4, 8, 16, 32]
+    ],
+    key=['N']
+)
+@triton.jit
+def l2norm_bwd_kernel(
+    X,
+    DY,
+    DX,
+    N,
+    eps,
+    BLOCK_N: tl.constexpr,
+):
+    i_m = tl.program_id(0)
+    X += i_m * N
+    DX += i_m * N
+    DY += i_m * N
+
+    # Y += i_m * stride_y_row
+    cols = tl.arange(0, BLOCK_N)
+    mask = cols < N
+    x = tl.load(X + cols, mask=mask, other=0.0).to(tl.float32)
+    var = tl.sum(x * x)
+    rstd = 1 / tl.sqrt(var + eps)
+    dy = tl.load(DY + cols, mask=mask, other=0.0).to(tl.float32)
+    dx = dy * rstd - tl.sum(dy * x) * (1 / (var+eps)) * rstd * x
+    tl.store(DX + cols, dx, mask=mask)
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({'BM': BM}, num_warps=num_warps)
+        for num_warps in [1, 2, 4, 8, 16]
+        for BM in [32, 64, 128]
+    ],
+    key=['N', 'autotune_aux']
+)
+@triton.jit
+def l2norm_fwd_kernel_small_N(
+    X,
+    Y,
+    eps,
+    autotune_aux: tl.constexpr,
+    BN: tl.constexpr,
+    BM: tl.constexpr,
+    M: tl.constexpr,
+    N: tl.constexpr,
+):
+    i_m = tl.program_id(0)
+    p_X = tl.make_block_ptr(X, (M, N), (N, 1), (i_m * BM, 0), (BM, BN), (1, 0))
+    b_X = tl.load(p_X, boundary_check=(0, 1)).to(tl.float32)
+    var = tl.sum(b_X * b_X, axis=1)
+    b_y = b_X / tl.sqrt(var + eps)[:, None]
+    p_Y = tl.make_block_ptr(Y, (M, N), (N, 1), (i_m * BM, 0), (BM, BN), (1, 0))
+    tl.store(p_Y, b_y.to(p_Y.dtype.element_ty), boundary_check=(0, 1))
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({'BM': BM}, num_warps=num_warps)
+        for num_warps in [1, 2, 4, 8, 16]
+        for BM in [32, 64, 128]
+    ],
+    key=['N', 'autotune_aux']
+)
+@triton.jit
+def l2norm_bwd_kernel_small_N(
+    X,
+    DY,
+    DX,
+    eps,
+    BN: tl.constexpr,
+    BM: tl.constexpr,
+    M: tl.constexpr,
+    N: tl.constexpr,
+    autotune_aux: tl.constexpr,
+):
+    i_m = tl.program_id(0)
+    p_X = tl.make_block_ptr(X, (M, N), (N, 1), (i_m * BM, 0), (BM, BN), (1, 0))
+    p_DY = tl.make_block_ptr(DY, (M, N), (N, 1), (i_m * BM, 0), (BM, BN), (1, 0))
+    p_DX = tl.make_block_ptr(DX, (M, N), (N, 1), (i_m * BM, 0), (BM, BN), (1, 0))
+    b_X = tl.load(p_X, boundary_check=(0, 1)).to(tl.float32)
+    b_DY = tl.load(p_DY, boundary_check=(0, 1)).to(tl.float32)
+    var = tl.sum(b_X * b_X, axis=1)[:, None]
+    rstd = 1 / tl.sqrt(var + eps)
+    dx = b_DY * rstd - tl.sum(b_DY * b_X, axis=1)[:, None] / (var+eps) * rstd * b_X
+    tl.store(p_DX, dx.to(p_DX.dtype.element_ty), boundary_check=(0, 1))
+
+
+def l2norm_fwd(
+    x: torch.Tensor,
+    eps: float = 1e-6,
+    output_dtype: Optional[torch.dtype] = None
+):
+    x_shape_og = x.shape
+    x = x.view(-1, x.shape[-1])
+    # allocate output
+    if output_dtype is None:
+        y = torch.empty_like(x)
+    else:
+        y = torch.empty_like(x, dtype=output_dtype)
+    assert y.stride(-1) == 1
+    N = x.shape[-1]
+    M = x.shape[0]
+    # rstd = torch.empty((M,), dtype=torch.float32, device=x.device)
+    # Less than 64KB per feature: enqueue fused kernel
+    MAX_FUSED_SIZE = 65536 // x.element_size()
+    BLOCK_N = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+    if N > BLOCK_N:
+        raise RuntimeError("This layer doesn't support feature dim >= 64KB.")
+
+    if N <= 512:
+        autotune_aux = triton.cdiv(M, 2048)
+        def grid(meta): return (triton.cdiv(M, meta['BM']), )
+        l2norm_fwd_kernel_small_N[grid](
+            x,
+            y,
+            eps,
+            BN=BLOCK_N,
+            M=M,
+            N=N,
+            autotune_aux=autotune_aux
+        )
+    else:
+        l2norm_fwd_kernel[(M,)](
+            x,
+            y,
+            N,
+            eps,
+            BLOCK_N,
+        )
+
+    return y.view(x_shape_og)
+
+
+def l2norm_bwd(
+    x: torch.Tensor,
+    dy: torch.Tensor,
+    eps: float = 1e-5
+):
+    x_shape_og = x.shape
+    x = x.view(-1, dy.shape[-1])
+    dy = dy.view(-1, dy.shape[-1])
+    if dy.stride(-1) != 1:
+        dy = dy.contiguous()
+    assert dy.shape == x.shape
+    # allocate output
+    dx = torch.empty_like(x)
+    M = x.shape[0]
+    N = x.shape[-1]
+    # rstd = torch.empty((M,), dtype=torch.float32, device=x.device)
+    # Less than 64KB per feature: enqueue fused kernel
+    MAX_FUSED_SIZE = 65536 // x.element_size()
+    BLOCK_N = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+    if N > BLOCK_N:
+        raise RuntimeError("This layer norm doesn't support feature dim >= 64KB.")
+    # heuristics for number of warps
+
+    if N <= 512:
+        autotune_aux = triton.cdiv(M, 2048)
+        def grid(meta): return (triton.cdiv(M, meta['BM']), )
+        l2norm_bwd_kernel_small_N[grid](
+            X=x,
+            DY=dy,
+            DX=dx,
+            eps=eps,
+            BN=BLOCK_N,
+            M=M,
+            N=N,
+            autotune_aux=autotune_aux
+        )
+    else:
+        l2norm_bwd_kernel[(M,)](
+            x,
+            dy,
+            dx,
+            N,
+            eps,
+            BLOCK_N,
+        )
+
+    return dx.view(x_shape_og)
+
+
+class L2NormFunction(torch.autograd.Function):
+
+    @staticmethod
+    @input_guard
+    def forward(
+        ctx,
+        x,
+        eps=1e-6,
+        output_dtype=None
+    ):
+        y = l2norm_fwd(x, eps, output_dtype)
+        ctx.eps = eps
+        ctx.x_dtype = x.dtype
+        ctx.save_for_backward(x)
+        return y
+
+    @staticmethod
+    @input_guard
+    def backward(ctx, dy):
+        x, = ctx.saved_tensors
+        dx = l2norm_bwd(x, dy, ctx.eps)
+        return dx, None, None
+
+
+def l2_norm(
+    x: torch.Tensor,
+    eps: float = 1e-6,
+    output_dtype: Optional[torch.dtype] = None
+) -> torch.Tensor:
+    return L2NormFunction.apply(x, eps, output_dtype)
