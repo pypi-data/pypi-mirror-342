df7d5cde00ca4d7f9c8977a06d8dc413f584eeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:39 +0000

    Add package bodit-analysis-module/0.1.9/bodit_analysis_module-0.1.9-py3-none-any.whl

---
+++ packages/bodit-analysis-module/bodit_analysis_module-0.1.9-py3-none-any.whl/bodit_analysis_module/__init__.py
@@ -0,0 +1,8 @@
+from .fhRawdata import *
+from .fhDatabase import *
+from .fhConst import *
+from .fhBasic import *
+from .fhFeature import *
+from .fhFeatExtractModule import *
+
+__version__ = "0.1.9" 
