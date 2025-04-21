279a372e3c2a3a9c6a6ee8d57a8371626bd7dbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:05:06 +0000

    Add package qurrium/0.12.0/qurrium-0.12.0-cp39-abi3-win32.whl

---
+++ packages/qurrium/qurrium-0.12.0-cp39-abi3-win32.whl/qurry/recipe/__init__.py
@@ -0,0 +1,11 @@
+"""Qurrium Recipe - The Simple Case Library (:mod:`qurry.qurry.recipe`)"""
+
+from .simple import (
+    Intracell,
+    Singlet,
+    TrivialParamagnet,
+    TopologicalParamagnet,
+    Cluster,
+    Cat,
+    GHZ,
+)
