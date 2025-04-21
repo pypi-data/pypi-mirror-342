433e6e8a194d079e58c40bc8f393e8bea31141a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:05:07 +0000

    Add package starmallow/0.9.0/starmallow-0.9.0-py3-none-any.whl

---
+++ packages/starmallow/starmallow-0.9.0-py3-none-any.whl/starmallow/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.9.0"
+
+from .applications import StarMallow
+from .exceptions import RequestValidationError
+from .params import Body, Cookie, Form, Header, NoParam, Path, Query, ResolvedParam, Security
+from .responses import HTTPValidationError
+from .routing import APIRoute, APIRouter
+from .websockets import APIWebSocket
