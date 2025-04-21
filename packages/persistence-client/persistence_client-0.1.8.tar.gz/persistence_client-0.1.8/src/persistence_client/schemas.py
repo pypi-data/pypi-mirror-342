53e7684021cd0537bd963fa3b579afa53a34458c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 16:04:30 +0000

    Add package persistence-client/0.1.8/persistence_client-0.1.8.tar.gz

---
+++ packages/persistence-client/persistence_client-0.1.8.tar.gz/persistence_client-0.1.8/src/persistence_client/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class I18n(BaseModel):
+    kz: str | None = None
+    ru: str | None = None
+    en: str | None = None
+
+class DataColumn(BaseModel):
+    title: I18n
+
+class DataSchema(BaseModel):
+    id: str
+    title: I18n
+    columns: list[DataColumn]
+
+class DataRow(BaseModel):
+    hash: str
+    bin_iin: str | None = None
+    fio: str | None = None
+    title: str | None = None
+    data: list[str | int | float]
+    data_schema: DataSchema
