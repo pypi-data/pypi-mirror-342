be5c0fdf2c13b2daee1af9a5752583759dc9dcc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:04:29 +0000

    Add package aegisx.ext.iam/0.0.1/aegisx_ext_iam-0.0.1.tar.gz

---
+++ packages/aegisx.ext.iam/aegisx_ext_iam-0.0.1.tar.gz/aegisx_ext_iam-0.0.1/aegisx/ext/iam/__init__.py
@@ -0,0 +1,10 @@
+from .models import AnonymousSubject
+from .models import AuthenticatedSubject
+from .models import AuthorizationContext
+
+
+__all__: list[str] = [
+    'AnonymousSubject',
+    'AuthenticatedSubject',
+    'AuthorizationContext',
+]
