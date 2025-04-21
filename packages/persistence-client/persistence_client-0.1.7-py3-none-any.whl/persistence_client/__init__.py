125652c131a07153b52d91228fc83f407667b269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 16:04:21 +0000

    Add package persistence-client/0.1.7/persistence_client-0.1.7-py3-none-any.whl

---
+++ packages/persistence-client/persistence_client-0.1.7-py3-none-any.whl/persistence_client/__init__.py
@@ -0,0 +1,15 @@
+from .client import RabbitmqPersistence
+from .schemas import (
+    DataColumn,
+    DataRow,
+    DataSchema,
+    I18n,
+)
+
+__all__ = [
+    "RabbitmqPersistence",
+    "DataColumn",
+    "DataRow",
+    "DataSchema",
+    "I18n",
+]
