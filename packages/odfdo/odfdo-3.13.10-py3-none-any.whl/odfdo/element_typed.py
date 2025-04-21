6ec63097c12cd9734b73d91734159156cf8391b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 05:05:04 +0000

    Add package odfdo/3.13.10/odfdo-3.13.10-py3-none-any.whl

---
+++ packages/odfdo/odfdo-3.13.10-py3-none-any.whl/odfdo/element_typed.py
@@ -0,0 +1,205 @@
+# Copyright 2018-2025 Jérôme Dumonteil
+# Copyright (c) 2009-2010 Ars Aperta, Itaapy, Pierlis, Talend.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Authors (odfdo project): jerome.dumonteil@gmail.com
+# The odfdo project is a derivative work of the lpod-python project:
+# https://github.com/lpod/lpod-python
+"""ElmentTyped subclass of Element."""
+
+from __future__ import annotations
+
+import contextlib
+from datetime import date, datetime, timedelta
+from decimal import Decimal
+from typing import Any
+
+from .datatype import Boolean, Date, DateTime, Duration
+from .element import Element
+from .utils import bytes_to_str
+
+
+class ElementTyped(Element):
+    def set_value_and_type(
+        self,
+        value: Any,
+        value_type: str | None = None,
+        text: str | None = None,
+        currency: str | None = None,
+    ) -> str | None:
+        # Remove possible previous value and type
+        for name in (
+            "office:value-type",
+            "office:boolean-value",
+            "office:value",
+            "office:date-value",
+            "office:string-value",
+            "office:time-value",
+            "table:formula",
+            "office:currency",
+            "calcext:value-type",
+            "loext:value-type",
+        ):
+            with contextlib.suppress(KeyError):
+                self.del_attribute(name)
+        if isinstance(value, bytes):
+            value = bytes_to_str(value)
+        if isinstance(value_type, bytes):
+            value_type = bytes_to_str(value_type)
+        if isinstance(text, bytes):
+            text = bytes_to_str(text)
+        if isinstance(currency, bytes):
+            currency = bytes_to_str(currency)
+        if value is None:
+            self._erase_text_content()
+            return text
+        if isinstance(value, bool):
+            if value_type is None:
+                value_type = "boolean"
+            if text is None:
+                text = "true" if value else "false"
+            value = Boolean.encode(value)
+        elif isinstance(value, (int, float, Decimal)):
+            if value_type == "percentage":
+                text = f"{int(value * 100)} %"
+            if value_type is None:
+                value_type = "float"
+            if text is None:
+                text = str(value)
+            value = str(value)
+        elif isinstance(value, datetime):
+            if value_type is None:
+                value_type = "date"
+            if text is None:
+                text = str(DateTime.encode(value))
+            value = DateTime.encode(value)
+        elif isinstance(value, date):
+            if value_type is None:
+                value_type = "date"
+            if text is None:
+                text = str(Date.encode(value))
+            value = Date.encode(value)
+        elif isinstance(value, str):
+            if value_type is None:
+                value_type = "string"
+            if text is None:
+                text = value
+        elif isinstance(value, timedelta):
+            if value_type is None:
+                value_type = "time"
+            if text is None:
+                text = str(Duration.encode(value))
+            value = Duration.encode(value)
+        elif value is not None:
+            raise TypeError(f"Type unknown: '{value!r}'")
+
+        if value_type is not None:
+            self.set_attribute("office:value-type", value_type)
+            self.set_attribute("calcext:value-type", value_type)
+        if value_type == "boolean":
+            self.set_attribute("office:boolean-value", value)
+        elif value_type == "currency":
+            self.set_attribute("office:value", value)
+            self.set_attribute("office:currency", currency)
+        elif value_type == "date":
+            self.set_attribute("office:date-value", value)
+        elif value_type in ("float", "percentage"):
+            self.set_attribute("office:value", value)
+            self.set_attribute("calcext:value", value)
+        elif value_type == "string":
+            self.set_attribute("office:string-value", value)
+        elif value_type == "time":
+            self.set_attribute("office:time-value", value)
+
+        return text
+
+    def _get_typed_value(
+        self,
+        value_type: str | None = None,
+        try_get_text: bool = True,
+    ) -> tuple[Any, str | None]:
+        """Return Python typed value.
+
+        Only for "with office:value-type" elements, not for meta fields."""
+        value: Decimal | str | bool | None = None
+        if value_type is None:
+            read_value_type = self.get_attribute("office:value-type")
+            if isinstance(read_value_type, bool):
+                raise TypeError(
+                    f'Wrong type for "office:value-type": {type(read_value_type)}'
+                )
+            value_type = read_value_type
+        # value_type = to_str(value_type)
+        if value_type == "boolean":
+            value = self.get_attribute("office:boolean-value")
+            return (value, value_type)
+        if value_type in {"float", "percentage", "currency"}:
+            read_number = self.get_attribute("office:value")
+            if not isinstance(read_number, (Decimal, str)):
+                raise TypeError(f'Wrong type for "office:value": {type(read_number)}')
+            value = Decimal(read_number)
+            # Return 3 instead of 3.0 if possible
+            with contextlib.suppress(ValueError):
+                if int(value) == value:
+                    return (int(value), value_type)
+            return (value, value_type)
+        if value_type == "date":
+            read_attribute = self.get_attribute("office:date-value")
+            if not isinstance(read_attribute, str):
+                raise TypeError(
+                    f'Wrong type for "office:date-value": {type(read_attribute)}'
+                )
+            if "T" in read_attribute:
+                return (DateTime.decode(read_attribute), value_type)
+            return (Date.decode(read_attribute), value_type)
+        if value_type == "string":
+            value = self.get_attribute("office:string-value")
+            if value is not None:
+                return (str(value), value_type)
+            if try_get_text:
+                list_value = [para.inner_text for para in self.get_elements("text:p")]
+                if list_value:
+                    return ("\n".join(list_value), value_type)
+            return (None, value_type)
+        if value_type == "time":
+            read_value = self.get_attribute("office:time-value")
+            if not isinstance(read_value, str):
+                raise TypeError(
+                    f'Wrong type for "office:time-value": {type(read_value)}'
+                )
+            time_value = Duration.decode(read_value)
+            return (time_value, value_type)
+        if value_type is None:
+            return (None, None)
+        raise ValueError(f'Unexpected value type: "{value_type}"')
+
+    def get_value(
+        self,
+        value_type: str | None = None,
+        try_get_text: bool = True,
+        get_type: bool = False,
+    ) -> Any | tuple[Any, str]:
+        """Return Python typed value.
+
+        Only for "with office:value-type" elements, not for meta fields."""
+        if get_type:
+            return self._get_typed_value(
+                value_type=value_type,
+                try_get_text=try_get_text,
+            )
+        return self._get_typed_value(
+            value_type=value_type,
+            try_get_text=try_get_text,
+        )[0]
---
+++ packages/odfdo/odfdo-3.13.10-py3-none-any.whl/odfdo/paragraph_base.py
@@ -0,0 +1,317 @@
+# Copyright 2018-2025 Jérôme Dumonteil
+# Copyright (c) 2009-2013 Ars Aperta, Itaapy, Pierlis, Talend.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Authors (odfdo project): jerome.dumonteil@gmail.com
+# The odfdo project is a derivative work of the lpod-python project:
+# https://github.com/lpod/lpod-python
+# Authors: David Versmisse <david.versmisse@itaapy.com>
+#          Hervé Cauwelier <herve@itaapy.com>
+#          Romain Gauthier <romain@itaapy.com>
+#          Jerome Dumonteil <jerome.dumonteil@itaapy.com>
+"""Base class ParagraphBase and Spacer "text:s", Tab "text:tab", LineBreak
+"text:line-break".
+"""
+
+from __future__ import annotations
+
+import contextlib
+from typing import Any
+
+from .element import Element, EText, PropDef, _get_lxml_tag, register_element_class
+from .mixin_md import MDLineBreak, MDSpacer, MDTab
+
+
+def _get_formatted_text(
+    element: Element,
+    context: dict | None = None,
+    with_text: bool = True,
+) -> str:
+    if context is None:
+        context = {}
+    document = context.get("document", None)
+    rst_mode = context.get("rst_mode", False)
+
+    result: list[str] = []
+    objects: list[Element | EText] = []
+    if with_text:
+        objects = element.xpath("*|text()")
+    else:
+        objects = [x for x in element.children]  # noqa: C416
+    for obj in objects:
+        if isinstance(obj, EText):
+            result.append(obj)
+            continue
+        tag = obj.tag
+        # Good tags with text
+        if tag in ("text:a", "text:p"):
+            result.append(_get_formatted_text(obj, context, with_text=True))
+            continue
+        # Try to convert some styles in rst_mode
+        if tag == "text:span":
+            text = _get_formatted_text(obj, context, with_text=True)
+            if not rst_mode:
+                result.append(text)
+                continue
+            if not text.strip():
+                result.append(text)
+                continue
+            if hasattr(obj, "style"):
+                style = obj.style
+            else:
+                style = None
+            if not style:
+                result.append(text)
+                continue
+            if document:
+                style = document.get_style("text", style)
+                properties = style.get_properties()
+            else:
+                properties = None
+            if properties is None:
+                result.append(text)
+                continue
+            # Compute before, text and after
+            before = ""
+            for c in text:
+                if c.isspace():
+                    before += c
+                else:
+                    break
+            after = ""
+            for c in reversed(text):
+                if c.isspace():
+                    after = c + after
+                else:
+                    break
+            text = text.strip()
+            # Bold ?
+            if properties.get("fo:font-weight") == "bold":
+                result.append(before)
+                result.append("**")
+                result.append(text)
+                result.append("**")
+                result.append(after)
+                continue
+            # Italic ?
+            if properties.get("fo:font-style") == "italic":
+                result.append(before)
+                result.append("*")
+                result.append(text)
+                result.append("*")
+                result.append(after)
+                continue
+            # Unknown style, ...
+            result.append(before)
+            result.append(text)
+            result.append(after)
+            continue
+        # Footnote or endnote
+        if tag == "text:note":
+            note_class = obj.note_class  # type:ignore
+            container = {
+                "footnote": context["footnotes"],
+                "endnote": context["endnotes"],
+            }[note_class]
+            citation = obj.citation  # type:ignore
+            if not citation:
+                # Would only happen with hand-made documents
+                citation = len(container)
+            body = obj.note_body  # type:ignore
+            container.append((citation, body))
+            if rst_mode:
+                marker = {"footnote": " [#]_ ", "endnote": " [*]_ "}[note_class]
+            else:
+                marker = {"footnote": "[{citation}]", "endnote": "({citation})"}[
+                    note_class
+                ]
+            result.append(marker.format(citation=citation))
+            continue
+        # Annotations
+        if tag == "office:annotation":
+            context["annotations"].append(obj.note_body)  # type:ignore
+            if rst_mode:
+                result.append(" [#]_ ")
+            else:
+                result.append("[*]")
+            continue
+        # Tabulation
+        if tag == "text:tab":
+            result.append("\t")
+            continue
+        # Line break
+        if tag == "text:line-break":
+            if rst_mode:
+                result.append("\n|")
+            else:
+                result.append("\n")
+            continue
+        # other cases:
+        result.append(obj.get_formatted_text(context))
+    return "".join(result)
+
+
+class Spacer(MDSpacer, Element):
+    """This element shall be used to represent the second and all following “ “
+    (U+0020, SPACE) characters in a sequence of “ “ (U+0020, SPACE) characters.
+    Note: It is not an error if the character preceding the element is not a
+    white space character, but it is good practice to use this element only for
+    the second and all following SPACE characters in a sequence.
+    """
+
+    _tag = "text:s"
+    _properties: tuple[PropDef, ...] = (PropDef("number", "text:c"),)
+
+    def __init__(self, number: int | None = 1, **kwargs: Any):
+        """
+        Arguments:
+
+            number -- int
+        """
+        super().__init__(**kwargs)
+        if self._do_init:
+            if number and number >= 2:
+                self.number = str(number)
+            else:
+                self.number = None
+
+    def __str__(self) -> str:
+        return self.text
+
+    @property
+    def text(self) -> str:
+        """Get / set the text content of the element."""
+        return " " * self.length
+
+    @text.setter
+    def text(self, text: str | None) -> None:
+        if text is None:
+            text = ""
+        self.length = len(text)
+
+    @property
+    def length(self) -> int:
+        name = _get_lxml_tag("text:c")
+        value = self._Element__element.get(name)
+        if value is None:
+            return 1  # minimum 1 space
+        return int(value)
+
+    @length.setter
+    def length(self, value: int | None) -> None:
+        name = _get_lxml_tag("text:c")
+        if value is None or value < 2:
+            with contextlib.suppress(KeyError):
+                del self._Element__element.attrib[name]
+            return
+        self._Element__element.set(name, str(value))
+
+
+Spacer._define_attribut_property()
+
+
+class Tab(MDTab, Element):
+    """This element represents the [UNICODE] tab character (HORIZONTAL
+    TABULATION, U+0009).
+
+    The position attribute contains the number of the tab-stop to which
+    a tab character refers. The position 0 marks the start margin of a
+    paragraph. Note: The position attribute is only a hint to help non-layout
+    oriented consumers to determine the tab/tab-stop association. Layout
+    oriented consumers should determine the tab positions based on the style
+    information
+    """
+
+    _tag = "text:tab"
+    _properties: tuple[PropDef, ...] = (PropDef("position", "text:tab-ref"),)
+
+    def __init__(self, position: int | None = None, **kwargs: Any) -> None:
+        """
+        Arguments:
+
+            position -- int
+        """
+        super().__init__(**kwargs)
+        if self._do_init and position is not None and position >= 0:
+            self.position = str(position)
+
+    def __str__(self) -> str:
+        return "\t"
+
+    @property
+    def text(self) -> str:
+        return "\t"
+
+
+Tab._define_attribut_property()
+
+
+class LineBreak(MDLineBreak, Element):
+    """This element represents a line break "text:line-break" """
+
+    _tag = "text:line-break"
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+    def __str__(self) -> str:
+        return "\n"
+
+    @property
+    def text(self) -> str:
+        return "\n"
+
+
+class ParagraphBase(Element):
+    """Base class for Paragraph like classes."""
+
+    _tag = "text:p-odfdo-notodf"
+    _properties: tuple[PropDef, ...] = (PropDef("style", "text:style-name"),)
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+
+    def __str__(self) -> str:
+        return self.inner_text
+
+    def get_formatted_text(
+        self,
+        context: dict | None = None,
+        simple: bool = False,
+    ) -> str:
+        if not context:
+            context = {
+                "document": None,
+                "footnotes": [],
+                "endnotes": [],
+                "annotations": [],
+                "rst_mode": False,
+                "img_counter": 0,
+                "images": [],
+                "no_img_level": 0,
+            }
+        content = _get_formatted_text(self, context, with_text=True)
+        if simple:
+            return content
+        else:
+            return content + "\n\n"
+
+
+ParagraphBase._define_attribut_property()
+
+register_element_class(Spacer)
+register_element_class(Tab)
+register_element_class(LineBreak)
+register_element_class(ParagraphBase)
