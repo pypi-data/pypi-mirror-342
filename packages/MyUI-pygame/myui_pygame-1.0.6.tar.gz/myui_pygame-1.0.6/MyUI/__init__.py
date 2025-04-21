84584c81fd3e526386b8946b6a40b2f2242c1242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:28 +0000

    Add package MyUI-pygame/1.0.6/myui_pygame-1.0.6.tar.gz

---
+++ packages/MyUI-pygame/myui_pygame-1.0.6.tar.gz/myui_pygame-1.0.6/MyUI/__init__.py
@@ -0,0 +1,19 @@
+"""
+MyUI - The perfect tool for UI operations. Everything from text input to buttons and sliders!
+
+Provides many utilities. Create a *App* reference to get started, and everything else should be 
+straight forward!
+"""
+
+print("Booting MyUI...")
+
+from .preseting import FontScheme, Theme
+from .ui import adjust_color, Text, Button, PushButton, ToggleButton, Slider, DropdownMenu, Tooltip, Dropbox, Window, Surface, HotbarItem, Hotbar, App
+from .plugins import PluginBase, PluginWrapper, load_plugins_from
+from typing import Union
+
+component = Union[ui.component, plugins.component, preseting.component]
+
+del Union
+
+print("Welcome to MyUI! If your interested in how we work, please check out our python package: https://pypi.org/project/MyUI-pygame/")
