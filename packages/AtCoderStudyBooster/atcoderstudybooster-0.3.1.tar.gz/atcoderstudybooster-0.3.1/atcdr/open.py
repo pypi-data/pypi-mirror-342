b3274daa8e2b4b6d22e98b89a5d190829b352448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:06:24 +0000

    Add package AtCoderStudyBooster/0.3.1/atcoderstudybooster-0.3.1.tar.gz

---
+++ packages/AtCoderStudyBooster/atcoderstudybooster-0.3.1.tar.gz/atcoderstudybooster-0.3.1/atcdr/open.py
@@ -0,0 +1,43 @@
+import webbrowser  # noqa: I001
+from rich.panel import Panel
+from rich.console import Console
+
+from atcdr.util.filetype import Lang
+from atcdr.util.execute import execute_files
+from atcdr.util.parse import ProblemHTML
+
+
+def open_html(file: str) -> None:
+    console = Console()
+    try:
+        with open(file, 'r') as f:
+            html_content = f.read()
+    except FileNotFoundError:
+        console.print(
+            Panel(
+                f"{file}' [red]が見つかりません[/]",
+                border_style='red',
+            )
+        )
+        return
+
+    url = ProblemHTML(html_content).link
+    if url:
+        webbrowser.open_new_tab(url)
+        console.print(
+            Panel(
+                f'[green]URLを開きました[/] {url}',
+                border_style='green',
+            )
+        )
+    else:
+        console.print(
+            Panel(
+                f'{file} [yellow]にURLが見つかりませんでした[/]',
+                border_style='yellow',
+            )
+        )
+
+
+def open_files(*args: str) -> None:
+    execute_files(*args, func=open_html, target_filetypes=[Lang.HTML])
