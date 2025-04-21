aee8491ddbaa4981a9cae3efba972fbe67389ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:32 +0000

    Add package bolster/0.3.4/bolster-0.3.4-py3-none-any.whl

---
+++ packages/bolster/bolster-0.3.4-py3-none-any.whl/bolster/utils/__init__.py
@@ -0,0 +1,45 @@
+"""
+This module contains utility functions and classes that are used throughout the package.
+"""
+
+import datetime
+import logging
+import time
+from functools import wraps
+
+import tqdm
+
+version_no = f"{(datetime.date.today() - datetime.date(1988, 5, 17)).total_seconds() / 31557600:.2f}"
+
+
+class TqdmLoggingHandler(logging.Handler):
+    """
+    Custom logging handler that uses tqdm to display log messages.
+    i.e. `logging.getLogger().addHandler(TqdmLoggingHandler())`
+    """
+
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.tqdm.write(msg)
+            self.flush()
+        except Exception:
+            self.handleError(record)
+
+
+def timed(func):
+    """This decorator prints the execution time for the decorated function."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        logging.info(f"Launching {func.__name__}")
+        result = func(*args, **kwargs)
+        end = time.time()
+        logging.info(f"{func.__name__} ran in {round(end - start, 2)}s")
+        return result
+
+    return wrapper
---
+++ packages/bolster/bolster-0.3.4-py3-none-any.whl/bolster/utils/web.py
@@ -0,0 +1,65 @@
+import io
+import logging
+import zipfile
+from io import BytesIO
+
+import pandas as pd
+import requests
+from waybackpy import WaybackMachineCDXServerAPI, exceptions
+
+from . import version_no
+
+ua = f"@Bolster/{version_no} (+http://bolster.online/)"
+
+session = requests.Session()
+session.headers.update({"User-Agent": ua})
+
+
+def get_last_valid(url):
+    return WaybackMachineCDXServerAPI(url).oldest().archive_url
+
+
+def resilient_get(url, **kwargs):
+    """
+    Attempt a get, but if it fails, try using the wayback machine to get the last valid version and get that.
+    If all else fails, raise a HTTPError from the inner "NoCDXRecordFound" exception
+    """
+
+    try:
+        res = requests.get(url, **kwargs)
+        res.raise_for_status()
+    except requests.HTTPError as outer_err:
+        try:
+            last_valid = get_last_valid(url)
+        except exceptions.NoCDXRecordFound as inner_err:
+            raise outer_err from inner_err
+        res = requests.get(last_valid, **kwargs)
+        res.raise_for_status()
+        logging.warning(f"Failed to get {url} directly, successfully used waybackmachine to get {last_valid}")
+    return res
+
+
+def get_excel_dataframe(file_url, requests_kwargs=None, read_kwargs=None):
+    if requests_kwargs is None:
+        requests_kwargs = {}
+    if read_kwargs is None:
+        read_kwargs = {}
+
+    with requests.get(file_url, **requests_kwargs) as response:
+        response.raise_for_status()
+        data = BytesIO(response.content)
+        df = pd.read_excel(data, **read_kwargs)
+        return df
+
+
+def download_extract_zip(url):
+    """
+    Download a ZIP file and extract its contents in memory
+    yields (filename, file-like object) pairs
+    """
+    with session.get(url, stream=True) as response:
+        response.raise_for_status()
+        with zipfile.ZipFile(io.BytesIO(response.content)) as thezip:
+            for zipinfo in thezip.infolist():
+                with thezip.open(zipinfo) as thefile:
+                    yield zipinfo.filename, thefile
