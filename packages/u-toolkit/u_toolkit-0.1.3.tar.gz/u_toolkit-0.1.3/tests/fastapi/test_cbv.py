ce4dab9fa51907d3fdaa8f02dcd07dab0fd18ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 16:04:23 +0000

    Add package u-toolkit/0.1.3/u_toolkit-0.1.3.tar.gz

---
+++ packages/u-toolkit/u_toolkit-0.1.3.tar.gz/u_toolkit-0.1.3/tests/fastapi/test_cbv.py
@@ -0,0 +1,101 @@
+from typing import Annotated, cast
+
+import sqlalchemy as sa
+from fastapi import Depends, FastAPI, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.testclient import TestClient
+
+from u_toolkit.fastapi.cbv import CBV
+
+
+cbv = CBV()
+
+
+def gen_value():
+    return id(object())
+
+
+dep1_value = gen_value()
+
+
+def dep1():
+    return dep1_value
+
+
+dep2_value = gen_value()
+
+
+def dep2():
+    return dep2_value
+
+
+dep3_value = gen_value()
+
+
+def dep3():
+    yield dep3_value
+
+
+def db():
+    with sa.create_engine(
+        "sqlite+pysqlite:///:memory:", echo=True, future=True
+    ).connect() as conn:
+        yield conn
+
+
+RESULT = "hello world"
+
+
+@cbv
+class R:
+    value = Depends(dep1)
+    value2: Annotated[int, Depends(dep2)]
+    value3 = Depends(dep3)
+    db_dep = cast(sa.Connection, Depends(db))
+
+    def get(self):
+        return self.value, self.value2, self.value3
+
+    @cbv.info(status=status.HTTP_201_CREATED)
+    def post(self):
+        record = self.db_dep.execute(sa.text(f"select '{RESULT}'")).one()
+        return record[0]
+
+    def get__wtf____(self):
+        pass
+
+    @cbv.info(path="/lalala")
+    def get_custom(self): ...
+
+
+@cbv
+class _NoPath:
+    value = Depends(dep1)
+
+    def post(self, data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+        return data.username
+
+
+app = FastAPI()
+
+
+app.include_router(cbv.router)
+client = TestClient(app)
+
+
+def test_cbv():
+    assert client.get("/r").json() == [dep1_value, dep2_value, dep3_value]
+    assert client.get("/_wtf____").is_success
+
+    post_resp = client.post("/r")
+    assert post_resp.status_code == status.HTTP_201_CREATED
+    assert post_resp.json() == RESULT
+
+    value = "example"
+    assert (
+        client.post(
+            "/",
+            data={"username": value, "password": value},
+        ).json()
+        == value
+    )
