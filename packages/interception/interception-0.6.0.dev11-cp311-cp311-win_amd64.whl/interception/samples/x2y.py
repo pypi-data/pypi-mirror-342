07dbe85c37893e47d90db8c114ef4d8f65d7e2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:08 +0000

    Add package interception/0.6.0.dev11/interception-0.6.0.dev11-cp311-cp311-win_amd64.whl

---
+++ packages/interception/interception-0.6.0.dev11-cp311-cp311-win_amd64.whl/interception/samples/x2y.py
@@ -0,0 +1,29 @@
+from interception import ffi, lib
+from interception.utils import raise_process_priority
+
+SCANCODE_X = 0x2D
+SCANCODE_Y = 0x15
+SCANCODE_ESC = 0x01
+
+if __name__ == '__main__':
+    raise_process_priority()
+
+    context = lib.interception_create_context()
+
+    lib.interception_set_filter(context, lib.interception_is_keyboard, lib.INTERCEPTION_FILTER_KEY_DOWN | lib.INTERCEPTION_FILTER_KEY_UP)
+
+    stroke = ffi.new('InterceptionKeyStroke *')
+    while True:
+        device = lib.interception_wait(context)
+        if not lib.interception_receive(context, device, stroke, 1):
+            break
+
+        if stroke.code == SCANCODE_X:
+            stroke.code = SCANCODE_Y
+
+        lib.interception_send(context, device, stroke, 1)
+
+        if stroke.code == SCANCODE_ESC:
+            break
+
+    lib.interception_destroy_context(context)
