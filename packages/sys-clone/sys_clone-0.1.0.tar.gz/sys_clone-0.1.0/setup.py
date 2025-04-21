869dab4d37a0f4b4789f23157f7462e06659be2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:07:05 +0000

    Add package sys-clone/0.1.0/sys_clone-0.1.0.tar.gz

---
+++ packages/sys-clone/sys_clone-0.1.0.tar.gz/sys_clone-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="sys_clone",  # Replace with your package name
+    version="0.1.0",           # Replace with your package version
+   # packages=["your_package"], # Replace with the directory name of your package/module
+)
