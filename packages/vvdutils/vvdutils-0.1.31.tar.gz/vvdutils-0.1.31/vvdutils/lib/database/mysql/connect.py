a4b63abe0db70d0a07dc791aebb6541149fd5216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:04:33 +0000

    Add package vvdutils/0.1.31/vvdutils-0.1.31.tar.gz

---
+++ packages/vvdutils/vvdutils-0.1.31.tar.gz/vvdutils-0.1.31/vvdutils/lib/database/mysql/connect.py
@@ -0,0 +1,216 @@
+import time
+from ...utils import lazy_import
+from loguru import logger
+
+
+class MysqlConnection:
+    # import mysql.connector
+    MySqlConnector = None
+
+    def __init__(self, username, password, host, port, database):
+        if self.MySqlConnector is None:
+            type(self).MySqlConnector = lazy_import('mysql.connector')
+
+        self.host = host
+        self.port = port
+        self.database = database
+        self.username = username
+        self.password = password
+
+        logger.debug(f" @@ Connecting to MySQL database {self.database}, host: {self.host}, port: {self.port}, username: {self.username}.")
+
+        self.db = self.make_connection()
+        if self.db is None:
+            logger.error(f" !! Error connecting to MySQL database: {self.database}, host: {self.host}, port: {self.port}")
+            raise Exception("Error connecting to MySQL database.")
+        else:
+            logger.debug(f" @@ Connected to MySQL database {self.database} successfully, host: {self.host}, port: {self.port}")
+        self.default_cursor = self.get_cursor(in_dict=True)
+
+    def make_connection(self):
+        try_time = 3
+        db_obj = self.connect()
+        while (db_obj is None or db_obj.is_closed()) and try_time > 0:
+            logger.debug(f" @@ Reconnecting to MySQL database {self.database}, host: {self.host}, port: {self.port}, username: {self.username}.")
+            db_obj = self.connect()
+            time.sleep(1)
+            try_time -= 1
+        return db_obj
+
+    def start_transaction(self):
+        if self.db.in_transaction:
+            self.db.rollback()
+        self.db.start_transaction()
+
+    def commit(self):
+        self.db.commit()
+    
+    def rollback(self):
+        self.db.rollback()
+
+    def connect(self):
+        db = None
+
+        try:
+            db = self.MySqlConnector.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.database, autocommit=True, allow_local_infile=True)
+        except Exception as e:
+            logger.exception(f" !! Error connecting to MySQL database. {e}")
+        
+        time.sleep(2)
+        if db is None:
+            try:
+                db = self.MySqlConnector.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.database, autocommit=True, allow_local_infile=True)
+            except Exception as e:
+                logger.exception(f" !! Error connecting to MySQL database. {e}")
+
+        return db
+
+    def close(self):
+        try:
+            self.db.close()
+        except:
+            logger.exception(" !! Error closing MySQL database.")
+
+    def __del__(self):
+        self.close()
+
+    def mysql_connection_check(self):
+        try:
+            if self.db.is_connected():
+                return
+        except:
+            logger.error(" !! connection check failed, try to reconnect mysql.")
+            self.db = self.make_connection()
+            if self.db.is_connected():
+                self.default_cursor = self.get_cursor(in_dict=True)
+                return
+            else:
+                logger.error(" !! Error connecting to MySQL database under <mysql_connection_check>.")
+                raise RuntimeError(" !! Error connecting to MySQL database.")
+
+    def insert_item(self, data_dict, table, cursor=None):
+        self.mysql_connection_check()
+        if cursor is None:
+            cursor = self.default_cursor
+
+        key_list = list(data_dict.keys())
+        for index, key in enumerate(key_list):
+            key_list[index] = f'`{key}`'
+
+        query = f"INSERT INTO {table} ({', '.join(key_list)}) VALUES ({', '.join(['%s'] * len(data_dict))})"
+        cursor.execute(query, list(data_dict.values()))
+
+    def update_item(self, set_dict, conditions, table, logical="AND", cursor=None):
+        self.mysql_connection_check()
+        if cursor is None:
+            cursor = self.default_cursor
+
+        try:
+            self.start_transaction()
+            key_str_list = list()
+            value_list = list()
+            for k, v in set_dict.items():
+                key_str_list.append(f"{k} = %s")
+                value_list.append(v)
+
+            set_str = ", ".join(key_str_list)
+            
+            query = ""
+            
+            if conditions:
+                query += " WHERE "
+
+                condition_str_list = list()
+                
+                for condition in conditions:
+                    condition_str_list.append(f"{condition[0]} {condition[2]} %s")
+                    value_list.append(str(condition[1]))
+                query += (' ' + logical + ' ').join(condition_str_list)
+
+            update_query = f"UPDATE {table} SET {set_str} {query}"
+            # 执行更新操作
+            cursor.execute(update_query, value_list)
+            self.commit()
+            return True
+        except Exception as e:
+            self.rollback()
+            logger.exception(f" !! Error updating item: {e}")
+            return False
+
+    def select_item(self, conditions, table, logical="AND", cursor=None, for_update=False):
+        """
+        :param conditions: list [[key1, value1, operator1], [key2, value2, operator2], ...]
+        :param logical: str
+        :return: list
+        """
+        self.mysql_connection_check()
+        assert logical in ["AND", "OR"], " !! logical must be 'AND' or 'OR'"
+
+        if cursor is None:
+            cursor = self.default_cursor
+
+        condition_value_list = list()
+        if conditions:
+            query = f"SELECT * FROM {table} WHERE "
+
+            condition_str_list = list()
+            
+            for condition in conditions:
+                if condition[2] == 'in':
+                    temp_str = "(" + ', '.join(['%s'] * len(condition[1])) + ")"
+                    condition_str_list.append(f"{condition[0]} {condition[2]} {temp_str}")
+                    condition_value_list.extend(condition[1])
+                else:
+                    condition_str_list.append(f"{condition[0]} {condition[2]} %s")
+                    condition_value_list.append(str(condition[1]))
+            query += (' ' + logical + ' ').join(condition_str_list)
+        else:
+            query = f"SELECT * FROM {table}"
+
+
+        if for_update:
+            query += " FOR UPDATE"
+
+        cursor.execute(query, condition_value_list)
+        result = cursor.fetchall()
+        return result
+    
+    def item_exists(self, conditions, table, logical="AND", cursor=None):
+        result = self.select_item(conditions, table, logical, cursor)
+        return len(result) > 0
+
+    def is_not_null(self, key, table, cursor=None):
+        self.mysql_connection_check()
+        if cursor is None:
+            cursor = self.default_cursor
+
+        query = f"SELECT * FROM {table} WHERE {key} IS NOT NULL"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+    def delete_item(self, conditions, table, logical='AND', cursor=None):
+        self.mysql_connection_check()
+        if cursor is None:
+            cursor = self.default_cursor
+
+        query = ""
+        
+        if conditions:
+            query += " WHERE "
+
+            condition_str_list = list()
+            value_list = list()
+            
+            for condition in conditions:
+                condition_str_list.append(f"{condition[0]} {condition[2]} %s")
+                value_list.append(str(condition[1]))
+            query += (' ' + logical + ' ').join(condition_str_list)
+
+        del_query = f"DELETE FROM {table} {query}"
+        cursor.execute(del_query, value_list)
+
+    
+    def get_cursor(self, in_dict=True):
+        self.mysql_connection_check()
+        return self.db.cursor(dictionary=in_dict)
