bff506988cbee50916aed741955a3327ae69d7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:04:32 +0000

    Add package AgroEcoMetrics/0.0.1/AgroEcoMetrics-0.0.1.tar.gz

---
+++ packages/AgroEcoMetrics/AgroEcoMetrics-0.0.1.tar.gz/AgroEcoMetrics-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='AgroEcoMetrics',
+version='0.0.1',
+author='Scarlett Olson',
+author_email='nwolson@wisc.edu',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
