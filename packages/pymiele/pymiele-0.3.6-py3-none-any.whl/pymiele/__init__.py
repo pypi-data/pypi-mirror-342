85e0a8a50fc3be12a5ac2ce62d2ffbce3fbadf03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:20:48 +0000

    Add package pymiele/0.3.6/pymiele-0.3.6-py3-none-any.whl

---
+++ packages/pymiele/pymiele-0.3.6-py3-none-any.whl/pymiele/__init__.py
@@ -0,0 +1,6 @@
+"""Library for Miele integration with Home Assistant."""
+
+from .const import *  # noqa: F403
+from .const import VERSION as __version__  # noqa: F401
+from .model import *  # noqa: F403
+from .pymiele import *  # noqa: F403
