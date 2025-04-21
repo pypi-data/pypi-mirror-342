351a735b5efbfb9e3ac345e566a3ca8368ed0dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 14:05:06 +0000

    Add package qurrium/0.12.0/qurrium-0.12.0-cp39-abi3-win_amd64.whl

---
+++ packages/qurrium/qurrium-0.12.0-cp39-abi3-win_amd64.whl/qurry/qurrium/experiment/__init__.py
@@ -0,0 +1,5 @@
+"""Experiment Instance and Analysis Container (:mod:`qurry.qurrium.experiment`)"""
+
+from .experiment import ExperimentPrototype
+from .arguments import Commonparams, ArgumentsPrototype
+from .analyses import AnalysesContainer
