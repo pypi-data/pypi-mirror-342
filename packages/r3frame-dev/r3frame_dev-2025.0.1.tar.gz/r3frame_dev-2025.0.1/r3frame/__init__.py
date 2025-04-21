1fe54b4b42023c9d9cd2961346a1ad02e15b5220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:31 +0000

    Add package r3frame-dev/2025.0.1/r3frame_dev-2025.0.1.tar.gz

---
+++ packages/r3frame-dev/r3frame_dev-2025.0.1.tar.gz/r3frame_dev-2025.0.1/r3frame/__init__.py
@@ -0,0 +1,11 @@
+import r3frame.app as app
+import r3frame.app.ui as ui
+import r3frame.game as game
+import r3frame.version as version
+from r3frame.util import sine_wave_value, damp_exp, damp_lin, point_inside, dist_to, angle_to
+
+from r3frame.globs import os
+if "R3FRAME_NO_PROMT" not in os.environ:
+    print(
+        f"r3frame {version.R3FRAME_YEAR}.{version.R3FRAME_MINOR}.{version.R3FRAME_PATCH} | [ random quote here ]"
+    )
