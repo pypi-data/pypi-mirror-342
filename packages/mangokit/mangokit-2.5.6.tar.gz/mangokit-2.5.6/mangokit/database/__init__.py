b3dc2fa8d3b4ff50a55223b52453309966843070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:32 +0000

    Add package mangokit/2.5.6/mangokit-2.5.6.tar.gz

---
+++ packages/mangokit/mangokit-2.5.6.tar.gz/mangokit-2.5.6/mangokit/database/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: # @Time   : 2023-04-16 8:18
+# @Author : 毛鹏
+from mangokit.database._mysql_connect import MysqlConnect
+from mangokit.database._sqlite_connect import SQLiteConnect
+
+__all__ = [
+    'MysqlConnect',
+    'SQLiteConnect'
+]
