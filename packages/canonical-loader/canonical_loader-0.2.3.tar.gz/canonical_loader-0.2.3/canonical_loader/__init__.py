8a8e7821dc6909b19005adad2896a9c9ba6954a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:16 +0000

    Add package canonical-loader/0.2.3/canonical_loader-0.2.3.tar.gz

---
+++ packages/canonical-loader/canonical_loader-0.2.3.tar.gz/canonical_loader-0.2.3/canonical_loader/__init__.py
@@ -0,0 +1,4 @@
+from .base_canonical_loader import *
+from .canonical_loader import *
+from .s3_canonical_loader import *
+from .simple_loder import *
---
+++ packages/canonical-loader/canonical_loader-0.2.3.tar.gz/canonical_loader-0.2.3/canonical_loader/general_utils/canonic_date_utils.py
@@ -0,0 +1,18 @@
+
+def extract_save_date_in_file_name(file_name):
+    return file_name.split('-save')[-1].split('.')[0]
+
+def extract_date_ref_in_file_name(file_name):
+    return file_name.split('-at')[-1].split('-')[0]
+
+def extract_start_date_in_file_name(file_name):
+    return file_name.split('-from')[-1].split('-')[0]
+
+def extract_end_date_in_file_name(file_name):
+    return file_name.split('-to')[-1].split('-')[0]
+    
+def extract_initial_date_in_file_name(file_name):
+    return file_name.split('-between')[-1].split('-')[0]
+
+def extract_final_date_in_file_name(file_name):
+    return file_name.split('-and')[-1].split('-')[0]
