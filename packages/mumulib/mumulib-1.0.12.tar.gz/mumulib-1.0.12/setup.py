e060b9c9ccc615e5149dbbbe73238a91a1bff541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:32 +0000

    Add package mumulib/1.0.12/mumulib-1.0.12.tar.gz

---
+++ packages/mumulib/mumulib-1.0.12.tar.gz/mumulib-1.0.12/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mumulib',
+      version='1.0.12',
+      description='There is no library, squared',
+      author='Donovan Preston',
+      author_email='46048+fzzzy@users.noreply.github.com',
+      url='https://rewnd.co/mumulib/',
+      packages=['mumulib'],
+      install_requires=[
+          'lxml',
+      ]
+     )
