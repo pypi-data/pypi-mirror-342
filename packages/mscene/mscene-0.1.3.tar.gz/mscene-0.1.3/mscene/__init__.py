194e5d397dde3c67a4673892b426f1da7e4e4d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:21:28 +0000

    Add package mscene/0.1.3/mscene-0.1.3.tar.gz

---
+++ packages/mscene/mscene-0.1.3.tar.gz/mscene-0.1.3/mscene/__init__.py
@@ -0,0 +1,33 @@
+"""
+Mscene is a Python library for creating science animations with Manim in Google Colab.
+
+Home: https://mscene.curiouswalk.com
+Repo: https://github.com/curiouswalk/mscene
+"""
+
+try:
+    from IPython import get_ipython
+
+except ImportError:
+    pass
+
+else:
+    ipy = get_ipython()
+
+    if ipy is not None:
+
+        from .__main__ import main
+
+        ipy.register_magic_function(main, "line", "mscene")
+
+
+__about__ = """Name: mscene
+Version: 0.1.3
+Summary: Science animation with Manim in Colab
+Homepage: https://mscene.curiouswalk.com
+Author: CuriousWalk
+License: MIT"""
+
+__version__ = "0.1.3"
+__author__ = "CuriousWalk"
+__license__ = "MIT"
