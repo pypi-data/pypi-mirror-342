84bc7679fec5e27f1e9c0457713a6e3153fd7d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:24 +0000

    Add package maleo-metadata/0.0.4/maleo_metadata-0.0.4-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.0.4-py3-none-any.whl/maleo_metadata/models/transfers/parameters/general/system_role.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from pydantic import Field
+from maleo_foundation.models.transfers.parameters.general import BaseGeneralParametersTransfers
+from maleo_foundation.types import BaseTypes
+from maleo_metadata.enums.system_role import MaleoMetadataSystemRoleEnums
+
+class MaleoMetadataSystemRoleGeneralParametersTransfers:
+    class GetSingleQuery(BaseGeneralParametersTransfers.GetSingleQuery): pass
+
+    class GetSingle(BaseGeneralParametersTransfers.GetSingle):
+        identifier:MaleoMetadataSystemRoleEnums.IdentifierType = Field(..., description="Identifier")
+        value:BaseTypes.IdentifierValue = Field(..., description="Value")
