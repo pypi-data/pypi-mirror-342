b25150c23adc999503ebb947ef924cb0223f8b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:18 +0000

    Add package nonebot-plugin-revoke/0.1.2/nonebot_plugin_revoke-0.1.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-revoke/nonebot_plugin_revoke-0.1.2-py3-none-any.whl/nonebot_plugin_revoke/__init__.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from nonebot import on_command, logger
+from nonebot.adapters.onebot.v11 import ActionFailed, Bot
+from nonebot.internal.params import Depends
+from nonebot.permission import SUPERUSER
+from nonebot.plugin import PluginMetadata
+from nonebot.matcher import Matcher
+
+from .utils import reply_message_id
+
+__plugin_meta__ = PluginMetadata(
+    name="主动消息撤回",
+    description="让机器人撤回自己发出的消息",
+    usage="向希望撤回的消息回复 /revoke",
+    type="application",
+    homepage="https://github.com/bot-ssttkkl/nonebot-plugin-revoke",
+    supported_adapters={"~onebot.v11"}
+)
+
+revoke_matcher = on_command("revoke", aliases={"撤回"}, permission=SUPERUSER)
+
+
+@revoke_matcher.handle()
+async def _(
+        bot: Bot,
+        matcher: Matcher,
+        reply_msg_id: Optional[int] = Depends(reply_message_id)
+):
+    
+    matcher.stop_propagation()  # ✨ 阻止其他 matcher（包括其他插件）继续处理
+
+    try:
+        if reply_msg_id is not None:
+            await bot.delete_msg(message_id=reply_msg_id)
+
+    except ActionFailed as e:
+        logger.exception(e)
