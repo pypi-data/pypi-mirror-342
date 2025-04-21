49125e5f50fe5453b90166a88770732a7cff0765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 04:05:20 +0000

    Add package django-requests-api/0.5.2/django_requests_api-0.5.2.tar.gz

---
+++ packages/django-requests-api/django_requests_api-0.5.2.tar.gz/django_requests_api-0.5.2/tests/test_requests_api.py
@@ -0,0 +1,32 @@
+"""Unit Tests for the module"""
+
+import logging
+
+from django.test import TestCase
+
+from requests_api.requests_api import RequestsApi
+
+LOGGER = logging.getLogger(name="django-requests-api")
+
+
+class ErrorsTestCase(TestCase):
+    """Test Case for django-requests-api"""
+
+    def setUp(self):
+        """Set up common assets for tests"""
+        LOGGER.debug("Tests setUp")
+
+    def tearDown(self):
+        """Remove Test Data"""
+        LOGGER.debug("Tests tearDown")
+
+    def test_init_client(self):
+        """Test the initialization of client"""
+        LOGGER.debug("Test the initialization of client")
+        api_client = RequestsApi("https://fakeapiurl.com")
+        self.assertIsInstance(api_client, RequestsApi)
+
+    def test_init_client_without_parameters(self):
+        """Test the initialization of client without parameters"""
+        LOGGER.debug("Test the initialization of client without parameters")
+        self.assertRaises(TypeError, lambda: RequestsApi())
