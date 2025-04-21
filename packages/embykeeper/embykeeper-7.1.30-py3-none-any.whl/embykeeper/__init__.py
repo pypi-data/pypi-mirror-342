b30d02f2d75b6687204dff7e92d4aec0aa9cb7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 03:20:56 +0000

    Add package embykeeper/7.1.30/embykeeper-7.1.30-py3-none-any.whl

---
+++ packages/embykeeper/embykeeper-7.1.30-py3-none-any.whl/embykeeper/__init__.py
@@ -0,0 +1,5 @@
+__author__ = {
+    "jackzzs": "jackzzs@outlook.com",
+}
+__version__ = "7.1.30"
+__url__ = "https://emby-keeper.github.io"
---
+++ packages/embykeeper/embykeeper-7.1.30-py3-none-any.whl/embykeeper/telegram/checkiner/datasgk.py
@@ -0,0 +1,8 @@
+from . import BotCheckin
+
+
+class DataSGKCheckin(BotCheckin):
+    name = "数据社工库"
+    bot_username = "DataSGK_bot"
+    bot_checkin_cmd = "/signin"
+    bot_checked_keywords = "请勿重复签到"
