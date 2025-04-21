71e75bb4d1538802ef769fa93abf2d4f7b49750e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:18 +0000

    Add package colormap-complex/0.1.1/colormap_complex-0.1.1.tar.gz

---
+++ packages/colormap-complex/colormap_complex-0.1.1.tar.gz/colormap_complex-0.1.1/src/colormap_complex/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.1"
+
+from ._main import ALL_COLORMAPS, ColormapType, colormap, interpnd
+
+__all__ = [
+    "ALL_COLORMAPS",
+    "ColormapType",
+    "colormap",
+    "interpnd",
+]
