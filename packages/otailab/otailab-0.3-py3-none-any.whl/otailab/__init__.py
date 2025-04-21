baceb87b2360b9354fb58b312176d86066839e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 15:04:28 +0000

    Add package otailab/0.3/otailab-0.3-py3-none-any.whl

---
+++ packages/otailab/otailab-0.3-py3-none-any.whl/otailab/__init__.py
@@ -0,0 +1,19 @@
+from .main import (
+    cpm,
+    run_cpm_analysis,
+    solve_assignment_problem,
+    cpm2,
+    calculate_pert,
+    visualize_pert,
+    solve_linear_programming,
+    run_linear_programming,
+    solve_dp_problems,
+    stochastic_gradient_descent,
+    minimize_quadratic_function,
+    fit_model,
+    solve_and_plot_lp,
+    minimize_kkt,
+    maximize_kkt
+)
+
+__version__ = '0.3'
