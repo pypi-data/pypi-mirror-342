49cb6c94a116accaa06e7f317db06d641b9fab48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-21 16:04:37 +0000

    Add package packaging-demo-py-to-prd-test/0.5.0/packaging_demo_py_to_prd_test-0.5.0.tar.gz

---
+++ packages/packaging-demo-py-to-prd-test/packaging_demo_py_to_prd_test-0.5.0.tar.gz/packaging_demo_py_to_prd_test-0.5.0/packaging_demo/my_file.py
@@ -0,0 +1,8 @@
+from my_folder import A
+from my_folder.my_nested_file import CONSTANT as CONSTANT2
+from my_other_file import CONSTANT
+
+print(CONSTANT)
+print(CONSTANT2)
+print(A)
+print('the wheels on the bus go round and round')
