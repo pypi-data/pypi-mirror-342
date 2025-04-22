f4ba3b168a7adfd6fc90665573af6f7f3e1c8769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 14:05:08 +0000

    Add package bayesflow/2.0.0/bayesflow-2.0.0.tar.gz

---
+++ packages/bayesflow/bayesflow-2.0.0.tar.gz/bayesflow-2.0.0/bayesflow/utils/dispatch/__init__.py
@@ -0,0 +1,5 @@
+from .find_cost import find_cost
+from .find_network import find_network
+from .find_permutation import find_permutation
+from .find_pooling import find_pooling
+from .find_recurrent_net import find_recurrent_net
