a8fbda3bbbd228570c09e412add8c64cd1077a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 16:05:23 +0000

    Add package aimodelshare/0.1.39/aimodelshare-0.1.39.tar.gz

---
+++ packages/aimodelshare/aimodelshare-0.1.39.tar.gz/aimodelshare-0.1.39/setup.py
@@ -0,0 +1,39 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='aimodelshare', #TODO:update
+    version='0.1.39',        #TODO:update
+    author="Michael Parrott",
+    author_email="mikedparrott@modelshare.org",
+    description="Deploy locally saved machine learning models to a live rest API and web-dashboard.  Share it with the world via modelshare.org",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.modelshare.org",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "boto3",
+        "onnx",
+        "onnxmltools",
+        "onnxruntime",
+        "skl2onnx",
+        "tf2onnx",
+        "wget",
+        "shortuuid",
+        "Pympler",
+        "scikeras"
+    ]
+
+
+      ,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    include_package_data=True)
+  
