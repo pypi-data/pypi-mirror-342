658136b583ca39e99a48488b8b7d82b9c03ba2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:05:08 +0000

    Add package abilian-devtools/0.7.7/abilian_devtools-0.7.7.tar.gz

---
+++ packages/abilian-devtools/abilian_devtools-0.7.7.tar.gz/abilian_devtools-0.7.7/src/abilian_devtools/commands/format.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2023 Abilian SAS <https://abilian.com/>
+#
+# SPDX-License-Identifier: MIT
+
+
+from cleez.colors import bold
+from cleez.command import Argument, Command
+
+from ..shell import run
+from ._util import get_targets
+
+
+class FormatCommand(Command):
+    """Format code in specified files or directories."""
+
+    name = "format"
+
+    arguments = [
+        Argument("args", nargs="*", help="Files or directories to format"),
+    ]
+
+    def run(self, args: list[str] | None = None):
+        print(bold("Formatting code..."))
+
+        args = get_targets(args)
+        args_str = " ".join(args)
+
+        run(f"black {args_str}")
+        run(f"isort {args_str}")
