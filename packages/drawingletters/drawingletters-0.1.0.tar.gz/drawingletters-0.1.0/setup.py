da4f97f8318a6761a3133d592445912a9295e005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:19:28 +0000

    Add package drawingletters/0.1.0/drawingletters-0.1.0.tar.gz

---
+++ packages/drawingletters/drawingletters-0.1.0.tar.gz/drawingletters-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drawingletters',
+    version='0.1.0',
+    description='A cute fish drawing module 🐟',
+    author='너의이름',
+    author_email='youremail@example.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
