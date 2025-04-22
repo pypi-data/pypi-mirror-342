f824dc2a7e93b29faa60816715f8dbb61b910273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:05:47 +0000

    Add package meshadmin/0.1.6/meshadmin-0.1.6-py3-none-any.whl

---
+++ packages/meshadmin/meshadmin-0.1.6-py3-none-any.whl/meshadmin/cli/commands/__init__.py
@@ -0,0 +1,15 @@
+from meshadmin.cli.commands.context import context_app
+from meshadmin.cli.commands.host import host_app
+from meshadmin.cli.commands.nebula import nebula_app
+from meshadmin.cli.commands.network import network_app
+from meshadmin.cli.commands.service import service_app
+from meshadmin.cli.commands.template import template_app
+
+__all__ = [
+    "nebula_app",
+    "service_app",
+    "network_app",
+    "template_app",
+    "host_app",
+    "context_app",
+]
