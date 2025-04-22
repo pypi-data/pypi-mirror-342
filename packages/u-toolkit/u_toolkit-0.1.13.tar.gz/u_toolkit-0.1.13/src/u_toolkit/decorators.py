352a762918532b598be797d40b3cf47b0f513718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 16:05:13 +0000

    Add package u-toolkit/0.1.13/u_toolkit-0.1.13.tar.gz

---
+++ packages/u-toolkit/u_toolkit-0.1.13.tar.gz/u_toolkit-0.1.13/src/u_toolkit/decorators.py
@@ -0,0 +1,51 @@
+import inspect
+from collections.abc import Callable
+from functools import wraps
+from typing import Generic, NamedTuple, TypeVar
+
+
+_FnT = TypeVar("_FnT", bound=Callable)
+
+_T = TypeVar("_T")
+
+
+class DefineMethodParams(NamedTuple, Generic[_T, _FnT]):
+    method_class: type[_T]
+    method_name: str
+    method: _FnT
+
+
+class DefineMethodDecorator(Generic[_T, _FnT]):
+    def __init__(self, fn: _FnT):
+        self.fn = fn
+        self.name = fn.__name__
+
+    def register_method(self, params: DefineMethodParams[_T, _FnT]): ...
+
+    def __set_name__(self, owner_class: type, name: str):
+        self.register_method(DefineMethodParams(owner_class, name, self.fn))
+
+    def __get__(self, instance: _T, owner_class: type[_T]):
+        if inspect.iscoroutinefunction(self.fn):
+
+            @wraps(self.fn)
+            async def awrapper(*args, **kwargs):
+                return await self.fn(instance, *args, **kwargs)
+
+            return awrapper
+
+        @wraps(self.fn)
+        def wrapper(*args, **kwargs):
+            return self.fn(instance, *args, **kwargs)
+
+        return wrapper
+
+
+def define_method_handler(
+    handle: Callable[[DefineMethodParams[_T, _FnT]], None],
+):
+    class Decorator(DefineMethodDecorator):
+        def register_method(self, params: DefineMethodParams):
+            handle(params)
+
+    return Decorator
