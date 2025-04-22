300e1646ad0b0dcd9244622620877788842936df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:19:07 +0000

    Add package rebotics-sdk/0.31.0/rebotics_sdk-0.31.0.tar.gz

---
+++ packages/rebotics-sdk/rebotics_sdk-0.31.0.tar.gz/rebotics_sdk-0.31.0/tests/test_providers/test_retailer.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from rebotics_sdk.providers.base import ProviderRequestProxy
+from rebotics_sdk.providers.retailer import RetailerProvider
+
+
+class RetailerProviderTestCase(TestCase):
+    def setUp(self) -> None:
+        self.provider = RetailerProvider('https://example.com')
+
+    @patch.object(ProviderRequestProxy, 'post')
+    def test_export_to_cvat(self, mock_func: MagicMock):
+        scan_ids = [4, 3, 4, 2, 1, 2, 1, 3]
+        image_quality = 70
+        segment_size = 30
+        workspace = 'Workspace'
+        self.provider.export_to_cvat(scan_ids, image_quality, segment_size, workspace)
+        mock_func.assert_called_once_with(json={
+            'scans': [1, 2, 3, 4],
+            'image_quality': image_quality,
+            'segment_size': segment_size,
+            'workspace': workspace,
+        })
