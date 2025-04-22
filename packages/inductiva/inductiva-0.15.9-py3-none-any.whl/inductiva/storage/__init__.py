376ae390de874ca91ed3e8d5a096a8467ad18af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:19:27 +0000

    Add package inductiva/0.15.9/inductiva-0.15.9-py3-none-any.whl

---
+++ packages/inductiva/inductiva-0.15.9-py3-none-any.whl/inductiva/storage/__init__.py
@@ -0,0 +1,16 @@
+"""Storage related functions."""
+from .storage import (
+    ExportDestination,
+    StorageOperation,
+    download,
+    export,
+    get_signed_urls,
+    get_space_used,
+    get_zip_contents,
+    listdir,
+    remove_workspace,
+    upload,
+    upload_from_url,
+    ZipArchiveInfo,
+    ZipFileInfo,
+)
