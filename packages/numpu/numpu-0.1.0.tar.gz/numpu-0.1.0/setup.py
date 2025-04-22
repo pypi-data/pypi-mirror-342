41473345a373508c87da651fab32d0dbaa390ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:06:05 +0000

    Add package numpu/0.1.0/numpu-0.1.0.tar.gz

---
+++ packages/numpu/numpu-0.1.0.tar.gz/numpu-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='numpu',                # the package name
+    version='0.1.0',
+    description='Simple wrapper for g4f chat client',
+    author='sidh2690',
+    author_email='you@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'g4f>=0.1.0',
+    ],
+    python_requires='>=3.7',
+)
