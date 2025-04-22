63c4aa7962432a4d028f7cbf7548e52e3d4b95b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 14:04:51 +0000

    Add package archgw/0.2.8/archgw-0.2.8.tar.gz

---
+++ packages/archgw/archgw-0.2.8.tar.gz/archgw-0.2.8/cli/consts.py
@@ -0,0 +1,14 @@
+import os
+
+
+KATANEMO_DOCKERHUB_REPO = "katanemo/archgw"
+KATANEMO_LOCAL_MODEL_LIST = [
+    "katanemo/Arch-Guard",
+]
+SERVICE_NAME_ARCHGW = "archgw"
+SERVICE_NAME_MODEL_SERVER = "model_server"
+SERVICE_ALL = "all"
+MODEL_SERVER_LOG_FILE = "~/archgw_logs/modelserver.log"
+ACCESS_LOG_FILES = "~/archgw_logs/access*"
+ARCHGW_DOCKER_NAME = "archgw"
+ARCHGW_DOCKER_IMAGE = os.getenv("ARCHGW_DOCKER_IMAGE", "katanemo/archgw:0.2.8")
