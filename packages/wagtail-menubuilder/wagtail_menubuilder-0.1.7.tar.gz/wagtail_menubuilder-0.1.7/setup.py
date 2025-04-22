189eb505784bfc71e5a819639fe9b423378f29bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:04:44 +0000

    Add package wagtail-menubuilder/0.1.7/wagtail_menubuilder-0.1.7.tar.gz

---
+++ packages/wagtail-menubuilder/wagtail_menubuilder-0.1.7.tar.gz/wagtail_menubuilder-0.1.7/setup.py
@@ -0,0 +1,32 @@
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+from setuptools import setup, find_packages
+
+setup(
+    name="wagtail-menubuilder",
+    version="0.1.7",
+    packages=find_packages(include=["wagtail_menubuilder", "wagtail_menubuilder.*"]),
+    include_package_data=True,
+    install_requires=[
+        "wagtail>=6.0",
+        "Django>=4.2",
+    ],
+    author="Bill Fleming",
+    description="A flexible menu builder for Wagtail CMS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TechBill/wagtail-menubuilder",
+    license="MIT",
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Wagtail",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.8",
+    project_urls={
+        "Documentation": "https://github.com/TechBill/wagtail-menubuilder",
+        "Source": "https://github.com/TechBill/wagtail-menubuilder",
+        "Tracker": "https://github.com/TechBill/wagtail-menubuilder/issues",
+    },
+)
