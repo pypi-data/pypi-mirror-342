417ac950d04f09e39d29ddb02966fba10b2ee157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:04:53 +0000

    Add package EventManagerPy/0.2.3/eventmanagerpy-0.2.3.tar.gz

---
+++ packages/EventManagerPy/eventmanagerpy-0.2.3.tar.gz/eventmanagerpy-0.2.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="EventManagerPy",
+    version="0.2.3",
+    author="Botan Celik",
+    author_email="botan.celik@icloud.com",
+    description="EventManager is a logging module designed to be used in a multi-threaded environment",
+    long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheBloodyAmateur/EventManagerPython",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
