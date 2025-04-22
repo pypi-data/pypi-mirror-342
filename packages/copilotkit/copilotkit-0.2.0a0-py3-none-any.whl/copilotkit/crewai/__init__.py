3751bef20bea776ac218381f935f3df3d811a656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:18 +0000

    Add package copilotkit/0.2.0a0/copilotkit-0.2.0a0-py3-none-any.whl

---
+++ packages/copilotkit/copilotkit-0.2.0a0-py3-none-any.whl/copilotkit/crewai/__init__.py
@@ -0,0 +1,33 @@
+"""
+CrewAI
+"""
+from .crewai_agent import CrewAIAgent
+from .crewai_sdk import (
+    CopilotKitProperties,
+    CopilotKitState,
+    copilotkit_emit_state,
+    copilotkit_emit_message,
+    copilotkit_emit_tool_call,
+    copilotkit_stream,
+    copilotkit_exit,
+    copilotkit_predict_state,
+)
+from .utils import (
+    create_copilotkit_tool_handlers, 
+    check_for_intercepted_actions,
+    COPILOTKIT_ACTION_INTERCEPTED_MARKER
+)
+__all__ = [
+    "CrewAIAgent",
+    "CopilotKitProperties",
+    "CopilotKitState",
+    "copilotkit_emit_state",
+    "copilotkit_emit_message",
+    "copilotkit_emit_tool_call",
+    "copilotkit_stream",
+    "copilotkit_exit",
+    "copilotkit_predict_state",
+    "create_copilotkit_tool_handlers",
+    "check_for_intercepted_actions",
+    "COPILOTKIT_ACTION_INTERCEPTED_MARKER",
+]
