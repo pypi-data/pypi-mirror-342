714331fbc77e4ced658e48443a9c1903a6fe7831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:05:11 +0000

    Add package maleo-foundation/0.0.47/maleo_foundation-0.0.47.tar.gz

---
+++ packages/maleo-foundation/maleo_foundation-0.0.47.tar.gz/maleo_foundation-0.0.47/maleo_foundation/models/transfers/parameters/general.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from maleo_foundation.enums import BaseEnums
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.types import BaseTypes
+
+class BaseGeneralParametersTransfers:
+    class GetSingleQuery(BaseGeneralSchemas.Statuses): pass
+
+    class BaseGetSingle(BaseModel):
+        identifier:BaseEnums.IdentifierTypes = Field(..., description="Identifier")
+        value:BaseTypes.IdentifierValue = Field(..., description="Value")
+
+    class GetSingle(BaseGeneralSchemas.Statuses, BaseGetSingle): pass
