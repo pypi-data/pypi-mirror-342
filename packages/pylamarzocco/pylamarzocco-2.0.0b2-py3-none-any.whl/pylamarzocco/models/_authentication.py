ec48de2a46dfb5ebf3aa4d98e2b983965f94cb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:23 +0000

    Add package pylamarzocco/2.0.0b2/pylamarzocco-2.0.0b2-py3-none-any.whl

---
+++ packages/pylamarzocco/pylamarzocco-2.0.0b2-py3-none-any.whl/pylamarzocco/models/_authentication.py
@@ -0,0 +1,36 @@
+"""Models for authentication."""
+
+from dataclasses import dataclass, field
+from time import time
+
+from mashumaro import field_options
+from mashumaro.config import BaseConfig
+from mashumaro.mixins.json import DataClassJSONMixin
+
+from pylamarzocco.const import TOKEN_EXPIRATION
+
+
+@dataclass(kw_only=True, frozen=True)
+class AccessToken(DataClassJSONMixin):
+    """Base for access token response model."""
+
+    access_token: str = field(metadata=field_options(alias="accessToken"))
+    refresh_token: str = field(metadata=field_options(alias="refreshToken"))
+    expires_at: float = field(default=time() + TOKEN_EXPIRATION)
+
+@dataclass(kw_only=True, frozen=True)
+class SigninTokenRequest(DataClassJSONMixin):
+    """Token request model."""
+
+    username: str
+    password: str
+
+@dataclass(kw_only=True, frozen=True)
+class RefreshTokenRequest(DataClassJSONMixin):
+    """Token request model."""
+
+    username: str
+    refresh_token: str = field(metadata=field_options(alias="refreshToken"))
+    class Config(BaseConfig):
+        """Config for Mashumaro serialization."""
+        serialize_by_alias = True
