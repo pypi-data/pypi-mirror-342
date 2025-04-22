9d5ba9628861a034ed6d8f13e89f951cd89d319f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:29 +0000

    Add package morelists/0.1.6/morelists-0.1.6.tar.gz

---
+++ packages/morelists/morelists-0.1.6.tar.gz/morelists-0.1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="morelists", 
+    version="0.1.6",
+    packages=find_packages(),  
+    install_requires=[], 
+    description="A small and easy list you can add together like a math equation and has a expiration date for items. Mostly useful for games with stats from multiple sources. More lists will come soon.",
+    author="Emanuel Odén Hesselroth",
+    author_email="emanuelodenhesselroth@gmail.com",
+    url="https://github.com/EmanuelNorsk/enlist", 
+)
