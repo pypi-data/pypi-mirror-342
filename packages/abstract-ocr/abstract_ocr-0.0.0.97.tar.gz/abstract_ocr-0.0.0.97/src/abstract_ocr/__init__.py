d8de427e0d0522db830ec9ed0cd04cb30324484a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 14:04:54 +0000

    Add package abstract-ocr/0.0.0.97/abstract_ocr-0.0.0.97.tar.gz

---
+++ packages/abstract-ocr/abstract_ocr-0.0.0.97.tar.gz/abstract_ocr-0.0.0.97/src/abstract_ocr/__init__.py
@@ -0,0 +1,7 @@
+from .audio_utils import *
+from .functions import *
+from .ocr_utils import *
+from .pdf_utils import *
+from .seo_utils import *
+from .text_utils import *
+from .transcribe_utils import *
