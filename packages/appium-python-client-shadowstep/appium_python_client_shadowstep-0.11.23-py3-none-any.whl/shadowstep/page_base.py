3f33e95654590a48755635b64173942e236e6e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:19:12 +0000

    Add package appium-python-client-shadowstep/0.11.23/appium_python_client_shadowstep-0.11.23-py3-none-any.whl

---
+++ packages/appium-python-client-shadowstep/appium_python_client_shadowstep-0.11.23-py3-none-any.whl/shadowstep/page_base.py
@@ -0,0 +1,38 @@
+# shadowstep/page_base.py
+from abc import ABC, abstractmethod
+from typing import Any, Dict
+
+
+class PageBase(ABC):
+    _instances = {}
+
+    def __new__(cls):
+        if cls not in cls._instances:
+            instance = super().__new__(cls)
+            cls._instances[cls] = instance
+
+            # 💡 Lazy import здесь
+            from shadowstep.shadowstep import Shadowstep
+            instance.app = Shadowstep.get_instance()
+
+        return cls._instances[cls]
+
+    @classmethod
+    def get_instance(cls) -> "PageBase":
+        """Get or create the singleton instance of the page."""
+        return cls()
+
+    @classmethod
+    def clear_instance(cls) -> None:
+        """Clear the stored instance and its arguments for this page."""
+        cls._instances.pop(cls, None)
+
+    @property
+    @abstractmethod
+    def edges(self) -> Dict[str, Any]:
+        """Each page must declare its navigation edges.
+
+        Returns:
+            Dict[str, Callable]: Dictionary mapping page class names to navigation methods.
+        """
+        pass
