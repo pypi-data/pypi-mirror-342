9df54d16a8d0468dbb1105fa89b762d5ac1f6e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:06:54 +0000

    Add package newberry-metrics/0.0.10/newberry_metrics-0.0.10.tar.gz

---
+++ packages/newberry-metrics/newberry_metrics-0.0.10.tar.gz/newberry_metrics-0.0.10/newberry_metrics/main.py
@@ -0,0 +1,2 @@
+def cost():
+    return ("3.5 per million tokens")
---
+++ packages/newberry-metrics/newberry_metrics-0.0.10.tar.gz/newberry_metrics-0.0.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+# with open("app/README.md", "r") as f:
+#     long_description = f.read()
+
+setup(
+    name="newberry_metrics",
+    version="0.0.10",
+    description="A model evaluation tool",
+    packages=find_packages(),
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/SatyaTheG/newberry_metrics",
+    # author="SatyaTheG",
+    # author_email="forsatyanarayansahoo@gmail.com",
+    # license="MIT",
+    # classifiers=[
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python :: 3.10",
+    #     "Operating System :: OS Independent",
+    # ],
+    # install_requires=["bson >= 0.5.10"],
+    # extras_require={
+    #     "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    # },
+    # python_requires=">=3.10",
+)
