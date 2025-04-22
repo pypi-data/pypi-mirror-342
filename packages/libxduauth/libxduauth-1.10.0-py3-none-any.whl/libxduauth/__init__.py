436372b30dc61671a71403023eb3c462d4c77ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:05:09 +0000

    Add package libxduauth/1.10.0/libxduauth-1.10.0-py3-none-any.whl

---
+++ packages/libxduauth/libxduauth-1.10.0-py3-none-any.whl/libxduauth/__init__.py
@@ -0,0 +1,10 @@
+from .sites.ehall import EhallSession
+from .sites.energy import EnergySession
+from .sites.ids import IDSSession
+from .sites.rsbbs import RSBBSSession
+from .sites.wx import WXSession
+from .sites.xk import XKSession
+from .sites.zfw import ZFWSession
+from .sites.sports import SportsSession
+
+import libxduauth.utils.update
