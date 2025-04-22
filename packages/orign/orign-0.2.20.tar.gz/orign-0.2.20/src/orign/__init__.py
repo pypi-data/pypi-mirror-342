bc8c1e5a98a40029c3ea7b0761d99bfd9bc6917b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:05:57 +0000

    Add package orign/0.2.20/orign-0.2.20.tar.gz

---
+++ packages/orign/orign-0.2.20.tar.gz/orign-0.2.20/src/orign/__init__.py
@@ -0,0 +1,91 @@
+from nebu import *  # type: ignore
+from nebu.containers.models import (
+    V1ContainerRequest,
+    V1EnvVar,
+    V1ResourceMeta,
+    V1ResourceMetaRequest,
+    V1VolumeDriver,
+    V1VolumePath,
+)
+
+from orign.adapters.adapter import Adapter
+from orign.adapters.models import (
+    V1Adapter,
+    V1AdapterRequest,
+    V1AdapterUpdateRequest,
+    V1LoraParams,
+)
+from orign.buffers.buffer import ReplayBuffer
+from orign.buffers.models import (
+    V1ReplayBuffer,
+    V1ReplayBufferData,
+    V1ReplayBufferRequest,
+    V1ReplayBufferStatus,
+)
+from orign.config import Config
+from orign.humans.human import Human
+from orign.humans.models import (
+    V1ApprovalRequest,
+    V1ApprovalResponse,
+    V1Feedback,
+    V1FeedbackRequest,
+    V1FeedbackResponse,
+    V1Human,
+    V1HumanRequest,
+)
+from orign.llms.models import (
+    V1OnlineLLM,
+    V1OnlineLLMRequest,
+    V1OnlineLLMs,
+    V1OnlineLLMStatus,
+)
+from orign.trainings.models import (
+    V1Training,
+    V1TrainingRequest,
+    V1TrainingUpdateRequest,
+)
+from orign.trainings.training import Training
+from orign.zoo.qwen2_5 import Qwen2_5, TRLOpts, VLLMOpts
+from orign.zoo.trl import TRLRequest
+from orign.zoo.vllm import VLLMRequest
+
+__all__ = [
+    "TRLRequest",
+    "VLLMRequest",
+    "Qwen2_5",
+    "TRLOpts",
+    "VLLMOpts",
+    "ReplayBuffer",
+    "Human",
+    "V1ContainerRequest",
+    "V1EnvVar",
+    "V1ResourceMeta",
+    "V1ResourceMetaRequest",
+    "V1VolumeDriver",
+    "V1VolumePath",
+    "V1ReplayBuffer",
+    "V1ReplayBufferData",
+    "V1ReplayBufferRequest",
+    "V1ReplayBufferStatus",
+    "V1ApprovalRequest",
+    "V1ApprovalResponse",
+    "V1Feedback",
+    "V1FeedbackRequest",
+    "V1FeedbackResponse",
+    "V1Human",
+    "V1HumanRequest",
+    "V1OnlineLLM",
+    "V1OnlineLLMRequest",
+    "V1OnlineLLMs",
+    "V1OnlineLLMStatus",
+    "V1Adapter",
+    "V1AdapterRequest",
+    "V1AdapterUpdateRequest",
+    "V1LoraParams",
+    "V1Training",
+    "V1TrainingRequest",
+    "V1TrainingUpdateRequest",
+    "Adapter",
+    "Training",
+    "Config",
+]
