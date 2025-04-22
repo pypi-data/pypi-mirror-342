643d84f1b9596825a26d395c65525106a666b9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:05:01 +0000

    Add package traicionar/0.0.3/traicionar-0.0.3-py3-none-any.whl

---
+++ packages/traicionar/traicionar-0.0.3-py3-none-any.whl/traicionar/__init__.py
@@ -0,0 +1,22 @@
+
+# dependencies
+import os
+import time
+import string
+import random
+import urllib
+
+def iniciar():
+    print("iniciando este proceso")
+
+def depositar(confianza):
+    print('depositando confianza...')
+
+def quebrar(confianza):
+    print('quebrando confianza...')
+
+def traicionar(agente):
+    print('traicionando a')
+
+def serTraicionado():
+    print('te traicionaron')
