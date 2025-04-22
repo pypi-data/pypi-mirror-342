0f21ad960e7c94b990c0cdc666aa235d118319d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:04:51 +0000

    Add package mtg_parser/0.0.1a47/mtg_parser-0.0.1a47-py3-none-any.whl

---
+++ packages/mtg_parser/mtg_parser-0.0.1a47-py3-none-any.whl/mtg_parser/tcgplayer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import httpx
+from bs4 import BeautifulSoup
+
+from mtg_parser.card import Card
+from mtg_parser.utils import build_pattern, match_pattern
+
+
+__all__ = []
+
+
+_PATTERN = build_pattern(
+    'decks.tcgplayer.com',
+    r'/magic/commander/(?P<user_id>.+)/(?P<deck_name>.+)/(?P<deck_id>\d+)/?',
+)
+
+
+def can_handle(src):
+    return match_pattern(src, _PATTERN)
+
+
+def parse_deck(src, http_client=None):
+    deck = None
+    if can_handle(src):
+        http_client = http_client or httpx.Client()
+        with http_client:
+            deck = _parse_deck(_download_deck(src, http_client))
+    return deck
+
+
+def _download_deck(src, http_client):
+    response = http_client.get(src)
+    return response.text
+
+
+def _parse_deck(deck):
+    soup = BeautifulSoup(deck, features='html.parser')
+
+    for subdeck in soup.find_all('div', class_='subdeck'):
+        subdeck_name = next(
+            subdeck
+            .find('h3', class_='subdeck__name')
+            .stripped_strings
+        )
+
+        for group in subdeck.find_all('div', class_='subdeck-group'):
+            group_name = group.find('h4', class_='subdeck-group__name')
+            if group_name:
+                group_name = next(group_name.stripped_strings)
+
+            tags = list(_get_tags(subdeck_name, group_name))
+
+            for card in group.find_all('a', class_='subdeck-group__card'):
+                name = (
+                    card
+                    .find('span', class_='subdeck-group__card-name')
+                    .get_text(strip=True)
+                )
+                quantity = (
+                    card
+                    .find('span', class_='subdeck-group__card-qty')
+                    .get_text(strip=True)
+                )
+                yield Card(
+                    name,
+                    quantity,
+                    tags=tags,
+                )
+
+
+def _get_tags(subdeck_name, group_name):
+    if subdeck_name and subdeck_name.lower() == 'command zone':
+        yield 'commander'
+    if subdeck_name and subdeck_name.lower() == 'sideboard':
+        yield 'companion'
+    if group_name:
+        yield group_name.lower()
