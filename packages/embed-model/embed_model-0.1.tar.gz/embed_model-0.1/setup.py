d75a6de265dac629487b758d6af3902e1ebea05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:21 +0000

    Add package embed-model/0.1/embed_model-0.1.tar.gz

---
+++ packages/embed-model/embed_model-0.1.tar.gz/embed_model-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="embed_model",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "discord"
+    ],
+    author="Luca Cunha (Frisk)",
+    description="Um modelo de embeds não oficial para discord.py. Feito em Português.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LucaCunha001/DiscordEmbedModel",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires=">=3.8",
+)
