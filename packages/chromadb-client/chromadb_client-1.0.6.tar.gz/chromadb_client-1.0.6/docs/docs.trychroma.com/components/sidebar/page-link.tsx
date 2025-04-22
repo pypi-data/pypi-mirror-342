6a788cfb4c7bbf3910fb57175355e63f857aa02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:05:39 +0000

    Add package chromadb-client/1.0.6/chromadb_client-1.0.6.tar.gz

---
+++ packages/chromadb-client/chromadb_client-1.0.6.tar.gz/chromadb_client-1.0.6/docs/docs.trychroma.com/components/sidebar/page-link.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const PageLink: React.FC<{
+  id: string;
+  name: string;
+  path: string;
+  sectionPage: boolean;
+}> = ({ id, name, path, sectionPage = true }) => {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const active = pathName === path;
+  const lang = searchParams?.get("lang");
+
+  return (
+    <div
+      key={id}
+      className={`px-3 py-1 border-l-4 ${active && "border-blue-500 bg-blue-50 text-blue-500 font-bold hover:bg-blue-50 dark:bg-blue-900 dark:text-blue-100"} ${!active && "border-transparent text-gray-600 dark:text-gray-300 hover:bg-gray-50 hover:border-gray-200 dark:hover:bg-gray-800 dark:hover:border-gray-400"} `}
+    >
+      <Link href={lang ? `${path}?lang=${lang}` : path}>
+        <p className="text-sm">{name}</p>
+      </Link>
+    </div>
+  );
+};
+
+export default PageLink;
---
+++ packages/chromadb-client/chromadb_client-1.0.6.tar.gz/chromadb_client-1.0.6/go/pkg/sysdb/coordinator/model/collection.go
@@ -0,0 +1,87 @@
+package model
+
+import (
+	"github.com/chroma-core/chroma/go/pkg/types"
+)
+
+type Collection struct {
+	ID                         types.UniqueID
+	Name                       string
+	ConfigurationJsonStr       string
+	Dimension                  *int32
+	Metadata                   *CollectionMetadata[CollectionMetadataValueType]
+	TenantID                   string
+	DatabaseName               string
+	Ts                         types.Timestamp
+	LogPosition                int64
+	Version                    int32
+	UpdatedAt                  types.Timestamp
+	TotalRecordsPostCompaction uint64
+	SizeBytesPostCompaction    uint64 // Note: This represents the size of the records off the log
+	LastCompactionTimeSecs     uint64
+}
+
+type CollectionToGc struct {
+	ID              types.UniqueID
+	TenantID        string
+	Name            string
+	VersionFilePath string
+	LatestVersion   int64
+}
+
+type CreateCollection struct {
+	ID                   types.UniqueID
+	Name                 string
+	ConfigurationJsonStr string
+	Dimension            *int32
+	Metadata             *CollectionMetadata[CollectionMetadataValueType]
+	GetOrCreate          bool
+	TenantID             string
+	DatabaseName         string
+	Ts                   types.Timestamp
+}
+
+type DeleteCollection struct {
+	ID           types.UniqueID
+	TenantID     string
+	DatabaseName string
+	Ts           types.Timestamp
+}
+
+type UpdateCollection struct {
+	ID                      types.UniqueID
+	Name                    *string
+	Dimension               *int32
+	Metadata                *CollectionMetadata[CollectionMetadataValueType]
+	ResetMetadata           bool
+	NewConfigurationJsonStr *string
+	TenantID                string
+	DatabaseName            string
+	Ts                      types.Timestamp
+}
+
+type FlushCollectionCompaction struct {
+	ID                         types.UniqueID
+	TenantID                   string
+	LogPosition                int64
+	CurrentCollectionVersion   int32
+	FlushSegmentCompactions    []*FlushSegmentCompaction
+	TotalRecordsPostCompaction uint64
+	SizeBytesPostCompaction    uint64
+}
+
+type FlushCollectionInfo struct {
+	ID                       string
+	CollectionVersion        int32
+	TenantLastCompactionTime int64
+}
+
+func FilterCollection(collection *Collection, collectionID types.UniqueID, collectionName *string) bool {
+	if collectionID != types.NilUniqueID() && collectionID != collection.ID {
+		return false
+	}
+	if collectionName != nil && *collectionName != collection.Name {
+		return false
+	}
+	return true
+}
