1bd7ea2d7426e02ce8640311d9de61f5f55f9e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:18:54 +0000

    Add package gllm-docproc-binary/0.1.8/gllm_docproc_binary-0.1.8-cp312-cp312-macosx_13_0_x86_64.whl

---
+++ packages/gllm-docproc-binary/gllm_docproc_binary-0.1.8-cp312-cp312-macosx_13_0_x86_64.whl/gllm_docproc/dpo_router/base_dpo_router.pyi
@@ -0,0 +1,17 @@
+import abc
+from abc import ABC, abstractmethod
+from typing import Any
+
+class BaseDPORouter(ABC, metaclass=abc.ABCMeta):
+    """Base class for routing in document processing."""
+    @abstractmethod
+    def route(self, *args: Any, **kwargs: Any) -> Any:
+        """Routes the input into different processing pipelines based on certain criteria.
+
+        Args:
+            *args (Any): Variable length argument list for routing parameters.
+            **kwargs (Any): Arbitrary keyword arguments for additional routing configuration.
+
+        Returns:
+            Any: The result of the routing process.
+        """
