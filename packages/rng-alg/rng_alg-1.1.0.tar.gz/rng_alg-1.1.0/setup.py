e8d4e493db29fa9053d936b3a985686414688d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:04:38 +0000

    Add package rng-alg/1.1.0/rng_alg-1.1.0.tar.gz

---
+++ packages/rng-alg/rng_alg-1.1.0.tar.gz/rng_alg-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rng_alg',
+    version='1.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='Nickels',
+    author_email="Nickels74130@outlook.fr",
+    description='A package for random number generation algorithms like hexadecimal, pi, and advanced RNGs.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
