a47299a81298665f4a464429ec67601001229c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:05:16 +0000

    Add package sd-probdist/0.1/sd_probdist-0.1.tar.gz

---
+++ packages/sd-probdist/sd_probdist-0.1.tar.gz/sd_probdist-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="sd_probdist",
+    version="0.1",
+    description="Gaussian distributions and Binomial distributions",
+    packages=["sd_probdist"],
+    author="Sudip Dey",
+    author_email="myemail@something.com",
+    license = "MIT",
+    zip_safe=False,
+)
