fe6bcab428f3f975b800833591ddaf0116e0a6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:05:16 +0000

    Add package ork.build/0.0.248/ork_build-0.0.248-py3-none-any.whl

---
+++ packages/ork.build/ork_build-0.0.248-py3-none-any.whl/ork_build-0.0.248.data/scripts/obt.osx.installdeps.py
@@ -0,0 +1,15 @@
+#!python
+
+import os
+
+deplist =  ["pkgconfig","cmake","wget","curl","libtiff","libpng", "git-lfs"]
+deplist += ["portaudio","fmt","m4","bison","flex","xz"]
+deplist += ["scons","zlib","tbb", "glew","flac","libsndfile"]
+deplist += ["mpfr","openssl","graphviz","doxygen","swig","tcl-tk"]
+deplist += ["pyqt5","qt5", "gcc@13", "xxhash", "autoconf", "automake" ]
+
+depliststr = " ".join(deplist)
+os.system("brew install %s" % depliststr)
+os.system("brew remove boost")
+
+os.system("python3 -m pip install --upgrade setuptools")
