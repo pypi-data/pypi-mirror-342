0605ee24d23c18f9ef24131c79102029845b8d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 16:05:28 +0000

    Add package cg-hermes/6.0.4/cg_hermes-6.0.4-py3-none-any.whl

---
+++ packages/cg-hermes/cg_hermes-6.0.4-py3-none-any.whl/cg_hermes/config/fluffy.py
@@ -0,0 +1,32 @@
+"""Tags that are used in the fluffy workflow."""
+
+FLUFFY_COMMON_TAGS = {
+    frozenset(["multiqc"]): {
+        "tags": ["multiqc-html"],
+        "is_mandatory": True,
+        "used_by": ["nipt"],
+    },
+    frozenset(["nipt_csv"]): {
+        "tags": ["metrics"],
+        "bundle_id": True,
+        "is_mandatory": True,
+        "used_by": ["nipt"],
+    },
+    frozenset(["nipt_first_pass_csv"]): {
+        "tags": ["first-pass"],
+        "bundle_id": True,
+        "is_mandatory": False,
+        "used_by": ["nipt"],
+    },
+    frozenset(["nipt_second_pass_csv"]): {
+        "tags": ["second-pass"],
+        "bundle_id": True,
+        "is_mandatory": False,
+        "used_by": ["nipt"],
+    },
+    frozenset(["wisecondor_aberrations"]): {
+        "tags": ["wisecondor", "cnv"],
+        "is_mandatory": True,
+        "used_by": ["nipt"],
+    },
+}
