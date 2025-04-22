546e76e82ff6e1379b3b6d837867b985ddafc9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:05:19 +0000

    Add package r3make/2025.1.9/r3make-2025.1.9.tar.gz

---
+++ packages/r3make/r3make-2025.1.9.tar.gz/r3make-2025.1.9/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+version = {
+    "year" :2025,
+    "minor" :1,
+    "patch" :9
+}
+
+setuptools.setup(
+    name='r3make',
+    version=f"{version["year"]}.{version["minor"]}.{version["patch"]}",
+    description='A straightforward command-line build tool for C.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Izaiyah Stokes',
+    author_email='d34d0s.dev@gmail.com',
+    url='https://github.com/r3shape/r3make',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'rich'
+    ],
+    entry_points={
+        'console_scripts': [
+            'r3make=r3make:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
