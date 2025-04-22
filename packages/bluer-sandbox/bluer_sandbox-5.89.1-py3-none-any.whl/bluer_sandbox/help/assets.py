08362ca62fe1fc7ded5d71e3756af1e45587fb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:20 +0000

    Add package bluer-sandbox/5.89.1/bluer_sandbox-5.89.1-py3-none-any.whl

---
+++ packages/bluer-sandbox/bluer_sandbox-5.89.1-py3-none-any.whl/bluer_sandbox/help/assets.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from bluer_options.terminal import show_usage, xtra
+
+
+def help_publish(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = "".join(
+        [
+            xtra("download,", mono=mono),
+            "extensions=png+txt",
+            xtra(",~pull,", mono=mono),
+            "push",
+        ]
+    )
+
+    args = [
+        "[--prefix <prefix>]",
+    ]
+
+    return show_usage(
+        [
+            "@assets",
+            "publish",
+            f"[{options}]",
+            "[.|<object-name>]",
+        ]
+        + args,
+        "<object-name>/<prefix> -> assets.",
+        mono=mono,
+    )
+
+
+help_functions = {
+    "publish": help_publish,
+}
