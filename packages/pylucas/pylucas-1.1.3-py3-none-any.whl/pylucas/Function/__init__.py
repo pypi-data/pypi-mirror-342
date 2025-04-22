b64fe8f37d4240a26bed357ed485b6e5ce963a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:04:45 +0000

    Add package pylucas/1.1.3/pylucas-1.1.3-py3-none-any.whl

---
+++ packages/pylucas/pylucas-1.1.3-py3-none-any.whl/pylucas/Function/__init__.py
@@ -0,0 +1,3 @@
+from pylucas.Function.Function import Author_Lucas, ASCII_Art, GetTimeStamp, Get_CurrentFrame_Info
+
+__all__ = ['Author_Lucas', 'ASCII_Art', 'GetTimeStamp', 'Get_CurrentFrame_Info']
