04e9e92c8af870ad83e259e248aecca9aca73bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:06:09 +0000

    Add package shopagent/0.8.0/shopagent-0.8.0.tar.gz

---
+++ packages/shopagent/shopagent-0.8.0.tar.gz/shopagent-0.8.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="shopagent",
+    version="0.8.0",
+    packages=['shopagent', 'shopagent.api', 'shopagent.code_gen', 'shopagent.constants', 'shopagent.lsd', 'shopagent.nav']
+)
