1abc5939fc06c6740f6de922afac0a5134d449f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 14:05:02 +0000

    Add package lightweight-s3/0.0.4/lightweight_s3-0.0.4.tar.gz

---
+++ packages/lightweight-s3/lightweight_s3-0.0.4.tar.gz/lightweight_s3-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='lightweight-s3',
+    version='0.0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "lightweight_s3": ["py.typed", "core/*.pyi"],
+    },
+    install_requires=[
+        'requests'
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Daniel Lasota",
+    author_email="grossmann.root@gmail.com",
+    description="Lightweight S3 client with backblaze support",
+    keywords="s3 backblaze boto3 client",
+    url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    project_urls={
+        "Source": "https://github.com/daniel-lasota/lightweight-s3"
+    },
+    python_requires='>=3.11',
+)
+
