e6f336e9f2cfc4c62e70bd7448377cc684ecf489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:05:18 +0000

    Add package merpe/3.2.1/merpe-3.2.1.tar.gz

---
+++ packages/merpe/merpe-3.2.1.tar.gz/merpe-3.2.1/README.md
@@ -0,0 +1,3 @@
+# Merbe Package
+
+This is an educational demonstration package for security research purposes.
---
+++ packages/merpe/merpe-3.2.1.tar.gz/merpe-3.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="merpe",
+    version="3.2.1",  # Higher version than the private package
+    packages=find_packages(),
+    description="Educational demonstration of dependency confusion",
+    author="BEKHTI DJALAL",
+    author_email="d.bekhti@esi-sba.dz",
+    url="https://github.com/merzouka/dependency-confusion.security/blob/main/malicious/src/merbe",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+    ],
+)
