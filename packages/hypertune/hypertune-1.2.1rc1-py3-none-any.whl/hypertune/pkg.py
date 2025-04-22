75fbe7177844ff56735dd82d6f1d2e27b1fae4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:24 +0000

    Add package hypertune/1.2.1rc1/hypertune-1.2.1rc1-py3-none-any.whl

---
+++ packages/hypertune/hypertune-1.2.1rc1-py3-none-any.whl/hypertune/pkg.py
@@ -0,0 +1,7 @@
+NAME = "hypertune"
+VERSION = "1.2.1-rc1"
+DESC = "A library for performing hyperparameter optimization with Polyaxon."
+URL = "https://github.com/polyaxon/hypertune"
+AUTHOR = "Polyaxon, Inc."
+EMAIL = "contact@polyaxon.com"
+LICENSE = "Apache 2.0"
