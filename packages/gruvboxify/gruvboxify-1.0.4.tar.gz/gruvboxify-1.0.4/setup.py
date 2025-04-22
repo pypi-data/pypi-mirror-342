bf4e224d6c0f530aa5dc447e5e61b2e2bef8b098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:15 +0000

    Add package gruvboxify/1.0.4/gruvboxify-1.0.4.tar.gz

---
+++ packages/gruvboxify/gruvboxify-1.0.4.tar.gz/gruvboxify-1.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    content = f.read()
+
+setup(
+    name="gruvboxify",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires = [
+        "numpy>=2.2.2",
+        "Pillow>=10.0.0"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "gruvboxify = gruvboxify:main",
+        ],
+    },
+    long_description=content,
+    long_description_content_type="text/markdown"
+)
