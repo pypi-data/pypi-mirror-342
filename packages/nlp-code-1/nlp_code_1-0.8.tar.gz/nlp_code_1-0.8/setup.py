4f183cc22d907aa339c89b56881d65380ada5b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:04:57 +0000

    Add package nlp-code-1/0.8/nlp_code_1-0.8.tar.gz

---
+++ packages/nlp-code-1/nlp_code_1-0.8.tar.gz/nlp_code_1-0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nlp_code_1",
+    version="0.8",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+    author="Your Name",
+    description="code",
+)
