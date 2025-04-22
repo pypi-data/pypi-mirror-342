a79d9ca8a8f043733a5136a5090f7464078ee768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:29 +0000

    Add package closive/0.0.0.dev9/closive-0.0.0.dev9-py3-none-any.whl

---
+++ packages/closive/closive-0.0.0.dev9-py3-none-any.whl/closive/__init__.py
@@ -0,0 +1,8 @@
+"""Initialization
+
+Exposes the public API for Closive, a domain-specific language
+designed for use in callback-heavy data flows.
+"""
+from .closures import closure # API for the closure framework
+
+__all__ = ["closure"]
