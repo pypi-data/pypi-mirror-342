51b44213af18120c237b6585748ae3c6ebb35eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:04:42 +0000

    Add package alqueda/0.6/alqueda-0.6.tar.gz

---
+++ packages/alqueda/alqueda-0.6.tar.gz/alqueda-0.6/README.md
@@ -0,0 +1,10 @@
+# alqueda Library
+
+`alqueda` is a Python library containing various machine learning functions, including neuron training, k-NN, perceptron classifiers, RNNs, CNNs, and more.
+
+## Installation
+
+You can install the library using pip:
+
+```bash
+pip install alqueda
---
+++ packages/alqueda/alqueda-0.6.tar.gz/alqueda-0.6/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='alqueda',
+    version='0.6',
+    description='A Python library with machine learning functions',
+    author='Parinith',
+    author_email='parinith99@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
