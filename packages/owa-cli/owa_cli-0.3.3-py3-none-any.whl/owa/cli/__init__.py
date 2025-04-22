6ad7697421aebfbd32ef985ca49bfe7f84869c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:04:44 +0000

    Add package owa-cli/0.3.3/owa_cli-0.3.3-py3-none-any.whl

---
+++ packages/owa-cli/owa_cli-0.3.3-py3-none-any.whl/owa/cli/__init__.py
@@ -0,0 +1,16 @@
+import platform
+
+import typer
+
+from . import mcap, video
+
+app = typer.Typer()
+app.add_typer(mcap.app, name="mcap")
+app.add_typer(video.app, name="video")
+
+if platform.system() == "Windows":
+    from . import window
+
+    app.add_typer(window.app, name="window")
+else:
+    typer.echo("Since you're not using Windows OS, `owa-cli window` command is disabled.", err=True)
