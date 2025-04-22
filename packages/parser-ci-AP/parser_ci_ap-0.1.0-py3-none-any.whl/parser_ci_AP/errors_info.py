6f800f5ca6d7bbb45f89b8d13d475f3c6eb10d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:19 +0000

    Add package parser-ci-AP/0.1.0/parser_ci_ap-0.1.0-py3-none-any.whl

---
+++ packages/parser-ci-AP/parser_ci_ap-0.1.0-py3-none-any.whl/parser_ci_AP/errors_info.py
@@ -0,0 +1,32 @@
+from clang.cindex import Diagnostic
+
+class DiagnosticInfo:
+    def __init__(self, diag: Diagnostic):
+        self.severity = diag.severity  # 1=warning, 2=error, 3=fatal, ...
+        self.spelling = diag.spelling
+        self.location = f"{diag.location.file}:{diag.location.line}"
+        self.category = diag.category_name
+        self.option = diag.option
+
+    def __eq__(self, other):
+        if not isinstance(other, DiagnosticInfo):
+            return False
+        return (
+            self.severity == other.severity and
+            self.spelling == other.spelling and
+            self.location == other.location and
+            self.category == other.category and
+            self.option == other.option
+        )
+
+    def __hash__(self):
+        return hash((
+            self.severity,
+            self.spelling,
+            self.location,
+            self.category,
+            self.option
+        ))
+
+    def __str__(self):
+        return f"[{self.severity}] {self.location}: {self.spelling}"
