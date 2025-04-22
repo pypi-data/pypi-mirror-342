a10b76cfc5bd007e55e5bc556081f01e05f7b938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:05:18 +0000

    Add package merpe/3.2.1/merpe-3.2.1-py3-none-any.whl

---
+++ packages/merpe/merpe-3.2.1-py3-none-any.whl/merbe/core.py
@@ -0,0 +1,5 @@
+def sample_function():
+    """A dummy function that would be called by applications using this package"""
+    print("[DEMO] Function from PyPI package called - in a real attack this could run additional malicious code")
+    return "Sample function executed"
+
