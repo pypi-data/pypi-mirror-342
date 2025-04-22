900615ddb6baac562889f62bf85c102df37f531a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:05:45 +0000

    Add package contohLIB/0.1.0/contohlib-0.1.0.tar.gz

---
+++ packages/contohLIB/contohlib-0.1.0.tar.gz/contohlib-0.1.0/contohLIB/__init__.py
@@ -0,0 +1,3 @@
+
+def hello_word();
+  Print("Hello Word")
---
+++ packages/contohLIB/contohlib-0.1.0.tar.gz/contohlib-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='contohLIB',
+    version='0.1.0',
+    author='Hamid Khaeruman',
+    author_email='email@contoh.com',
+    description='Library Python untuk konversi suhu antara Celsius, Fahrenheit, dan Kelvin',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.6',
+)
