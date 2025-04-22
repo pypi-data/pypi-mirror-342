5fa9eda0fe342607fe2502b35a62826ad73e42eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 16:05:21 +0000

    Add package StellarPolAnalyzer/0.1.19/stellarpolanalyzer-0.1.19.tar.gz

---
+++ packages/StellarPolAnalyzer/stellarpolanalyzer-0.1.19.tar.gz/stellarpolanalyzer-0.1.19/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='StellarPolAnalyzer',
+    version='0.1.19',
+    description='Librería Python que automatiza un completo un pipeline polarimétrico sobre imágenes FITS astronómicas: Empareja, fotometriza y astrometriza imágenes polarimétricas de campo estelar en un solo flujo.',
+    author='Oscar Mellizo Angulo',
+    author_email='omellizo@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/oscarmellizo/StellarPolAnalyzer',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'astropy',
+        'photutils',
+        'scikit-learn',
+        'scikit-image',
+        'scipy',
+        'astroquery',
+        'reportlab'
+    ],
+    license='Apache License 2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
