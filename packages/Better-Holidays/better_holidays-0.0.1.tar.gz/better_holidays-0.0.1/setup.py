3e7bac6d3dca15d90b7a3029b964a5ce6524c0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 07:04:53 +0000

    Add package Better-Holidays/0.0.1/better_holidays-0.0.1.tar.gz

---
+++ packages/Better-Holidays/better_holidays-0.0.1.tar.gz/better_holidays-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "A better way to get market holidays"
+
+def read(path):
+    with open(path, "r") as f:
+        return f.read()
+
+setup(
+    name="Better-Holidays",
+    version=VERSION,
+    author="R5dan",
+    description=DESCRIPTION,
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude="tests"),
+    install_requires=[],
+    extras_require={},
+    keywords=["python", "better holidays", "better", "market", "stocks", "finance", "holidays"],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    url="https://github.com/Better/better-holidays"
+)
