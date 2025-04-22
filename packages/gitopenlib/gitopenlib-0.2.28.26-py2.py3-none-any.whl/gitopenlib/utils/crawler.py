c973cced8efc565e20f61ce3e5f24f4533fc8c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:18:56 +0000

    Add package gitopenlib/0.2.28.26/gitopenlib-0.2.28.26-py2.py3-none-any.whl

---
+++ packages/gitopenlib/gitopenlib-0.2.28.26-py2.py3-none-any.whl/gitopenlib/utils/crawler.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Copyright (c) 2021
+# @Author :  GitOPEN
+# @Email  :  gitopen@gmail.com
+# @Date   :  2021-12-06 14:10:44
+# @Description :  有关爬虫的一些工具函数
+
+
+__version__ = "0.0.4"
+
+
+import random
+import time
+from pathlib import Path
+
+from fake_useragent import UserAgent
+from gitopenlib.utils import files as gf
+
+
+def have_a_sleep(min=1, max=10):
+    """
+    随机睡眠一段时间，小数秒
+    """
+    time.sleep(random.uniform(min, max))
+
+
+def update_useragent():
+    uas = set()
+    for i in range(1000):
+        ua = UserAgent()
+        ua1 = ua.getGoogle().strip().strip('"').strip("'")
+        ua2 = ua.getFirefox().strip().strip('"').strip("'")
+        uas.add(ua1)
+        uas.add(ua2)
+
+    gf.file_writer(uas, "./ua/", "ua.txt", backup=False)
+
+
+def get_ua():
+    """随机获取一个 User-Agent"""
+    ua_path = "./ua/ua.txt"
+    if not Path(ua_path).exists():
+        update_useragent()
+    uas = gf.read_content(ua_path)
+    idx = random.randint(0, len(uas) - 1)
+    return uas[idx]
