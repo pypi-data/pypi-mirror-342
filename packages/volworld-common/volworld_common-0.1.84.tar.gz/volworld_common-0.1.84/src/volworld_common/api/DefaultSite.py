e12da154c5764ce6bdec8551652155f3363d9cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 16:04:27 +0000

    Add package volworld-common/0.1.84/volworld_common-0.1.84.tar.gz

---
+++ packages/volworld-common/volworld_common-0.1.84.tar.gz/volworld_common-0.1.84/src/volworld_common/api/DefaultSite.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+class GaiaDefaultTutorialSite:
+    class InitTutorial1:
+        UUID: Final[str] = "c42edc7f-f382-40c6-b042-68858daa8ec3"
+    class InitTutorial2:
+        UUID: Final[str] = "26630f2f-ab60-417f-bae9-a3cb7385692f"
+    class InitTutorialEnd:
+        UUID: Final[str] = "5a2edb29-5d2b-49af-8456-57e332a33e8f"
