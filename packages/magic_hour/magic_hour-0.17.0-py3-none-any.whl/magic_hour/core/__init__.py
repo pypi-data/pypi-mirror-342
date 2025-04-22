2a89e7569460cfa479d984a1e7737c6272e35a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 07:04:49 +0000

    Add package magic_hour/0.17.0/magic_hour-0.17.0-py3-none-any.whl

---
+++ packages/magic_hour/magic_hour-0.17.0-py3-none-any.whl/magic_hour/core/__init__.py
@@ -0,0 +1,52 @@
+from .api_error import ApiError
+from .auth import (
+    AuthKeyQuery,
+    AuthBasic,
+    AuthBearer,
+    AuthProvider,
+    AuthKeyCookie,
+    AuthKeyHeader,
+    GrantType,
+    OAuth2,
+    OAuth2ClientCredentialsForm,
+    OAuth2PasswordForm,
+)
+from .base_client import AsyncBaseClient, BaseClient, SyncBaseClient
+from .binary_response import BinaryResponse
+from .query import encode_query_param, QueryParams
+from .request import (
+    filter_not_given,
+    to_content,
+    to_encodable,
+    RequestOptions,
+    default_request_options,
+)
+from .response import from_encodable, AsyncStreamResponse, StreamResponse
+
+__all__ = [
+    "ApiError",
+    "AsyncBaseClient",
+    "BaseClient",
+    "BinaryResponse",
+    "RequestOptions",
+    "default_request_options",
+    "SyncBaseClient",
+    "AuthKeyQuery",
+    "AuthBasic",
+    "AuthBearer",
+    "AuthProvider",
+    "AuthKeyCookie",
+    "AuthKeyHeader",
+    "GrantType",
+    "OAuth2",
+    "OAuth2ClientCredentialsForm",
+    "OAuth2PasswordForm",
+    "to_encodable",
+    "filter_not_given",
+    "to_content",
+    "encode_query_param",
+    "from_encodable",
+    "AsyncStreamResponse",
+    "StreamResponse",
+    "QueryParams",
+]
