9755334f7672d6f3c9945a932d6f12c8caa9e220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 14:05:07 +0000

    Add package quantybt/0.1.8.9/quantybt-0.1.8.9.tar.gz

---
+++ packages/quantybt/quantybt-0.1.8.9.tar.gz/quantybt-0.1.8.9/quantybt/__init__.py
@@ -0,0 +1,12 @@
+
+
+from quantybt.strategy import Strategy
+from quantybt.analyzer import Analyzer
+from quantybt.optimizer import Optimizer
+from quantybt.montecarlo import MonteCarloBootstrapping
+
+from quantybt.stats import Stats
+from quantybt.utils import Utils
+
+from quantybt.models.filter import UnscentedKalmanFilter, SimpleKalmanFilter
+
