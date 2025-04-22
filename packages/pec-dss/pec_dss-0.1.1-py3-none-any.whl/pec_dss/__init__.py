8d84780829e359374ec6de29a00e1a6363f71caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 16:04:37 +0000

    Add package pec-dss/0.1.1/pec_dss-0.1.1-py3-none-any.whl

---
+++ packages/pec-dss/pec_dss-0.1.1-py3-none-any.whl/pec_dss/__init__.py
@@ -0,0 +1,8 @@
+"""
+PEC-DSS: Paralinguistic Event Classification from Diarized Speaker Segments.
+
+An advanced audio analysis system that identifies paralinguistic vocal events
+and attributes them to specific speakers through sophisticated speaker diarization.
+"""
+
+__version__ = "0.1.1"
