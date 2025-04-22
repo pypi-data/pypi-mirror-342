1b719fda4e3367cb938b0b86baa27ec310b16410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:04:51 +0000

    Add package traicionar/0.0.1/traicionar-0.0.1.tar.gz

---
+++ packages/traicionar/traicionar-0.0.1.tar.gz/traicionar-0.0.1/README.md
@@ -0,0 +1,15 @@
+# traicionarpy
+
+## Description
+
+A project by Aarón Montoya-Moraga.
+
+## Installation
+
+```bash
+pip install traicionar
+```
+
+## License
+
+MIT
---
+++ packages/traicionar/traicionar-0.0.1.tar.gz/traicionar-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+# taken from https://tom-christie.github.io/articles/pypi/
+here = path.abspath(path.dirname(__file__))
+
+# taken from https://tom-christie.github.io/articles/pypi/
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='traicionar',
+    version='0.0.1',
+    url='https://github.com/montoyamoraga/traicionar.py',
+    author='montoyamoraga',
+    license='MIT',
+    description='library for electronic betrayal',
+    long_description='library for electronic betrayal',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
+    programming_language='Python',
+
+)
