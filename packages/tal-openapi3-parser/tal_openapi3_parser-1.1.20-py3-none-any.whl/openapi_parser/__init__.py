4588c2712d64d73ab4a4c1c2c1b7cef887fbd3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:05:22 +0000

    Add package tal-openapi3-parser/1.1.20/tal_openapi3_parser-1.1.20-py3-none-any.whl

---
+++ packages/tal-openapi3-parser/tal_openapi3_parser-1.1.20-py3-none-any.whl/openapi_parser/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.1.20'
+__title__ = 'tal-openapi3-parser'
+__author__ = 'Artem Manchenkov'
+__email__ = 'artem@manchenkoff.me'
+__description__ = 'OpenAPI v3 parser'
+
+from .parser import parse
+
+__all__ = [
+    "parse"
+]
