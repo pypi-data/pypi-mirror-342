fb299e0c82096166844d3807f2eb26ee40d13701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:25 +0000

    Add package my-unique-calculator/0.1/my_unique_calculator-0.1.tar.gz

---
+++ packages/my-unique-calculator/my_unique_calculator-0.1.tar.gz/my_unique_calculator-0.1/calculator/__init__.py
@@ -0,0 +1,6 @@
+class Calculator:
+    def add(self, a, b): return a + b
+    def subtract(self, a, b): return a - b
+    def multiply(self, a, b): return a * b
+    def divide(self, a, b): return a / b if b != 0 else float('inf')
+    def remainder(self, a, b): return a % b
---
+++ packages/my-unique-calculator/my_unique_calculator-0.1.tar.gz/my_unique_calculator-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='my-unique-calculator',  # Choose a unique name
+    version='0.1',
+    packages=['calculator'],
+    install_requires=[],
+)
