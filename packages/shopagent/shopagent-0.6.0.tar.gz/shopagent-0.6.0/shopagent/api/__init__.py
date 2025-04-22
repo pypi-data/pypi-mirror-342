46902450ed73b63febcc6ee2ec8331a3a429a3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 15:06:03 +0000

    Add package shopagent/0.6.0/shopagent-0.6.0.tar.gz

---
+++ packages/shopagent/shopagent-0.6.0.tar.gz/shopagent-0.6.0/shopagent/api/__init__.py
@@ -0,0 +1,6 @@
+from .api import get_api_information, get_data
+from .code_examples import get_api_examples
+from .list import get_list_of_items
+from .mutation import get_mutation_info
+from .object import get_object_info
+from .query import get_query_info
---
+++ packages/shopagent/shopagent-0.6.0.tar.gz/shopagent-0.6.0/shopagent/nav/nav.py
@@ -0,0 +1,30 @@
+import json
+import os.path
+
+from requests import get
+from rich import print
+
+from shopagent.models import ShopifyAPI, ShopifyNavResponse
+from shopagent.constants import SHOPIFY_BASE_URL, SHOPIFY_NAV_API_URL
+
+
+def complete_nav_url(href):
+    return f"{SHOPIFY_BASE_URL}{href}"
+
+
+def get_top_level_operations():
+    res = get(SHOPIFY_NAV_API_URL).json()
+    shopify_nav = ShopifyNavResponse(**res)
+    return shopify_nav
+
+
+def print_data():
+    if not os.path.exists("shopify_api.json"):
+        sys.exit("Missing [shopify_api.json] file")
+        return
+
+    with open("shopify_api.json", "r") as shopify_api_file:
+        data = json.load(shopify_api_file)
+        api_information = ShopifyAPI(**data)
+
+        print(api_information)
