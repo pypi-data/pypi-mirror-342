a05e35adb890abb0a67958f56cd175f2fd2d8817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:05:04 +0000

    Add package pypql/0.1.0.1/pypql-0.1.0.1.tar.gz

---
+++ packages/pypql/pypql-0.1.0.1.tar.gz/pypql-0.1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypql',
+    version='0.1.0.1',
+    description='A lightweight Pythonic interface for PostgreSQL',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jake Biesinger',
+    author_email='your@email.com',
+    url='https://github.com/yourusername/pypql',
+    packages=find_packages(),
+    install_requires=[
+        'asyncpg',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
