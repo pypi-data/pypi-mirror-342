421b61de12506e433be73fe10bfff3482929fb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:05:03 +0000

    Add package fragfit/0.1.0/fragfit-0.1.0-py3-none-any.whl

---
+++ packages/fragfit/fragfit-0.1.0-py3-none-any.whl/fragfit/version.py
@@ -0,0 +1,6 @@
+"""Version information."""
+
+__version__ = "0.1.0"
+__author__ = "Gabe Reder <gk@reder.io>"
+__license__ = "MIT"
+__description__ = "Utilities for handling chemical formulas and mass spec MS2 fragments" 
