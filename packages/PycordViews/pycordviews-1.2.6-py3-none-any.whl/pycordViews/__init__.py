8837b8828dc0c59fef6214cbb76efa84131b6821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:32 +0000

    Add package PycordViews/1.2.6/pycordviews-1.2.6-py3-none-any.whl

---
+++ packages/PycordViews/pycordviews-1.2.6-py3-none-any.whl/pycordViews/__init__.py
@@ -0,0 +1,6 @@
+from .views.easy_modified_view import EasyModifiedViews
+from .pagination.pagination_view import Pagination
+from .multibot import Multibot
+from .menu.selectMenu import SelectMenu
+from .menu.menu import Menu, CustomSelect
+from .kit import *
