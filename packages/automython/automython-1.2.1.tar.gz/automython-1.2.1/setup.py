679d4bff2dd2f9754cab456205f8a226f98df389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 06:05:19 +0000

    Add package automython/1.2.1/automython-1.2.1.tar.gz

---
+++ packages/automython/automython-1.2.1.tar.gz/automython-1.2.1/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='automython',
+    version='1.2.1',
+    description='A simplistic programming language interpreter to Python to help students grasp finite automata theory programmatically and with a computed graph through visualization libraries.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mkantrr/automython',
+    author='Matthew Kanter',
+    author_email='matthew@mattkanter.com',
+    license='MIT',
+    keywords=['automata', "finite", 'compiler', 'interpreter', 'theory', 'computational theory', "non-deterministic", "turing", "machine", "state"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={'':'automython'},
+    packages=find_packages('automython'),
+    install_requires=[
+        "automata-lib[visual]",
+        "pandas",
+        "ipython",
+        "forbiddenfruit"
+    ],
+    entry_points={
+        'console_scripts': [
+            'automython=interpret.interpreter:interpret'
+        ]
+    }
+)
