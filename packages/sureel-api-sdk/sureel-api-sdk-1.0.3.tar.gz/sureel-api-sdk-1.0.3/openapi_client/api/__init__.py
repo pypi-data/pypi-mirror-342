a8b265f5ad058891e8f6c9516fce6bca990ae5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 14:05:08 +0000

    Add package sureel-api-sdk/1.0.3/sureel-api-sdk-1.0.3.tar.gz

---
+++ packages/sureel-api-sdk/sureel-api-sdk-1.0.3.tar.gz/sureel-api-sdk-1.0.3/openapi_client/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.analysis_history_api import AnalysisHistoryApi
+from openapi_client.api.artist_api import ArtistApi
+from openapi_client.api.audio_ai_api import AudioAiApi
+from openapi_client.api.image_ai_api import ImageAiApi
+from openapi_client.api.prompt_history_api import PromptHistoryApi
+
