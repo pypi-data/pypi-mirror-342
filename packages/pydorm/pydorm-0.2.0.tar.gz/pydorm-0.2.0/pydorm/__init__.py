bd809e5d24322cd47f417292cb00becf346b98c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:04:43 +0000

    Add package pydorm/0.2.0/pydorm-0.2.0.tar.gz

---
+++ packages/pydorm/pydorm-0.2.0.tar.gz/pydorm-0.2.0/pydorm/__init__.py
@@ -0,0 +1,22 @@
+from ._dorm import dorm
+from ._initializer import init
+from ._insert import insert, insert_bulk, upsert, upsert_bulk
+from ._model import model
+from ._query import query, dict_query
+from ._update import update
+
+__author__ = 'melon'
+__version__ = '0.2.0'
+
+__all__ = [
+    'init',
+    'dorm',
+    'query',
+    'update',
+    'insert',
+    'insert_bulk',
+    'upsert',
+    'upsert_bulk',
+    'dict_query',
+    'model'
+]
