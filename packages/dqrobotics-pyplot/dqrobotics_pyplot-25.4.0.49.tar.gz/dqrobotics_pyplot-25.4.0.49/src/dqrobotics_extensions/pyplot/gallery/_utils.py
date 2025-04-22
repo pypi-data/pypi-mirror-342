9147da2fefd916c54c8bde57da3011f8cc73c7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 17:04:50 +0000

    Add package dqrobotics-pyplot/25.4.0.49/dqrobotics_pyplot-25.4.0.49.tar.gz

---
+++ packages/dqrobotics-pyplot/dqrobotics_pyplot-25.4.0.49.tar.gz/dqrobotics_pyplot-25.4.0.49/src/dqrobotics_extensions/pyplot/gallery/_utils.py
@@ -0,0 +1,30 @@
+"""
+Copyright (C) 2025 Murilo Marques Marinho (www.murilomarinho.info)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Author: Murilo M. Marinho
+"""
+from matplotlib import pyplot as plt
+
+def _set_plot_labels():
+    plt.xlabel('x [m]')
+    plt.ylabel('y [m]')
+    plt.gca().set_zlabel('z [m]')
+
+def _set_plot_limits():
+    plt.xlim([-0.5, 0.5])
+    plt.ylim([-0.5, 0.5])
+    plt.gca().set_zlim([-0.5, 0.5])
