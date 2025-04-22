c6112cd07f8bb36145f52de4a5befdf46a31dc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:05:19 +0000

    Add package workflow-graph/0.3.1/workflow_graph-0.3.1.tar.gz

---
+++ packages/workflow-graph/workflow_graph-0.3.1.tar.gz/workflow_graph-0.3.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="workflow-graph",
+    version="0.3.1",
+    description="A lightweight package for managing workflow graphs",
+    author="Dexter Awoyemi",
+    author_email="dexter@dextersjab.xyz",
+    url="https://github.com/dextersjab/workflow-graph",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[],
+    license="MIT",
+)
