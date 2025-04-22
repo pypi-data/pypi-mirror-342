86b9cc5da7f123a8125219984900d518f644179b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:05:18 +0000

    Add package thsdk/0.0.9/thsdk-0.0.9.tar.gz

---
+++ packages/thsdk/thsdk-0.0.9.tar.gz/thsdk-0.0.9/thsdk/__init__.py
@@ -0,0 +1,16 @@
+from .ths_api import *  # Import everything from ths_api
+from .ths_api import __all__ as ths_api_all  # Import __all__ from ths_api
+
+__all__ = (
+    *ths_api_all,
+    "ZhuTHS",
+    "BlockTHS",
+    "InfoTHS"
+)
+
+# Aliases for compatibility
+ZhuTHS = THS
+InfoTHS = ZxTHS
+BlockTHS = BkTHS
+
+__version__ = '1.0.0'
