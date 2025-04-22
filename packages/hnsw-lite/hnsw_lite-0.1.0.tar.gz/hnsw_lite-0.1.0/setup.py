d266378c8459e2a591ed58f04e0b2960382365be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:04:48 +0000

    Add package hnsw-lite/0.1.0/hnsw_lite-0.1.0.tar.gz

---
+++ packages/hnsw-lite/hnsw_lite-0.1.0.tar.gz/hnsw_lite-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hnsw-lite",
+    version="0.1.0",
+    author="Chand",
+    author_email="chandbud5@gmail.com",
+    description="A lightweight, pure Python implementation of HNSW algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chandb5/HNSW-Lite/hnsw-lite",
+    project_urls={
+        "Bug Tracker": "https://github.com/chandb5/HNSW-Lite/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=["plots", "plots.*", "test", "test.*", "venv", "venv.*"]),
+    py_modules=["hnsw", "node", "distance", "search"],
+    python_requires=">=3.10",
+    install_requires=[
+        "numpy",
+    ],
+)
