8af362e66fc14dda0b99d7c5b6653151d3fbb612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 05:04:23 +0000

    Add package packupdate/1.0.5/packupdate-1.0.5.tar.gz

---
+++ packages/packupdate/packupdate-1.0.5.tar.gz/packupdate-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent / "../docs"
+long_description = (this_directory / "python.md").read_text()
+
+setup(
+    name="packupdate",
+    version="1.0.5",
+    description="A Python utility to update Node.js project dependencies safely.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Manish Shrestha",
+    author_email="sth.pratik@gmail.com",
+    url="https://github.com/sthpratik/PackUpdate",
+    packages=find_packages(),  # Automatically finds the 'packUpdate' package
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "updatenpmpackages=packUpdate.updatePackages:main",  # Change the command name here
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
