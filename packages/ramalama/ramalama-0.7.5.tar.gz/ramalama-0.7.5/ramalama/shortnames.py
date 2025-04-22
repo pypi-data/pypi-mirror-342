15c17a3e5f3b09f78e398afb37964723f842599b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:18:53 +0000

    Add package ramalama/0.7.5/ramalama-0.7.5.tar.gz

---
+++ packages/ramalama/ramalama-0.7.5.tar.gz/ramalama-0.7.5/ramalama/shortnames.py
@@ -0,0 +1,45 @@
+import configparser
+import os
+import tempfile
+
+
+class Shortnames:
+    """Shortnames utility class"""
+
+    shortnames = {}
+
+    def __init__(self):
+        file_paths = [
+            "/usr/share/ramalama/shortnames.conf",
+            "/usr/local/share/ramalama/shortnames.conf",
+            "/etc/ramalama/shortnames.conf",
+            os.path.expanduser("~/.local/share/ramalama/shortnames.conf"),
+            os.path.expanduser("~/.config/ramalama/shortnames.conf"),
+            os.path.expanduser("~/.local/pipx/venvs/ramalama/share/ramalama/shortnames.conf"),
+            "./shortnames/shortnames.conf",  # for development
+            "./shortnames.conf",  # for development
+        ]
+
+        for file_path in file_paths:
+            config = configparser.ConfigParser(delimiters=("="))
+            config.read(file_path)
+            if "shortnames" in config:
+                self.shortnames.update(config["shortnames"])
+
+        # Remove leading and trailing quotes from keys and values
+        self.shortnames = {self._strip_quotes(key): self._strip_quotes(value) for key, value in self.shortnames.items()}
+
+    def _strip_quotes(self, s):
+        return s.strip("'\"")
+
+    def resolve(self, model):
+        return self.shortnames.get(model)
+
+    def create_shortname_file(self):
+        shortnamefile = tempfile.NamedTemporaryFile(prefix='RamaLama_shortname_', delete=False)
+        # Open the file for writing.
+        with open(shortnamefile.name, 'w') as c:
+            c.write('[shortnames]\n')
+            for shortname in self.shortnames:
+                c.write('"%s"="%s"\n' % (shortname, self.shortnames.get(shortname)))
+        return shortnamefile.name
---
+++ packages/ramalama/ramalama-0.7.5.tar.gz/ramalama-0.7.5/ramalama/version.py
@@ -0,0 +1,17 @@
+import importlib.metadata
+
+"""Version of RamaLamaPy."""
+
+
+def version():
+    version = "0.7.5"
+    try:
+        return importlib.metadata.version("ramalama")
+    except importlib.metadata.PackageNotFoundError:
+        pass
+
+    return version
+
+
+def print_version(args):
+    print("ramalama version %s" % version())
