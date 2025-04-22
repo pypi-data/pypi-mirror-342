d60ef298d06ce6ffa4b08c1de17f45767b97fa2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 04:05:07 +0000

    Add package adams-beautiful-dataframes/2.0.0/adams_beautiful_dataframes-2.0.0-py3-none-any.whl

---
+++ packages/adams-beautiful-dataframes/adams_beautiful_dataframes-2.0.0-py3-none-any.whl/adams_beautiful_dataframes.py
@@ -0,0 +1,30 @@
+def load_ipython_extension(ipython):
+  from IPython.core.display import HTML
+
+  def hook(result):
+    display(HTML('<style>table { font-family: "Comic Sans MS"; }</style>'))
+    display(HTML('<img src="https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png" />'))
+    # ipython.events.unregister('post_run_cell', hook)
+
+  ipython.events.register('post_run_cell', hook)
+
+def unload_ipython_extension(ipython):
+  pass
+
+
+def install():
+  from pathlib import Path
+
+  try:
+    from IPython.paths import get_ipython_dir
+  except ImportError:
+    return
+
+  path = Path(get_ipython_dir()) / 'profile_default/startup/adams_beautiful_dataframes.py'
+  path.write_text('''try:
+  import adams_beautiful_dataframes
+except ImportError:
+  pass
+else:
+  get_ipython().run_line_magic('load_ext', 'adams_beautiful_dataframes')
+''')
