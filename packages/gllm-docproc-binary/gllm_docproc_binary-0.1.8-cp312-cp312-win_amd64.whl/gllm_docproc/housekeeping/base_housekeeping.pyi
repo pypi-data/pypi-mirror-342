2d7965650ef7f8a148c88a059e72892ed96789cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-22 03:18:36 +0000

    Add package gllm-docproc-binary/0.1.8/gllm_docproc_binary-0.1.8-cp312-cp312-win_amd64.whl

---
+++ packages/gllm-docproc-binary/gllm_docproc_binary-0.1.8-cp312-cp312-win_amd64.whl/gllm_docproc/housekeeping/base_housekeeping.pyi
@@ -0,0 +1,15 @@
+import abc
+from abc import ABC, abstractmethod
+
+class BaseHouseKeeping(ABC, metaclass=abc.ABCMeta):
+    """Base class for document converter."""
+    @abstractmethod
+    def housekeeping(self, folder_path: str) -> None:
+        """Placeholder method for performing housekeeping tasks on a specified folder.
+
+        Args:
+            folder_path (str): The path to the folder to perform housekeeping on.
+
+        Returns:
+            None
+        """
---
+++ packages/gllm-docproc-binary/gllm_docproc_binary-0.1.8-cp312-cp312-win_amd64.whl/gllm_docproc/loader/exception/__init__.pyi
@@ -0,0 +1,3 @@
+from .unsupported_file_extension_error import UnsupportedFileExtensionError as UnsupportedFileExtensionError
+
+__all__ = ['UnsupportedFileExtensionError']
