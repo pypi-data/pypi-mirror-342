bab5cbe6ec154e28bf6980663d9f463a931d568f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:50 +0000

    Add package mflt-build-id/1.1.1/mflt_build_id-1.1.1.tar.gz

---
+++ packages/mflt-build-id/mflt_build_id-1.1.1.tar.gz/mflt_build_id-1.1.1/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# This is a stub file generated so that `pip` can install this package in "editable mode".
+setup(name="mflt-build-id", packages=["mflt_build_id"], version="0.0.5", package_dir={"": "src"})
