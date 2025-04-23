2b2a44d78efef349076397bf4f7253a2533b1cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:06:21 +0000

    Add package amulet-leveldb/2.0.0a8/amulet_leveldb-2.0.0a8.tar.gz

---
+++ packages/amulet-leveldb/amulet_leveldb-2.0.0a8.tar.gz/amulet_leveldb-2.0.0a8/setup.py
@@ -0,0 +1,86 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+from setuptools import setup, Extension, Command
+from setuptools.command.build_ext import build_ext
+
+import versioneer
+
+dependencies = [
+    "amulet-compiler-target==1.0",
+]
+setup_args = {}
+
+try:
+    import amulet_compiler_version
+except ImportError:
+    dependencies.append("amulet-compiler-version==1.3.0")
+else:
+    dependencies.append(
+        f"amulet-compiler-version=={amulet_compiler_version.__version__}"
+    )
+    setup_args["options"] = {
+        "bdist_wheel": {
+            "build_number": f"1.{amulet_compiler_version.compiler_id}.{amulet_compiler_version.compiler_version}"
+        }
+    }
+
+
+cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
+
+
+class CMakeBuild(cmdclass.get("build_ext", build_ext)):
+    def build_extension(self, ext):
+        import pybind11
+        import pybind11_extensions
+
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath("")
+        src_dir = ext_fullpath.parent.resolve()
+
+        platform_args = []
+        if sys.platform == "win32":
+            platform_args.extend(["-G", "Visual Studio 17 2022"])
+            if sys.maxsize > 2**32:
+                platform_args.extend(["-A", "x64"])
+            else:
+                platform_args.extend(["-A", "Win32"])
+            platform_args.extend(["-T", "v143"])
+
+        if subprocess.run(["cmake", "--version"]).returncode:
+            raise RuntimeError("Could not find cmake")
+        if subprocess.run(
+            [
+                "cmake",
+                *platform_args,
+                f"-DPYTHON_EXECUTABLE={sys.executable}",
+                f"-Dpybind11_DIR={pybind11.get_cmake_dir().replace(os.sep, '/')}",
+                f"-Dpybind11_extensions_DIR={pybind11_extensions.__path__[0].replace(os.sep, '/')}",
+                f"-DCMAKE_INSTALL_PREFIX=install",
+                f"-DSRC_INSTALL_DIR={src_dir}",
+                "-B",
+                "build",
+            ]
+        ).returncode:
+            raise RuntimeError("Error configuring amulet_leveldb")
+        if subprocess.run(
+            ["cmake", "--build", "build", "--config", "Release"]
+        ).returncode:
+            raise RuntimeError("Error building amulet_leveldb")
+        if subprocess.run(
+            ["cmake", "--install", "build", "--config", "Release"]
+        ).returncode:
+            raise RuntimeError("Error installing amulet_leveldb")
+
+
+cmdclass["build_ext"] = CMakeBuild
+
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=cmdclass,
+    ext_modules=[Extension("amulet.leveldb._leveldb", [])],
+    install_requires=dependencies,
+    **setup_args,
+)
