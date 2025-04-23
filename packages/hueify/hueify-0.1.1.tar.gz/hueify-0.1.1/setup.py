458a5120a6fc4c9d41c16450d047671f533e6c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 07:06:11 +0000

    Add package hueify/0.1.1/hueify-0.1.1.tar.gz

---
+++ packages/hueify/hueify-0.1.1.tar.gz/hueify-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hueify",
+    version="0.1.1",
+    description="Python bib to control Philips Hue lights and groups.",
+    author="Mathis Arends",
+    url="https://github.com/mathisarends/hueify",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.11.0",
+        "python-dotenv>=1.0.0",
+    ],
+    include_package_data=True,
+    python_requires=">=3.7",
+    license="MIT",
+)
