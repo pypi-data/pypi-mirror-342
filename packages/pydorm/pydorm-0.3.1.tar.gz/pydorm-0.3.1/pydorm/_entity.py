46d7e1e141260059d224246b175d2e56df4111db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 04:06:11 +0000

    Add package pydorm/0.3.1/pydorm-0.3.1.tar.gz

---
+++ packages/pydorm/pydorm-0.3.1.tar.gz/pydorm-0.3.1/pydorm/_entity.py
@@ -0,0 +1,19 @@
+from dataclasses import make_dataclass
+from typing import get_type_hints
+
+
+def entity(table: str):
+    def _entity(cls):
+        annotations = get_type_hints(cls)
+        new_fields = []
+        for name, typ in annotations.items():
+            if name.startswith('__'):
+                continue
+            new_fields.append((name, typ, None))
+        new_cls = make_dataclass(cls.__name__, new_fields, bases=cls.__bases__)
+        new_cls.__table_name__ = table
+        for new_field in new_fields:
+            setattr(new_cls, new_field[0], new_field[0])
+        return new_cls
+
+    return _entity
