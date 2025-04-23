9a68fbb5419178b284ccc531f52b81c3297981e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:16 +0000

    Add package rara-digitizer/1.4.1/rara_digitizer-1.4.1.tar.gz

---
+++ packages/rara-digitizer/rara_digitizer-1.4.1.tar.gz/rara_digitizer-1.4.1/rara_digitizer/__init__.py
@@ -0,0 +1,24 @@
+import logging.config
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "rara-digitizer": {"level": "INFO", "handlers": ["console"]},
+    },
+}
+
+logging.config.dictConfig(logging_config)
