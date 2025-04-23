73de018f39d92781257bc6a0eb5d5115cda12f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:20:06 +0000

    Add package maleo-metadata/0.0.14/maleo_metadata-0.0.14.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.0.14.tar.gz/maleo_metadata-0.0.14/maleo_metadata/models/transfers/results/query/gender.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.models.transfers.results.service.query import BaseServiceQueryResultsTransfers
+
+class MaleoMetadataGenderQueryResultsTransfers:
+    class Row(
+        BaseGeneralSchemas.Name,
+        BaseGeneralSchemas.Key,
+        BaseGeneralSchemas.Order,
+        BaseServiceQueryResultsTransfers.Row
+    ): pass
+
+    class Fail(BaseServiceQueryResultsTransfers.Fail): pass
+
+    class NoData(BaseServiceQueryResultsTransfers.NoData): pass
+
+    class SingleData(BaseServiceQueryResultsTransfers.SingleData):
+        data:MaleoMetadataGenderQueryResultsTransfers.Row
+
+    class MultipleData(BaseServiceQueryResultsTransfers.UnpaginatedMultipleData):
+        data:list[MaleoMetadataGenderQueryResultsTransfers.Row]
