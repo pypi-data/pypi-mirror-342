0b8097346653f21028732942e4b1294c91e2382c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:05:42 +0000

    Add package draccus/0.11.1/draccus-0.11.1.tar.gz

---
+++ packages/draccus/draccus-0.11.1.tar.gz/draccus-0.11.1/draccus/wrappers/docstring.py
@@ -0,0 +1,326 @@
+"""Utility for retrieveing the docstring of a dataclass's attributes
+@author: Fabrice Normandin
+"""
+import inspect
+import re
+from dataclasses import dataclass
+from functools import lru_cache
+from logging import getLogger
+from typing import Dict, List, Optional, Type, Union
+
+from draccus.utils import StringHolderEnum
+
+logger = getLogger(__name__)
+
+# TODO(dlwh): this code sucks so much
+
+
+@dataclass
+class AttributeDocString:
+    """Simple dataclass for holding the comments of a given field."""
+
+    comment_above: str = ""
+    comment_inline: str = ""
+    docstring_below: str = ""
+
+
+class HelpOrder(StringHolderEnum):
+    inline = "inline"
+    above = "above"
+    below = "below"
+
+
+# Default Help Orderings
+HELP_ORDERS: Dict[str, List[str]] = {
+    HelpOrder.inline: ["comment_inline", "docstring_below", "comment_above"],
+    HelpOrder.above: ["comment_above", "docstring_below", "comment_inline"],
+    HelpOrder.below: ["docstring_below", "comment_inline", "comment_above"],
+}
+
+
+def get_preferred_help_text(doc: AttributeDocString, preferred_help: str = HelpOrder.inline) -> Optional[str]:
+    # Iterate in the preferred order with "default" order = < inline | above | below >
+    for doc_attr in HELP_ORDERS[preferred_help]:
+        help_text = getattr(doc, doc_attr)
+        if help_text != "":
+            return help_text
+
+    # No valid help description found... return None
+    return None
+
+
+# at least we can lru this
+@lru_cache(maxsize=100)
+def _get_class_source(some_dataclass: Type) -> Union[list[str], None]:
+    try:
+        source = inspect.getsource(some_dataclass)
+        code_lines: List[str] = source.splitlines()
+        return code_lines
+    except (TypeError, OSError) as e:
+        logger.debug(f"Couldn't find source: {e}")
+        return None
+
+
+# @lru_cache(maxsize=100)
+def get_attribute_docstring(some_dataclass: Type, field_name: str) -> AttributeDocString:
+    """Returns the docstrings of a dataclass field.
+    NOTE: a docstring can either be:
+    - An inline comment, starting with <#>
+    - A Comment on the preceding line, starting with <#>
+    - A docstring on the following line, starting with either <\"\"\"> or <'''>
+
+    Arguments:
+        some_dataclass {type} -- a dataclass
+        field_name {str} -- the name of the field.
+
+    Returns:
+        AttributeDocString -- an object holding the three possible comments
+    """
+
+    code_lines = _get_class_source(some_dataclass)  # type: ignore
+    if code_lines is None:
+        return AttributeDocString()
+
+    # the first line is the class definition, we skip it.
+    start_line_index = 1
+    # starting at the second line, there might be the docstring for the class.
+    # We want to skip over that until we reach an attribute definition.
+    while start_line_index < len(code_lines):
+        if _contains_attribute_definition(code_lines[start_line_index]):
+            break
+        start_line_index += 1
+
+    lines_with_attribute_defs = [
+        (index, line) for index, line in enumerate(code_lines) if _contains_attribute_definition(line)
+    ]
+    for i, line in lines_with_attribute_defs:
+        parts: List[str] = line.split(":", maxsplit=1)
+        if parts[0].strip() == field_name:
+            # we found the line with the definition of this field.
+            comment_above = _get_comment_ending_at_line(code_lines, i - 1)
+            comment_inline = _get_inline_comment_at_line(code_lines, i)
+            docstring_below = _get_docstring_starting_at_line(code_lines, i + 1)
+            complete_docstring = AttributeDocString(comment_above, comment_inline, docstring_below)
+            return complete_docstring
+
+    # we didn't find the attribute.
+    mro = inspect.getmro(some_dataclass)
+    if len(mro) == 1:
+        raise RuntimeWarning(f"Couldn't find the given attribute name {field_name}' within the given class.")
+    for base_class in mro[1:]:
+        try:
+            return get_attribute_docstring(base_class, field_name)
+        except OSError:
+            continue
+
+    logger.debug(
+        f"Couldn't find the attribute docstring for {field_name} in {some_dataclass} or any of its base classes."
+    )
+    return AttributeDocString()
+
+
+DATACLASS_ATTR_RE = re.compile(
+    r"""
+    ^\s*                       # optional leading whitespace
+    (\w+)                      # attribute name
+    \s*:\s*                    # colon with optional whitespace
+    ([\w\[\], .|]+)            # type annotation (very loose match)
+    (\s*=\s*.+)?               # optional default value
+    \s*$                      # optional trailing whitespace
+""",
+    re.VERBOSE,
+)
+
+
+def is_dataclass_attr_line(line: str) -> bool:
+    if line.strip().startswith("def "):
+        return False
+    return bool(DATACLASS_ATTR_RE.match(line))
+
+
+def _contains_attribute_definition(line_str: str) -> bool:
+    """Returns whether or not a line contains a dataclass field definition.
+
+    Arguments:
+        line_str {str} -- the line content
+
+    Returns:
+        bool -- True if there is an attribute definition in the line.
+    """
+    line_str = line_str.split("#", 1)[0].strip()
+    return is_dataclass_attr_line(line_str)
+    # parts = line_str.split("#", maxsplit=1)
+    # before_comment = parts[0].strip()
+    #
+    # before_first_equal = before_comment.split("=", maxsplit=1)[0]
+    # parts = before_first_equal.split(":")
+    # if len(parts) != 2:
+    #     # For now, I don't think it's possible to have a type annotation contain :
+    #     return False
+    # attr_name = parts[0]
+    # attr_type = parts[1]
+    # return not attr_name.isspace() and (attr_type.strip() != "")
+
+
+def _is_empty(line_str: str) -> bool:
+    return line_str.strip() == ""
+
+
+def _is_comment(line_str: str) -> bool:
+    return line_str.strip().startswith("#")
+
+
+def _get_comment_at_line(code_lines: List[str], line: int) -> str:
+    """Gets the comment at line `line` in `code_lines`.
+
+    Arguments:
+        line {int} -- the index of the line in code_lines
+
+    Returns:
+        str -- the comment at the given line. empty string if not present.
+    """
+    line_str = code_lines[line]
+    assert not _contains_attribute_definition(line_str)
+    if "#" not in line_str:
+        return ""
+    parts = line_str.split("#", maxsplit=1)
+    comment = parts[1].strip()
+    return comment
+
+
+def _get_inline_comment_at_line(code_lines: List[str], line: int) -> str:
+    """Gets the inline comment at line `line`.
+
+    Arguments:
+        line {int} -- the index of the line in code_lines
+
+    Returns:
+        str -- the inline comment at the given line, else an empty string.
+    """
+    assert 0 <= line < len(code_lines)
+    assert _contains_attribute_definition(code_lines[line])
+    line_str = code_lines[line]
+    parts = line_str.split("#", maxsplit=1)
+    if len(parts) != 2:
+        return ""
+    comment = parts[1].strip()
+    return comment
+
+
+def _get_comment_ending_at_line(code_lines: List[str], line: int) -> str:
+    start_line = line
+    end_line = line
+    # print(f"Get comment ending at line {line}")
+    # for i, l in enumerate(code_lines):
+    #     print(f"line {i}: {l}")
+
+    # move up the code, one line at a time, while we don't hit the start,
+    # an attribute definition, or the end of a docstring.
+    while start_line > 0:
+        line_str = code_lines[start_line]
+        if _contains_attribute_definition(line_str):
+            break  # previous line is an assignment
+        if '"""' in line_str or "'''" in line_str:
+            break  # previous line has a docstring
+        start_line -= 1
+    start_line += 1
+
+    lines = []
+    for i in range(start_line, end_line + 1):
+        # print(f"line {i}: {code_lines[i]}")
+        if _is_empty(code_lines[i]):
+            continue
+        assert not _contains_attribute_definition(code_lines[i])
+        comment = _get_comment_at_line(code_lines, i)
+        lines.append(comment)
+    return "\n".join(lines)
+
+
+def _get_docstring_starting_at_line(code_lines: List[str], line: int) -> str:
+    i = line
+    token: Optional[str] = None
+    triple_single = "'''"
+    triple_double = '"""'
+    # print("finding docstring starting from line", line)
+
+    # if we are looking further down than the end of the code, there is no
+    # docstring.
+    if line >= len(code_lines):
+        return ""
+    # the list of lines making up the docstring.
+    docstring_contents: List[str] = []
+
+    while i <= len(code_lines):
+        line_str = code_lines[i]
+        # print(f"(docstring) line {line}: {line_str}")
+
+        # we haven't identified the starting line yet.
+        if token is None:
+            if _is_empty(line_str):
+                i += 1
+                continue
+
+            elif _contains_attribute_definition(line_str) or _is_comment(line_str):
+                # we haven't reached the start of a docstring yet (since token
+                # is None), and we reached a line with an attribute definition,
+                # or a comment, hence the docstring is empty.
+                return ""
+
+            elif triple_single in line_str and triple_double in line_str:
+                # This handles something stupid like:
+                # @dataclass
+                # class Bob:
+                #     a: int
+                #     """ hello '''
+                #     bob
+                #     ''' bye
+                #     """
+                triple_single_index = line_str.index(triple_single)
+                triple_double_index = line_str.index(triple_double)
+                if triple_single_index < triple_double_index:
+                    token = triple_single
+                else:
+                    token = triple_double
+            elif triple_double in line_str:
+                token = triple_double
+            elif triple_single in line_str:
+                token = triple_single
+            else:
+                # for i, line in enumerate(code_lines):
+                #     print(f"line {i}: <{line}>")
+                # print(f"token: <{token}>")
+                # print(line_str)
+                logger.debug(f"Warning: Unable to parse attribute docstring: {line_str}")
+                return ""
+
+            # get the string portion of the line (after a token or possibly
+            # between two tokens).
+            parts = line_str.split(token, maxsplit=2)
+            if len(parts) == 3:
+                # This takes care of cases like:
+                # @dataclass
+                # class Bob:
+                #     a: int
+                #     """ hello """
+                between_tokens = parts[1].strip()
+                # print("Between tokens:", between_tokens)
+                docstring_contents.append(between_tokens)
+                break
+
+            elif len(parts) == 2:
+                after_token = parts[1].strip()
+                # print("After token:", after_token)
+                docstring_contents.append(after_token)
+        else:
+            # print(f"token is <{token}>")
+            if token in line_str:
+                # print(f"Line {line} End of a docstring:", line_str)
+                before = line_str.split(token, maxsplit=1)[0]
+                docstring_contents.append(before.strip())
+                break
+            else:
+                # intermediate line without the token.
+                docstring_contents.append(line_str.strip())
+        i += 1
+    # print("Docstring contents:", docstring_contents)
+    return "\n".join(docstring_contents)
---
+++ packages/draccus/draccus-0.11.1.tar.gz/draccus-0.11.1/tests/testutils.py
@@ -0,0 +1,163 @@
+import shlex
+import sys
+import tempfile
+from contextlib import contextmanager, redirect_stderr
+from io import StringIO
+from os import terminal_size
+from typing import Any, Callable, List, Optional, Type, TypeVar
+
+import pytest
+
+import draccus
+from draccus import ParsingError
+
+xfail = pytest.mark.xfail
+parametrize = pytest.mark.parametrize
+
+
+def xfail_param(*args, reason: str):
+    if len(args) == 1 and isinstance(args[0], tuple):
+        args = args[0]
+    return pytest.param(*args, marks=pytest.mark.xfail(reason=reason))
+
+
+Dataclass = TypeVar("Dataclass")
+
+
+@contextmanager
+def raises(exception=ParsingError, match=None, code: Optional[int] = None):
+    with pytest.raises(exception, match=match):
+        yield
+
+
+@contextmanager
+def exits_and_writes_to_stderr(match: str = ""):
+    s = StringIO()
+    with redirect_stderr(s), raises(SystemExit):
+        yield
+    s.seek(0)
+    err_string = s.read()
+    if match:
+        assert match in err_string, err_string
+    else:
+        assert err_string, err_string
+
+
+@contextmanager
+def raises_missing_required_arg():
+    with exits_and_writes_to_stderr("the following arguments are required"):
+        yield
+
+
+@contextmanager
+def raises_expected_n_args(n: int):
+    with exits_and_writes_to_stderr(f"expected {n} arguments"):
+        yield
+
+
+@contextmanager
+def raises_unrecognized_args(*args: str):
+    with exits_and_writes_to_stderr("unrecognized arguments: " + " ".join(args or [])):
+        yield
+
+
+def assert_help_output_equals(actual: str, expected: str):
+    # Replace the start with `prog`, since the tests runner might not always be
+    # `pytest`, could also be __main__ when debugging with VSCode
+    prog = sys.argv[0].split("/")[-1]
+    if prog != "pytest":
+        expected = expected.replace("usage: pytest", f"usage: {prog}")
+    # remove = string.punctuation + string.whitespace
+    actual_str = "".join(actual.split())
+    expected_str = "".join(expected.split())
+    assert actual_str == expected_str, f"{actual_str} != {expected_str}"
+
+
+T = TypeVar("T")
+
+
+class TestSetup:
+    @classmethod
+    def setup(cls: Type[Dataclass], arguments: Optional[str] = "", config: Optional[str] = None, **kwargs) -> Dataclass:
+        """Basic setup for a tests.
+
+        Keyword Arguments:
+            arguments {Optional[str]} -- The arguments to pass to the parser (default: {""})
+            dest {Optional[str]} -- the attribute where the argument should be stored. (default: {None})
+
+        Returns:
+            {cls}} -- the class's type.
+        """
+        if arguments is not None:
+            arguments = shlex.split(arguments)  # type: ignore
+        if config is not None:
+            f = tempfile.NamedTemporaryFile(suffix=".yaml")
+            with open(f.name, "w") as fd:
+                fd.write(config)
+            cfg = draccus.parse(
+                config_class=cls, args=arguments, prog="draccus", config_path=f.name, **kwargs, exit_on_error=False
+            )
+        else:
+            cfg = draccus.parse(config_class=cls, args=arguments, prog="draccus", **kwargs, exit_on_error=False)
+        return cfg
+
+    @classmethod
+    def get_help_text(cls, flag="--help") -> str:
+        import argparse
+        import contextlib
+        from io import StringIO
+
+        # we need to mock/override shutils terminal width for consistent test results
+        from unittest.mock import patch
+
+        forced_size = terminal_size((80, 120))
+
+        f = StringIO()
+        with (
+            contextlib.suppress(SystemExit),
+            contextlib.redirect_stdout(f),
+            patch("shutil.get_terminal_size", return_value=forced_size),
+        ):
+            _ = cls.setup(
+                arguments=flag,
+            )
+        s = f.getvalue().strip()
+
+        # python changed "optional arguments:" to "options" in 3.9
+        import re
+
+        replace_options = re.compile(r"(?m)^(optional arguments:|options:)$")
+        s = replace_options.sub("options:", s)
+        return s
+
+
+ListFormattingFunction = Callable[[List[Any]], str]
+ListOfListsFormattingFunction = Callable[[List[List[Any]]], str]
+
+
+def format_list_using_spaces(value_list: List[Any]) -> str:
+    return " ".join(str(p) for p in value_list)
+
+
+def format_list_using_brackets(value_list: List[Any]) -> str:
+    return f"[{','.join(str(p) for p in value_list)}]"
+
+
+def format_list_using_single_quotes(value_list: List[Any]) -> str:
+    return f"'{format_list_using_spaces(value_list)}'"
+
+
+def format_list_using_double_quotes(value_list: List[Any]) -> str:
+    return f'"{format_list_using_spaces(value_list)}"'
+
+
+def format_lists_using_brackets(list_of_lists: List[List[Any]]) -> str:
+    return " ".join(format_list_using_brackets(value_list) for value_list in list_of_lists)
+
+
+def format_lists_using_double_quotes(list_of_lists: List[List[Any]]) -> str:
+    return " ".join(format_list_using_double_quotes(value_list) for value_list in list_of_lists)
+
+
+def format_lists_using_single_quotes(list_of_lists: List[List[Any]]) -> str:
+    return " ".join(format_list_using_single_quotes(value_list) for value_list in list_of_lists)
