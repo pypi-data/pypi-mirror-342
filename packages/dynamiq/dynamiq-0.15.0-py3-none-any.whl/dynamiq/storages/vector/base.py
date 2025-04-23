a3eeaf84558fa8e15eb2f0a8c816a106d7cdc69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:28 +0000

    Add package dynamiq/0.15.0/dynamiq-0.15.0-py3-none-any.whl

---
+++ packages/dynamiq/dynamiq-0.15.0-py3-none-any.whl/dynamiq/storages/vector/base.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class BaseVectorStoreParams(BaseModel):
+    """Base parameters for vector store.
+
+    Attributes:
+        index_name (str): Name of the index. Defaults to "default".
+        content_key (str): Key for content field. Defaults to "content".
+    """
+    index_name: str = "default"
+    content_key: str = "content"
+
+
+class BaseWriterVectorStoreParams(BaseVectorStoreParams):
+    """Parameters for writer vector store.
+
+    Attributes:
+        create_if_not_exist (bool): Flag to create index if it does not exist. Defaults to True.
+    """
+
+    create_if_not_exist: bool = False
