da8b4db476db29f4dfb6b7f3a72f47d696cdd890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:20:08 +0000

    Add package fluxiondb/0.0.1/fluxiondb-0.0.1-py3-none-any.whl

---
+++ packages/fluxiondb/fluxiondb-0.0.1-py3-none-any.whl/fluxiondb/__init__.py
@@ -0,0 +1,8 @@
+"""
+FluxionDB Python client.
+
+A client library for interacting with Fluxion, an open-source, self-hosted 
+multi-modal embedding and vector database solution.
+"""
+
+__version__ = "0.0.1"
