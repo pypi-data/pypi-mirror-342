157850079a216e3f90548937ba7b5bcf892435eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:47 +0000

    Add package myfeeds-ai/1.105.0/myfeeds_ai-1.105.0-py3-none-any.whl

---
+++ packages/myfeeds-ai/myfeeds_ai-1.105.0-py3-none-any.whl/myfeeds_ai/providers/cyber_security/owasp/routes/Routes__Owasp.py
@@ -0,0 +1,34 @@
+from fastapi                                                                    import Response
+from osbot_fast_api.api.Fast_API_Routes                                         import Fast_API_Routes
+from myfeeds_ai.data_feeds.Data_Feeds__S3__Key_Generator                        import S3_Key__File__Content_Type
+from myfeeds_ai.providers.cyber_security.owasp.actions.Owasp__Files__Top_10     import Owasp__Files__Top_10
+from myfeeds_ai.providers.cyber_security.owasp.files.Owasp__File__Top_10        import Owasp__File__Top_10
+from myfeeds_ai.providers.cyber_security.owasp.schemas.Owasp__Top_10__Category  import Owasp__Top_10__Category
+
+ROUTE_PATH__OWASP = 'owasp'
+
+ROUTES_PATHS__OWASP = [f'/{ROUTE_PATH__OWASP}/all-files'    ,
+                       f'/{ROUTE_PATH__OWASP}/raw-data'     ,
+                       f'/{ROUTE_PATH__OWASP}/raw-data-json']
+
+class Routes__Owasp(Fast_API_Routes):
+    tag                : str = ROUTE_PATH__OWASP
+    owasp_files_top_10 : Owasp__Files__Top_10
+
+    def raw_data(self, category: Owasp__Top_10__Category):
+        raw_data = self.owasp_files_top_10.raw_data(category)
+        return Response(content=raw_data, media_type=str(S3_Key__File__Content_Type.MARKDOWN))
+
+    def raw_data_json(self, category: Owasp__Top_10__Category):
+        raw_data_json = self.owasp_files_top_10.raw_data__json(category)
+        return raw_data_json
+
+
+    def all_files(self):
+        storage = Owasp__File__Top_10().hacker_news_storage
+        return storage.s3_db.s3_folder_files__all('public-data/owasp')
+
+    def setup_routes(self):
+        self.add_route_get(self.raw_data        )
+        self.add_route_get(self.raw_data_json   )
+        self.add_route_get(self.all_files       )
