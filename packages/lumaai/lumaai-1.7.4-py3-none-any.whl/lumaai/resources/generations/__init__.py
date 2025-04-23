a40cec5079f7d7d960e69dd87e5182aac7d8b976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:05:53 +0000

    Add package lumaai/1.7.4/lumaai-1.7.4-py3-none-any.whl

---
+++ packages/lumaai/lumaai-1.7.4-py3-none-any.whl/lumaai/resources/generations/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .image import (
+    ImageResource,
+    AsyncImageResource,
+    ImageResourceWithRawResponse,
+    AsyncImageResourceWithRawResponse,
+    ImageResourceWithStreamingResponse,
+    AsyncImageResourceWithStreamingResponse,
+)
+from .video import (
+    VideoResource,
+    AsyncVideoResource,
+    VideoResourceWithRawResponse,
+    AsyncVideoResourceWithRawResponse,
+    VideoResourceWithStreamingResponse,
+    AsyncVideoResourceWithStreamingResponse,
+)
+from .generations import (
+    GenerationsResource,
+    AsyncGenerationsResource,
+    GenerationsResourceWithRawResponse,
+    AsyncGenerationsResourceWithRawResponse,
+    GenerationsResourceWithStreamingResponse,
+    AsyncGenerationsResourceWithStreamingResponse,
+)
+from .camera_motion import (
+    CameraMotionResource,
+    AsyncCameraMotionResource,
+    CameraMotionResourceWithRawResponse,
+    AsyncCameraMotionResourceWithRawResponse,
+    CameraMotionResourceWithStreamingResponse,
+    AsyncCameraMotionResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "CameraMotionResource",
+    "AsyncCameraMotionResource",
+    "CameraMotionResourceWithRawResponse",
+    "AsyncCameraMotionResourceWithRawResponse",
+    "CameraMotionResourceWithStreamingResponse",
+    "AsyncCameraMotionResourceWithStreamingResponse",
+    "ImageResource",
+    "AsyncImageResource",
+    "ImageResourceWithRawResponse",
+    "AsyncImageResourceWithRawResponse",
+    "ImageResourceWithStreamingResponse",
+    "AsyncImageResourceWithStreamingResponse",
+    "VideoResource",
+    "AsyncVideoResource",
+    "VideoResourceWithRawResponse",
+    "AsyncVideoResourceWithRawResponse",
+    "VideoResourceWithStreamingResponse",
+    "AsyncVideoResourceWithStreamingResponse",
+    "GenerationsResource",
+    "AsyncGenerationsResource",
+    "GenerationsResourceWithRawResponse",
+    "AsyncGenerationsResourceWithRawResponse",
+    "GenerationsResourceWithStreamingResponse",
+    "AsyncGenerationsResourceWithStreamingResponse",
+]
