2d181eb781f804668ccbf7286cf8212128ce0bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:20:04 +0000

    Add package tesorotools-python/0.0.1/tesorotools_python-0.0.1-py2.py3-none-any.whl

---
+++ packages/tesorotools-python/tesorotools_python-0.0.1-py2.py3-none-any.whl/tesorotools/utils/globals.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+DEBUG: Path = Path("debug")
+CONFIG: Path = Path("config")
+EXAMPLES: Path = Path("examples")
+
+ASSETS: Path = Path("assets")
+FONTS: Path = ASSETS / "fonts"
+
+STYLE_SHEET: Path = Path("tesoro.mplstyle")
+
+PLOT_CONFIG_FILE: Path = EXAMPLES / "plots.yaml"
