9b0c46c82b1e0bc238a8485e692bc3cc2249344c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:31 +0000

    Add package viprs/0.1.3/viprs-0.1.3-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/viprs/viprs-0.1.3-cp39-cp39-macosx_10_9_x86_64.whl/viprs/utils/data_utils.py
@@ -0,0 +1,13 @@
+
+def download_ld_matrix(target_dir='.', chromosome=None):
+    """
+    Download LD matrices for VIPRS software.
+
+    TODO: Update this once data is made available.
+
+    :param target_dir: The path or directory where to store the LD matrix
+    :param chromosome: An integer or list of integers with the chromosome numbers for which to download
+    the LD matrices from Zenodo.
+    """
+
+    raise NotImplementedError("This function is not yet implemented.")
