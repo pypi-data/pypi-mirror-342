70101c35c6f5eb6ae3e368ef3bad5d284322a17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:48 +0000

    Add package rbyte/0.22.1/rbyte-0.22.1.tar.gz

---
+++ packages/rbyte/rbyte-0.22.1.tar.gz/rbyte-0.22.1/src/rbyte/io/__init__.py
@@ -0,0 +1,80 @@
+from ._json import JsonDataFrameBuilder
+from ._numpy import NumpyTensorSource
+from .dataframe import (
+    DataFrameAligner,
+    DataFrameConcater,
+    DataFrameFpsResampler,
+    DataFrameIndexer,
+    DataFrameJoiner,
+    DataFrameWithColumns,
+    FixedWindowSampleBuilder,
+)
+from .path import PathDataFrameBuilder, PathTensorSource
+
+__all__: list[str] = [
+    "DataFrameAligner",
+    "DataFrameConcater",
+    "DataFrameFpsResampler",
+    "DataFrameIndexer",
+    "DataFrameJoiner",
+    "DataFrameWithColumns",
+    "FixedWindowSampleBuilder",
+    "JsonDataFrameBuilder",
+    "NumpyTensorSource",
+    "PathDataFrameBuilder",
+    "PathTensorSource",
+]
+
+
+try:
+    from .hdf5 import Hdf5DataFrameBuilder, Hdf5TensorSource
+except ImportError:
+    pass
+else:
+    __all__ += ["Hdf5DataFrameBuilder", "Hdf5TensorSource"]
+
+try:
+    from ._mcap import (
+        JsonMcapDecoderFactory,
+        McapDataFrameBuilder,
+        McapTensorSource,
+        ProtobufMcapDecoderFactory,
+    )
+except ImportError:
+    pass
+else:
+    __all__ += [
+        "JsonMcapDecoderFactory",
+        "McapDataFrameBuilder",
+        "McapTensorSource",
+        "ProtobufMcapDecoderFactory",
+    ]
+
+try:
+    from .rrd import RrdDataFrameBuilder, RrdFrameSource
+except ImportError:
+    pass
+else:
+    __all__ += ["RrdDataFrameBuilder", "RrdFrameSource"]
+
+try:
+    from .video import TorchCodecFrameSource, VideoDataFrameBuilder
+except ImportError:
+    pass
+else:
+    __all__ += ["TorchCodecFrameSource", "VideoDataFrameBuilder"]
+
+try:
+    from .yaak.metadata import YaakMetadataDataFrameBuilder
+except ImportError:
+    pass
+else:
+    __all__ += ["YaakMetadataDataFrameBuilder"]
+
+
+try:
+    from .geo import GeoDataFrameBuilder, WaypointBuilder, WaypointNormalizer
+except ImportError:
+    pass
+else:
+    __all__ += ["GeoDataFrameBuilder", "WaypointBuilder", "WaypointNormalizer"]
