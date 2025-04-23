72f49db56db86e30ddd61a8977fe8d89259c0890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:56 +0000

    Add package maleo-metadata/0.0.16/maleo_metadata-0.0.16-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.0.16-py3-none-any.whl/maleo_metadata/models/transfers/parameters/general/gender.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from pydantic import Field
+from maleo_foundation.models.transfers.parameters.general import BaseGeneralParametersTransfers
+from maleo_foundation.types import BaseTypes
+from maleo_metadata.enums.gender import MaleoMetadataGenderEnums
+
+class MaleoMetadataGenderGeneralParametersTransfers:
+    class GetSingleQuery(BaseGeneralParametersTransfers.GetSingleQuery): pass
+
+    class GetSingle(BaseGeneralParametersTransfers.GetSingle):
+        identifier:MaleoMetadataGenderEnums.IdentifierType = Field(..., description="Identifier")
