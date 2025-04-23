26dd646147de2487311dc0c952d96c9e796f3985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 07:05:45 +0000

    Add package trismik/0.9.1/trismik-0.9.1-py3-none-any.whl

---
+++ packages/trismik/trismik-0.9.1-py3-none-any.whl/trismik/_mapper.py
@@ -0,0 +1,79 @@
+from typing import List, Any
+
+from dateutil.parser import parse as parse_date
+
+from .exceptions import TrismikApiError
+from .types import (
+    TrismikItem,
+    TrismikResult,
+    TrismikMultipleChoiceTextItem,
+    TrismikTextChoice,
+    TrismikAuth,
+    TrismikTest,
+    TrismikSession,
+    TrismikResponse,
+)
+
+
+class TrismikResponseMapper:
+
+    @staticmethod
+    def to_auth(json: dict[str, Any]) -> TrismikAuth:
+        return TrismikAuth(
+                token=json["token"],
+                expires=parse_date(json["expires"]),
+        )
+
+    @staticmethod
+    def to_tests(json: List[dict[str, Any]]) -> List[TrismikTest]:
+        return [
+            TrismikTest(
+                    id=item["id"],
+                    name=item["name"],
+            ) for item in json
+        ]
+
+    @staticmethod
+    def to_session(json: dict[str, Any]) -> TrismikSession:
+        return TrismikSession(
+                id=json["id"],
+                url=json["url"],
+                status=json["status"],
+        )
+
+    @staticmethod
+    def to_item(json: dict[str, Any]) -> TrismikItem:
+        if json["type"] == "multiple_choice_text":
+            return TrismikMultipleChoiceTextItem(
+                    id=json["id"],
+                    question=json["question"],
+                    choices=[
+                        TrismikTextChoice(
+                                id=choice["id"],
+                                text=choice["text"],
+                        ) for choice in json["choices"]
+                    ]
+            )
+        else:
+            raise TrismikApiError(
+                    f"API has returned unrecognized item type: {json['type']}")
+
+    @staticmethod
+    def to_results(json: List[dict[str, Any]]) -> List[TrismikResult]:
+        return [
+            TrismikResult(
+                    trait=item["trait"],
+                    name=item["name"],
+                    value=item["value"],
+            ) for item in json
+        ]
+
+    @staticmethod
+    def to_responses(json: List[dict[str, Any]]) -> List[TrismikResponse]:
+        return [
+            TrismikResponse(
+                    item_id=response["itemId"],
+                    value=response["value"],
+                    score=response["score"],
+            ) for response in json
+        ]
---
+++ packages/trismik/trismik-0.9.1-py3-none-any.whl/trismik/types.py
@@ -0,0 +1,163 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Dict, List, Any, Optional
+
+
+@dataclass
+class TrismikAuth:
+    """
+    Authentication token.
+
+    Attributes:
+        token (str): Authentication token value.
+        expires (datetime): Expiration date.
+    """
+    token: str
+    expires: datetime
+
+
+@dataclass
+class TrismikTest:
+    """
+    Available test.
+
+    Attributes:
+        id (str): Test ID.
+        name (str): Test name.
+    """
+    id: str
+    name: str
+
+
+@dataclass
+class TrismikSession:
+    """
+    Test session.
+
+    Attributes:
+        id (str): Session ID.
+        url (str): Session URL.
+        status (str): Session status
+    """
+    id: str
+    url: str
+    status: str
+
+
+@dataclass
+class TrismikItem:
+    """
+    Base class for test items.
+
+    Attributes:
+        id (str): Item ID.
+    """
+    id: str
+
+
+@dataclass
+class TrismikChoice:
+    """
+    Base class for choices in items that use them.
+
+    Attributes:
+        id (str): Choice ID.
+    """
+    id: str
+
+
+@dataclass
+class TrismikTextChoice(TrismikChoice):
+    """
+    Text choice.
+
+    Attributes:
+        text (str): Choice text.
+    """
+    text: str
+
+
+@dataclass
+class TrismikMultipleChoiceTextItem(TrismikItem):
+    """
+    Multiple choice text item.
+
+    Attributes:
+        question (str): Question text.
+        choices (List[TrismikTextChoice]): List of choices.
+    """
+    question: str
+    choices: List[TrismikTextChoice]
+
+
+@dataclass
+class TrismikResult:
+    """
+    Test result.
+
+    Attributes:
+        trait (str): Trait name.
+        name (str): Result name/type.
+        value (Any): Result value.
+    """
+    trait: str
+    name: str
+    value: Any
+
+
+@dataclass
+class TrismikResponse:
+    """
+    Test result.
+
+    Attributes:
+        item_id (str): Item ID.
+        value (Any): Result value.
+        score (float): Score.
+    """
+    item_id: str
+    value: Any
+    score: float
+
+
+@dataclass
+class TrismikRunResults:
+    """
+    Test results and responses.
+
+    Attributes:
+        results (List[TrismikResult]): Results.
+        responses (List[TrismikResponse]): Responses.
+    """
+    session_id: str
+    results: List[TrismikResult]
+    responses: Optional[List[TrismikResponse]] = None
+
+@dataclass
+class TrismikSessionMetadata:
+    """
+    Metadata associated to a session
+
+    Attributes:
+        model_metadata (dict[str, Any]): Metadata about the model.
+        test_configuration (dict[str, Any]): Metadata about the test.
+        inference_setup (dict[str, Any]): Metadata about the inference setup.
+    """   
+
+    class ModelMetadata:
+        def __init__(self, name: str, **kwargs: Any):
+            self.name = name
+            for key, value in kwargs.items():
+                setattr(self, key, value)                
+
+    model_metadata: ModelMetadata
+    test_configuration: dict[str, Any]
+    inference_setup: dict[str, Any]
+
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "model_metadata": vars(self.model_metadata),
+            "test_configuration": self.test_configuration,
+            "inference_setup": self.inference_setup,
+        }
+
