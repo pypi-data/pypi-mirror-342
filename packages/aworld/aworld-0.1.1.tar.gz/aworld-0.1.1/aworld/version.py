85dc6faf3c7ce44e739776b2303dd58c9fba84eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:29 +0000

    Add package aworld/0.1.1/aworld-0.1.1.tar.gz

---
+++ packages/aworld/aworld-0.1.1.tar.gz/aworld-0.1.1/aworld/version.py
@@ -0,0 +1,14 @@
+
+# auto generated
+class VersionInfo(object):
+    @property
+    def build_date(self):
+      return "2025-04-23 08:33:41"
+
+    @property
+    def version(self):
+      return "0.1.1"
+
+    @property
+    def build_user(self):
+      return "zhuxiaoer"
