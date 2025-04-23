75148b657833cd2eae697c418d1ff9b5b4634110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 16:06:16 +0000

    Add package Mypackage-dev2025/0.0.7/mypackage_dev2025-0.0.7.tar.gz

---
+++ packages/Mypackage-dev2025/mypackage_dev2025-0.0.7.tar.gz/mypackage_dev2025-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mypackage_dev2025',
+    version='0.0.7',
+    packages=find_packages(),
+    description='A simple math package',
+    author='Jayadev',
+    author_email='swarnamathi@gmail.com',
+    install_requires=[],
+)
+
