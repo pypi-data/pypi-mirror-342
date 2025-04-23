63c9793260405cb65b633e525b51da994c4802ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:07:33 +0000

    Add package mtcli/1.5.2/mtcli-1.5.2-py3-none-any.whl

---
+++ packages/mtcli/mtcli-1.5.2-py3-none-any.whl/mtcli/mt.py
@@ -0,0 +1,31 @@
+"""
+Aplicativo CLI para exibir gráficos do MetaTrader 5
+"""
+
+import click
+from mtcli import __version__
+from mtcli.bars import bars
+from mtcli.mm import mm
+from mtcli.rm import rm
+from mtcli.conf import conf
+from mtcli.ma import ma
+
+
+@click.group(invoke_without_command=True)
+@click.option("--version", "-v", is_flag=True, help="Exibe a versao do mtcli.")
+def mt(version):
+    """Exibe o grafico de velas do MetaTrader 5 em texto."""
+    if version:
+        click.echo("mtcli %s" % __version__)
+        return 0
+
+
+mt.add_command(bars)
+mt.add_command(mm)
+mt.add_command(rm)
+mt.add_command(conf)
+mt.add_command(ma)
+
+
+if __name__ == "__main__":
+    mt()
