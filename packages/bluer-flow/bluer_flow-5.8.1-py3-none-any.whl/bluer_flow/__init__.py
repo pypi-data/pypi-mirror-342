6778e694117da047c27ab3ff8e6c13e6495d9fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:45 +0000

    Add package bluer-flow/5.8.1/bluer_flow-5.8.1-py3-none-any.whl

---
+++ packages/bluer-flow/bluer_flow-5.8.1-py3-none-any.whl/bluer_flow/__init__.py
@@ -0,0 +1,17 @@
+NAME = "bluer_flow"
+
+ICON = "📜"
+
+DESCRIPTION = f"{ICON} workflow management on aws batch and more."
+
+VERSION = "5.8.1"
+
+REPO_NAME = "bluer-flow"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true"
+
+ALIAS = "@flow"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
