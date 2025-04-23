91ce2a0be4d1a2f0a3e44a04c7ddbc6050b7270c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:45 +0000

    Add package bluer-flow/5.8.1/bluer_flow-5.8.1.tar.gz

---
+++ packages/bluer-flow/bluer_flow-5.8.1.tar.gz/bluer_flow-5.8.1/bluer_flow/.abcli/alias.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+alias @flow=bluer_flow
+
+alias workflow="bluer_flow_workflow"
---
+++ packages/bluer-flow/bluer_flow-5.8.1.tar.gz/bluer_flow-5.8.1/bluer_flow/workflow/runners/__init__.py
@@ -0,0 +1,11 @@
+from typing import List
+from enum import Enum, auto
+
+
+class RunnerType(Enum):
+    GENERIC = auto()
+    LOCAL = auto()
+
+
+def list_of_runners() -> List[str]:
+    return sorted([runner_type.name.lower() for runner_type in RunnerType])
