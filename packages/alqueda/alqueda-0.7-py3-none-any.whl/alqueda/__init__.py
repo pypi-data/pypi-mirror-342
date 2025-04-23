06d9400fb20fe9a73c8a01bcff882b4f2b00d2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:44 +0000

    Add package alqueda/0.7/alqueda-0.7-py3-none-any.whl

---
+++ packages/alqueda/alqueda-0.7-py3-none-any.whl/alqueda/__init__.py
@@ -0,0 +1,17 @@
+from .alqueda import alqueda
+
+error_correction_learning_algo = alqueda.error_correction_learning_algo
+memory_based_algo = alqueda.memory_based_algo
+hebbian_learning = alqueda.hebbian_learning
+gate_operation_single_layer = alqueda.gate_operation_single_layer
+mlp_xor_gate = alqueda.mlp_xor_gate
+xor_with_rbf = alqueda.xor_with_rbf
+hebbian_vs_pca = alqueda.hebbian_vs_pca
+som = alqueda.som
+bptt_rnn = alqueda.bptt_rnn
+hopfield = alqueda.hopfield
+neural_network_mnsit = alqueda.neural_network_mnsit
+optimization_methods = alqueda.optimization_methods
+resNet_alexNet_vgg_placenet = alqueda.resNet_alexNet_vgg_placenet
+vgg = alqueda.vgg
+rnn_lstm = alqueda.rnn_lstm
