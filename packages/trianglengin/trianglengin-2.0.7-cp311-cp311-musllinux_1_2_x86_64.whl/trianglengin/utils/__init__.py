20eccd74bdc02cd0d0ff53e95fa9b8566ad1db26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 07:05:51 +0000

    Add package trianglengin/2.0.7/trianglengin-2.0.7-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/trianglengin/trianglengin-2.0.7-cp311-cp311-musllinux_1_2_x86_64.whl/trianglengin/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+General utilities for the Triangle Engine.
+"""
+
+# --- ADDED: Export geometry ---
+from . import geometry
+from .types import ActionType
+
+__all__ = ["ActionType", "geometry"]
