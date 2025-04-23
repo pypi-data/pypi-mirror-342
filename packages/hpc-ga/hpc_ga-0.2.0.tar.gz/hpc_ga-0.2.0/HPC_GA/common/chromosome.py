7bb1437b6a915ccac54b189defa2cb751f8cef7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:34 +0000

    Add package hpc-ga/0.2.0/hpc_ga-0.2.0.tar.gz

---
+++ packages/hpc-ga/hpc_ga-0.2.0.tar.gz/hpc_ga-0.2.0/HPC_GA/common/chromosome.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Chromosome(ABC):
+    def __init__(self, genes):
+        self.genes = genes
+        self._fitness = None
+
+    @abstractmethod
+    def evaluate(self) -> float:
+        pass
+
+    @property
+    def fitness(self) -> float:
+        if self._fitness is None:
+            self._fitness = self.evaluate()
+        return self._fitness
+
+    def __lt__(self, other: 'Chromosome') -> bool:
+        return self.fitness < other.fitness
