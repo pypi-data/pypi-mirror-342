78ceeb681048f34137561812c76ab93a548d8188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:07:28 +0000

    Add package runtimepy/5.12.1/runtimepy-5.12.1-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-5.12.1-py3-none-any.whl/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.2.1
+# hash=ba0d30a48267eea48f57090cd3a871b2
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "5.12.1"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
