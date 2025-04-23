b1312682b3367422379c968f0459748ef8217afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:15 +0000

    Add package mm-btc/0.4.2/mm_btc-0.4.2-py3-none-any.whl

---
+++ packages/mm-btc/mm_btc-0.4.2-py3-none-any.whl/mm_btc/cli/cmd/address_cmd.py
@@ -0,0 +1,10 @@
+from mm_std import print_json
+
+from mm_btc.blockstream import BlockstreamClient
+from mm_btc.wallet import is_testnet_address
+
+
+async def run(address: str) -> None:
+    client = BlockstreamClient(testnet=is_testnet_address(address))
+    res = await client.get_address(address)
+    print_json(res.value_or_error())
