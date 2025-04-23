1a467db5f94f82f54e860c681ac32626f1548a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 11:05:43 +0000

    Add package modland/1.1.2/modland-1.1.2-py3-none-any.whl

---
+++ packages/modland/modland-1.1.2-py3-none-any.whl/modland/modland.py
@@ -0,0 +1,37 @@
+"""
+This module handles georeferencing MODIS land tiles.
+"""
+# rotation in affine transformation is not supported
+# latitude and longitude are in WGS84 geographic coordinate system
+import warnings
+import geopandas as gpd
+import numpy as np
+import pyproj
+import rasterio
+from affine import Affine
+from numpy import where, nan, isnan, all
+from pyproj import Proj
+from scipy.spatial import ConvexHull
+from shapely.geometry import Polygon, Point, MultiPolygon
+from shapely.geometry import mapping
+from shapely.geometry.base import BaseGeometry
+from shapely.geometry.polygon import LinearRing
+
+from .constants import *
+from .projection import *
+from .dimensions import *
+from .get_proj4 import get_proj4
+from .transform_shape import *
+from .latlon_to_sinusoidal import latlon_to_sinusoidal
+from .sinusoidal_to_latlon import sinusoidal_to_latlon
+from .sinusoidal_to_modland import sinusoidal_to_modland
+from .latlon_to_modland import latlon_to_modland
+from .MODIS_land_tile import MODISLandTile
+from .modland_pixels import *
+from .outlines import *
+from .find_modland_tiles import find_modland_tiles
+from .modland_affine import *
+from .parsehv import parsehv
+from .indices import *
+
+__author__ = 'Gregory H. Halverson'
