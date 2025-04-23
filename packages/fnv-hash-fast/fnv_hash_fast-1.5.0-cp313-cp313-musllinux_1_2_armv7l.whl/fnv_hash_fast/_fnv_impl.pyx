5b94a19eb78a4ad31aa953db440e1b67d3c48e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:31 +0000

    Add package fnv-hash-fast/1.5.0/fnv_hash_fast-1.5.0-cp313-cp313-musllinux_1_2_armv7l.whl

---
+++ packages/fnv-hash-fast/fnv_hash_fast-1.5.0-cp313-cp313-musllinux_1_2_armv7l.whl/fnv_hash_fast/_fnv_impl.pyx
@@ -0,0 +1,10 @@
+# distutils: language = c
+
+import cython
+
+
+cdef extern from "fnv_wrapper.h":
+    unsigned int _c_fnv1a_32(const unsigned char *data, size_t len)
+
+def _fnv1a_32(data: bytes) -> int:
+    return _c_fnv1a_32(data, len(data))
