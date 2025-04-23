1faae13e441a6abe0e87ed2093cb4f52173e08a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 04:05:28 +0000

    Add package aare/2025.4.22/aare-2025.4.22-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aare/aare-2025.4.22-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aare/__init__.py
@@ -0,0 +1,28 @@
+# Make the compiled classes that live in _aare available from aare.
+from . import _aare
+
+
+from ._aare import File, RawMasterFile, RawSubFile, JungfrauDataFile
+from ._aare import Pedestal_d, Pedestal_f, ClusterFinder, VarClusterFinder
+from ._aare import DetectorType
+from ._aare import ClusterFile
+from ._aare import hitmap
+from ._aare import ROI
+
+from ._aare import ClusterFinderMT, ClusterCollector, ClusterFileSink, ClusterVector_i
+
+from ._aare import fit_gaus, fit_pol1
+from ._aare import Interpolator
+
+
+from ._aare import apply_custom_weights
+
+from .CtbRawFile import CtbRawFile
+from .RawFile import RawFile
+from .ScanParameters import ScanParameters
+
+from .utils import random_pixels, random_pixel, flat_list, add_colorbar
+
+
+#make functions available in the top level API
+from .func import *
