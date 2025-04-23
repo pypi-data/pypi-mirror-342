ef351c148c6d6622062be4997c4d78f9d6f988f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:44 +0000

    Add package bengalinlp/2.0.1/bengalinlp-2.0.1.tar.gz

---
+++ packages/bengalinlp/bengalinlp-2.0.1.tar.gz/bengalinlp-2.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="bengalinlp",
+    version="2.0.1",
+    author="KhulnaSoft DevOps",
+    author_email="info@khulnasoft.com",
+    description="BengaliNLP is a natural language processing toolkit for Bengali Language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/banglawiki/bengalinlp",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "sentencepiece==0.2.0",
+        "gensim==4.3.2",
+        "nltk",
+        "numpy",
+        "scipy==1.10.1",
+        "sklearn-crfsuite==0.3.6",
+        "tqdm==4.66.3",
+        "ftfy==6.2.0",
+        "emoji==1.7.0",
+        "requests",
+    ],
+    extras_require={
+        "fasttext": ["fasttext==0.9.2"],
+    },
+)
