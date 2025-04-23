20a4509d12c33f29fc0e31037b06db278c7caf7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:16 +0000

    Add package eotdl/2025.4.22.post3/eotdl-2025.4.22.post3.tar.gz

---
+++ packages/eotdl/eotdl-2025.4.22.post3.tar.gz/eotdl-2025.4.22.post3/eotdl/datasets/ingest.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from ..repos import DatasetsAPIRepo
+from ..files.ingest import prep_ingest_stac, prep_ingest_folder, ingest, ingest_virtual, ingest_catalog
+
+def retrieve_dataset(metadata, user):
+	repo = DatasetsAPIRepo()
+	data, error = repo.retrieve_dataset(metadata.name)
+	if data and data["uid"] != user["uid"]:
+		raise Exception("Dataset already exists.")
+	if error and error == "Dataset doesn't exist":
+		# create dataset
+		data, error = repo.create_dataset(metadata.dict(), user)
+		# print(data, error)
+		if error:
+			raise Exception(error)
+	return data
+
+def ingest_dataset(
+	path,
+	verbose=False,
+	logger=print,
+	force_metadata_update=False,
+	sync_metadata=False,
+):
+	path = Path(path)
+	if not path.is_dir():
+		raise Exception("Path must be a folder")
+	if "catalog.json" in [f.name for f in path.iterdir()]:
+		prep_ingest_stac(path, logger)
+	else:
+		prep_ingest_folder(path, verbose, logger, force_metadata_update, sync_metadata)
+	return ingest(path, DatasetsAPIRepo(), retrieve_dataset, 'datasets')
+
+
+def ingest_virtual_dataset( # could work for a list of paths with minimal changes...
+	path,
+	links,
+	metadata = None, 
+	logger=print,
+	user=None,
+):
+	return ingest_virtual(path, links, DatasetsAPIRepo(), retrieve_dataset, 'datasets', metadata, logger)
+
+def ingest_dataset_catalog(path, logger=None):
+	path = Path(path)
+	return ingest_catalog(path, DatasetsAPIRepo(), retrieve_dataset, 'datasets')
