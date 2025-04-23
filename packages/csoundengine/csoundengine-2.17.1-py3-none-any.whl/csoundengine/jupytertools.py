da598ef75ffe513a47761d3921be950587f7edcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:31 +0000

    Add package csoundengine/2.17.1/csoundengine-2.17.1-py3-none-any.whl

---
+++ packages/csoundengine/csoundengine-2.17.1-py3-none-any.whl/csoundengine/jupytertools.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+from typing import Callable
+
+from emlib.envir import inside_jupyter
+
+
+if inside_jupyter():
+    import ipywidgets as _ipywidgets
+    from IPython.display import display as _ipythonDisplay
+
+
+lightPalette = {
+    'name.color': 'MediumSeaGreen'
+}
+
+darkPalette = lightPalette.copy()
+
+defaultPalette = lightPalette
+
+palettes = {
+    'light': lightPalette,
+    'dark': darkPalette
+}
+
+
+def displayButton(buttonText: str, callback: Callable[[], None]
+                  ) -> None:
+    """
+    Create and display an html button inside a jupyter notebook
+
+    If not inside a jupyter notebook this function will raise RuntimeError
+
+    Args:
+        buttonText: the text of the button
+        callback: the function to call when the button is pressed. This function
+            takes no arguments and should not return anything
+    """
+    if not inside_jupyter():
+        raise RuntimeError("This function is only available when running inside a jupyter notebook")
+
+    button = _ipywidgets.Button(description=buttonText)
+    output = _ipywidgets.Output()
+
+    def clicked(b):
+        with output:
+            callback()
+
+    button.on_click(clicked)
+    _ipythonDisplay(button, output)
+
+
+def htmlName(text: str, palette='light') -> str:
+    """A name as html
+
+    It will use the colors as defined in the palette
+    """
+    colors = palettes.get(palette)
+    if not colors:
+        raise ValueError(f"palette {palette} not known. Possible palettes: {palettes.keys()}")
+    return f'<strong style="color:{colors["name.color"]}">{text}</strong>'
+
+
+safeColors = {
+    'blue1': '#9090FF',
+    'blue2': '#6666E0',
+    'red1': '#FF9090',
+    'red2': '#E08080',
+    'green1': '#90FF90',
+    'green2': '#8080E0',
+    'magenta1': '#F090F0',
+    'magenta2': '#E080E0',
+    'cyan': '#70D0D0',
+    'grey1': '#BBBBBB',
+    'grey2': '#A0A0A0',
+    'grey3': '#909090'
+}
+
+
+def htmlSpan(text, color='', fontsize='', italic=False, bold=False, tag='span') -> str:
+    """
+    Create a html span with the given attributes
+
+    Args:
+        text: the text inside the span
+        color: a valid css color. If it starts with ':', one of the 'safe' colors as
+            defined in `safeColors` (blue1, blue2, red1, red2, green1, green2, cyan, grey1, grey2,
+            grey3, magenta1, magenta2). For example, ':blue1' will use the blue1 safe color
+        fontsize: a valid size, such as '12px'.
+        italic: if True, use italic text
+        bold: if True, use bold text
+        tag: the tag to use (span by default, can be code, for example)
+
+    Returns:
+        the resulting html
+
+    """
+    text = str(text)
+    if color.startswith(':'):
+        color = safeColors[color[1:]]
+    styleitems = {}
+    if color:
+        styleitems['color'] = color
+    if fontsize:
+        styleitems['font-size'] = fontsize
+    stylestr = ";".join(f"{k}:{v}" for k, v in styleitems.items())
+    text = str(text)
+    if italic and bold:
+        text = f'<i><b>{text}</b></i>'
+    elif italic:
+        text = f'<i>{text}</i>'
+    elif bold:
+        text = f'<b>{text}</b>'
+    return f'<{tag} style="{stylestr}">{text}</{tag}>'
---
+++ packages/csoundengine/csoundengine-2.17.1-py3-none-any.whl/csoundengine/tools.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import fnmatch
+import os
+import platform
+import sys
+import tempfile
+from collections import UserString
+from functools import cache
+
+import emlib.common
+import emlib.misc
+
+from .config import logger
+
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    import sndfileio
+
+
+def makeUniqueFilename(ext: str, prefix='', folder='.') -> str:
+    """
+    Create a unique filename
+
+    Args:
+        ext: the extension of the filename
+        prefix: a prefix to the unique part of the filename
+        folder: where should this file be created? NB: the file itself is not
+            created, but will be unique in the given folder.
+
+    Returns:
+        the generated filename
+    """
+    if not ext.startswith('.'):
+        ext = '.' + ext
+    return tempfile.mktemp(suffix=ext, dir=folder, prefix=prefix)
+
+
+@emlib.common.runonce
+def defaultSoundfontPath() -> str:
+    """
+    Returns the path of the fluid sf2 file
+
+    Returns:
+        the path of the default soundfont or an empty path if this does not apply
+    """
+    if sys.platform == 'linux':
+        paths = ["/usr/share/sounds/sf2/FluidR3_GM.sf2"]
+        path = next((path for path in paths if os.path.exists(path)), '')
+        return path
+    else:
+        logger.info("Default path for soundfonts only defined in linux")
+    return ''
+
+
+def showSoundfontPrograms(sfpath: str, glob="") -> None:
+    """
+    Print a list of sounfont presets/programs
+
+    Args:
+        sfpath: the path to the soundfont
+        glob: if given, it is used to filter the presets to only those
+            whose name matches the given glob pattern
+    """
+    from . import csoundlib
+    progs = csoundlib.soundfontPresets(sfpath)
+    if glob:
+        progs = [p for p in progs
+                 if fnmatch.fnmatch(p[2], glob)]
+    emlib.misc.print_table(progs, headers=('bank', 'num', 'name'), showindex=False)
+
+
+class PlatformId(UserString):
+    def __init__(self, osname: str, arch: str):
+        assert osname in ('windows', 'linux', 'macos')
+        assert arch in ('x86_64', 'arm64', 'arm32')
+
+        self.osname = osname
+        self.arch = arch
+        super().__init__(osname + "-" + arch)
+
+
+@cache
+def platformId() -> PlatformId:
+    """
+    Query the platform id for the current system
+
+    The paltform id is the duplet <osname>-<architecture>, and is one of
+    'linux-x86_64', 'windows-x86_64', 'macos-x86_64', 'linux-arm64', 'windows-arm64',
+    'macos-arm64', ...
+    """
+    osname = {
+        'linux': 'linux',
+        'darwin': 'macos',
+        'win32': 'windows'
+    }[sys.platform]
+    return PlatformId(osname, _platformArch())
+
+
+def _platformArch() -> str:
+    machine = platform.machine().lower()
+    bits, linkage = platform.architecture()
+    if machine == 'arm':
+        if bits == '64bit':
+            return 'arm64'
+        elif bits == '32bit':
+            return 'arm32'
+    elif machine.startswith('arm64'):
+        return 'arm64'
+    elif machine == 'x86_64' or machine.startswith('amd64') or machine.startswith('intel64'):
+        return 'x86_64'
+    elif machine == 'i386':
+        if bits == '64bit':
+            return 'x86_64'
+        elif bits == '32bit':
+            return 'x86'
+
+    raise RuntimeError(f"Architecture not supported ({machine=}, {bits=}, {linkage=})")
+
+
+def sndfileInfo(path: str) -> sndfileio.SndInfo:
+    import sndfileio
+    return sndfileio.sndinfo(path)
