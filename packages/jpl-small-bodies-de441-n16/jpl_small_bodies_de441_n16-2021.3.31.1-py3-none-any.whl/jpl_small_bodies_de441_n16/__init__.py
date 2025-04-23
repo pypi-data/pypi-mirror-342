46d3619e294d74d24176140b900139b1b5b9766a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:11 +0000

    Add package jpl-small-bodies-de441-n16/2021.3.31.1/jpl_small_bodies_de441_n16-2021.3.31.1-py3-none-any.whl

---
+++ packages/jpl-small-bodies-de441-n16/jpl_small_bodies_de441_n16-2021.3.31.1-py3-none-any.whl/jpl_small_bodies_de441_n16/__init__.py
@@ -0,0 +1,4 @@
+from importlib.resources import files
+
+de441_n16 = files("jpl_small_bodies_de441_n16").joinpath("sb441-n16.bsp").as_posix()
+_de441_n16_md5 = files("jpl_small_bodies_de441_n16").joinpath("sb441-n16.md5").as_posix()
