72e6b014f688df3aee954e589219cb6eda8fc5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 17:07:04 +0000

    Add package Mypackage-jay2025/0.0.7/mypackage_jay2025-0.0.7.tar.gz

---
+++ packages/Mypackage-jay2025/mypackage_jay2025-0.0.7.tar.gz/mypackage_jay2025-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mypackage_jay2025',
+    version='0.0.7',
+    packages=find_packages(),
+    description='A simple math package',
+    author='Jayadev',
+    author_email='swarnamathi@gmail.com',
+   
+)
+
