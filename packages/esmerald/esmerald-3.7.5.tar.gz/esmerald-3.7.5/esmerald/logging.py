dc4465e1d8d29dc21a83456bb4a0b7b03678b3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 04:05:46 +0000

    Add package esmerald/3.7.5/esmerald-3.7.5.tar.gz

---
+++ packages/esmerald/esmerald-3.7.5.tar.gz/esmerald-3.7.5/esmerald/logging.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Any, cast
+
+from esmerald.core.protocols.logging import LoggerProtocol
+
+
+class LoggerProxy:
+    """
+    Proxy for the real logger used by Esmerald.
+    """
+
+    def __init__(self) -> None:
+        self._logger: LoggerProtocol | None = None
+
+    def bind_logger(self, logger: LoggerProtocol | None) -> None:  # noqa
+        self._logger = logger
+
+    def __getattr__(self, item: str) -> Any:
+        if not self._logger:
+            raise RuntimeError("Logger is not configured yet. Please call setup_logging() first.")
+        return getattr(self._logger, item)
+
+
+logger: LoggerProtocol = cast(LoggerProtocol, LoggerProxy())
