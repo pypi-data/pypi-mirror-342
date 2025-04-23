6feba0295e2b99fdd6ee54c2b1e61f15e9db985c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:26 +0000

    Add package core-for-ai/0.1.9/core_for_ai-0.1.9.tar.gz

---
+++ packages/core-for-ai/core_for_ai-0.1.9.tar.gz/core_for_ai-0.1.9/aicore/llm/providers/deepseek.py
@@ -0,0 +1,30 @@
+from aicore.llm.providers.openai import OpenAiLlm
+from openai.types.chat import ChatCompletion
+from deepseek_tokenizer import ds_token
+from pydantic import model_validator
+from typing import Optional, Self
+
+class DeepSeekLlm(OpenAiLlm):
+    """
+    most nvidia hosted models are limited to 4K max output tokens
+    """
+    
+    base_url :str="https://api.deepseek.com"
+
+    @model_validator(mode="after")
+    def pass_deepseek_tokenizer_fn(self)->Self:
+        self.tokenizer_fn = ds_token.encode
+
+        return self
+
+    def normalize(self, chunk :ChatCompletion, completion_id :Optional[str]=None):
+        usage = chunk.usage
+        if usage is not None:
+            ### https://api-docs.deepseek.com/news/news0802
+            self.usage.record_completion(
+                prompt_tokens=usage.prompt_cache_miss_tokens,
+                response_tokens=usage.completion_tokens,
+                cached_tokens=usage.prompt_cache_hit_tokens,
+                completion_id=completion_id or chunk.id
+            )
+        return chunk.choices
