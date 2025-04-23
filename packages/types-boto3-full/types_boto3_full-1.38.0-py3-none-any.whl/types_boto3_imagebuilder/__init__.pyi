0dd7315a0c152619f90bb5ce18c05351054f574d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:45 +0000

    Add package types-boto3-full/1.38.0/types_boto3_full-1.38.0-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.38.0-py3-none-any.whl/types_boto3_imagebuilder/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+Main interface for imagebuilder service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_imagebuilder/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_imagebuilder import (
+        Client,
+        ImagebuilderClient,
+    )
+
+    session = Session()
+    client: ImagebuilderClient = session.client("imagebuilder")
+    ```
+"""
+
+from .client import ImagebuilderClient
+
+Client = ImagebuilderClient
+
+__all__ = ("Client", "ImagebuilderClient")
