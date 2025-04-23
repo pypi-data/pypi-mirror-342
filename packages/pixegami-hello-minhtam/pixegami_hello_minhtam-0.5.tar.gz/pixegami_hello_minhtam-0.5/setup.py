c152146589c43fba51b344cd181812136244267e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:07:30 +0000

    Add package pixegami-hello-minhtam/0.5/pixegami_hello_minhtam-0.5.tar.gz

---
+++ packages/pixegami-hello-minhtam/pixegami_hello_minhtam-0.5.tar.gz/pixegami_hello_minhtam-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r") as file:
+    description = file.read()
+
+
+setup(
+    name="pixegami_hello_minhtam",
+    version="0.5",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={"console_scripts": ["pixegami-hello = pixegami_hello:hello"]},
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
