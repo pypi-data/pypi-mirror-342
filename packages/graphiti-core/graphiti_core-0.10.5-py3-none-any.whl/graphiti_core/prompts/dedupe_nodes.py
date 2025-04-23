5cd79a2ed60a6962fe48a5250944aa86253d3379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:21 +0000

    Add package graphiti-core/0.10.5/graphiti_core-0.10.5-py3-none-any.whl

---
+++ packages/graphiti-core/graphiti_core-0.10.5-py3-none-any.whl/graphiti_core/prompts/dedupe_nodes.py
@@ -0,0 +1,131 @@
+"""
+Copyright 2024, Zep Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+from typing import Any, Protocol, TypedDict
+
+from pydantic import BaseModel, Field
+
+from .models import Message, PromptFunction, PromptVersion
+
+
+class NodeDuplicate(BaseModel):
+    is_duplicate: bool = Field(..., description='true or false')
+    uuid: str | None = Field(
+        None,
+        description="uuid of the existing node like '5d643020624c42fa9de13f97b1b3fa39' or null",
+    )
+    name: str = Field(
+        ...,
+        description="Updated name of the new node (use the best name between the new node's name, an existing duplicate name, or a combination of both)",
+    )
+
+
+class Prompt(Protocol):
+    node: PromptVersion
+    node_list: PromptVersion
+
+
+class Versions(TypedDict):
+    node: PromptFunction
+    node_list: PromptFunction
+
+
+def node(context: dict[str, Any]) -> list[Message]:
+    return [
+        Message(
+            role='system',
+            content='You are a helpful assistant that de-duplicates nodes from node lists.',
+        ),
+        Message(
+            role='user',
+            content=f"""
+        <PREVIOUS MESSAGES>
+        {json.dumps([ep for ep in context['previous_episodes']], indent=2)}
+        </PREVIOUS MESSAGES>
+        <CURRENT MESSAGE>
+        {context['episode_content']}
+        </CURRENT MESSAGE>
+
+        <EXISTING NODES>
+        {json.dumps(context['existing_nodes'], indent=2)}
+        </EXISTING NODES>
+        
+        Given the above EXISTING NODES and their attributes, MESSAGE, and PREVIOUS MESSAGES. Determine if the NEW NODE extracted from the conversation
+        is a duplicate entity of one of the EXISTING NODES.
+
+        <NEW NODE>
+        {json.dumps(context['extracted_nodes'], indent=2)}
+        </NEW NODE>
+        Task:
+        1. If the New Node represents the same entity as any node in Existing Nodes, return 'is_duplicate: true' in the 
+            response. Otherwise, return 'is_duplicate: false'
+        2. If is_duplicate is true, also return the uuid of the existing node in the response
+        3. If is_duplicate is true, return a name for the node that is the most complete full name.
+
+        Guidelines:
+        1. Use both the name and summary of nodes to determine if the entities are duplicates, 
+            duplicate nodes may have different names
+
+        Respond with a JSON object in the following format:
+            {{
+                "is_duplicate": true or false,
+                "uuid": "uuid of the existing node like 5d643020624c42fa9de13f97b1b3fa39 or null",
+                "name": "Updated name of the new node (use the best name between the new node's name, an existing duplicate name, or a combination of both)"
+            }}
+        """,
+        ),
+    ]
+
+
+def node_list(context: dict[str, Any]) -> list[Message]:
+    return [
+        Message(
+            role='system',
+            content='You are a helpful assistant that de-duplicates nodes from node lists.',
+        ),
+        Message(
+            role='user',
+            content=f"""
+        Given the following context, deduplicate a list of nodes:
+
+        Nodes:
+        {json.dumps(context['nodes'], indent=2)}
+
+        Task:
+        1. Group nodes together such that all duplicate nodes are in the same list of uuids
+        2. All duplicate uuids should be grouped together in the same list
+        3. Also return a new summary that synthesizes the summary into a new short summary
+
+        Guidelines:
+        1. Each uuid from the list of nodes should appear EXACTLY once in your response
+        2. If a node has no duplicates, it should appear in the response in a list of only one uuid
+
+        Respond with a JSON object in the following format:
+        {{
+            "nodes": [
+                {{
+                    "uuids": ["5d643020624c42fa9de13f97b1b3fa39", "node that is a duplicate of 5d643020624c42fa9de13f97b1b3fa39"],
+                    "summary": "Brief summary of the node summaries that appear in the list of names."
+                }}
+            ]
+        }}
+        """,
+        ),
+    ]
+
+
+versions: Versions = {'node': node, 'node_list': node_list}
---
+++ packages/graphiti-core/graphiti_core-0.10.5-py3-none-any.whl/graphiti_core/prompts/extract_edges.py
@@ -0,0 +1,113 @@
+"""
+Copyright 2024, Zep Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+from typing import Any, Protocol, TypedDict
+
+from pydantic import BaseModel, Field
+
+from .models import Message, PromptFunction, PromptVersion
+
+
+class Edge(BaseModel):
+    relation_type: str = Field(..., description='RELATION_TYPE_IN_CAPS')
+    source_entity_name: str = Field(..., description='name of the source entity')
+    target_entity_name: str = Field(..., description='name of the target entity')
+    fact: str = Field(..., description='extracted factual information')
+
+
+class ExtractedEdges(BaseModel):
+    edges: list[Edge]
+
+
+class MissingFacts(BaseModel):
+    missing_facts: list[str] = Field(..., description="facts that weren't extracted")
+
+
+class Prompt(Protocol):
+    edge: PromptVersion
+    reflexion: PromptVersion
+
+
+class Versions(TypedDict):
+    edge: PromptFunction
+    reflexion: PromptFunction
+
+
+def edge(context: dict[str, Any]) -> list[Message]:
+    return [
+        Message(
+            role='system',
+            content='You are an expert fact extractor that extracts fact triples from text.',
+        ),
+        Message(
+            role='user',
+            content=f"""
+        <PREVIOUS MESSAGES>
+        {json.dumps([ep for ep in context['previous_episodes']], indent=2)}
+        </PREVIOUS MESSAGES>
+        <CURRENT MESSAGE>
+        {context['episode_content']}
+        </CURRENT MESSAGE>
+        
+        <ENTITIES>
+        {context['nodes']}
+        </ENTITIES>
+        
+        {context['custom_prompt']}
+
+        Given the above MESSAGES and ENTITIES, extract all facts pertaining to the listed ENTITIES from the CURRENT MESSAGE. 
+        
+        Guidelines:
+        1. Extract facts only between the provided entities.
+        2. Each fact should represent a clear relationship between two DISTINCT nodes.
+        3. The relation_type should be a concise, all-caps description of the fact (e.g., LOVES, IS_FRIENDS_WITH, WORKS_FOR).
+        4. Provide a more detailed fact containing all relevant information.
+        5. Consider temporal aspects of relationships when relevant.
+        """,
+        ),
+    ]
+
+
+def reflexion(context: dict[str, Any]) -> list[Message]:
+    sys_prompt = """You are an AI assistant that determines which facts have not been extracted from the given context"""
+
+    user_prompt = f"""
+<PREVIOUS MESSAGES>
+{json.dumps([ep for ep in context['previous_episodes']], indent=2)}
+</PREVIOUS MESSAGES>
+<CURRENT MESSAGE>
+{context['episode_content']}
+</CURRENT MESSAGE>
+
+<EXTRACTED ENTITIES>
+{context['nodes']}
+</EXTRACTED ENTITIES>
+
+<EXTRACTED FACTS>
+{context['extracted_facts']}
+</EXTRACTED FACTS>
+
+Given the above MESSAGES, list of EXTRACTED ENTITIES entities, and list of EXTRACTED FACTS; 
+determine if any facts haven't been extracted.
+"""
+    return [
+        Message(role='system', content=sys_prompt),
+        Message(role='user', content=user_prompt),
+    ]
+
+
+versions: Versions = {'edge': edge, 'reflexion': reflexion}
