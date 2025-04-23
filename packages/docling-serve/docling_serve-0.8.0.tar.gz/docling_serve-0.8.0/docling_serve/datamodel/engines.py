416b034af4caabd965b1e037789c1dc160331b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:03 +0000

    Add package docling-serve/0.8.0/docling_serve-0.8.0.tar.gz

---
+++ packages/docling-serve/docling_serve-0.8.0.tar.gz/docling_serve-0.8.0/docling_serve/datamodel/engines.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class TaskStatus(str, enum.Enum):
+    SUCCESS = "success"
+    PENDING = "pending"
+    STARTED = "started"
+    FAILURE = "failure"
+
+
+class AsyncEngine(str, enum.Enum):
+    LOCAL = "local"
