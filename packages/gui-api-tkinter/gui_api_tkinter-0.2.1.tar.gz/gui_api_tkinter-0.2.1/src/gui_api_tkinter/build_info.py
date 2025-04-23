03f72094d42c8d31afb8ac92a6566865b2831ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:44 +0000

    Add package gui-api-tkinter/0.2.1/gui_api_tkinter-0.2.1.tar.gz

---
+++ packages/gui-api-tkinter/gui_api_tkinter-0.2.1.tar.gz/gui_api_tkinter-0.2.1/src/gui_api_tkinter/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.17'
+    os_name = 'posix:ubuntu'
+    version = '0.2.1'
+    git_sha = 'cf01b9d7d9970763de211cda3180925562ba7a4c'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
---
+++ packages/gui-api-tkinter/gui_api_tkinter-0.2.1.tar.gz/gui_api_tkinter-0.2.1/src/gui_api_tkinter/logger_null.py
@@ -0,0 +1,10 @@
+# --------------------
+## Null version of the Logger
+class LoggerNull:
+    # --------------------
+    ## null info call
+    #
+    # @param msg  ignored
+    # @return None
+    def info(self, msg):
+        pass
