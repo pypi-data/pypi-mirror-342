f3b9a79150c751ea29871af735db6db4b411ff0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 04:05:27 +0000

    Add package endoc/1.7.0/endoc-1.7.0.tar.gz

---
+++ packages/endoc/endoc-1.7.0.tar.gz/endoc-1.7.0/endoc/client.py
@@ -0,0 +1,15 @@
+from gql import Client
+from gql.transport.requests import RequestsHTTPTransport
+
+class APIClient:
+    def __init__(self, api_key):
+        self.client = Client(
+            transport = RequestsHTTPTransport(
+                url="https://endoc.ethz.ch/graphql",
+                headers={'x-api-key': api_key}
+            ),
+            fetch_schema_from_transport=True,
+        )
+
+    def execute_query(self, query, variable_values=None):
+        return self.client.execute(query, variable_values or {})
---
+++ packages/endoc/endoc-1.7.0.tar.gz/endoc-1.7.0/endoc/models/single_paper.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel, field_validator
+from typing import List, Optional
+
+class Author(BaseModel):
+    FamilyName: str
+    GivenName: str
+
+class PublicationDate(BaseModel):
+    Year: Optional[int] = None
+    Month: Optional[int] = None
+    Day: Optional[int] = None
+    Name: Optional[str] = None
+
+    @field_validator("Year", "Month", "Day", mode="before")
+    def empty_str_to_none(cls, value):
+        if isinstance(value, str) and value.strip() == "":
+            return None
+        return value
+    
+class PaperID(BaseModel):
+    collection: str
+    id_field: str
+    id_type: str
+    id_value: str
+
+class PaperReference(BaseModel):
+    Title: str
+    Author: List[Author]
+    Venue: str
+    PublicationDate: PublicationDate
+    ReferenceText: str
+    PaperID: Optional["PaperID"] = None
+
+class SinglePaperContent(BaseModel):
+    Abstract: str
+    Abstract_Parsed: List[dict]
+    Fullbody_Parsed: List[dict]
+    Fullbody: str
+
+class SinglePaperResponseBody(BaseModel):
+    _id: str
+    id_int: int
+    DOI: str
+    Title: str
+    Content: SinglePaperContent
+    Author: List[Author]
+    Venue: str
+    PublicationDate: PublicationDate
+    Reference: List[PaperReference]
+
+class SinglePaperData(BaseModel):
+    status: str
+    message: str
+    response: Optional[SinglePaperResponseBody] = None
