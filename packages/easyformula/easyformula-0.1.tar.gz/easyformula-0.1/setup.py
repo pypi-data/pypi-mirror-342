37aeea5c52afd225c29ab073b71746bd1961bde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:05 +0000

    Add package easyformula/0.1/easyformula-0.1.tar.gz

---
+++ packages/easyformula/easyformula-0.1.tar.gz/easyformula-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easyformula',  
+    version='0.1',       
+    description='A simple library to calculate areas of geometric shapes',  
+    author='Rafsan Chowdhury',     
+    author_email='none@example.com', 
+    packages=find_packages(),  
+    install_requires=[],  
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
