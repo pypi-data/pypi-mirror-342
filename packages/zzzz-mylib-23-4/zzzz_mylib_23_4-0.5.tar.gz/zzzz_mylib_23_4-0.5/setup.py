1772e44f699e7e52f9afd0392122ec3c300402c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:46 +0000

    Add package zzzz-mylib-23-4/0.5/zzzz_mylib_23_4-0.5.tar.gz

---
+++ packages/zzzz-mylib-23-4/zzzz_mylib_23_4-0.5.tar.gz/zzzz_mylib_23_4-0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r") as file:
+    description = file.read()
+
+MYLIB_NAME = "zzzz_mylib_23_4"
+__version__ = "0.5"  # Chỉnh phiên bản ở đây sau mỗi lần sửa code
+
+REQUIRED_LIBS = [
+    "tensorflow==2.18.0",
+    "keras-cv==0.9.0",
+    "pandas",
+    "numpy",
+    "matplotlib",
+    "scikit-learn==1.3.0",
+    "python-box==6.0.2",
+    "pyYAML",
+    "ensure==1.0.2",
+    "types-PyYAML",
+    "plotly",
+    "seaborn",
+    "xgboost",
+    "lightgbm",
+    "numpy==2.0.0",
+]
+
+setup(
+    name=MYLIB_NAME,
+    version=__version__,
+    packages=find_packages(),
+    install_requires=REQUIRED_LIBS,
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
