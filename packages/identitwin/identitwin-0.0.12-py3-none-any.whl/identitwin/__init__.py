80410b52c13ecdda9122af47d09cb40c5beff690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 15:06:50 +0000

    Add package identitwin/0.0.12/identitwin-0.0.12-py3-none-any.whl

---
+++ packages/identitwin/identitwin-0.0.12-py3-none-any.whl/identitwin/__init__.py
@@ -0,0 +1,8 @@
+"""
+IdentiTwin - Structural Vibration Monitoring Library
+
+This library provides tools for monitoring structural vibrations using
+LVDT sensors and accelerometers.
+"""
+
+__version__ = '0.1.0'
