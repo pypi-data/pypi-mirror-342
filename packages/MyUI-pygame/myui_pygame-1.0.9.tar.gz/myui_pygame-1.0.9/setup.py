6513cad273679d4851e75f86d1d2dd075d8d5373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 15:07:02 +0000

    Add package MyUI-pygame/1.0.9/myui_pygame-1.0.9.tar.gz

---
+++ packages/MyUI-pygame/myui_pygame-1.0.9.tar.gz/myui_pygame-1.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MyUI_pygame",
+    version="1.0.9",
+    author="Neo Zetterberg",
+    author_email="20091103neo@gmail.com",
+    description="An easy-to-use extension to the very well-known package Pygame.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    # url="https://github.com/yourusername/MyUI",  # optional
+    packages=find_packages(),
+    install_requires=[
+        "pygame",
+        "pyperclip"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
