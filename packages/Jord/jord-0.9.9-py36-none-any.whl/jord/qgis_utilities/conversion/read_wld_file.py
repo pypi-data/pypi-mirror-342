3312b6e428f8aa80d6da42baaa2ff518fb6e4d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:12 +0000

    Add package Jord/0.9.9/jord-0.9.9-py36-none-any.whl

---
+++ packages/Jord/jord-0.9.9-py36-none-any.whl/jord/qgis_utilities/conversion/read_wld_file.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Any
+
+from PyQt5.QtGui import QTransform
+
+__all__ = ["read_wld_file"]
+
+
+def read_wld_file(geom: Any, wld_file_path: Path) -> None:
+    assert wld_file_path is not None
+    assert wld_file_path.exists()
+
+    with open(wld_file_path) as wld_file:
+        m32 = (float(c) for c in wld_file.readlines())
+
+        transformer = QTransform(*m32)
+
+        geom.transform(transformer)
