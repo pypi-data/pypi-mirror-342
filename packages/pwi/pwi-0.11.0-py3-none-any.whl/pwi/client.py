153ca880912229c6e3bcfc04a32fd66ac3b60fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:20:59 +0000

    Add package pwi/0.11.0/pwi-0.11.0-py3-none-any.whl

---
+++ packages/pwi/pwi-0.11.0-py3-none-any.whl/pwi/client.py
@@ -0,0 +1,21 @@
+# **************************************************************************************
+
+# @package        pwi
+# @license        MIT License Copyright (c) 2025 Michael J. Roberts
+
+# **************************************************************************************
+
+from httpx import Client, Timeout
+
+# **************************************************************************************
+
+
+class PlaneWaveHTTPXClient:
+    def __init__(self, host: str = "localhost", port: int = 8220, timeout: float = 3.0):
+        self._client = Client(
+            base_url=f"http://{host}:{port}",
+            timeout=Timeout(timeout, connect=timeout, read=timeout, write=timeout),
+        )
+
+
+# **************************************************************************************
