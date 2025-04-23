2a6d4f9eb1ca172d342392706e0f6b537c4916b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 04:06:04 +0000

    Add package galtea/1.0.12/galtea-1.0.12.tar.gz

---
+++ packages/galtea/galtea-1.0.12.tar.gz/galtea-1.0.12/galtea/domain/models/inference_result.py
@@ -0,0 +1,23 @@
+
+from typing import Optional
+from ...utils.from_camel_case_base_model import FromCamelCaseBaseModel
+
+
+class CostInfoProperties(FromCamelCaseBaseModel):
+  cost_per_input_token: Optional[float] = None
+  cost_per_output_token: Optional[float] = None
+  cost_per_cache_read_input_token: Optional[float] = None
+
+class UsageInfoProperties(FromCamelCaseBaseModel):
+  input_tokens: Optional[int] = None
+  output_tokens: Optional[int] = None
+  cache_read_input_tokens: Optional[int] = None
+  
+class InferenceResultBase(CostInfoProperties, UsageInfoProperties):
+  evaluation_id: str
+  actual_output: str
+  latency: Optional[float] = None
+  retrieval_context: Optional[str] = None
+  
+class InferenceResult(InferenceResultBase):
+  id: str
