06077045594df6c397fa673a92f96219de1025dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:57 +0000

    Add package rpgFactory/0.0.1/rpgfactory-0.0.1.tar.gz

---
+++ packages/rpgFactory/rpgfactory-0.0.1.tar.gz/rpgfactory-0.0.1/README.md
@@ -0,0 +1,28 @@
+# tartaro
+
+Description.
+The package tartaro is used to: - creation of list with monsters for basics adventures of RPG
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install tartaro
+
+```bash
+pip install tartaro
+```
+
+## Usage
+
+```python
+from tartaro import equidna
+equidna = Equidna("Equidna", "Equidna", 0)
+horde1 = equidna.create_horde("Beast", "Beast", 1, 2, 5, 10)
+```
+
+## Author
+
+[Marcelo Batista](https://github.com/Marcelo-Batista)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/rpgFactory/rpgfactory-0.0.1.tar.gz/rpgfactory-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+
+setup(
+    name="rpgFactory",
+    version="0.0.1",
+    author="Marcelo Batista",
+    author_email="marcelo.silva238@fatec.sp.gov.br",
+    description="A simple RPG factory to create monsters",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Marcelo-Batista/Equidna",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
