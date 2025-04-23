8a0486ebfec8fe1ab7dc47d6d8a4b6009380f5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 04:05:28 +0000

    Add package pypi-attestations/0.0.24/pypi_attestations-0.0.24-py3-none-any.whl

---
+++ packages/pypi-attestations/pypi_attestations-0.0.24-py3-none-any.whl/pypi_attestations/__init__.py
@@ -0,0 +1,37 @@
+"""The `pypi-attestations` APIs."""
+
+__version__ = "0.0.24"
+
+from ._impl import (
+    Attestation,
+    AttestationBundle,
+    AttestationError,
+    AttestationType,
+    ConversionError,
+    Distribution,
+    Envelope,
+    GitHubPublisher,
+    GitLabPublisher,
+    Provenance,
+    Publisher,
+    TransparencyLogEntry,
+    VerificationError,
+    VerificationMaterial,
+)
+
+__all__ = [
+    "Attestation",
+    "AttestationBundle",
+    "AttestationError",
+    "AttestationType",
+    "ConversionError",
+    "Distribution",
+    "Envelope",
+    "GitHubPublisher",
+    "GitLabPublisher",
+    "Provenance",
+    "Publisher",
+    "TransparencyLogEntry",
+    "VerificationError",
+    "VerificationMaterial",
+]
