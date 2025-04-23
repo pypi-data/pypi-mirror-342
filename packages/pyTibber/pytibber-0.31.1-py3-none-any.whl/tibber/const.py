40e5f8ccfa77711274ad42562b74a4a84677b869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:34 +0000

    Add package pyTibber/0.31.1/pytibber-0.31.1-py3-none-any.whl

---
+++ packages/pyTibber/pytibber-0.31.1-py3-none-any.whl/tibber/const.py
@@ -0,0 +1,24 @@
+"""Constants used by pyTibber"""
+
+from http import HTTPStatus
+from typing import Final
+
+__version__ = "0.31.1"
+
+API_ENDPOINT: Final = "https://api.tibber.com/v1-beta/gql"
+DEFAULT_TIMEOUT: Final = 10
+DEMO_TOKEN: Final = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
+
+RESOLUTION_HOURLY: Final = "HOURLY"
+RESOLUTION_DAILY: Final = "DAILY"
+RESOLUTION_WEEKLY: Final = "WEEKLY"
+RESOLUTION_MONTHLY: Final = "MONTHLY"
+RESOLUTION_ANNUAL: Final = "ANNUAL"
+
+API_ERR_CODE_UNKNOWN: Final = "UNKNOWN"
+API_ERR_CODE_UNAUTH: Final = "UNAUTHENTICATED"
+HTTP_CODES_RETRIABLE: Final = [
+    HTTPStatus.TOO_MANY_REQUESTS,
+    HTTPStatus.PRECONDITION_REQUIRED,
+]
+HTTP_CODES_FATAL: Final = [HTTPStatus.BAD_REQUEST]
