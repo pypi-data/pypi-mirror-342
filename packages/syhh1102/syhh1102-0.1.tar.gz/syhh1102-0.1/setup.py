c7a3d51b8748b00384e16f4da1e75ed58b583720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:22 +0000

    Add package syhh1102/0.1/syhh1102-0.1.tar.gz

---
+++ packages/syhh1102/syhh1102-0.1.tar.gz/syhh1102-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name='syhh1102',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 列出你的项目所需的所有依赖包
+    ],
+    author='cxy',
+    author_email='2831922355@qq.com',
+    description='A simple example package',
+    url='https://github.com/diavu/my_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
