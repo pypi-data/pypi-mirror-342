86e34b3effebf8b2d1a6d80649e4a7315d4fcf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:16 +0000

    Add package melli-code/1.0.0/melli_code-1.0.0-py3-none-any.whl

---
+++ packages/melli-code/melli_code-1.0.0-py3-none-any.whl/melli_code/__init__.py
@@ -0,0 +1,21 @@
+# src/melli_code/__init__.py
+"""
+Melli Code: Iranian National Code Validator and Generator.
+
+Provides functions to validate existing codes and generate new valid codes
+according to the official algorithm. Package name: melli-code.
+"""
+__version__ = "1.0.0" # <--- Set initial version
+
+from .validator import is_valid, validate
+from .generator import generate
+from .exceptions import InvalidNationalCode
+
+# Controls what 'from melli_code import *' imports
+__all__ = [
+    'is_valid',
+    'validate',
+    'generate',
+    'InvalidNationalCode',
+    '__version__'
+]
