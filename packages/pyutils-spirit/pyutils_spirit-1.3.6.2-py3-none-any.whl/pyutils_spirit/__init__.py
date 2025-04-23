c3797cd0d62de6212120aa9be98507f475fac1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:51 +0000

    Add package pyutils-spirit/1.3.6.2/pyutils_spirit-1.3.6.2-py3-none-any.whl

---
+++ packages/pyutils-spirit/pyutils_spirit-1.3.6.2-py3-none-any.whl/pyutils_spirit/__init__.py
@@ -0,0 +1,102 @@
+# @Coding: UTF-8
+# @Time: 2025/3/26 23:42
+# @Author: xieyang_ls
+# @Filename: __init__.py.py
+
+from pyutils_spirit.annotation import connection, get_instance_signature, singleton
+
+from pyutils_spirit.concurrent_thread import ThreadExecutor, ThreadHolder
+
+from pyutils_spirit.util import (Assemble,
+                                 HashAssemble,
+                                 JsonUtil,
+                                 ReentryLock,
+                                 Regional,
+                                 Queue,
+                                 LinkedQueue,
+                                 BlockingQueue,
+                                 Set,
+                                 HashSet)
+
+from pyutils_spirit.database import Handler, MySQLHandler
+
+from pyutils_spirit.exception import (ArgumentException, ConflictSignatureError,
+                                      NoneSignatureError, BusinessException,
+                                      SystemException)
+
+from pyutils_spirit.python_spark import PySparkHandler
+
+from pyutils_spirit.style import (BLACK, RED, GREEN, YELLOW,
+                                  BLUE, MAGENTA, CYAN, WHITE, RESET,
+                                  set_spirit_banner,
+                                  set_websocket_banner)
+
+from pyutils_spirit.spirit_container import (Get, Post, Put, Delete,
+                                             ExceptionAdvice, ThrowsException,
+                                             RequestInterceptor,
+                                             InterceptorBefore, InterceptorAfter,
+                                             Result, SpiritApplication,
+                                             Component, Mapper,
+                                             Service, Controller,
+                                             Resource, MultipartFile)
+
+from pyutils_spirit.tcp import WebSocketServer, Session, onopen, onmessage, onclose, onerror
+
+__all__ = ['connection',
+           'get_instance_signature',
+           'singleton',
+           'ReentryLock',
+           'Regional',
+           'ThreadExecutor',
+           'Assemble',
+           'HashAssemble',
+           'JsonUtil',
+           'Queue',
+           'LinkedQueue',
+           'BlockingQueue',
+           'Set',
+           'HashSet',
+           'Handler',
+           'MySQLHandler',
+           'ArgumentException',
+           'ConflictSignatureError',
+           'NoneSignatureError',
+           'PySparkHandler',
+           "BLACK",
+           "RED",
+           "GREEN",
+           "YELLOW",
+           "BLUE",
+           "MAGENTA",
+           "CYAN",
+           "WHITE",
+           "RESET",
+           "set_spirit_banner",
+           "set_websocket_banner",
+           "Get",
+           "Post",
+           "Put",
+           "Delete",
+           "ExceptionAdvice",
+           "ThrowsException",
+           "RequestInterceptor",
+           "InterceptorBefore",
+           "InterceptorAfter",
+           "SpiritApplication",
+           "Component",
+           "Mapper",
+           "Service",
+           "Controller",
+           "Resource",
+           'WebSocketServer',
+           'Session',
+           'onopen',
+           'onmessage',
+           'onclose',
+           'onerror',
+           'ThreadHolder',
+           'BusinessException',
+           'SystemException',
+           'PySparkHandler',
+           'Result',
+           'MultipartFile']
---
+++ packages/pyutils-spirit/pyutils_spirit-1.3.6.2-py3-none-any.whl/pyutils_spirit/concurrent_thread/__init__.py
@@ -0,0 +1,10 @@
+# @Coding: UTF-8
+# @Time: 2025/3/30 1:21
+# @Author: xieyang_ls
+# @Filename: __init__.py.py
+
+from pyutils_spirit.concurrent_thread.thread_executor import ThreadExecutor
+
+from pyutils_spirit.concurrent_thread.thread_holder import ThreadHolder
+
+__all__ = ['ThreadExecutor', 'ThreadHolder']
