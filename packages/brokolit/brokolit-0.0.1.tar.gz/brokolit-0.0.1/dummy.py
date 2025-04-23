004157e04e0bb7c6d82a65b588b6e737f22d2387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:19:58 +0000

    Add package brokolit/0.0.1/brokolit-0.0.1.tar.gz

---
+++ packages/brokolit/brokolit-0.0.1.tar.gz/brokolit-0.0.1/dummy.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+ 
+setup(
+    name="brokolit",       # Must be unique on PyPI!
+    version="0.0.1",
+    description="A harmless dummy test package",
+    py_modules=["dummy"],
+    author="Your Name",
+    author_email="you@example.com",
+)
