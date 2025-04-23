89ab127e9e86e5c527c501a14522111c2fc220c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:28 +0000

    Add package dynarr/0.2.0/dynarr-0.2.0.tar.gz

---
+++ packages/dynarr/dynarr-0.2.0.tar.gz/dynarr-0.2.0/README.md
@@ -0,0 +1,2 @@
+# dynarray
+Dynarray é uma estrutura de dados personalizada em Python que organiza e armazena tuplas dinamicamente com base no primeiro dígito de uma string fornecida.
---
+++ packages/dynarr/dynarr-0.2.0.tar.gz/dynarr-0.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dynarr",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Ryan Flores",
+    author_email="ryanmigul@gmail.com",
+    description="A dynamic way of storing things, what, I don't know",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.7",
+)
