c489c733969b72c90a49065209aed9529187cbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:16 +0000

    Add package cg/69.5.0/cg-69.5.0-py3-none-any.whl

---
+++ packages/cg/cg-69.5.0-py3-none-any.whl/cg/services/delivery_message/messages/utils.py
@@ -0,0 +1,37 @@
+from cg.store.models import Case
+
+
+def get_caesar_delivery_path(case: Case) -> str:
+    ticket_id: str = case.latest_ticket
+    customer_id: str = case.customer.internal_id
+    return f"/home/{customer_id}/inbox/{ticket_id}"
+
+
+def get_scout_link(case: Case) -> str:
+    customer_id: str = case.customer.internal_id
+    case_name: str = case.name
+    return f"https://scout.scilifelab.se/{customer_id}/{case_name}"
+
+
+def get_scout38_link(case: Case) -> str:
+    customer_id: str = case.customer.internal_id
+    case_name: str = case.name
+    return f"https://scout38.scilifelab.se/{customer_id}/{case_name}"
+
+
+def get_scout_links_row_separated(cases: list[Case]) -> str:
+    scout_links: list[str] = [get_scout_link(case) for case in cases]
+    return "\n".join(scout_links)
+
+
+def get_pangolin_delivery_path(case: Case) -> str:
+    customer_id: str = case.customer.internal_id
+    return f"/home/{customer_id}/inbox/wwLab_automatisk_hamtning"
+
+
+def get_batch_id_for_case(case: Case) -> str:
+    return case.name.split("-")[1]
+
+
+def get_statina_link(batch_id: str) -> str:
+    return f"https://statina.clinicalgenomics.se/batches/{batch_id}"
