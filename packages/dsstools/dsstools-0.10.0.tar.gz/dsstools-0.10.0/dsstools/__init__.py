2c3887a8a1c56096d8e546a545d20203b4493635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 16:07:46 +0000

    Add package dsstools/0.10.0/dsstools-0.10.0.tar.gz

---
+++ packages/dsstools/dsstools-0.10.0.tar.gz/dsstools-0.10.0/dsstools/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 dssTools Developers
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+dssTools
+========
+dssTools is a Python wrapper around NetworkX to ease usage for network analysis
+for our case of Digital Social Sciences.
+"""
+
+from .inputs import *
+from .processing import *
+from .textsearch import *
+from .draw import *
+from .mapping import *
+from .supplier import *
