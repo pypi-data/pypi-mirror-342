5e8189309247c9871e2a5b540f35917e28efca94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:48 +0000

    Add package maleo-metadata/0.0.21/maleo_metadata-0.0.21-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.0.21-py3-none-any.whl/maleo_metadata/models/schemas/gender.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+class MaleoMetadataGenderSchemas:
+    class Key(BaseModel):
+        key:str = Field(..., max_length=20, description="Gender's key")
+
+    class Name(BaseModel):
+        name:str = Field(..., max_length=20, description="Gender's name")
