02eba2ecf6bc1fc8b9c37fb76a2fc1c415ca48f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:05:53 +0000

    Add package lumaai/1.7.4/lumaai-1.7.4.tar.gz

---
+++ packages/lumaai/lumaai-1.7.4.tar.gz/lumaai-1.7.4/src/lumaai/types/generations/__init__.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .image_create_params import ImageCreateParams as ImageCreateParams
+from .video_create_params import VideoCreateParams as VideoCreateParams
+from .camera_motion_list_response import CameraMotionListResponse as CameraMotionListResponse
