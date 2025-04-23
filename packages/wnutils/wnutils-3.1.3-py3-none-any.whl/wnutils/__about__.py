94df535f437241d02896cf252fa3a94f2b598679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:15 +0000

    Add package wnutils/3.1.3/wnutils-3.1.3-py3-none-any.whl

---
+++ packages/wnutils/wnutils-3.1.3-py3-none-any.whl/wnutils/__about__.py
@@ -0,0 +1,17 @@
+"""
+A package of python routines to work with webnucleo.org data.
+"""
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__title__ = "wnutils"
+__summary__ = "Python project to read and plot webnucleo files"
+__version__ = "3.1.3"
+__author__ = "Clemson University"
+__copyright__ = "Clemson University, 2018-2025"
