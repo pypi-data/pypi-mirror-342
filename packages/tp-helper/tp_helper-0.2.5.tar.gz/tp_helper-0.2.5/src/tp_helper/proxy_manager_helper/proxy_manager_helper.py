ac1603aecd2f6f60620ea8ededcaa72019035a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 16:06:32 +0000

    Add package tp-helper/0.2.5/tp_helper-0.2.5.tar.gz

---
+++ packages/tp-helper/tp_helper-0.2.5.tar.gz/tp_helper-0.2.5/src/tp_helper/proxy_manager_helper/proxy_manager_helper.py
@@ -0,0 +1,22 @@
+import aiohttp
+
+from .schemas.proxy_schema import ProxySchema
+
+
+class ProxyManagerHelper:
+    def __init__(self, proxy_manager_url: str):
+        self.proxy_manager_url = proxy_manager_url
+        self.proxy_schema = None
+
+    async def get_one_proxy(self, queue: str) -> ProxySchema:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                    self.proxy_manager_url + "/proxies", params={"queue": queue}
+            ) as response:
+                proxy_text = await response.text()
+                schema = ProxySchema.model_validate_json(proxy_text)
+                self.proxy_schema = schema
+                return schema
+
+    def get_http(self) -> str:
+        return f"http://{self.proxy_schema.login}:{self.proxy_schema.password}@{self.proxy_schema.ip}:{self.proxy_schema.port}"
