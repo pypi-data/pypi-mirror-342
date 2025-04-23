5bf88363b95182e21f1f938e0821fa7028d269ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:05:50 +0000

    Add package maleo-metadata/0.0.15/maleo_metadata-0.0.15.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.0.15.tar.gz/maleo_metadata-0.0.15/maleo_metadata/models/transfers/parameters/general/blood_type.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from pydantic import Field
+from maleo_foundation.models.transfers.parameters.general import BaseGeneralParametersTransfers
+from maleo_foundation.types import BaseTypes
+from maleo_metadata.enums.blood_type import MaleoMetadataBloodTypeEnums
+
+class MaleoMetadataBloodTypeGeneralParametersTransfers:
+    class GetSingleQuery(BaseGeneralParametersTransfers.GetSingleQuery): pass
+
+    class GetSingle(BaseGeneralParametersTransfers.GetSingle):
+        identifier:MaleoMetadataBloodTypeEnums.IdentifierType = Field(..., description="Identifier")
---
+++ packages/maleo-metadata/maleo_metadata-0.0.15.tar.gz/maleo_metadata-0.0.15/maleo_metadata/models/transfers/results/query/user_type.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from maleo_foundation.models.schemas.general import BaseGeneralSchemas
+from maleo_foundation.models.transfers.results.service.query import BaseServiceQueryResultsTransfers
+
+class MaleoMetadataUserTypeQueryResultsTransfers:
+    class Row(
+        BaseGeneralSchemas.Name,
+        BaseGeneralSchemas.Key,
+        BaseGeneralSchemas.Order,
+        BaseServiceQueryResultsTransfers.Row
+    ): pass
+
+    class Fail(BaseServiceQueryResultsTransfers.Fail): pass
+
+    class NoData(BaseServiceQueryResultsTransfers.NoData): pass
+
+    class SingleData(BaseServiceQueryResultsTransfers.SingleData):
+        data:MaleoMetadataUserTypeQueryResultsTransfers.Row
+
+    class MultipleData(BaseServiceQueryResultsTransfers.UnpaginatedMultipleData):
+        data:list[MaleoMetadataUserTypeQueryResultsTransfers.Row]
