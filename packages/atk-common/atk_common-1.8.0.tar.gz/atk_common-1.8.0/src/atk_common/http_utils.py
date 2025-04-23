bd1b34e82d2d7579b6e89348e83aa88289e5c18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:21 +0000

    Add package atk-common/1.8.0/atk_common-1.8.0.tar.gz

---
+++ packages/atk-common/atk_common-1.8.0.tar.gz/atk_common-1.8.0/src/atk_common/http_utils.py
@@ -0,0 +1,23 @@
+import json
+from atk_common.datetime_utils import get_utc_date_time
+from atk_common.docker_utils import get_current_container_info
+from atk_common.env_utils import get_env_value
+
+def is_status_code_ok(status_code):
+    return status_code >= 200 and status_code < 300
+
+def get_test_response():
+    data = {}
+    container_info = get_current_container_info()
+    if container_info is not None:
+        data['imageName'] = container_info['imageName']
+        data['imageVersion'] = container_info['imageVersion']
+        data['containerName'] = container_info['containerName']
+        data['containerPorts'] = container_info['ports']
+    else:
+        data['imageName'] = None
+        data['imageVersion'] = None
+        data['containerName'] = None
+        data['containerPorts'] = None
+    data['timestamp'] = get_utc_date_time()
+    return data
