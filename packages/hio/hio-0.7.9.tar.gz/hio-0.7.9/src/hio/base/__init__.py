eeaae06ad042bbef784b2cdccb8a348c6dc19971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 05:05:50 +0000

    Add package hio/0.7.9/hio-0.7.9.tar.gz

---
+++ packages/hio/hio-0.7.9.tar.gz/hio-0.7.9/src/hio/base/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+hio.base Package
+"""
+
+from .tyming import Tymist, Tymee, Tymer
+from .doing import Doist, doize, doify, Doer, DoDoer
+from .filing import openFiler, Filer, FilerDoer
+from .multidoing import Bosser, Crewer, TagDex
