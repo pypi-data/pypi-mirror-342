2763bbe59861a1ccefcc34dc479ce032baf9eb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:45 +0000

    Add package bluer-geo/5.8.1/bluer_geo-5.8.1.tar.gz

---
+++ packages/bluer-geo/bluer_geo-5.8.1.tar.gz/bluer_geo-5.8.1/bluer_geo/.abcli/tests/datacube_label.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+function test_bluer_geo_datacube_label() {
+    local datacube_id=$BLUE_GEO_PALISADES_TEST_DATACUBE
+
+    bluer_geo_datacube_ingest \
+        scope=rgb \
+        $datacube_id
+
+    bluer_geo_datacube_label ~QGIS \
+        $datacube_id
+}
