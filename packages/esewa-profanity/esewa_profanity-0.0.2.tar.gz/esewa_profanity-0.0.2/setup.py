bfa889f18cc8c2f84610241d189d3bb9676df0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:19:55 +0000

    Add package esewa-profanity/0.0.2/esewa_profanity-0.0.2.tar.gz

---
+++ packages/esewa-profanity/esewa_profanity-0.0.2.tar.gz/esewa_profanity-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='esewa-profanity',
+       version='0.0.2',
+       author='Ankit Lamsal',
+       author_email='ankit.lamsal@esewa.com.np',
+       description='A library for detecting profanity from different languages.',
+       long_description= open('README.md').read(),
+       long_description_content_type='text/markdown',
+       packages = find_packages(),
+       classifiers=["Programming Language :: Python :: 3.8",
+                #     "License :: OSI Approved :: MIT License",
+                    "Operating System :: OS Independent"],
+        python_requires='>=3.8'
+)
