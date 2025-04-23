258cb0821669d290ac4a69de38c1b016c6cfe777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:45 +0000

    Add package maleo-metadata/0.0.19/maleo_metadata-0.0.19.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.0.19.tar.gz/maleo_metadata-0.0.19/maleo_metadata/models/schemas/service.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+class MaleoMetadataServiceSchemas:
+    class Key(BaseModel):
+        key:str = Field(..., max_length=20, description="Service's key")
+
+    class Name(BaseModel):
+        name:str = Field(..., max_length=20, description="Service's name")
+
+    class Secret(BaseModel):
+        secret:UUID = Field(..., description="Service's secret")
