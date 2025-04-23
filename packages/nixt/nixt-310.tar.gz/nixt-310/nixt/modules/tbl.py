144a61f1086959b05ca29ffb116337841328b907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:06:48 +0000

    Add package nixt/310/nixt-310.tar.gz

---
+++ packages/nixt/nixt-310.tar.gz/nixt-310/nixt/modules/tbl.py
@@ -0,0 +1,47 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "err": "2aca6bd937ceb5aa2b4233f1700804b1",
+    "flt": "fa0e02c5426157546b7a7f3ed6fe9b64",
+    "fnd": "6b5a030019981e28f624ffc2d0c679f3",
+    "irc": "333bbbdf8ed709971663a4d8fb542492",
+    "log": "8b88daace922042e0ac4d386859317f1",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "rss": "3e1503162c0633a633d3dd55bc216dbd",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "bfccec4688cd12752b03d95027d7be4f",
+    "thr": "b04ea6fc90c5d38341cd4a2dcbfd3511",
+    "tmr": "36777415b5cb1b70d6cd1042987553da",
+    "upt": "6f6fdb1397b57db6e68498e03b793d7a",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
