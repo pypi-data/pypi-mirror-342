f7f08f6b92c967bececf4069a36664a8ca874f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 03:21:20 +0000

    Add package qe-api-client/2.6.0/qe_api_client-2.6.0.tar.gz

---
+++ packages/qe-api-client/qe_api_client-2.6.0.tar.gz/qe_api_client-2.6.0/qe_api_client/api_classes/engine_generic_dimension_api.py
@@ -0,0 +1,38 @@
+import json
+
+
+class EngineGenericDimensionApi:
+    """
+    API class for interacting with Qlik Sense engine's generic dimension objects.
+
+    Methods:
+        get_dimension(handle, dimension_id): Retrieves the definition of a specific dimension.
+    """
+
+    def __init__(self, socket):
+        """
+        Initializes the EngineGenericDimensionApi with a given socket connection.
+
+        Parameters:
+            socket (object): The socket connection to the Qlik Sense engine.
+        """
+        self.engine_socket = socket
+
+    def get_dimension(self, app_handle: int, dimension_id: str):
+        """
+        Retrieves the definition of a specific dimension from the Qlik Sense engine.
+
+        Parameters:
+            app_handle (int): The handle identifying the application.
+            dimension_id (str): The unique identifier (qId) of the dimension to retrieve.
+
+        Returns:
+            dict: The definition of the requested dimension (qReturn). In case of an error, returns the error information.
+        """
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": app_handle, "method": "GetDimension",
+                          "params": {"qId": dimension_id}})
+        response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
+        try:
+            return response["result"]["qReturn"]
+        except KeyError:
+            return response["error"]
