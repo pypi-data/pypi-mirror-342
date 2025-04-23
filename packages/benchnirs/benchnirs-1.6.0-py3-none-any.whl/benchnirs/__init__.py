27e5d0dd1f7f29a47a1260a0f10c7b80eb207375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:05:47 +0000

    Add package benchnirs/1.6.0/benchnirs-1.6.0-py3-none-any.whl

---
+++ packages/benchnirs/benchnirs-1.6.0-py3-none-any.whl/benchnirs/__init__.py
@@ -0,0 +1,20 @@
+import lazy_loader as lazy
+
+from importlib.metadata import version
+
+
+try:
+    __version__ = version("benchnirs")
+except Exception:
+    __version__ = "dev"
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submod_attrs={
+        'load': ['load_dataset', 'load_homer'],
+        'viz': ['epochs_viz'],
+        'process': ['process_epochs', 'extract_features'],
+        'learn': ['machine_learn', 'deep_learn', 'deep_transfer_learn',
+                  'train_final', 'select_proxy']
+    }
+)
