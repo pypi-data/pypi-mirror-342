f05d03b0cc52a37a7bb73a02d2c6e17dd8fa3a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 06:06:44 +0000

    Add package bluer-flow/5.9.1/bluer_flow-5.9.1.tar.gz

---
+++ packages/bluer-flow/bluer_flow-5.9.1.tar.gz/bluer_flow-5.9.1/bluer_flow/workflow/runners/factory.py
@@ -0,0 +1,10 @@
+from typing import Dict
+
+from bluer_flow.workflow.runners import RunnerType
+from bluer_flow.workflow.runners.local import LocalRunner
+from bluer_flow.workflow.runners.generic import GenericRunner
+
+runner_class: Dict[RunnerType, GenericRunner] = {
+    RunnerType.GENERIC: GenericRunner,
+    RunnerType.LOCAL: LocalRunner,
+}
