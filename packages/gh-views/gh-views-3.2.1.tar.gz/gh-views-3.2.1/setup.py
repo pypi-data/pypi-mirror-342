a78d7f121472ac83bb7977dd1dda1b9591426af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-23 14:07:29 +0000

    Add package gh-views/3.2.1/gh-views-3.2.1.tar.gz

---
+++ packages/gh-views/gh-views-3.2.1.tar.gz/gh-views-3.2.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='gh-views',
+    version="3.2.1",
+    author='@readwithai',
+    long_description_content_type='text/markdown',
+    author_email='talwrii@gmail.com',
+    description='Fetch number of views or clones of a github repo and mantain a timeline.',
+    license='BSD',
+    keywords='github, statistics, views',
+    url='https://github.com/talwrii/gh-views',
+    packages=["gh_views"],
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': ['gh-views=gh_views.main:main']
+    }
+)
