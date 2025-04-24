abeb1bb3e6de6a38f4a38cd42e998f71c7cb2110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:22:12 +0000

    Add package pretix-worldlinedirect/1.0.0/pretix_worldlinedirect-1.0.0-py3-none-any.whl

---
+++ packages/pretix-worldlinedirect/pretix_worldlinedirect-1.0.0-py3-none-any.whl/pretix_worldlinedirect/apps.py
@@ -0,0 +1,27 @@
+from django.utils.translation import gettext_lazy
+
+from . import __compatibility__, __version__
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(PluginConfig):
+    default = True
+    name = "pretix_worldlinedirect"
+    verbose_name = "Worldline Direct"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Worldline Direct")
+        author = "pretix team"
+        description = gettext_lazy("Accept payments through Worldline Direct")
+        visible = True
+        version = __version__
+        category = "PAYMENT"
+        picture = "pretix_worldlinedirect/logo.svg"
+        compatibility = __compatibility__
+
+    def ready(self):
+        from . import signals  # NOQA
