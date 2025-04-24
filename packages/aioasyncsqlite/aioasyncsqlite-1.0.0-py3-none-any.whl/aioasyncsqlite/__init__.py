7695d525d1cd7a3c4878cdeb25ac6f8f08ad9605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:21:52 +0000

    Add package aioasyncsqlite/1.0.0/aioasyncsqlite-1.0.0-py3-none-any.whl

---
+++ packages/aioasyncsqlite/aioasyncsqlite-1.0.0-py3-none-any.whl/aioasyncsqlite/__init__.py
@@ -0,0 +1,43 @@
+# Licensed under the MIT license
+
+"""asyncio bridge to the standard sqlite3 module"""
+
+from sqlite3 import (  # pylint: disable=redefined-builtin
+    DatabaseError,
+    Error,
+    IntegrityError,
+    NotSupportedError,
+    OperationalError,
+    paramstyle,
+    ProgrammingError,
+    register_adapter,
+    register_converter,
+    Row,
+    sqlite_version,
+    sqlite_version_info,
+    Warning,
+)
+
+__author__ = "xacepok933"
+from .__version__ import __version__
+from .core import connect, Connection, Cursor
+
+__all__ = [
+    "__version__",
+    "paramstyle",
+    "register_adapter",
+    "register_converter",
+    "sqlite_version",
+    "sqlite_version_info",
+    "connect",
+    "Connection",
+    "Cursor",
+    "Row",
+    "Warning",
+    "Error",
+    "DatabaseError",
+    "IntegrityError",
+    "ProgrammingError",
+    "OperationalError",
+    "NotSupportedError",
+]
