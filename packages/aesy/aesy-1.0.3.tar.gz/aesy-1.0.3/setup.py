f5891e5509c26cab7acbbc2ee1a44e47dbcb31fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:23:21 +0000

    Add package aesy/1.0.3/aesy-1.0.3.tar.gz

---
+++ packages/aesy/aesy-1.0.3.tar.gz/aesy-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="aesy",
+    version="1.0.2",
+    description="AESY multi-layer encryption algorithm",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Gubir34",
+    author_email="canteoman15@gmail.com",
+    url="https://gitlab.com/gubir34/aesy/",
+    packages=["aesy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.8',
+)
