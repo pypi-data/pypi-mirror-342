0e5c51ec3b09d2ef99a0c9b5022be43cc9c62261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:23:20 +0000

    Add package turbigen/2.1.0/turbigen-2.1.0-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/turbigen/turbigen-2.1.0-cp312-cp312-musllinux_1_2_x86_64.whl/turbigen/inlet.py
@@ -0,0 +1,89 @@
+"""Classes for initialising an inlet state."""
+
+import dataclasses
+from abc import ABC, abstractmethod
+import turbigen.fluid
+import numpy as np
+
+
+@dataclasses.dataclass
+class InletConfig(ABC):
+    """Base class for inlet boundary condition settings."""
+
+    spf: list = None
+    """Span fraction of some radial stations running 0 to 1."""
+
+    fac_Po: list = None
+    """Factor multiplying nominal stagnation pressure at each station."""
+
+    fac_To: list = None
+    """Factor multiplying nominal stagnation temperature at each station."""
+
+    dAlpha: list = None
+    """Perturbation to inlet swirl angle at each station."""
+
+    dBeta: list = None
+    """Perturbation to inlet pitch angle at each station."""
+
+    @abstractmethod
+    def get_inlet(self):
+        """Return a State object for the inlet working fluid."""
+        raise NotImplementedError
+
+
+@dataclasses.dataclass
+class PerfectInletConfig(InletConfig):
+    """Settings for a perfect gas inlet boundary condition."""
+
+    Po: float = np.nan
+    """Stagnation pressure of the inlet [Pa]."""
+
+    To: float = np.nan
+    """Stagnation temperature of the inlet [K]."""
+
+    cp: float = np.nan
+    """Specific heat capacity of the inlet [J/kgK]."""
+
+    gamma: float = np.nan
+    """Specific heat ratio of the inlet [--]."""
+
+    mu: float = np.nan
+    """Dynamic viscosity of the inlet, or NaN if not specified [Pa.s]."""
+
+    def get_inlet(self):
+        """Return a State object for the inlet working fluid."""
+        return turbigen.fluid.PerfectState.from_properties(
+            self.cp,
+            self.gamma,
+            self.mu,
+        ).set_P_T(self.Po, self.To)
+
+
+@dataclasses.dataclass
+class RealInletConfig(InletConfig):
+    """Settings for a real gas inlet boundary condition."""
+
+    Po: float = np.nan
+    """Stagnation pressure of the inlet [Pa]."""
+
+    To: float = np.nan
+    """Stagnation temperature of the inlet [K]."""
+
+    fluid_name: str = ""
+    """Name of the working fluid in CoolProp database."""
+
+    def get_inlet(self):
+        """Return a State object for the inlet working fluid."""
+        return turbigen.fluid.RealState.from_fluid_name(self.fluid_name).set_P_T(
+            self.Po, self.To
+        )
+
+
+#
+#
+# # data = turbigen.yaml.read("examples/quad_camber.yaml")
+# if __name__ == "__main__":
+#     inlet_dat = {"Po": 101325, "To": 288.15, "cp": 1000, "gamma": 1.4}
+#     inlet = util.init_subclass_by_signature(InletConfig, inlet_dat)
+#     print(type(inlet))
+#     print(inlet)
