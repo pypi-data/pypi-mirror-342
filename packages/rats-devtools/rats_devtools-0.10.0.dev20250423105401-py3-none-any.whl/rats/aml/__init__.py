6d4bafbae1d8950d81b891df55220eaf8b85f9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:21:58 +0000

    Add package rats-devtools/0.10.0.dev20250423105401/rats_devtools-0.10.0.dev20250423105401-py3-none-any.whl

---
+++ packages/rats-devtools/rats_devtools-0.10.0.dev20250423105401-py3-none-any.whl/rats/aml/__init__.py
@@ -0,0 +1,26 @@
+"""Submit applications as aml jobs, sending necessary context from the driver node."""
+
+from ._app import AppConfigs, Application, AppServices, main
+from ._configs import (
+    AmlEnvironment,
+    AmlIO,
+    AmlJobContext,
+    AmlJobDetails,
+    AmlWorkspace,
+    BuildContext,
+)
+from ._submission import submit
+
+__all__ = [
+    "AmlEnvironment",
+    "AmlIO",
+    "AmlJobContext",
+    "AmlJobDetails",
+    "AmlWorkspace",
+    "AppConfigs",
+    "AppServices",
+    "Application",
+    "BuildContext",
+    "main",
+    "submit",
+]
