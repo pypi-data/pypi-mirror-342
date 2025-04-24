9959847728e59519b134b7c4ad6ed6fdbc56df2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:21:41 +0000

    Add package subaligner/0.3.9/subaligner-0.3.9-py3-none-any.whl

---
+++ packages/subaligner/subaligner-0.3.9-py3-none-any.whl/subaligner/__init__.py
@@ -0,0 +1,19 @@
+import os
+import warnings
+import logging
+import multiprocessing as mp
+from ._version import __version__
+
+__all__ = ["__version__"]
+
+warnings.filterwarnings("ignore")
+warnings.simplefilter("ignore")
+
+mp.set_start_method("spawn", force=True)
+os.environ["KMP_WARNINGS"] = "0"
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["TF_CPP_MIN_VLOG_LEVEL"] = "0"
+logging.getLogger("tensorflow").disabled = True
+
+os.environ["TF_USE_LEGACY_KERAS"] = "1"
