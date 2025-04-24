14f2f830f0662a14f6c55e3d73d93be5c1e2c6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-24 03:23:18 +0000

    Add package aesy/1.0.3/aesy-1.0.3-py3-none-any.whl

---
+++ packages/aesy/aesy-1.0.3-py3-none-any.whl/aesy/__init__.py
@@ -0,0 +1,7 @@
+# aesy klasörünün içindeki __init__.py dosyasına bunu ekle!
+# super_sifre kısmını, AESY sınıfının gerçekte bulunduğu dosyanın adıyla değiştir! 👇
+from aesy import AESY # <-- İşte bu satır sihirli! ✨
+
+# Eğer başka sınıflar veya fonksiyonlar da doğrudan erişilsin istiyorsan,
+# onları da aynı şekilde buraya ekleyebilirsin.
+# from .baska_dosya import baska_fonksiyon
